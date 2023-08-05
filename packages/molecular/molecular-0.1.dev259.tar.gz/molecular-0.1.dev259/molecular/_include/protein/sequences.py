dfd3a5c054e7dcc2a52ae3c49c6f54d54a2f9555
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:29:22 +0000

    Add package molecular/0.1.dev259/molecular-0.1.dev259.tar.gz

---
+++ packages/molecular/molecular-0.1.dev259.tar.gz/molecular-0.1.dev259/molecular/_include/protein/sequences.py
@@ -0,0 +1,8 @@
+
+# Alzheimer's disease amyloid beta peptide
+amyloid_beta = 'DAEFRHDSGYEVHHQKLVFFAEDVGSNKGAIIGLMVGGVVIA'
+
+# Antimicrobial peptides from Xenopus laevis
+mag2 = 'GIGKFLHSAKKFGKAFVGEIMNS'
+pgla = 'GMASKAGAIAGKIAKVALKAL'
+
