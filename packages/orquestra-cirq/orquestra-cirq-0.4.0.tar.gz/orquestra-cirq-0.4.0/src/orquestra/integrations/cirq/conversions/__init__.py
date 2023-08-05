ed1e1302cb9506075213502a70a1ed6b80c2d3f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:59 +0000

    Add package orquestra-cirq/0.4.0/orquestra-cirq-0.4.0.tar.gz

---
+++ packages/orquestra-cirq/orquestra-cirq-0.4.0.tar.gz/orquestra-cirq-0.4.0/src/orquestra/integrations/cirq/conversions/__init__.py
@@ -0,0 +1,5 @@
+################################################################################
+# © Copyright 2021-2022 Zapata Computing Inc.
+################################################################################
+from ._circuit_conversions import export_to_cirq, import_from_cirq
+from ._openfermion_conversions import qubitop_to_paulisum
