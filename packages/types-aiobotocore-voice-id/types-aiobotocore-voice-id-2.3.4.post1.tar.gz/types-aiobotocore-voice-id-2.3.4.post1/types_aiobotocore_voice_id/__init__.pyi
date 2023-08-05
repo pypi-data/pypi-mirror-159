011d7f9f570aa2e9e0ba879fce2dee9b427983cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:28:48 +0000

    Add package types-aiobotocore-voice-id/2.3.4.post1/types-aiobotocore-voice-id-2.3.4.post1.tar.gz

---
+++ packages/types-aiobotocore-voice-id/types-aiobotocore-voice-id-2.3.4.post1.tar.gz/types-aiobotocore-voice-id-2.3.4.post1/types_aiobotocore_voice_id/__init__.pyi
@@ -0,0 +1,24 @@
+"""
+Main interface for voice-id service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_voice_id import (
+        Client,
+        VoiceIDClient,
+    )
+
+    session = get_session()
+    async with session.create_client("voice-id") as client:
+        client: VoiceIDClient
+        ...
+
+    ```
+"""
+from .client import VoiceIDClient
+
+Client = VoiceIDClient
+
+__all__ = ("Client", "VoiceIDClient")
