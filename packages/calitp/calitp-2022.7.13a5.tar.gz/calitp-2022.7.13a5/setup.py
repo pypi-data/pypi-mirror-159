3ab18ea4411e7212d43e3f5a74400be1e6d42dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:26 +0000

    Add package calitp/2022.7.13a5/calitp-2022.7.13a5.tar.gz

---
+++ packages/calitp/calitp-2022.7.13a5.tar.gz/calitp-2022.7.13a5/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['calitp']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Jinja2<3.1.0',
+ 'gcsfs>=2022.5.0,<2023.0.0',
+ 'google-api-core==1.31.6',
+ 'google-cloud-bigquery>=3.2.0,<4.0.0',
+ 'gtfs-realtime-bindings>=0.0.7,<0.0.8',
+ 'humanize>=4.2.3,<5.0.0',
+ 'pandas-gbq>=0.17.6,<0.18.0',
+ 'pandas>=1.4.3,<2.0.0',
+ 'pendulum>=2.1.2,<3.0.0',
+ 'pydantic>=1.9.1,<2.0.0',
+ 'siuba>=0.3.0,<0.4.0',
+ 'sqlalchemy-bigquery>=1.4.4,<2.0.0']
+
+setup_kwargs = {
+    'name': 'calitp',
+    'version': '2022.7.13a5',
+    'description': 'Shared code for the Cal-ITP data codebases',
+    'long_description': None,
+    'author': 'Andrew Vaccaro',
+    'author_email': 'atvaccaro@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
