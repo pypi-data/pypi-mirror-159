2ab9db1657af7c4d5ede34db0bad7a601f30b07d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:29:05 +0000

    Add package types-aiobotocore-route53-recovery-control-config/2.3.4.post1/types-aiobotocore-route53-recovery-control-config-2.3.4.post1.tar.gz

---
+++ packages/types-aiobotocore-route53-recovery-control-config/types-aiobotocore-route53-recovery-control-config-2.3.4.post1.tar.gz/types-aiobotocore-route53-recovery-control-config-2.3.4.post1/types_aiobotocore_route53_recovery_control_config/__init__.py
@@ -0,0 +1,55 @@
+"""
+Main interface for route53-recovery-control-config service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_route53_recovery_control_config import (
+        Client,
+        ClusterCreatedWaiter,
+        ClusterDeletedWaiter,
+        ControlPanelCreatedWaiter,
+        ControlPanelDeletedWaiter,
+        Route53RecoveryControlConfigClient,
+        RoutingControlCreatedWaiter,
+        RoutingControlDeletedWaiter,
+    )
+
+    session = get_session()
+    async with session.create_client("route53-recovery-control-config") as client:
+        client: Route53RecoveryControlConfigClient
+        ...
+
+
+    cluster_created_waiter: ClusterCreatedWaiter = client.get_waiter("cluster_created")
+    cluster_deleted_waiter: ClusterDeletedWaiter = client.get_waiter("cluster_deleted")
+    control_panel_created_waiter: ControlPanelCreatedWaiter = client.get_waiter("control_panel_created")
+    control_panel_deleted_waiter: ControlPanelDeletedWaiter = client.get_waiter("control_panel_deleted")
+    routing_control_created_waiter: RoutingControlCreatedWaiter = client.get_waiter("routing_control_created")
+    routing_control_deleted_waiter: RoutingControlDeletedWaiter = client.get_waiter("routing_control_deleted")
+    ```
+"""
+from .client import Route53RecoveryControlConfigClient
+from .waiter import (
+    ClusterCreatedWaiter,
+    ClusterDeletedWaiter,
+    ControlPanelCreatedWaiter,
+    ControlPanelDeletedWaiter,
+    RoutingControlCreatedWaiter,
+    RoutingControlDeletedWaiter,
+)
+
+Client = Route53RecoveryControlConfigClient
+
+
+__all__ = (
+    "Client",
+    "ClusterCreatedWaiter",
+    "ClusterDeletedWaiter",
+    "ControlPanelCreatedWaiter",
+    "ControlPanelDeletedWaiter",
+    "Route53RecoveryControlConfigClient",
+    "RoutingControlCreatedWaiter",
+    "RoutingControlDeletedWaiter",
+)
