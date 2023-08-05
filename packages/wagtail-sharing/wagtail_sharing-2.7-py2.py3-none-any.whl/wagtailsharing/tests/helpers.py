167c045ecd73712d571d9f26467a9fb4168b9ca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:39 +0000

    Add package wagtail-sharing/2.7/wagtail_sharing-2.7-py2.py3-none-any.whl

---
+++ packages/wagtail-sharing/wagtail_sharing-2.7-py2.py3-none-any.whl/wagtailsharing/tests/helpers.py
@@ -0,0 +1,26 @@
+from django.utils.text import slugify
+
+from wagtail import VERSION as WAGTAIL_VERSION
+
+
+if WAGTAIL_VERSION >= (3, 0):
+    from wagtail.test.routablepage.models import RoutablePageTest
+    from wagtail.test.testapp.models import SimplePage
+else:
+    from wagtail.tests.routablepage.models import RoutablePageTest
+    from wagtail.tests.testapp.models import SimplePage
+
+
+def create_draft_page(site, title):
+    page = SimplePage(
+        title=title, slug=slugify(title), content="content", live=False
+    )
+
+    site.root_page.add_child(instance=page)
+    return page
+
+
+def create_draft_routable_page(site, title):
+    page = RoutablePageTest(title=title, live=False)
+    site.root_page.add_child(instance=page)
+    return page
