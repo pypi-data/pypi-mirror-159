d8bfda1667e440d157eb1c996d29389d77fc6951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:53:55 +0000

    Add package docufix/0.3.0/docufix-0.3.0.tar.gz

---
+++ packages/docufix/docufix-0.3.0.tar.gz/docufix-0.3.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['docufix', 'docufix.rules', 'docufix.utils']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['docufix = docufix.__main__:main']}
+
+setup_kwargs = {
+    'name': 'docufix',
+    'version': '0.3.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Nyakku Shigure',
+    'author_email': 'sigure.qaq@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
