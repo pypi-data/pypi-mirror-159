4b3b7f8866310479e1bc743b59d1260938151e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:08 +0000

    Add package types-aiobotocore-cloudcontrol/2.3.4.post1/types_aiobotocore_cloudcontrol-2.3.4.post1-py3-none-any.whl

---
+++ packages/types-aiobotocore-cloudcontrol/types_aiobotocore_cloudcontrol-2.3.4.post1-py3-none-any.whl/types_aiobotocore_cloudcontrol/__init__.py
@@ -0,0 +1,29 @@
+"""
+Main interface for cloudcontrol service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_cloudcontrol import (
+        Client,
+        CloudControlApiClient,
+        ResourceRequestSuccessWaiter,
+    )
+
+    session = get_session()
+    async with session.create_client("cloudcontrol") as client:
+        client: CloudControlApiClient
+        ...
+
+
+    resource_request_success_waiter: ResourceRequestSuccessWaiter = client.get_waiter("resource_request_success")
+    ```
+"""
+from .client import CloudControlApiClient
+from .waiter import ResourceRequestSuccessWaiter
+
+Client = CloudControlApiClient
+
+
+__all__ = ("Client", "CloudControlApiClient", "ResourceRequestSuccessWaiter")
