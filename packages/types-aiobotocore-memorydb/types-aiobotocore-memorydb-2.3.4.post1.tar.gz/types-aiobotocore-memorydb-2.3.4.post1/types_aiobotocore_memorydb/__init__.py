e43025c298286843221a6f8c65722c5dbe20db7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:28:50 +0000

    Add package types-aiobotocore-memorydb/2.3.4.post1/types-aiobotocore-memorydb-2.3.4.post1.tar.gz

---
+++ packages/types-aiobotocore-memorydb/types-aiobotocore-memorydb-2.3.4.post1.tar.gz/types-aiobotocore-memorydb-2.3.4.post1/types_aiobotocore_memorydb/__init__.py
@@ -0,0 +1,25 @@
+"""
+Main interface for memorydb service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_memorydb import (
+        Client,
+        MemoryDBClient,
+    )
+
+    session = get_session()
+    async with session.create_client("memorydb") as client:
+        client: MemoryDBClient
+        ...
+
+    ```
+"""
+from .client import MemoryDBClient
+
+Client = MemoryDBClient
+
+
+__all__ = ("Client", "MemoryDBClient")
