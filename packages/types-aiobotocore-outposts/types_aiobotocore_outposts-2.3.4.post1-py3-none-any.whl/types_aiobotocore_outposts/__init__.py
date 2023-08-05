7aa4138c4504833b7a1db25914999e81d15113f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:28:53 +0000

    Add package types-aiobotocore-outposts/2.3.4.post1/types_aiobotocore_outposts-2.3.4.post1-py3-none-any.whl

---
+++ packages/types-aiobotocore-outposts/types_aiobotocore_outposts-2.3.4.post1-py3-none-any.whl/types_aiobotocore_outposts/__init__.py
@@ -0,0 +1,25 @@
+"""
+Main interface for outposts service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_outposts import (
+        Client,
+        OutpostsClient,
+    )
+
+    session = get_session()
+    async with session.create_client("outposts") as client:
+        client: OutpostsClient
+        ...
+
+    ```
+"""
+from .client import OutpostsClient
+
+Client = OutpostsClient
+
+
+__all__ = ("Client", "OutpostsClient")
