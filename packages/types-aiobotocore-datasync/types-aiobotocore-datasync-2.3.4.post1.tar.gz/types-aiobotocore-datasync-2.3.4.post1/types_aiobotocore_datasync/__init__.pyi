db55b61bf939748d84f979ecef521885b571a4b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:13 +0000

    Add package types-aiobotocore-datasync/2.3.4.post1/types-aiobotocore-datasync-2.3.4.post1.tar.gz

---
+++ packages/types-aiobotocore-datasync/types-aiobotocore-datasync-2.3.4.post1.tar.gz/types-aiobotocore-datasync-2.3.4.post1/types_aiobotocore_datasync/__init__.pyi
@@ -0,0 +1,50 @@
+"""
+Main interface for datasync service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_datasync import (
+        Client,
+        DataSyncClient,
+        ListAgentsPaginator,
+        ListLocationsPaginator,
+        ListTagsForResourcePaginator,
+        ListTaskExecutionsPaginator,
+        ListTasksPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("datasync") as client:
+        client: DataSyncClient
+        ...
+
+
+    list_agents_paginator: ListAgentsPaginator = client.get_paginator("list_agents")
+    list_locations_paginator: ListLocationsPaginator = client.get_paginator("list_locations")
+    list_tags_for_resource_paginator: ListTagsForResourcePaginator = client.get_paginator("list_tags_for_resource")
+    list_task_executions_paginator: ListTaskExecutionsPaginator = client.get_paginator("list_task_executions")
+    list_tasks_paginator: ListTasksPaginator = client.get_paginator("list_tasks")
+    ```
+"""
+from .client import DataSyncClient
+from .paginator import (
+    ListAgentsPaginator,
+    ListLocationsPaginator,
+    ListTagsForResourcePaginator,
+    ListTaskExecutionsPaginator,
+    ListTasksPaginator,
+)
+
+Client = DataSyncClient
+
+__all__ = (
+    "Client",
+    "DataSyncClient",
+    "ListAgentsPaginator",
+    "ListLocationsPaginator",
+    "ListTagsForResourcePaginator",
+    "ListTaskExecutionsPaginator",
+    "ListTasksPaginator",
+)
