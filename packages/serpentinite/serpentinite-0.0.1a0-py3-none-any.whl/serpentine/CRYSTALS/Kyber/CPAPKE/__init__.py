1e3561be3239ff8dfdf8bafbafeef4e7784c3e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:44 +0000

    Add package serpentinite/0.0.1a0/serpentinite-0.0.1a0-py3-none-any.whl

---
+++ packages/serpentinite/serpentinite-0.0.1a0-py3-none-any.whl/serpentine/CRYSTALS/Kyber/CPAPKE/__init__.py
@@ -0,0 +1,8 @@
+def KeyGen():
+    return 0x1
+
+def Enc(pk , m, r):
+    return 0x1
+
+def Dec(sk , c):
+    return 0x1
---
+++ packages/serpentinite/serpentinite-0.0.1a0-py3-none-any.whl/serpentine/CRYSTALS/Kyber/symmetric_primitives/90s.py
@@ -0,0 +1,18 @@
+def PRF():
+    return 0xdeadbeef
+
+
+def XOF():
+    return 0xdeadbeef
+
+
+def H():
+    return 0xdeadbeef
+
+
+def G():
+    return 0xdeadbeef
+
+
+def KDF():
+    return 0xdeadbeef
