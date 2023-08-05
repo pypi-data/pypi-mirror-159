d079074cc1cc53322eed3a249c9e4cd9f409e5a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:24 +0000

    Add package strat-backtest/0.0.1/strat_backtest-0.0.1-py3-none-any.whl

---
+++ packages/strat-backtest/strat_backtest-0.0.1-py3-none-any.whl/strat_backtest/backtest/__init__.py
@@ -0,0 +1,3 @@
+from .finance_data import download_data, load_data
+from .optimize import Optimize
+from .strats import Backtest, Strategy
---
+++ packages/strat-backtest/strat_backtest-0.0.1-py3-none-any.whl/strat_backtest/config.py
@@ -0,0 +1,6 @@
+""" Configuration items for the rest of the framework """
+
+from pathlib import Path
+
+ROOT_PATH = Path().absolute()
+CONFIG_PATH = str(Path().absolute() / "config.ini")
