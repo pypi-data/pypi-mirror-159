beebf4428e34e04a2f70a467f785cf76278d005a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:18 +0000

    Add package docufix/0.3.1/docufix-0.3.1.tar.gz

---
+++ packages/docufix/docufix-0.3.1.tar.gz/docufix-0.3.1/docufix/__init__.py
@@ -0,0 +1,19 @@
+# Meta information for the project.
+__version__ = "0.3.1"
+__author__ = "Nyakku Shigure"
+__year__ = "2022"
+__project_info__ = {
+    "name": __name__,
+    "version": __version__,
+    "copyright": f"{__year__}, {__author__}",
+    "author": __author__,
+}
+
+# The exported modules.
+from .core import File, Line, Rule
+
+__all__ = [
+    "Line",
+    "File",
+    "Rule",
+]
