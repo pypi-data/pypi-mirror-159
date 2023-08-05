fbc33fd0861762bb01bc998bd02f0c939c39e334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:05 +0000

    Add package py-gs1-barcode-engine/0.0.8/py_gs1_barcode_engine-0.0.8.tar.gz

---
+++ packages/py-gs1-barcode-engine/py_gs1_barcode_engine-0.0.8.tar.gz/py_gs1_barcode_engine-0.0.8/hatch_build.py
@@ -0,0 +1,11 @@
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+
+class CustomBuildHook(BuildHookInterface):
+    def finalize(self,version, build_data, artifact_path) :
+        print(version)
+        print(build_data)
+        print(artifact_path)
+        print('finalizing')
+        asdf
+        
