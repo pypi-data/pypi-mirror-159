2af057edad57eb0184f72a17bbc2db4bd03e2015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:18 +0000

    Add package redshift_client/0.10.0/redshift_client-0.10.0.tar.gz

---
+++ packages/redshift_client/redshift_client-0.10.0.tar.gz/redshift_client-0.10.0/redshift_client/column.py
@@ -0,0 +1,21 @@
+from dataclasses import (
+    dataclass,
+)
+from redshift_client.data_type.core import (
+    DataType,
+)
+from redshift_client.sql_client import (
+    PrimitiveVal,
+)
+
+
+@dataclass(frozen=True)
+class ColumnId:
+    name: str
+
+
+@dataclass(frozen=True)
+class Column:
+    data_type: DataType
+    nullable: bool
+    default: PrimitiveVal
