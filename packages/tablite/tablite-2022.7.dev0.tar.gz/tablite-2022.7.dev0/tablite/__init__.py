847024d9a2dc89fc963b94aa85b5be9eef936e23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:25 +0000

    Add package tablite/2022.7.dev0/tablite-2022.7.dev0.tar.gz

---
+++ packages/tablite/tablite-2022.7.dev0.tar.gz/tablite-2022.7.dev0/tablite/__init__.py
@@ -0,0 +1,6 @@
+from tablite.core import Table, Column #, GroupBy
+from tablite.datatypes import DataTypes
+from tablite.groupby_utils import GroupBy
+from tablite.file_reader_utils import get_headers
+
+
