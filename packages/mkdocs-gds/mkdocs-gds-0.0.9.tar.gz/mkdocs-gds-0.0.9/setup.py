4158c371bc8979311451f6774c0c149df3c2cda8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:00 +0000

    Add package mkdocs-gds/0.0.9/mkdocs-gds-0.0.9.tar.gz

---
+++ packages/mkdocs-gds/mkdocs-gds-0.0.9.tar.gz/mkdocs-gds-0.0.9/setup.py
@@ -0,0 +1,25 @@
+import json
+from setuptools import setup, find_packages
+
+# Load package.json contents
+with open("package.json") as f:
+    package = json.load(f)
+
+setup(
+    name="mkdocs-gds",
+    version=package["version"],
+    url="https://github.com/uktrade/gds-technical-documentation-template",
+    license=package["license"],
+    description=package["description"],
+    long_description_content_type="text/markdown",
+    author=package["author"]["name"],
+    author_email=package["author"]["email"],
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        "mkdocs.themes": [
+            "gds = mkdocs_gds",
+        ]
+    },
+    zip_safe=False,
+)
