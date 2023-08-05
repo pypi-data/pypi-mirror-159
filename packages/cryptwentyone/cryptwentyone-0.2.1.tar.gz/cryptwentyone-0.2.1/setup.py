9f5c3af2a8922ebc5ebb45aacf8814d984ff23a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:49:59 +0000

    Add package cryptwentyone/0.2.1/cryptwentyone-0.2.1.tar.gz

---
+++ packages/cryptwentyone/cryptwentyone-0.2.1.tar.gz/cryptwentyone-0.2.1/setup.py
@@ -0,0 +1,19 @@
+# setup.py
+# CodeWriter21
+
+from setuptools import setup, find_packages
+
+setup(
+    name='cryptwentyone',
+    version='0.2.1',
+    description='A fun package for cryptography',
+    author='CodeWriter21',
+    author_email='CodeWriter21@gmail.com',
+    url='https://github.com/MPCodeWriter21/cryptwentyone',
+    packages=find_packages(),
+    license='Apache License, Version 2.0',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ]
+)
