549af19e63ae02a610b92852174dfe84e8533d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:25:25 +0000

    Add package pyqir-evaluator/0.5.0a1/pyqir_evaluator-0.5.0a1-cp36-abi3-macosx_10_7_x86_64.whl

---
+++ packages/pyqir-evaluator/pyqir_evaluator-0.5.0a1-cp36-abi3-macosx_10_7_x86_64.whl/pyqir/evaluator/_gateset.py
@@ -0,0 +1,62 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+class GateSet:
+    """
+    Defines the quantum circuit operations which may be registered for
+    callbacks during evaluation of QIR
+    """
+
+    def cx(self, control: str, target: str):
+        pass
+
+    def cz(self, control: str, target: str):
+        pass
+
+    def h(self, target: str):
+        pass
+
+    def m(self, qubit: str, target: str):
+        pass
+
+    def mz(self, qubit: str, target: str):
+        pass
+
+    def reset(self, target: str):
+        pass
+
+    def rx(self, theta: float, qubit: str):
+        pass
+
+    def ry(self, theta: float, qubit: str):
+        pass
+
+    def rz(self, theta: float, qubit: str):
+        pass
+
+    def s(self, qubit: str):
+        pass
+
+    def s_adj(self, qubit: str):
+        pass
+
+    def t(self, qubit: str):
+        pass
+
+    def t_adj(self, qubit: str):
+        pass
+
+    def x(self, qubit: str):
+        pass
+
+    def y(self, qubit: str):
+        pass
+
+    def z(self, qubit: str):
+        pass
+
+    def finish(self, metadata: dict):
+        """
+        Called at the end of QIR evaluation supplying run metadata.
+        """
+        pass
