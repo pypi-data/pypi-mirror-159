dcdbceb230a33eb590fa6c5842eaa83ef03ab5dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:36 +0000

    Add package zanshincli/1.2.9/zanshincli-1.2.9.tar.gz

---
+++ packages/zanshincli/zanshincli-1.2.9.tar.gz/zanshincli-1.2.9/setup.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from os.path import join
+
+from setuptools import setup
+
+exec(open(join('zanshincli','version.py')).read())
+
+setup(
+    name='zanshincli',
+    description='Python command line utility to the Tenchi Security Zanshin API v1',
+    long_description=Path('README.rst').read_text(),
+    author='Tenchi Security',
+    author_email='contact@tenchisecurity.com',
+    version=__version__,
+    url='https://github.com/tenchi-security/zanshin-cli',
+    license='Apache Software License',
+    install_requires=['zanshinsdk==1.2.5', 'typer==0.3.2', 'prettytable==2.5.0', 'boto3~=1.21.24', 'boto3_type_annotations~=0.3.1'],
+    tests_require=['pytest>=6.2,<7'],
+    setup_requires=['pytest-runner==6.0.0'],
+    packages=['zanshincli'],
+    # include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Development Status :: 5 - Production/Stable',
+        'Natural Language :: English',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Security'
+    ],
+    entry_points={
+        'console_scripts': ['zanshin=zanshincli.main:main_app']
+    }
+)
+# flake8: noqa
