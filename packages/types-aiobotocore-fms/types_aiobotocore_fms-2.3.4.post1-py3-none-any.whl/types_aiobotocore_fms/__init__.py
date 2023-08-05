9742748e296f7cfd8e9f22998e4ac85deb17500e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:16 +0000

    Add package types-aiobotocore-fms/2.3.4.post1/types_aiobotocore_fms-2.3.4.post1-py3-none-any.whl

---
+++ packages/types-aiobotocore-fms/types_aiobotocore_fms-2.3.4.post1-py3-none-any.whl/types_aiobotocore_fms/__init__.py
@@ -0,0 +1,51 @@
+"""
+Main interface for fms service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_fms import (
+        Client,
+        FMSClient,
+        ListAppsListsPaginator,
+        ListComplianceStatusPaginator,
+        ListMemberAccountsPaginator,
+        ListPoliciesPaginator,
+        ListProtocolsListsPaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("fms") as client:
+        client: FMSClient
+        ...
+
+
+    list_apps_lists_paginator: ListAppsListsPaginator = client.get_paginator("list_apps_lists")
+    list_compliance_status_paginator: ListComplianceStatusPaginator = client.get_paginator("list_compliance_status")
+    list_member_accounts_paginator: ListMemberAccountsPaginator = client.get_paginator("list_member_accounts")
+    list_policies_paginator: ListPoliciesPaginator = client.get_paginator("list_policies")
+    list_protocols_lists_paginator: ListProtocolsListsPaginator = client.get_paginator("list_protocols_lists")
+    ```
+"""
+from .client import FMSClient
+from .paginator import (
+    ListAppsListsPaginator,
+    ListComplianceStatusPaginator,
+    ListMemberAccountsPaginator,
+    ListPoliciesPaginator,
+    ListProtocolsListsPaginator,
+)
+
+Client = FMSClient
+
+
+__all__ = (
+    "Client",
+    "FMSClient",
+    "ListAppsListsPaginator",
+    "ListComplianceStatusPaginator",
+    "ListMemberAccountsPaginator",
+    "ListPoliciesPaginator",
+    "ListProtocolsListsPaginator",
+)
