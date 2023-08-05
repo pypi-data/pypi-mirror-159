f30ec3960b36b7a0dcb21115fd7474b4e1449709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:52 +0000

    Add package basicco/4.2.1/basicco-4.2.1.tar.gz

---
+++ packages/basicco/basicco-4.2.1.tar.gz/basicco-4.2.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools  # type: ignore
+
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="basicco",
+    version="4.2.1",
+    author="Bruno Nicko",
+    author_email="brunonicko@gmail.com",
+    description="Collection of lower-level utilities that enhance code compatibility and validation.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/brunonicko/basicco",
+    packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
+    package_data={"basicco": ["py.typed"]},
+    install_requires=["six", "tippo"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    python_requires=(">= 2.7, " "!= 3.0.*, != 3.1.*, != 3.2.*, != 3.3.*, != 3.4.*, != 3.5.*, != 3.6.*"),
+    tests_require=["pytest"],
+)
