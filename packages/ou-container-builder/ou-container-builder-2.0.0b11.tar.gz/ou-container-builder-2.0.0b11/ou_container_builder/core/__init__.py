fc5e4637e897f1f67225a51e69b2dfc50b8bff79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:33 +0000

    Add package ou-container-builder/2.0.0b11/ou-container-builder-2.0.0b11.tar.gz

---
+++ packages/ou-container-builder/ou-container-builder-2.0.0b11.tar.gz/ou-container-builder-2.0.0b11/ou_container_builder/core/__init__.py
@@ -0,0 +1,6 @@
+"""Core settings processing functionality."""
+from .content import apply_core as content  # noqa
+from .env import apply_core as env  # noqa
+from .scripts import apply_core as scripts  # noqa
+from .services import apply_core as services  # noqa
+from .web_apps import apply_core as web_apps  # noqa
