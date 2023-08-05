c171a32bd5e728bf48c717abe9517c3cf09bcc93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:28 +0000

    Add package kong-control-plane/3.0.0.dev26/kong-control-plane-3.0.0.dev26.tar.gz

---
+++ packages/kong-control-plane/kong-control-plane-3.0.0.dev26.tar.gz/kong-control-plane-3.0.0.dev26/src/kong_control_plane/_jsii/__init__.py
@@ -0,0 +1,26 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import constructs._jsii
+import kong_core._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "kong-control-plane",
+    "3.0.0-dev.26",
+    __name__[0:-6],
+    "kong-control-plane@3.0.0-dev.26.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
