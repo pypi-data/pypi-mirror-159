360a2c32366653ee4b7d16b6f816137018ff487d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:45 +0000

    Add package flask-FormRequest/0.0.32/flask-FormRequest-0.0.32.tar.gz

---
+++ packages/flask-FormRequest/flask-FormRequest-0.0.32.tar.gz/flask-FormRequest-0.0.32/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="flask-FormRequest",
+    version="0.0.32",
+    author="edcilo",
+    description="Request validator for flask",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    exclude=["test"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    include_package_data=True,
+    packages=['flaskFormRequest', 'flaskFormRequest.decorators', 'flaskFormRequest.filters', 'flaskFormRequest.validators'],
+    package_dir={'':'src'},
+    install_requires=[
+        'email-validator==1.1.3',
+    ],
+)
---
+++ packages/flask-FormRequest/flask-FormRequest-0.0.32.tar.gz/flask-FormRequest-0.0.32/src/flaskFormRequest/validators/validator.py
@@ -0,0 +1,45 @@
+import abc
+from typing import Any, Union
+
+
+class ValidationError(ValueError):
+    def __init__(self, message="", *args, **kwargs):
+        ValueError.__init__(self, message, *args, **kwargs)
+
+
+class CollectionErrors(Exception):
+    def __init__(self, message="", *args, **kwargs):
+        self.errors = message
+        Exception.__init__(self, message, *args, **kwargs)
+
+
+class StopValidation(Exception):
+    def __init__(self, message="", *args, **kwargs):
+        Exception.__init__(self, message, *args, **kwargs)
+
+
+class NoneValueException(Exception):
+    def __init__(self, message="", *args, **kwargs):
+        Exception.__init__(self, message, *args, **kwargs)
+
+
+class Validator(abc.ABC):
+    def __init__(self, message: Union[str, None] = None, parse: bool = True) -> None:
+        self.data = {}
+        self.parse = parse
+        self.message = message or 'This field is invalid'
+
+    def parse_data(self, value: Any) -> Any:
+        return value
+
+    @abc.abstractmethod
+    def handler(self, value, field, request):
+        pass
+
+    def __call__(self, value, field, data, request):
+        self.data = data
+        self.field = field
+        self.value = value
+        self.request = request
+        self.handler(self.value, self.field, self.request)
+        return self.parse_data(self.value) if self.parse else self.value
