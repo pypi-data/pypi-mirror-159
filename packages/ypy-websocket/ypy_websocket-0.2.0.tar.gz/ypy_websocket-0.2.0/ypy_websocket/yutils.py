dc56bcc6dc0de5582f6842a17956074d158de754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:49:34 +0000

    Add package ypy-websocket/0.2.0/ypy_websocket-0.2.0.tar.gz

---
+++ packages/ypy-websocket/ypy_websocket-0.2.0.tar.gz/ypy_websocket-0.2.0/ypy_websocket/yutils.py
@@ -0,0 +1,64 @@
+from enum import IntEnum
+
+
+class YMessageType(IntEnum):
+    SYNC = 0
+    SYNC_STEP1 = 0
+    SYNC_STEP2 = 1
+    SYNC_UPDATE = 2
+
+
+def write_var_uint(num: int) -> bytes:
+    res = []
+    while num > 127:
+        res.append(128 | (127 & num))
+        num >>= 7
+    res.append(num)
+    return bytes(res)
+
+
+def create_message(data: bytes, msg_type: int) -> bytes:
+    return bytes([YMessageType.SYNC, msg_type]) + write_var_uint(len(data)) + data
+
+
+def create_sync_step1_message(data: bytes) -> bytes:
+    return create_message(data, YMessageType.SYNC_STEP1)
+
+
+def create_sync_step2_message(data: bytes) -> bytes:
+    return create_message(data, YMessageType.SYNC_STEP2)
+
+
+def create_update_message(data: bytes) -> bytes:
+    return create_message(data, YMessageType.SYNC_UPDATE)
+
+
+def get_messages(message):
+    length = len(message)
+    if not length:
+        return
+    i0 = 0
+    while True:
+        msg_len = 0
+        i = 0
+        while True:
+            byte = message[i0]
+            msg_len += (byte & 127) << i
+            i += 7
+            i0 += 1
+            length -= 1
+            if byte < 128:
+                break
+        i1 = i0 + msg_len
+        msg = message[i0:i1]
+        length -= msg_len
+        yield msg
+        if length == 0:
+            return
+        if length < 0:
+            raise RuntimeError("Y protocol error")
+        i0 = i1
+
+
+def get_message(message):
+    return next(get_messages(message))
