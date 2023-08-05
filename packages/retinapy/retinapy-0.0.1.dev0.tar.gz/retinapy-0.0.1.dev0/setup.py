7e6fe8fe92884d12673aeee0079a22df43a6002f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:51:10 +0000

    Add package retinapy/0.0.1.dev0/retinapy-0.0.1.dev0.tar.gz

---
+++ packages/retinapy/retinapy-0.0.1.dev0.tar.gz/retinapy-0.0.1.dev0/setup.py
@@ -0,0 +1,20 @@
+import setuptools 
+
+setuptools.setup(
+    name='retinapy',
+    version='0.0.1.dev0',
+    author='Baden Lab members',
+    author_email='kevin@kdoran.com',
+    description=('RetinaPy is a Python package for working with recordings of '
+        'retina activity'),
+    packages=['retinapy'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    license = 'BSD-3',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Science/Research']
+)
+
