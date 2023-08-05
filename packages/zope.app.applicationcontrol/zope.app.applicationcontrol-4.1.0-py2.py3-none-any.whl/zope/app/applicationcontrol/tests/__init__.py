32e6802d8d553fecc38704be984459a177c13540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:50:33 +0000

    Add package zope.app.applicationcontrol/4.1.0/zope.app.applicationcontrol-4.1.0-py2.py3-none-any.whl

---
+++ packages/zope.app.applicationcontrol/zope.app.applicationcontrol-4.1.0-py2.py3-none-any.whl/zope/app/applicationcontrol/tests/__init__.py
@@ -0,0 +1,18 @@
+#
+# This file is necessary to make this directory a package.
+
+from zope.applicationcontrol.interfaces import IServerControl
+from zope.interface import implementer
+
+
+@implementer(IServerControl)
+class MockServerControl(object):
+
+    did_restart = None
+    did_shutdown = None
+
+    def restart(self, time):
+        self.did_restart = time
+
+    def shutdown(self, time):
+        self.did_shutdown = time
