99dac4bec80aaf8550091abbfb6f6e6b8c9badba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:44 +0000

    Add package common-model/0.4.5/common-model-0.4.5.tar.gz

---
+++ packages/common-model/common-model-0.4.5.tar.gz/common-model-0.4.5/commonmodel/__init__.py
@@ -0,0 +1,22 @@
+# flake8: noqa F401
+
+from .base import Field, Schema
+from .field_types import (
+    DEFAULT_FIELD_TYPE,
+    DEFAULT_FIELD_TYPE_CLASS,
+    Array,
+    Binary,
+    Boolean,
+    Date,
+    DateTime,
+    Decimal,
+    FieldType,
+    Float,
+    Integer,
+    Json,
+    LongBinary,
+    LongText,
+    Struct,
+    Text,
+    Time,
+)
