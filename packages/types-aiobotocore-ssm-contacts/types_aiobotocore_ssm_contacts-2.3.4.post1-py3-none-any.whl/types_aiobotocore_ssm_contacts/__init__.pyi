8d852769a02f63acf2f1ffc4d2fd6f74ffd09b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:29:14 +0000

    Add package types-aiobotocore-ssm-contacts/2.3.4.post1/types_aiobotocore_ssm_contacts-2.3.4.post1-py3-none-any.whl

---
+++ packages/types-aiobotocore-ssm-contacts/types_aiobotocore_ssm_contacts-2.3.4.post1-py3-none-any.whl/types_aiobotocore_ssm_contacts/__init__.pyi
@@ -0,0 +1,54 @@
+"""
+Main interface for ssm-contacts service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_ssm_contacts import (
+        Client,
+        ListContactChannelsPaginator,
+        ListContactsPaginator,
+        ListEngagementsPaginator,
+        ListPageReceiptsPaginator,
+        ListPagesByContactPaginator,
+        ListPagesByEngagementPaginator,
+        SSMContactsClient,
+    )
+
+    session = get_session()
+    async with session.create_client("ssm-contacts") as client:
+        client: SSMContactsClient
+        ...
+
+
+    list_contact_channels_paginator: ListContactChannelsPaginator = client.get_paginator("list_contact_channels")
+    list_contacts_paginator: ListContactsPaginator = client.get_paginator("list_contacts")
+    list_engagements_paginator: ListEngagementsPaginator = client.get_paginator("list_engagements")
+    list_page_receipts_paginator: ListPageReceiptsPaginator = client.get_paginator("list_page_receipts")
+    list_pages_by_contact_paginator: ListPagesByContactPaginator = client.get_paginator("list_pages_by_contact")
+    list_pages_by_engagement_paginator: ListPagesByEngagementPaginator = client.get_paginator("list_pages_by_engagement")
+    ```
+"""
+from .client import SSMContactsClient
+from .paginator import (
+    ListContactChannelsPaginator,
+    ListContactsPaginator,
+    ListEngagementsPaginator,
+    ListPageReceiptsPaginator,
+    ListPagesByContactPaginator,
+    ListPagesByEngagementPaginator,
+)
+
+Client = SSMContactsClient
+
+__all__ = (
+    "Client",
+    "ListContactChannelsPaginator",
+    "ListContactsPaginator",
+    "ListEngagementsPaginator",
+    "ListPageReceiptsPaginator",
+    "ListPagesByContactPaginator",
+    "ListPagesByEngagementPaginator",
+    "SSMContactsClient",
+)
