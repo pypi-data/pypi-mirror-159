0a2e03f077a0c14ef1cd62a6bf022eded16aec75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:46:12 +0000

    Add package metastreamlit/0.4.3rc36/metastreamlit-0.4.3rc36-py3-none-any.whl

---
+++ packages/metastreamlit/metastreamlit-0.4.3rc36-py3-none-any.whl/metastreamlit/image_crop/build/precache-manifest.18a7d63bd706aa35caaca7db9767fa9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b6c0db6cc875335bd8e8c04ed1997ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f97646ce6f77c57c9143",
+    "url": "./static/css/2.a7708dfc.chunk.css"
+  },
+  {
+    "revision": "f97646ce6f77c57c9143",
+    "url": "./static/js/2.d9724828.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.d9724828.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8b15f531eb2042d1bbb",
+    "url": "./static/js/main.f06476bb.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.f06476bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
