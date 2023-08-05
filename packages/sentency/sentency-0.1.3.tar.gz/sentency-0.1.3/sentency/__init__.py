c202272e63b635c8f00d75e8b8c6ec638ff329c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:55 +0000

    Add package sentency/0.1.3/sentency-0.1.3.tar.gz

---
+++ packages/sentency/sentency-0.1.3.tar.gz/sentency-0.1.3/sentency/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for sentency."""
+
+__author__ = """Grant DeLong"""
+__email__ = "gdelong1@geisinger.edu"
+__version__ = "0.1.3"
---
+++ packages/sentency/sentency-0.1.3.tar.gz/sentency-0.1.3/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+import sentency
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [ ]
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="Grant DeLong",
+    author_email='gdelong1@geisinger.edu',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="A small lspaCy pipeline component for matching within document sentences using regular expressi",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='sentency',
+    name='sentency',
+    packages=find_packages(include=['sentency', 'sentency.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/g-delong/sentency',
+    version=sentency.__version__,
+    zip_safe=False,
+)
