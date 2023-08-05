c2c3ed48e70d3a310d8514889892047cf0257ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:28:46 +0000

    Add package types-aiobotocore-marketplace-catalog/2.3.4.post1/types_aiobotocore_marketplace_catalog-2.3.4.post1-py3-none-any.whl

---
+++ packages/types-aiobotocore-marketplace-catalog/types_aiobotocore_marketplace_catalog-2.3.4.post1-py3-none-any.whl/types_aiobotocore_marketplace_catalog/__init__.py
@@ -0,0 +1,25 @@
+"""
+Main interface for marketplace-catalog service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_marketplace_catalog import (
+        Client,
+        MarketplaceCatalogClient,
+    )
+
+    session = get_session()
+    async with session.create_client("marketplace-catalog") as client:
+        client: MarketplaceCatalogClient
+        ...
+
+    ```
+"""
+from .client import MarketplaceCatalogClient
+
+Client = MarketplaceCatalogClient
+
+
+__all__ = ("Client", "MarketplaceCatalogClient")
