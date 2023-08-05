4f05092f85afed53a0b3777f50345c7cd0c49fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:24 +0000

    Add package notify-function/1.5.2/notify_function-1.5.2.tar.gz

---
+++ packages/notify-function/notify_function-1.5.2.tar.gz/notify_function-1.5.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="notify_function",
+    version="1.5.2",
+    author="Enmanuel Magallanes Pinargote",
+    author_email="enmanuelmag@cardor.dev",
+    description="Notify you when a function finished with option to send a email, telegram message or message to discord channel",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/enmanuel-mag/notify_function",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests',
+        'py-notifier',
+        'discord_webhook'
+    ],
+    python_requires='>=3.6',
+)
