cabb4e1dac81682bfd813937e344b3f3ac92b4e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:46 +0000

    Add package bTagScript/3.1.0/bTagScript-3.1.0.tar.gz

---
+++ packages/bTagScript/bTagScript-3.1.0.tar.gz/bTagScript-3.1.0/bTagScript/adapter/int_adapter.py
@@ -0,0 +1,28 @@
+from ..interface import Adapter
+from ..verb import Verb
+
+
+class IntAdapter(Adapter):
+    """
+    IntAdapter, so far no use for this?
+    """
+
+    __slots__ = ("integer",)
+
+    def __init__(self, integer: int) -> None:
+        """
+        Construct the int adapter
+        """
+        self.integer: int = int(integer)
+
+    def __repr__(self) -> str:
+        """
+        String repr
+        """
+        return f"<{type(self).__qualname__} integer={repr(self.integer)}>"
+
+    def get_value(self, ctx: Verb) -> str:
+        """
+        Get the value of the int into string, not sure why this even exists
+        """
+        return str(self.integer)
