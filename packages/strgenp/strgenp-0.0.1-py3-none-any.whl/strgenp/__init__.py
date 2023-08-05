13b4e7183d9e3273bbe6a9ef6a265af9ab2c9fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:43 +0000

    Add package strgenp/0.0.1/strgenp-0.0.1-py3-none-any.whl

---
+++ packages/strgenp/strgenp-0.0.1-py3-none-any.whl/strgenp/__init__.py
@@ -0,0 +1,1 @@
+from strgen.strgen import stringgen
---
+++ packages/strgenp/strgenp-0.0.1-py3-none-any.whl/strgenp/strgenp.py
@@ -0,0 +1,16 @@
+import random
+class stringgen:
+	def __init__(self):
+		pl = []
+		l = 5
+		pl.append(l)
+		alphabet = "abcdefghijkRTYUIOPMNBVCXZlmnopqrstuvwxyzASDFGHJKLQWE"
+		for i in pl:
+			gstring = ""
+			for j in range(i):
+				next_letter_index = random.randrange(len(alphabet))
+				gstring = gstring + alphabet[next_letter_index]
+
+			return gstring
+
+
