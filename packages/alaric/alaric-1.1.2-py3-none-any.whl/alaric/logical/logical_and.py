3337746edf445bdf00020e074a41a7d3723344fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:49 +0000

    Add package alaric/1.1.2/alaric-1.1.2-py3-none-any.whl

---
+++ packages/alaric/alaric-1.1.2-py3-none-any.whl/alaric/logical/logical_and.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, List, Dict, Union
+
+if TYPE_CHECKING:
+    from alaric.abc import ComparisonT, LogicalT
+
+
+class AND:
+    """Conduct a logical AND between all items passed to the constructor.
+
+
+    Lets build a query which returns all results where the field ``discord``
+    is equal to ``Skelmis#9135`` and ``gamer_tag`` is equal to ``Skelmis``
+
+    .. code-block:: python
+        :linenos:
+
+        from alaric import AQ
+        from alaric.logical import AND
+        from alaric.comparison import EQ
+
+        query = AQ(AND(EQ("discord", "Skelmis#9135"), EQ("gamer_tag", "Skelmis")))
+    """
+
+    def __init__(
+        self,
+        *comparisons: Union[
+            Union[ComparisonT, LogicalT],
+            List[Union[ComparisonT, LogicalT]],
+        ],
+    ):
+        self.comparisons: List[Union[ComparisonT, LogicalT]] = list(comparisons)
+
+    def __repr__(self):
+        return f"AND(comparisons={self.comparisons})"
+
+    def build(self) -> Dict[str, List[Dict]]:
+        """Return this instance as a usable Mongo filter."""
+        comparisons: List[Dict] = [c.build() for c in self.comparisons]
+        return {"$and": comparisons}
---
+++ packages/alaric/alaric-1.1.2-py3-none-any.whl/alaric/query_builder.py
@@ -0,0 +1,24 @@
+import asyncio
+import os
+
+from motor.motor_asyncio import AsyncIOMotorClient
+
+from alaric import *
+from alaric.comparison import *
+from alaric.logical import *
+from alaric.meta import *
+from alaric.projections import *
+from alaric.types import *
+
+
+async def main():
+    client = AsyncIOMotorClient(os.environ["MONGO"])
+    db = client["COMPX323"]
+    document: Document = Document(db, "movies")
+
+    r_1 = await document.find(EQ("_id", ObjectId("6297cf0186d144fe9b619135")))
+    print(r_1)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
