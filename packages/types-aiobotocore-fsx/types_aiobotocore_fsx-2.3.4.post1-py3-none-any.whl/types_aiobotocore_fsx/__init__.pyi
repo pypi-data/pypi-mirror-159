a951079db6ceaa8c85c5444f5f9f17839df2cd09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:16 +0000

    Add package types-aiobotocore-fsx/2.3.4.post1/types_aiobotocore_fsx-2.3.4.post1-py3-none-any.whl

---
+++ packages/types-aiobotocore-fsx/types_aiobotocore_fsx-2.3.4.post1-py3-none-any.whl/types_aiobotocore_fsx/__init__.pyi
@@ -0,0 +1,42 @@
+"""
+Main interface for fsx service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_fsx import (
+        Client,
+        DescribeBackupsPaginator,
+        DescribeFileSystemsPaginator,
+        FSxClient,
+        ListTagsForResourcePaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("fsx") as client:
+        client: FSxClient
+        ...
+
+
+    describe_backups_paginator: DescribeBackupsPaginator = client.get_paginator("describe_backups")
+    describe_file_systems_paginator: DescribeFileSystemsPaginator = client.get_paginator("describe_file_systems")
+    list_tags_for_resource_paginator: ListTagsForResourcePaginator = client.get_paginator("list_tags_for_resource")
+    ```
+"""
+from .client import FSxClient
+from .paginator import (
+    DescribeBackupsPaginator,
+    DescribeFileSystemsPaginator,
+    ListTagsForResourcePaginator,
+)
+
+Client = FSxClient
+
+__all__ = (
+    "Client",
+    "DescribeBackupsPaginator",
+    "DescribeFileSystemsPaginator",
+    "FSxClient",
+    "ListTagsForResourcePaginator",
+)
