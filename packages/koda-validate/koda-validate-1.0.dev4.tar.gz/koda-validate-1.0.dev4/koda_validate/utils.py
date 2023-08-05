8be6cd9bcf0f52ee923d54bc600d664ecb37092b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:29:36 +0000

    Add package koda-validate/1.0.dev4/koda-validate-1.0.dev4.tar.gz

---
+++ packages/koda-validate/koda-validate-1.0.dev4.tar.gz/koda-validate-1.0.dev4/koda_validate/utils.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from typing import Iterable
+
+from koda import Err, Ok, Result
+
+from koda_validate._cruft import _chain, _validate_and_map
+from koda_validate._generics import A, FailT
+from koda_validate.typedefs import Predicate
+
+
+def expected(val: str) -> str:
+    return f"expected {val}"
+
+
+def accum_errors(
+    val: A, validators: Iterable[Predicate[A, FailT]]
+) -> Result[A, list[FailT]]:
+    errors: list[FailT] = []
+    result: Result[A, FailT] = Ok(val)
+    for validator in validators:
+        result = validator(val)
+        if isinstance(result, Err):
+            errors.append(result.val)
+        else:
+            val = result.val
+
+    if len(errors) > 0:
+        return Err(errors)
+    else:
+        # has to be because there are no errors
+        assert isinstance(result, Ok)
+        return Ok(result.val)
+
+
+chain = _chain
+validate_and_map = _validate_and_map
