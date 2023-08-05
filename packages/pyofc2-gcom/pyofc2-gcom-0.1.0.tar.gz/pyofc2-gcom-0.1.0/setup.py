459132b3ff0732daf68fe2210b3f23bbbf9a1b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:42:41 +0000

    Add package pyofc2-gcom/0.1.0/pyofc2-gcom-0.1.0.tar.gz

---
+++ packages/pyofc2-gcom/pyofc2-gcom-0.1.0.tar.gz/pyofc2-gcom-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyofc2_gcom']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pyofc2-gcom',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'eltonmeurer2022@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=2.7,<3.0',
+}
+
+
+setup(**setup_kwargs)
