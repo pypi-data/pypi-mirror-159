e8bc548e1aab3866e0ad518650ef0e8ea10855e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:43:03 +0000

    Add package tkinterDev/2.0.0/tkinterDev-2.0.0.tar.gz

---
+++ packages/tkinterDev/tkinterDev-2.0.0.tar.gz/tkinterDev-2.0.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="tkinterDev",
+    version="2.0.0",
+    author="XiangQinxi",
+    author_email="XiangQinxi@outlook.com",
+    description="tkinter Tool",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://xiangqinxidevelopment.jetbrains.space/p/tkinterdev",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
