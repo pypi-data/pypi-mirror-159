8c5eee83106243c52d07c8f85de72507dceba3bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:29:06 +0000

    Add package types-aiobotocore-s3outposts/2.3.4.post1/types_aiobotocore_s3outposts-2.3.4.post1-py3-none-any.whl

---
+++ packages/types-aiobotocore-s3outposts/types_aiobotocore_s3outposts-2.3.4.post1-py3-none-any.whl/types_aiobotocore_s3outposts/__init__.py
@@ -0,0 +1,31 @@
+"""
+Main interface for s3outposts service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_s3outposts import (
+        Client,
+        ListEndpointsPaginator,
+        ListSharedEndpointsPaginator,
+        S3OutpostsClient,
+    )
+
+    session = get_session()
+    async with session.create_client("s3outposts") as client:
+        client: S3OutpostsClient
+        ...
+
+
+    list_endpoints_paginator: ListEndpointsPaginator = client.get_paginator("list_endpoints")
+    list_shared_endpoints_paginator: ListSharedEndpointsPaginator = client.get_paginator("list_shared_endpoints")
+    ```
+"""
+from .client import S3OutpostsClient
+from .paginator import ListEndpointsPaginator, ListSharedEndpointsPaginator
+
+Client = S3OutpostsClient
+
+
+__all__ = ("Client", "ListEndpointsPaginator", "ListSharedEndpointsPaginator", "S3OutpostsClient")
