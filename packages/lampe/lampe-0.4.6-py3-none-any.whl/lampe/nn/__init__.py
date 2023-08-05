add376ff240c2461122db75521bbb9f545dbc8a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:46 +0000

    Add package lampe/0.4.6/lampe-0.4.6-py3-none-any.whl

---
+++ packages/lampe/lampe-0.4.6-py3-none-any.whl/lampe/nn/__init__.py
@@ -0,0 +1,395 @@
+r"""Neural networks, layers and modules."""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch import Tensor, BoolTensor
+from typing import *
+
+
+__all__ = ['MLP', 'ResBlock', 'ResMLP', 'MaskedMLP', 'MonotonicMLP']
+
+
+class Affine(nn.Module):
+    r"""Creates an element-wise affine layer.
+
+    .. math:: y = x \times \alpha + \beta
+
+    Arguments:
+        shift: The shift term :math:`\beta`.
+        scale: The scale factor :math:`\alpha`.
+    """
+
+    def __init__(self, shift: Tensor, scale: Tensor):
+        super().__init__()
+
+        self.shift = nn.Parameter(torch.tensor(shift).float())
+        self.scale = nn.Parameter(torch.tensor(scale).float())
+
+    def forward(self, x: Tensor) -> Tensor:
+        return x * self.scale + self.shift
+
+
+class BatchNorm0d(nn.BatchNorm1d):
+    r"""Creates a batch normalization (BatchNorm) layer for scalars.
+
+    References:
+        Batch Normalization: Accelerating Deep Network Training by Reducing Internal Covariate Shift
+        (Ioffe et al., 2015)
+        https://arxiv.org/abs/1502.03167
+
+    Arguments:
+        args: Positional arguments passed to :class:`torch.nn.BatchNorm1d`.
+        kwargs: Keyword arguments passed to :class:`torch.nn.BatchNorm1d`.
+    """
+
+    def forward(self, x: Tensor) -> Tensor:
+        shape = x.shape
+
+        x = x.reshape(-1, shape[-1])
+        x = super().forward(x)
+        x = x.reshape(shape)
+
+        return x
+
+
+class MLP(nn.Sequential):
+    r"""Creates a multi-layer perceptron (MLP).
+
+    Also known as fully connected feedforward network, an MLP is a series of
+    non-linear parametric transformations
+
+    .. math:: h_{i + 1} = a_{i + 1}(W_{i + 1}^T h_i + b_{i + 1}),
+
+    over feature vectors :math:`h_i`, with the input and output feature vectors
+    :math:`x = h_0` and :math:`y = h_L`, respectively. The non-linear functions
+    :math:`a_i` are called activation functions. The trainable parameters of an MLP
+    are its weights and biases :math:`\phi = \{W_i, b_i | i = 1, \dots, L\}`.
+
+    Wikipedia:
+        https://wikipedia.org/wiki/Feedforward_neural_network
+
+    Arguments:
+        in_features: The number of input features.
+        out_features: The number of output features.
+        hidden_features: The numbers of hidden features.
+        activation: The activation layer type.
+        batchnorm: Whether to use batch normalization or not.
+        dropout: The dropout rate.
+        kwargs: Keyword arguments passed to :class:`torch.nn.Linear`.
+
+    Example:
+        >>> net = MLP(64, 1, [32, 16], activation='ELU')
+        >>> net
+        MLP(
+          (0): Linear(in_features=64, out_features=32, bias=True)
+          (1): ELU(alpha=1.0)
+          (2): Linear(in_features=32, out_features=16, bias=True)
+          (3): ELU(alpha=1.0)
+          (4): Linear(in_features=16, out_features=1, bias=True)
+        )
+    """
+
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        hidden_features: List[int] = [64, 64],
+        activation: str = 'ReLU',
+        batchnorm: bool = False,
+        dropout: float = 0.,
+        **kwargs,
+    ):
+        activation = {
+            'ReLU': nn.ReLU,
+            'ELU': nn.ELU,
+            'CELU': nn.CELU,
+            'SELU': nn.SELU,
+            'GELU': nn.GELU,
+        }.get(activation, nn.ReLU)
+
+        batchnorm = BatchNorm0d if batchnorm else lambda _: None
+        dropout = nn.Dropout(dropout) if dropout > 0 else None
+
+        layers = []
+
+        for before, after in zip(
+            [in_features] + hidden_features,
+            hidden_features + [out_features],
+        ):
+            layers.extend([
+                nn.Linear(before, after, **kwargs),
+                batchnorm(after),
+                activation(),
+                dropout,
+            ])
+
+        layers = layers[:-3]
+        layers = filter(lambda l: l is not None, layers)
+
+        super().__init__(*layers)
+
+        self.in_features = in_features
+        self.out_features = out_features
+
+
+class ResBlock(MLP):
+    r"""Creates a residual block.
+
+    A residual block is a function of the type
+
+    .. math:: y = x + f(x),
+
+    where :math:`f` is a non-linear parametric transformation. An MLP with a
+    constant number of features in hidden layers is commonly used as :math:`f`.
+
+    Arguments:
+        features: The input, output and hidden features.
+        hidden_layers: The number of hidden layers.
+        kwargs: Keyword arguments passed to :class:`MLP`.
+
+    Example:
+        >>> net = ResBlock(32, hidden_layers=3, activation='ELU')
+        >>> net
+        ResBlock(
+          (0): Linear(in_features=32, out_features=32, bias=True)
+          (1): ELU(alpha=1.0)
+          (2): Linear(in_features=32, out_features=32, bias=True)
+          (3): ELU(alpha=1.0)
+          (4): Linear(in_features=32, out_features=32, bias=True)
+          (5): ELU(alpha=1.0)
+          (6): Linear(in_features=32, out_features=32, bias=True)
+        )
+    """
+
+    def __init__(self, features: int, hidden_layers: int = 2, **kwargs):
+        super().__init__(
+            features,
+            features,
+            [features] * hidden_layers,
+            **kwargs,
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        return x + super().forward(x)
+
+
+class ResMLP(nn.Sequential):
+    r"""Creates a residual multi-layer perceptron (ResMLP).
+
+    Like the regular MLP, the ResMLP is a series of non-linear parametric
+    transformations. However, it uses residual blocks as transformations, which
+    reduces the vanishing of gradients and allows for deeper networks.
+
+    Arguments:
+        in_features: The number of input features.
+        out_features: The number of output features.
+        hidden_features: The numbers of hidden features.
+        kwargs: Keyword arguments passed to :class:`ResBlock`.
+
+    Example:
+        >>> net = ResMLP(64, 1, [32, 16], activation='ELU')
+        >>> net
+        ResMLP(
+          (0): Linear(in_features=64, out_features=32, bias=True)
+          (1): ResBlock(
+            (0): Linear(in_features=32, out_features=32, bias=True)
+            (1): ELU(alpha=1.0)
+            (2): Linear(in_features=32, out_features=32, bias=True)
+            (3): ELU(alpha=1.0)
+            (4): Linear(in_features=32, out_features=32, bias=True)
+          )
+          (2): Linear(in_features=32, out_features=16, bias=True)
+          (3): ResBlock(
+            (0): Linear(in_features=16, out_features=16, bias=True)
+            (1): ELU(alpha=1.0)
+            (2): Linear(in_features=16, out_features=16, bias=True)
+            (3): ELU(alpha=1.0)
+            (4): Linear(in_features=16, out_features=16, bias=True)
+          )
+          (4): Linear(in_features=16, out_features=1, bias=True)
+        )
+    """
+
+    def __init__(
+        self,
+        in_features: int,
+        out_features: int,
+        hidden_features: List[int] = [64, 64],
+        **kwargs,
+    ):
+        blocks = []
+
+        for before, after in zip(
+            [in_features] + hidden_features,
+            hidden_features + [out_features],
+        ):
+            if after != before:
+                blocks.append(nn.Linear(before, after))
+
+            blocks.append(ResBlock(after, **kwargs))
+
+        blocks = blocks[:-1]
+
+        super().__init__(*blocks)
+
+        self.in_features = in_features
+        self.out_features = out_features
+
+
+class MaskedLinear(nn.Linear):
+    r"""Creates a masked linear layer.
+
+    .. math:: y = x (W \odot A)^T + b
+
+    Arguments:
+        adjacency: The adjacency matrix :math:`A \in \{0, 1\}^{M \times N}`.
+        kwargs: Keyword arguments passed to :class:`torch.nn.Linear`.
+    """
+
+    def __init__(self, adjacency: BoolTensor, **kwargs):
+        super().__init__(*adjacency.shape, **kwargs)
+
+        self.register_buffer('mask', adjacency.t())
+
+    def forward(self, x: Tensor) -> Tensor:
+        return F.linear(x, self.mask * self.weight, self.bias)
+
+
+class MaskedMLP(MLP):
+    r"""Creates a masked multi-layer perceptron.
+
+    The resulting MLP is a transformation :math:`y = f(x)` whose Jacobian entries
+    :math:`\frac{\partial y_j}{\partial x_i}` are null if :math:`A_{ij} = 0`.
+
+    Arguments:
+        adjacency: The adjacency matrix :math:`A \in \{0, 1\}^{M \times N}`.
+        args: Positional arguments passed to :class:`MLP`.
+        kwargs: Keyword arguments passed to :class:`MLP`.
+
+    Example:
+        >>> adjacency = torch.rand(4, 4) < 0.5
+        >>> adjacency
+        tensor([[False,  True, False, False],
+                [ True, False,  True, False],
+                [False,  True, False, False],
+                [False,  True,  True, False]])
+        >>> net = MaskedMLP(adjacency, [16, 32], activation='ELU')
+        >>> net
+        MaskedMLP(
+          (0): MaskedLinear(in_features=4, out_features=16, bias=True)
+          (1): ELU(alpha=1.0)
+          (2): MaskedLinear(in_features=16, out_features=32, bias=True)
+          (3): ELU(alpha=1.0)
+          (4): MaskedLinear(in_features=32, out_features=4, bias=True)
+        )
+        >>> x = torch.randn(4)
+        >>> torch.autograd.functional.jacobian(net, x).t()
+        tensor([[ 0.0000,  0.0031,  0.0000,  0.0000],
+                [-0.0323,  0.0000, -0.0547,  0.0000],
+                [ 0.0000, -0.0245,  0.0000,  0.0000],
+                [ 0.0000,  0.0060, -0.0063,  0.0000]])
+    """
+
+    def __init__(
+        self,
+        adjacency: BoolTensor,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*adjacency.shape, *args, **kwargs)
+
+        # i precedes j if A_kj = 1 for all k such that A_ki = 1
+        precedence = adjacency.t().int() @ adjacency.int() == adjacency.sum(dim=0).unsqueeze(-1)
+
+        for i, layer in enumerate(self):
+            if isinstance(layer, nn.Linear):
+                if i > 0:
+                    mask = precedence[indices]
+                else:
+                    mask = adjacency
+
+                if i < len(self) - 1:
+                    reachable = mask.sum(0).nonzero().squeeze()
+                    indices = reachable[torch.arange(layer.out_features) % len(reachable)]
+                    mask = mask[:, indices]
+
+                self[i] = MaskedLinear(adjacency=mask)
+
+
+class MonotonicLinear(nn.Linear):
+    r"""Creates a monotonic linear layer.
+
+    .. math:: y = x |W|^T + b
+
+    Arguments:
+        args: Positional arguments passed to :class:`torch.nn.Linear`.
+        kwargs: Keyword arguments passed to :class:`torch.nn.Linear`.
+    """
+
+    def forward(self, x: Tensor) -> Tensor:
+        return F.linear(x, self.weight.abs(), self.bias)
+
+
+class TwoWayELU(nn.ELU):
+    r"""Creates a layer that splits the input into two groups and applies
+    :math:`\text{ELU}(x)` to the first and :math:`-\text{ELU}(-x)` to the second.
+
+    Arguments:
+        args: Positional arguments passed to :class:`torch.nn.ELU`.
+        kwargs: Keyword arguments passed to :class:`torch.nn.ELU`.
+    """
+
+    def forward(self, x: Tensor) -> Tensor:
+        x0, x1 = torch.chunk(x, 2, dim=-1)
+
+        return torch.cat((
+            super().forward(x0),
+            -super().forward(-x1),
+        ), dim=-1)
+
+
+class MonotonicMLP(MLP):
+    r"""Creates a monotonic multi-layer perceptron.
+
+    The resulting MLP is a transformation :math:`y = f(x)` whose Jacobian entries
+    :math:`\frac{\partial y_j}{\partial x_i}` are positive.
+
+    Arguments:
+        args: Positional arguments passed to :class:`MLP`.
+        kwargs: Keyword arguments passed to :class:`MLP`.
+
+    Example:
+        >>> net = MonotonicMLP(4, 4, [16, 32])
+        >>> net
+        MonotonicMLP(
+          (0): MonotonicLinear(in_features=4, out_features=16, bias=True)
+          (1): TwoWayELU(alpha=1.0)
+          (2): MonotonicLinear(in_features=16, out_features=32, bias=True)
+          (3): TwoWayELU(alpha=1.0)
+          (4): MonotonicLinear(in_features=32, out_features=4, bias=True)
+        )
+        >>> x = torch.randn(4)
+        >>> torch.autograd.functional.jacobian(net, x).t()
+        tensor([[0.8742, 0.9439, 0.9759, 1.1040],
+                [0.8969, 0.9716, 0.9866, 1.1321],
+                [1.0780, 1.1651, 1.2056, 1.3674],
+                [0.8596, 0.9400, 0.9502, 1.0916]])
+    """
+
+    def __init__(
+        self,
+        *args,
+        **kwargs,
+    ):
+        kwargs['activation'] = 'ELU'
+        kwargs['batchnorm'] = False
+
+        super().__init__(*args, **kwargs)
+
+        for i, layer in enumerate(self):
+            if isinstance(layer, nn.Linear):
+                layer.__class__ = MonotonicLinear
+            elif isinstance(layer, nn.ELU):
+                layer.__class__ = TwoWayELU
---
+++ packages/lampe/lampe-0.4.6-py3-none-any.whl/lampe/plots.py
@@ -0,0 +1,443 @@
+r"""Plotting helpers."""
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+from numpy import ndarray as Array
+from typing import *
+
+
+__all__ = ['nice_rc', 'corner', 'rank_ecdf']
+
+
+def nice_rc(latex: bool = False) -> Dict[str, Any]:
+    r"""Returns a dictionary of runtime configuration (rc) settings for nicer
+    :mod:`matplotlib` plots. The settings include 12pt font size, higher DPI,
+    tight layout, transparent background, etc.
+
+    Arguments:
+        latex: Whether to use LaTeX typesetting or not.
+
+    Example:
+        >>> plt.rcParams.update(nice_rc())
+    """
+
+    rc = {
+        'axes.axisbelow': True,
+        'axes.linewidth': .8,
+        'figure.autolayout': True,
+        'figure.dpi': 150,
+        'figure.figsize': (6.4, 4.8),
+        'font.size': 12.,
+        'legend.fontsize': 'x-small',
+        'lines.linewidth': 1.,
+        'lines.markersize': 3.,
+        'savefig.bbox': 'tight',
+        'savefig.transparent': True,
+        'xtick.labelsize': 'x-small',
+        'xtick.major.width': .8,
+        'ytick.labelsize': 'x-small',
+        'ytick.major.width': .8,
+    }
+
+    if mpl.checkdep_usetex(latex):
+        rc.update({
+            'font.family': ['serif'],
+            'font.serif': ['Computer Modern'],
+            'text.usetex': True,
+        })
+
+    return rc
+
+
+class LinearAlphaColormap(mpl.colors.LinearSegmentedColormap):
+    r"""Linear segmented transparency colormap.
+
+    Arguments:
+        color: A color.
+        levels: A sequence of levels dividing the domain into segments.
+        alpha: The transparency range.
+        name: A name for the colormap.
+    """
+
+    def __new__(
+        self,
+        color: Union[str, tuple],
+        levels: Array = None,
+        alpha: Tuple[float, float] = (0., 1.),
+        name: str = None,
+    ):
+        if name is None:
+            if type(color) is str:
+                name = f'alpha_{color}'
+            else:
+                name = f'alpha_{hash(color)}'
+
+        if levels is None:
+            levels = [0., 1.]
+
+        levels = np.asarray(levels)
+        levels = (levels - levels.min()) / (levels.max() - levels.min())
+        alpha = np.linspace(*alpha, len(levels))
+
+        rgb = mpl.colors.to_rgb(color)
+
+        colors = sorted([
+            (l, rgb + (a,))
+            for l, a in zip(levels, alpha)
+        ])
+
+        return mpl.colors.LinearSegmentedColormap.from_list(
+            name=name,
+            colors=colors,
+        )
+
+
+def gaussian_blur(img: Array, sigma: float = 1.) -> Array:
+    r"""Applies a Gaussian blur to an image.
+
+    Arguments:
+        img: An image array.
+        sigma: The standard deviation of the Gaussian kernel.
+
+    Returns:
+        The blurred image.
+
+    Example:
+        >>> img = np.random.rand(128, 128)
+        >>> gaussian_blur(img, sigma=2.)
+        array([...])
+    """
+
+    size = 2 * int(3 * sigma) + 1
+
+    k = np.arange(size) - size / 2
+    k = np.exp(-k ** 2 / (2 * sigma ** 2))
+    k = k / np.sum(k)
+
+    smooth = lambda x: np.convolve(x, k, mode='same')
+
+    for i in range(len(img.shape)):
+        img = np.apply_along_axis(smooth, i, img)
+
+    return img
+
+
+def credible_levels(hist: Array, creds: Array) -> Array:
+    r"""Returns the levels of credibility region contours.
+
+    Arguments:
+        hist: An histogram.
+        creds: The region credibilities.
+    """
+
+    x = np.sort(hist, axis=None)[::-1]
+    cdf = np.cumsum(x)
+    idx = np.searchsorted(cdf, creds * cdf[-1])
+
+    return x[idx]
+
+
+def corner(
+    data: Array,
+    bins: Union[int, List[int]] = 100,
+    bounds: Tuple[Array, Array] = None,
+    creds: Array = [.6827, .9545, .9973],
+    color: Union[str, tuple] = None,
+    alpha: Tuple[float, float] = (0., .5),
+    legend: str = None,
+    labels: List[str] = None,
+    markers: List[Array] = [],
+    smooth: float = 0,
+    figure: mpl.figure.Figure = None,
+    **kwargs,
+) -> mpl.figure.Figure:
+    r"""Displays each 1 or 2-d projection of multi-dimensional data, as a triangular
+    matrix of histograms, known as corner plot. For 2-d histograms, highest density
+    credibility regions are delimited.
+
+    Arguments:
+        data: Multi-dimensional data, either as a table or as a matrix of histograms.
+        bins: The number(s) of bins per dimension.
+        bounds: A tuple of lower and upper domain bounds. If :py:`None`, inferred from data.
+        creds: The region credibilities (in :math:`[0, 1]`) to delimit.
+        color: A color for histograms.
+        alpha: A transparency range.
+        legend: A legend.
+        labels: The dimension labels.
+        markers: A list of points to mark on the histograms.
+        smooth: The standard deviation of the smoothing kernels.
+        figure: A corner plot over which to draw the new one.
+        kwargs: Keyword arguments passed to :func:`matplotlib.pyplot.subplots`.
+
+    Returns:
+        The figure instance for the corner plot.
+
+    Example:
+        >>> data = np.random.randn(2**16, 3)
+        >>> labels = [r'$\alpha$', r'$\beta$', r'$\gamma$']
+        >>> corner(data, bins=42, labels=labels, figsize=(4.8, 4.8))
+
+    .. image:: ../static/images/corner.png
+        :align: center
+        :width: 600
+    """
+
+    # Histograms
+    data = np.asarray(data)
+
+    if np.isscalar(data[0, 0]):
+        D = data.shape[-1]
+        data = data.reshape(-1, D)
+
+        if type(bins) is int:
+            bins = [bins] * D
+
+        if bounds is None:
+            lower, upper = data.min(axis=0), data.max(axis=0)
+        else:
+            lower, upper = map(np.asarray, bounds)
+
+        bins = [
+            np.histogram_bin_edges(data, bins[i], range=(lower[i], upper[i]))
+            for i in range(D)
+        ]
+
+        hists = np.ndarray((D, D), dtype=object)
+
+        for i in range(D):
+            for j in range(i + 1):
+                if i == j:
+                    hist, _ = np.histogram(
+                        data[..., i],
+                        bins=bins[i],
+                        density=True,
+                    )
+                else:
+                    hist, _, _ = np.histogram2d(
+                        data[..., i], data[..., j],
+                        bins=(bins[i], bins[j]),
+                        density=True,
+                    )
+
+                hists[i, j] = hist
+    else:
+        D = len(data)
+
+        if bounds is None:
+            lower, upper = np.zeros(D), np.ones(D)
+        else:
+            lower, upper = map(np.asarray, bounds)
+
+        bins = [None] * D
+        for i in range(D):
+            if data[i, i] is not None:
+                bins[i] = np.linspace(lower[i], upper[i], len(data[i, i]) + 1)
+
+        hists = data
+
+    # Figure
+    if figure is None:
+        kwargs.setdefault('figsize', (6.4, 6.4))
+
+        figure, axes = plt.subplots(
+            D, D,
+            squeeze=False,
+            sharex='col',
+            gridspec_kw={'wspace': 0., 'hspace': 0.},
+            **kwargs,
+        )
+        new = True
+    else:
+        axes = np.asarray(figure.axes).reshape(D, D)
+        new = False
+
+    # Legend
+
+    ## Color
+    lines = axes[0, -1].plot([], [], color=color, label=legend)
+    color = lines[-1].get_color()
+
+    handles, texts = axes[0, -1].get_legend_handles_labels()
+
+    ## Quantiles
+    creds = np.sort(np.asarray(creds))[::-1]
+    creds = np.append(creds, 0)
+
+    cmap = LinearAlphaColormap('black', levels=creds, alpha=alpha)
+
+    levels = (creds - creds.min()) / (creds.max() - creds.min())
+    levels = (levels[:-1] + levels[1:]) / 2
+
+    for c, l in zip(creds[:-1], levels):
+        handles.append(mpl.patches.Patch(color=cmap(l), linewidth=0))
+        texts.append(r'${:.1f}\,\%$'.format(c * 100))
+
+    ## Update
+    if not new:
+        figure.legends.clear()
+
+    figure.legend(handles, texts, loc='upper right', bbox_to_anchor=(0.975, 0.975), frameon=False)
+
+    # Plot
+    for i in range(D):
+        for j in range(D):
+            ax = axes[i, j]
+            hist = hists[i, j]
+
+            if j > i:
+                ax.axis('off')
+                continue
+
+            if hist is None:
+                continue
+
+            if smooth > 0:
+                hist = gaussian_blur(hist, smooth)
+
+            ## Draw
+            x, y = bins[j], bins[i]
+            x = (x[1:] + x[:-1]) / 2
+            y = (y[1:] + y[:-1]) / 2
+
+            if i == j:
+                ax.plot(x, hist, color=color)
+
+                _, top = ax.get_ylim()
+                bottom = 0.
+                top = max(top, hist.max() * 1.0625)
+
+                ax.set_xlim(left=bins[i][0], right=bins[i][-1])
+                ax.set_ylim(bottom=bottom, top=top)
+            else:
+                levels = np.unique(credible_levels(hist, creds))
+
+                cf = ax.contourf(
+                    x, y, hist,
+                    levels=levels,
+                    cmap=LinearAlphaColormap(color, levels, alpha=alpha),
+                )
+                ax.contour(cf, colors=color)
+
+                if j > 0:
+                    ax.sharey(axes[i, j - 1])
+                else:
+                    ax.set_ylim(bottom=bins[i][0], top=bins[i][-1])
+
+            ## Markers
+            for marker in map(np.asarray, markers):
+                ax.axvline(
+                    marker[j],
+                    color='k',
+                    linestyle='--',
+                    zorder=420,
+                )
+
+                if i != j:
+                    ax.axhline(
+                        marker[i],
+                        color='k',
+                        linestyle='--',
+                        zorder=420,
+                    )
+
+                    ax.plot(
+                        marker[j], marker[i],
+                        color='k',
+                        marker='s',
+                        zorder=420,
+                    )
+
+            ## Ticks
+            if i == D - 1:
+                ax.xaxis.set_major_locator(mpl.ticker.MaxNLocator(3, prune='both'))
+                plt.setp(
+                    ax.get_xticklabels(),
+                    rotation=45.,
+                    horizontalalignment='right',
+                    rotation_mode='anchor',
+                )
+            else:
+                ax.xaxis.set_ticks_position('none')
+
+            if i == j:
+                ax.set_yticks([])
+            elif j == 0:
+                ax.yaxis.set_major_locator(mpl.ticker.MaxNLocator(3, prune='both'))
+            else:
+                ax.yaxis.set_ticks_position('none')
+
+            ## Labels
+            if labels is not None:
+                if i == D - 1:
+                    ax.set_xlabel(labels[j])
+
+                if j == 0 and i != j:
+                    ax.set_ylabel(labels[i])
+
+            ax.label_outer()
+
+    figure.align_labels()
+
+    return figure
+
+
+def rank_ecdf(
+    ranks: Array,
+    color: Union[str, tuple] = None,
+    legend: str = None,
+    figure: mpl.figure.Figure = None,
+    **kwargs,
+) -> mpl.figure.Figure:
+    r"""Draws the empirical cumulative distribution function (ECDF) of a rank
+    statistic :math:`r \in [0, 1]`.
+
+    Arguments:
+        ranks: Samples of the rank statistic.
+        color: A color.
+        legend: A legend.
+        figure: A ECDF plot over which to draw the new one.
+        kwargs: Keyword arguments passed to :func:`matplotlib.pyplot.subplots`.
+
+    Returns:
+        The figure instance for the ECDF plot.
+
+    Example:
+        >>> ranks = np.random.rand(2**12)**2
+        >>> rank_ecdf(ranks)
+
+    .. image:: ../static/images/rank_ecdf.png
+        :align: center
+        :width: 400
+    """
+
+    # Figure
+    if figure is None:
+        kwargs.setdefault('figsize', (3.2, 3.2))
+
+        figure, ax = plt.subplots(**kwargs)
+        new = True
+    else:
+        ax = figure.axes.squeeze()
+        new = False
+
+    # ECDF
+    ranks = np.sort(np.asarray(ranks))
+    ranks = np.hstack([0, ranks, 1])
+    ecdf = np.linspace(0, 1, len(ranks))
+
+    # Plot
+    if new:
+        ax.plot([0, 1], [0, 1], color='k', linestyle='--')
+
+    ax.plot(ranks, ecdf, color=color, label=legend)
+
+    ax.grid()
+    ax.set_xlabel(r'$r$')
+    ax.set_ylabel(r'$\mathrm{ECDF}(r)$')
+
+    if legend is not None:
+        ax.legend(loc='upper left')
+
+    return figure
