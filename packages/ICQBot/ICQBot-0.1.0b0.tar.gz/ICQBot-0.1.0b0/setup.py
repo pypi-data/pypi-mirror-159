add79a8ffa6fc7224e3ee2fc51842e43b2abfa34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:37 +0000

    Add package ICQBot/0.1.0b0/ICQBot-0.1.0b0.tar.gz

---
+++ packages/ICQBot/ICQBot-0.1.0b0.tar.gz/ICQBot-0.1.0b0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages
+from distutils.core import setup
+
+setup(name="ICQBot",
+      version = "0.1.0-beta",
+      author="Kamuri Amorim",
+      author_email='luiz.k.amorim@gmail.com',
+      url="https://github.com/kamuridesu/ICQBotPy",
+      download_url = "https://github.com/kamuridesu/ICQBotPy/archive/refs/heads/main.zip",
+      long_description="""ICQ bot framework""",
+      license="MIT",
+      packages=find_packages("."),
+      install_requires=[
+        'aiohttp'
+      ]
+)
