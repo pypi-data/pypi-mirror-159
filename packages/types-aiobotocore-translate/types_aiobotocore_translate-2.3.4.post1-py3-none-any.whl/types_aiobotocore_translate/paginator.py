de664eb51ac8008d0e527ef1142bece7a2b59326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:28:46 +0000

    Add package types-aiobotocore-translate/2.3.4.post1/types_aiobotocore_translate-2.3.4.post1-py3-none-any.whl

---
+++ packages/types-aiobotocore-translate/types_aiobotocore_translate-2.3.4.post1-py3-none-any.whl/types_aiobotocore_translate/paginator.py
@@ -0,0 +1,62 @@
+"""
+Type annotations for translate service client paginators.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_translate/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_translate.client import TranslateClient
+    from types_aiobotocore_translate.paginator import (
+        ListTerminologiesPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("translate") as client:
+        client: TranslateClient
+
+        list_terminologies_paginator: ListTerminologiesPaginator = client.get_paginator("list_terminologies")
+    ```
+"""
+import sys
+from typing import Generic, Iterator, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .type_defs import ListTerminologiesResponseTypeDef, PaginatorConfigTypeDef
+
+if sys.version_info >= (3, 8):
+    from typing import AsyncIterator
+else:
+    from typing_extensions import AsyncIterator
+
+
+__all__ = ("ListTerminologiesPaginator",)
+
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListTerminologiesPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/translate.html#Translate.Paginator.ListTerminologies)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_translate/paginators/#listterminologiespaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListTerminologiesResponseTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/translate.html#Translate.Paginator.ListTerminologies.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_translate/paginators/#listterminologiespaginator)
+        """
