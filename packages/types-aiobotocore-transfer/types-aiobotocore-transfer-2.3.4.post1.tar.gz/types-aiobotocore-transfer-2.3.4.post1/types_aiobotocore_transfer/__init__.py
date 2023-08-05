e8e6a77e775b061e12848c023fd88364ad3292df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:28:47 +0000

    Add package types-aiobotocore-transfer/2.3.4.post1/types-aiobotocore-transfer-2.3.4.post1.tar.gz

---
+++ packages/types-aiobotocore-transfer/types-aiobotocore-transfer-2.3.4.post1.tar.gz/types-aiobotocore-transfer-2.3.4.post1/types_aiobotocore_transfer/__init__.py
@@ -0,0 +1,67 @@
+"""
+Main interface for transfer service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_transfer import (
+        Client,
+        ListAccessesPaginator,
+        ListExecutionsPaginator,
+        ListSecurityPoliciesPaginator,
+        ListServersPaginator,
+        ListTagsForResourcePaginator,
+        ListUsersPaginator,
+        ListWorkflowsPaginator,
+        ServerOfflineWaiter,
+        ServerOnlineWaiter,
+        TransferClient,
+    )
+
+    session = get_session()
+    async with session.create_client("transfer") as client:
+        client: TransferClient
+        ...
+
+
+    server_offline_waiter: ServerOfflineWaiter = client.get_waiter("server_offline")
+    server_online_waiter: ServerOnlineWaiter = client.get_waiter("server_online")
+
+    list_accesses_paginator: ListAccessesPaginator = client.get_paginator("list_accesses")
+    list_executions_paginator: ListExecutionsPaginator = client.get_paginator("list_executions")
+    list_security_policies_paginator: ListSecurityPoliciesPaginator = client.get_paginator("list_security_policies")
+    list_servers_paginator: ListServersPaginator = client.get_paginator("list_servers")
+    list_tags_for_resource_paginator: ListTagsForResourcePaginator = client.get_paginator("list_tags_for_resource")
+    list_users_paginator: ListUsersPaginator = client.get_paginator("list_users")
+    list_workflows_paginator: ListWorkflowsPaginator = client.get_paginator("list_workflows")
+    ```
+"""
+from .client import TransferClient
+from .paginator import (
+    ListAccessesPaginator,
+    ListExecutionsPaginator,
+    ListSecurityPoliciesPaginator,
+    ListServersPaginator,
+    ListTagsForResourcePaginator,
+    ListUsersPaginator,
+    ListWorkflowsPaginator,
+)
+from .waiter import ServerOfflineWaiter, ServerOnlineWaiter
+
+Client = TransferClient
+
+
+__all__ = (
+    "Client",
+    "ListAccessesPaginator",
+    "ListExecutionsPaginator",
+    "ListSecurityPoliciesPaginator",
+    "ListServersPaginator",
+    "ListTagsForResourcePaginator",
+    "ListUsersPaginator",
+    "ListWorkflowsPaginator",
+    "ServerOfflineWaiter",
+    "ServerOnlineWaiter",
+    "TransferClient",
+)
