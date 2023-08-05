03933450ec97879fc1ec5cf2199f896d548a588e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:50:35 +0000

    Add package ez-toolkits/1.20.0/ez-toolkits-1.20.0.tar.gz

---
+++ packages/ez-toolkits/ez-toolkits-1.20.0.tar.gz/ez-toolkits-1.20.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ez-toolkits',
+    version='1.20.0',
+    author='septvean',
+    author_email='septvean@gmail.com',
+    description='Easy Toolkits',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    python_requires='>=3.10',
+    install_requires=[
+        'loguru>=0.6.0',
+        'matplotlib>=3.0.0',
+        'pandas>=1.0.0',
+        'requests>=2.0.0',
+        'SQLAlchemy>=1.4.0'
+    ]
+)
