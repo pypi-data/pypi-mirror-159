57ac247f20f9638c3069621cd756ab612540c00a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:53:55 +0000

    Add package pydflow/1.1.17/pydflow-1.1.17-py3-none-any.whl

---
+++ packages/pydflow/pydflow-1.1.17-py3-none-any.whl/dflow/config.py
@@ -0,0 +1,10 @@
+config = {
+    "host": "https://127.0.0.1:2746",
+    "token": None,
+    "k8s_config_file": None,
+    "k8s_api_server": None,
+    "private_key_host_path": "/home/docker/.ssh",
+    "save_path_as_parameter": False,
+    "catalog_dir_name": ".dflow",
+    "archive_mode": "tar",
+}
