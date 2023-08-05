19b91f4787e05489b5a4178a7c8b49cd5110b4f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:39 +0000

    Add package dhlab/2.4.6/dhlab-2.4.6.tar.gz

---
+++ packages/dhlab/dhlab-2.4.6.tar.gz/dhlab-2.4.6/dhlab/text/parse.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+from dhlab.api.dhlab_api import ner_from_urn, show_spacy_models
+
+class Models:
+    def __init__(self):
+        self.models = show_spacy_models()
+        
+class NER:
+    def __init__(self, urn=None, model=None):
+        self.model = model
+        self.ner = ner_from_urn(urn=urn, model=self.spacy_model)
+
