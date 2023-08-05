570b98ee02a75d0a63861faf9cbf35e6890ba9ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:28 +0000

    Add package datacraft/0.6.0/datacraft-0.6.0-py3-none-any.whl

---
+++ packages/datacraft/datacraft-0.6.0-py3-none-any.whl/datacraft/__init__.py
@@ -0,0 +1,20 @@
+""" init for datacraft """
+
+# model classes that may be implemented externally
+from .supplier.model import (
+    DataSpec, ValueSupplierInterface, Distribution, CasterInterface, RecordProcessor, OutputHandlerInterface,
+    ResettableIterator, KeyProviderInterface)
+# programmatic spec building
+from . import builder
+# expose this at root too
+from .builder import parse_spec, entries
+# registry decorators
+from .registries import Registry as registry
+# exceptions and errors thrown
+from .exceptions import SpecException, ResourceError
+from .supplier.exceptions import SupplierException
+# commonly used by client code
+from .loader import preprocess_and_format, field_loader, Loader
+from . import suppliers, distributions, outputs, utils
+# to trigger registered functions
+from . import cli
