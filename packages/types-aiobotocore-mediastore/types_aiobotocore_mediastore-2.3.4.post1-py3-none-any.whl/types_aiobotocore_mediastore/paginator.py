22741f6db898a8495e598bb8125e87872f9539ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:28:49 +0000

    Add package types-aiobotocore-mediastore/2.3.4.post1/types_aiobotocore_mediastore-2.3.4.post1-py3-none-any.whl

---
+++ packages/types-aiobotocore-mediastore/types_aiobotocore_mediastore-2.3.4.post1-py3-none-any.whl/types_aiobotocore_mediastore/paginator.py
@@ -0,0 +1,62 @@
+"""
+Type annotations for mediastore service client paginators.
+
+[Open documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore/paginators/)
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+
+    from types_aiobotocore_mediastore.client import MediaStoreClient
+    from types_aiobotocore_mediastore.paginator import (
+        ListContainersPaginator,
+    )
+
+    session = get_session()
+    with session.create_client("mediastore") as client:
+        client: MediaStoreClient
+
+        list_containers_paginator: ListContainersPaginator = client.get_paginator("list_containers")
+    ```
+"""
+import sys
+from typing import Generic, Iterator, TypeVar
+
+from aiobotocore.paginate import AioPaginator
+from botocore.paginate import PageIterator
+
+from .type_defs import ListContainersOutputTypeDef, PaginatorConfigTypeDef
+
+if sys.version_info >= (3, 8):
+    from typing import AsyncIterator
+else:
+    from typing_extensions import AsyncIterator
+
+
+__all__ = ("ListContainersPaginator",)
+
+
+_ItemTypeDef = TypeVar("_ItemTypeDef")
+
+
+class _PageIterator(Generic[_ItemTypeDef], PageIterator):
+    def __iter__(self) -> Iterator[_ItemTypeDef]:
+        """
+        Proxy method to specify iterator item type.
+        """
+
+
+class ListContainersPaginator(AioPaginator):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore.html#MediaStore.Paginator.ListContainers)
+    [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore/paginators/#listcontainerspaginator)
+    """
+
+    def paginate(
+        self, *, PaginationConfig: PaginatorConfigTypeDef = ...
+    ) -> AsyncIterator[ListContainersOutputTypeDef]:
+        """
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/mediastore.html#MediaStore.Paginator.ListContainers.paginate)
+        [Show types-aiobotocore documentation](https://youtype.github.io/types_aiobotocore_docs/types_aiobotocore_mediastore/paginators/#listcontainerspaginator)
+        """
