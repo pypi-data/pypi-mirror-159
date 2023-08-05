6887cd78c894e220e7fed197493ca915832e0fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:32 +0000

    Add package pyfsdb-parquet/1.0/pyfsdb_parquet-1.0.tar.gz

---
+++ packages/pyfsdb-parquet/pyfsdb_parquet-1.0.tar.gz/pyfsdb_parquet-1.0/README.md
@@ -0,0 +1,14 @@
+# Purpose
+
+Adds two `pyfsdb` utilities for converting too and from parquet files.
+
+`pyfsdb` is intended to be light weight, and parquet support requires
+both `pyarrow` and `fastparquet` and are thus distributed separately.
+
+# Installation
+
+    pipx install pyfsdb_parquet
+
+# See also
+
+The [main pyfsdb](https://github.com/hardaker/pyfsdb) documentation.
---
+++ packages/pyfsdb-parquet/pyfsdb_parquet-1.0.tar.gz/pyfsdb_parquet-1.0/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyfsdb_parquet",
+    version="1.0",
+    author="Wes Hardaker",
+    author_email="opensource@hardakers.net",
+    description="Python tools to convert to and from FSDB and parquet files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gawseed/pyfsdb_parquet",
+    packages=setuptools.find_packages(),
+    entry_points={
+        "console_scripts": [
+            "pdb2parquet = pyfsdb_parquet.tools.fsdb2parquet:main",
+            "parquet2pdb = pyfsdb_parquet.tools.parquet2fsdb:main",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pyfsdb>=2.0.2',
+        'cramjam', # fails to auto install?
+        'pandas',
+        'fastparquet',
+        'pyarrow'
+    ],
+    python_requires=">=3.6",
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
