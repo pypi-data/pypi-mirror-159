2247843c0f5167f9c993d4205685de6cdcb8ce62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:57 +0000

    Add package whylogs/1.0.6/whylogs-1.0.6.tar.gz

---
+++ packages/whylogs/whylogs-1.0.6.tar.gz/whylogs-1.0.6/whylogs/viz/__init__.py
@@ -0,0 +1,8 @@
+import IPython  # type: ignore  # noqa  # import ipython to make
+
+from whylogs.viz.jupyter_notebook_viz import NotebookProfileVisualizer
+
+__ALL__ = [
+    # column
+    NotebookProfileVisualizer
+]
