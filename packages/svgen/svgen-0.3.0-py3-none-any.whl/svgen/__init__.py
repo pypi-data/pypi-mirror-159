cde358b55cfadf01b19566bff06b6d570aecba8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:00 +0000

    Add package svgen/0.3.0/svgen-0.3.0-py3-none-any.whl

---
+++ packages/svgen/svgen-0.3.0-py3-none-any.whl/svgen/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=2.1.0
+# hash=076c6a03c7e06f35cccd45b38e340ba9
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A tool for working with scalable vector graphics."
+PKG_NAME = "svgen"
+VERSION = "0.3.0"
---
+++ packages/svgen/svgen-0.3.0-py3-none-any.whl/svgen/__main__.py
@@ -0,0 +1,17 @@
+# =====================================
+# generator=datazen
+# version=2.1.0
+# hash=c899afd4b2308889f0dc1260eede4d46
+# =====================================
+"""
+svgen - Package's default entry-point.
+"""
+
+# built-in
+import sys
+
+# internal
+from svgen.entry import main
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
