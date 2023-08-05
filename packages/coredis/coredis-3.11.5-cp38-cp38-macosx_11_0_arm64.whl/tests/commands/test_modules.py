57a0d0d43e5dd31d4cec3539f1d18b2c195cace1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:40 +0000

    Add package coredis/3.11.5/coredis-3.11.5-cp38-cp38-macosx_11_0_arm64.whl

---
+++ packages/coredis/coredis-3.11.5-cp38-cp38-macosx_11_0_arm64.whl/tests/commands/test_modules.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+import pytest
+
+from tests.conftest import targets
+
+pytestmark = pytest.mark.flaky
+
+
+@pytest.mark.asyncio
+@pytest.mark.xfail
+@targets(
+    "redis_stack",
+    "redis_stack_raw",
+    "redis_stack_resp3",
+    "redis_stack_raw_resp3",
+)
+async def test_modules_list(client, _s):
+    module_info = await client.module_list()
+    assert {_s("args"), _s("name"), _s("path"), _s("ver")} & module_info[0].keys()
+
+
+@pytest.mark.asyncio
+@targets(
+    "redis_basic",
+    "redis_basic_raw",
+    "redis_basic_resp3",
+    "redis_basic_raw_resp3",
+)
+async def test_no_modules(client):
+    module_info = await client.module_list()
+    assert module_info == ()
