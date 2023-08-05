4ee5d47443725f3459d52beab91d914fbdaedf19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:50 +0000

    Add package Logges/1.0.4.1/Logges-1.0.4.1.tar.gz

---
+++ packages/Logges/Logges-1.0.4.1.tar.gz/Logges-1.0.4.1/setup.py
@@ -0,0 +1,28 @@
+"""Setup tool for making a library."""
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+setup(
+    name="Logges",
+    version="1.0.4.1",
+    license='MIT',
+    author='Serkan UYSAL',
+    maintainer="Ozkan UYSAL",
+    author_email='uysalserkan08@gmail.com',
+    maintainer_email="ozkan.uysal.2009@gmail.com",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url="https://github.com/uysalserkan/Logges",
+    description="A simple Logging tool can extract as Markdown, PDF, or print console.",
+    long_description=long_description,
+    keywords="Log, Logging, Logges, Logger",
+    install_requires=[
+        'matplotlib==3.5.1',
+        'rich==10.16.2',
+        'reportlab==3.5.67'
+    ],
+)
