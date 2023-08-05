1b4b9597dc75a7e266597156a304e3db0d3a6f4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:50:30 +0000

    Add package python-temporal/0.0.2/python_temporal-0.0.2.tar.gz

---
+++ packages/python-temporal/python_temporal-0.0.2.tar.gz/python_temporal-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='python_temporal',
+    version='0.0.2',
+    author='X.X',
+    author_email='xx@example.com',
+    url='http://www.example.com/',
+    license='LICENSE',
+    packages=['src'],
+    description="The description of the package",
+    long_description_content_type="text/markdown",
+    long_description="test",
+    install_requires=['temporal-python-sdk',
+                      'boto3~=1.24.22',
+                      'PyYAML',
+                      'pandas~=1.4.3',
+                      'numpy',
+                      'pydantic~=1.9.1'
+                      ]
+)
