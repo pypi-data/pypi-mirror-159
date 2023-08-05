686061392a7b939e7443f5fdc088642ad1b17c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:14 +0000

    Add package types-aiobotocore-ds/2.3.4.post1/types-aiobotocore-ds-2.3.4.post1.tar.gz

---
+++ packages/types-aiobotocore-ds/types-aiobotocore-ds-2.3.4.post1.tar.gz/types-aiobotocore-ds-2.3.4.post1/types_aiobotocore_ds/__init__.py
@@ -0,0 +1,67 @@
+"""
+Main interface for ds service.
+
+Usage::
+
+    ```python
+    from aiobotocore.session import get_session
+    from types_aiobotocore_ds import (
+        Client,
+        DescribeDirectoriesPaginator,
+        DescribeDomainControllersPaginator,
+        DescribeSharedDirectoriesPaginator,
+        DescribeSnapshotsPaginator,
+        DescribeTrustsPaginator,
+        DirectoryServiceClient,
+        ListIpRoutesPaginator,
+        ListLogSubscriptionsPaginator,
+        ListSchemaExtensionsPaginator,
+        ListTagsForResourcePaginator,
+    )
+
+    session = get_session()
+    async with session.create_client("ds") as client:
+        client: DirectoryServiceClient
+        ...
+
+
+    describe_directories_paginator: DescribeDirectoriesPaginator = client.get_paginator("describe_directories")
+    describe_domain_controllers_paginator: DescribeDomainControllersPaginator = client.get_paginator("describe_domain_controllers")
+    describe_shared_directories_paginator: DescribeSharedDirectoriesPaginator = client.get_paginator("describe_shared_directories")
+    describe_snapshots_paginator: DescribeSnapshotsPaginator = client.get_paginator("describe_snapshots")
+    describe_trusts_paginator: DescribeTrustsPaginator = client.get_paginator("describe_trusts")
+    list_ip_routes_paginator: ListIpRoutesPaginator = client.get_paginator("list_ip_routes")
+    list_log_subscriptions_paginator: ListLogSubscriptionsPaginator = client.get_paginator("list_log_subscriptions")
+    list_schema_extensions_paginator: ListSchemaExtensionsPaginator = client.get_paginator("list_schema_extensions")
+    list_tags_for_resource_paginator: ListTagsForResourcePaginator = client.get_paginator("list_tags_for_resource")
+    ```
+"""
+from .client import DirectoryServiceClient
+from .paginator import (
+    DescribeDirectoriesPaginator,
+    DescribeDomainControllersPaginator,
+    DescribeSharedDirectoriesPaginator,
+    DescribeSnapshotsPaginator,
+    DescribeTrustsPaginator,
+    ListIpRoutesPaginator,
+    ListLogSubscriptionsPaginator,
+    ListSchemaExtensionsPaginator,
+    ListTagsForResourcePaginator,
+)
+
+Client = DirectoryServiceClient
+
+
+__all__ = (
+    "Client",
+    "DescribeDirectoriesPaginator",
+    "DescribeDomainControllersPaginator",
+    "DescribeSharedDirectoriesPaginator",
+    "DescribeSnapshotsPaginator",
+    "DescribeTrustsPaginator",
+    "DirectoryServiceClient",
+    "ListIpRoutesPaginator",
+    "ListLogSubscriptionsPaginator",
+    "ListSchemaExtensionsPaginator",
+    "ListTagsForResourcePaginator",
+)
