5c40399cf4ecd47476611f17b19c356b435e14da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:49 +0000

    Add package xlrp/0.5.3/xlrp-0.5.3.tar.gz

---
+++ packages/xlrp/xlrp-0.5.3.tar.gz/xlrp-0.5.3/XLRP/xl_logger.py
@@ -0,0 +1,39 @@
+import datetime
+import threading
+import platform
+import os
+
+
+_lock = threading.RLock()
+device = platform.system()
+if device == 'Windows':
+    os.system("")
+
+def _set_color(color, msg):
+    return f'\033[1;50;{color}m{msg}\033[0m'
+
+def _now_time():
+    return str(datetime.datetime.now())
+
+def _logger_write(filename=None, msg=None):
+    _lock.acquire()
+    if filename is None:
+        return
+    else:
+        with open(filename, 'a', encoding='utf8') as f:
+            f.writelines(msg)
+            f.close()
+    _lock.release()
+
+class Logger:
+    def __init__(self, path=None):
+        self.path = path
+
+    def success(self, msg):
+        print(_set_color(32, _now_time() + ' ' + msg))
+        _logger_write(self.path, _now_time() + ' ' + msg + '\n')
+
+    def error(self, msg):
+        print(_set_color(31, _now_time() + ' ' + msg))
+        _logger_write(self.path, _now_time() + ' ' + msg + '\n')
+
