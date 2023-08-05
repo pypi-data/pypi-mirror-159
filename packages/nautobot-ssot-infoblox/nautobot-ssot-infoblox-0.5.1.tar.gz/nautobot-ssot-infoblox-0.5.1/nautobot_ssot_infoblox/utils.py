05f265a45d9cbf253ce0f7c130125cd142ad442d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:46 +0000

    Add package nautobot-ssot-infoblox/0.5.1/nautobot-ssot-infoblox-0.5.1.tar.gz

---
+++ packages/nautobot-ssot-infoblox/nautobot-ssot-infoblox-0.5.1.tar.gz/nautobot-ssot-infoblox-0.5.1/nautobot_ssot_infoblox/utils.py
@@ -0,0 +1,26 @@
+"""Utilities."""
+
+
+def get_vlan_view_name(reference):
+    """Get the Infoblox vlanview name by the reference resource string.
+
+    Args:
+        reference (str): Vlan view Reference resource.
+
+    Returns:
+        (str): Vlan view name.
+
+    Returns Response:
+        "Nautobot"
+    """
+    return reference.split("/")[1].split(":")[-1]
+
+
+def nautobot_vlan_status(status: str) -> str:
+    """Return VLAN Status from mapping."""
+    statuses = {
+        "Active": "ASSIGNED",
+        "Deprecated": "UNASSIGNED",
+        "Reserved": "RESERVED",
+    }
+    return statuses[status]
