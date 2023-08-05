42dd9d85f2a30bc136acd801e23e28c3aae4ccea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:57 +0000

    Add package algora-sdk/1.1.70/algora-sdk-1.1.70.tar.gz

---
+++ packages/algora-sdk/algora-sdk-1.1.70.tar.gz/algora-sdk-1.1.70/algoralabs/research/runner.py
@@ -0,0 +1,23 @@
+"""
+Module containing methods for interacting with the algoralabs' research runners API
+"""
+from algoralabs.common.requests import __put_request, __async_put_request
+from algoralabs.decorators.data import data_request, async_data_request
+
+
+def _get_or_create_runner_request_info() -> dict:
+    return {
+        'endpoint': f"research-service/runner/deployment"
+    }
+
+
+@data_request(transformer=lambda data: data)
+def get_or_create_runner() -> str:
+    request_info = _get_or_create_runner_request_info()
+    return __put_request(**request_info)
+
+
+@async_data_request(transformer=lambda data: data)
+async def async_get_or_create_runner() -> str:
+    request_info = _get_or_create_runner_request_info()
+    return await __async_put_request(**request_info)
