1c9dd7b3794a0ab53501c063a0a4f1f90d4d2dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:43 +0000

    Add package shangchuan/1.6/shangchuan-1.6.tar.gz

---
+++ packages/shangchuan/shangchuan-1.6.tar.gz/shangchuan-1.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup,find_packages
+version = '1.1'
+setup(name='shangchuan',
+      version='1.6',
+      description='mHapTk is a tool kit for analysis of DNA methylation haplotypes. It has 5 sub-commands: tanghulu, stat, genomeWide, R2 and MHBDiscovery',
+      author='ckw',
+      author_email='1353595807@qq.com',
+
+      packages=['src','console'],
+      # entry_points={
+      #       'console_scripts': [
+      #             'shangchuan = console.mhap_console:main'
+      #       ]
+      # }
+
+      )
+
