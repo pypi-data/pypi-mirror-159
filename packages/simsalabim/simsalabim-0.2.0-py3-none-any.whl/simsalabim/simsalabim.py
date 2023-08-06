2a2abca0e4b797dbf38114ff238b9ea9e7ab617d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:22 +0000

    Add package simsalabim/0.2.0/simsalabim-0.2.0-py3-none-any.whl

---
+++ packages/simsalabim/simsalabim-0.2.0-py3-none-any.whl/simsalabim/simsalabim.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+
+__version__ = "0.1.0"
+__copyright__ = '''Copyright (c) 2020-2022 Matthew The. All rights reserved.
+Written by Matthew The (matthew.the@tum.de) in the
+Chair of Proteomics and Bioanalytics at the 
+Technical University of Munich.'''
