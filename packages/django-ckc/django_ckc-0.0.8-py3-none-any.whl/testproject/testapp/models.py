52fa572d3e1de4956be8c146f1bcd07f5f465073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:01:17 +0000

    Add package django-ckc/0.0.8/django_ckc-0.0.8-py3-none-any.whl

---
+++ packages/django-ckc/django_ckc-0.0.8-py3-none-any.whl/testproject/testapp/models.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+from django.contrib.gis.db.models import PointField
+from django.db import models
+
+from ckc.models import SoftDeletableModel
+
+
+User = get_user_model()
+
+
+class AModel(SoftDeletableModel):
+    title = models.CharField(max_length=255, default="I'm a test!")
+
+
+class BModel(models.Model):
+    a = models.ForeignKey(AModel, on_delete=models.CASCADE)
+
+
+class ModelWithACreator(models.Model):
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class ModelWithADifferentNamedCreator(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Location(models.Model):
+    geo_point = PointField()
