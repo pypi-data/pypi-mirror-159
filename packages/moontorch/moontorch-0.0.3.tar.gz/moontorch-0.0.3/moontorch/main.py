9c081ce2d3c49e24fc76d16c0cdbeee93a5fc8c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:55 +0000

    Add package moontorch/0.0.3/moontorch-0.0.3.tar.gz

---
+++ packages/moontorch/moontorch-0.0.3.tar.gz/moontorch-0.0.3/moontorch/main.py
@@ -0,0 +1,76 @@
+import torch
+
+
+def cpu():
+    return torch.device("cpu")
+
+
+def gpu(i=0):
+    return torch.device(f"cuda:{i}")
+
+
+def num_gpus():
+    return torch.cuda.device_count()
+
+
+def try_gpu(i=0):
+    """Return gpu(i) if exists, otherwise return cpu()."""
+    if num_gpus() >= i + 1:
+        return gpu(i)
+    return cpu()
+
+
+def try_all_gpus():
+    """Return all available GPUs, or [cpu(),] if no GPU exists."""
+    return [gpu(i) for i in range(num_gpus())]
+
+
+def check_len(a, n):
+    assert len(a) == n, f"list's len {len(a)} != expected length {n}"
+
+
+def check_shape(a, shape):
+    assert a.shape == shape, f"tensor's shape {a.shape} != expected shape {shape}"
+
+
+ones_like = torch.ones_like
+ones = torch.ones
+zeros_like = torch.zeros_like
+zeros = torch.zeros
+tensor = torch.tensor
+arange = torch.arange
+meshgrid = torch.meshgrid
+sin = torch.sin
+sinh = torch.sinh
+cos = torch.cos
+cosh = torch.cosh
+tanh = torch.tanh
+linspace = torch.linspace
+exp = torch.exp
+log = torch.log
+normal = torch.normal
+rand = torch.rand
+randn = torch.randn
+matmul = torch.matmul
+int32 = torch.int32
+int64 = torch.int64
+float32 = torch.float32
+concat = torch.cat
+stack = torch.stack
+abs = torch.abs
+eye = torch.eye
+sigmoid = torch.sigmoid
+batch_matmul = torch.bmm
+numpy = lambda x, *args, **kwargs: x.detach().numpy(*args, **kwargs)
+size = lambda x, *args, **kwargs: x.numel(*args, **kwargs)
+shape = lambda x: x.shape
+reshape = lambda x, *args, **kwargs: x.reshape(*args, **kwargs)
+to = lambda x, *args, **kwargs: x.to(*args, **kwargs)
+reduce_sum = lambda x, *args, **kwargs: x.sum(*args, **kwargs)
+argmax = lambda x, *args, **kwargs: x.argmax(*args, **kwargs)
+astype = lambda x, *args, **kwargs: x.type(*args, **kwargs)
+transpose = lambda x, *args, **kwargs: x.t(*args, **kwargs)
+reduce_mean = lambda x, *args, **kwargs: x.mean(*args, **kwargs)
+expand_dims = lambda x, *args, **kwargs: x.unsqueeze(*args, **kwargs)
+swapaxes = lambda x, *args, **kwargs: x.swapaxes(*args, **kwargs)
+repeat = lambda x, *args, **kwargs: x.repeat(*args, **kwargs)
