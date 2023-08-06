7e51d3e75ed70b1600dd8d81ad83cbf8099c24d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:23 +0000

    Add package OpenFisca-Survey-Manager/0.46.14/OpenFisca-Survey-Manager-0.46.14.tar.gz

---
+++ packages/OpenFisca-Survey-Manager/OpenFisca-Survey-Manager-0.46.14.tar.gz/OpenFisca-Survey-Manager-0.46.14/openfisca_survey_manager/read_sas.py
@@ -0,0 +1,21 @@
+import logging
+
+
+log = logging.getLogger(__name__)
+
+
+def read_sas(sas_file_path, clean = False):
+    try:
+        import pyreadstat
+        data_frame, _ = pyreadstat.read_sas7bdat(sas_file_path)
+    except ImportError as e1:
+        log.info("pyreadstat not available trying SAS7BDAT")
+        try:
+            from sas7bdat import SAS7BDAT
+            data_frame = SAS7BDAT(sas_file_path).to_data_frame()
+        except ImportError as e2:
+            log.info("Neither pyreadstat nor SAS7BDAT are available")
+            print(e1)
+            raise(e2)
+
+    return data_frame
