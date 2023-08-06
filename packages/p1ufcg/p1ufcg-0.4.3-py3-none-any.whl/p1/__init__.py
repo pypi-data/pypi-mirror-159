a70258027c17ac43e935f4ceb0d0383a39a08f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:58 +0000

    Add package p1ufcg/0.4.3/p1ufcg-0.4.3-py3-none-any.whl

---
+++ packages/p1ufcg/p1ufcg-0.4.3-py3-none-any.whl/p1/__init__.py
@@ -0,0 +1,11 @@
+from .utils import _assert, cprint
+from .colors import *
+
+from .p1 import coverit
+from .p1 import get_config
+from .p1 import dirtype
+from .p1 import get_site
+
+from .array import Array
+from .p1 import P1STYLE
+
