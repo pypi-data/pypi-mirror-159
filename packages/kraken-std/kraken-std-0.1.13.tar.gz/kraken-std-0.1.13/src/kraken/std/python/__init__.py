32b8064081f974d236b31631abe074793c5bd957
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:33 +0000

    Add package kraken-std/0.1.13/kraken-std-0.1.13.tar.gz

---
+++ packages/kraken-std/kraken-std-0.1.13.tar.gz/kraken-std-0.1.13/src/kraken/std/python/__init__.py
@@ -0,0 +1,24 @@
+from .install import InstallTask, install
+from .settings import PythonSettings, python_settings
+from .tools.black import BlackTask, black
+from .tools.flake8 import Flake8Task, flake8
+from .tools.isort import IsortTask, isort
+from .tools.mypy import MypyTask, mypy
+from .tools.pytest import PytestTask, pytest
+
+__all__ = [
+    "PythonSettings",
+    "python_settings",
+    "BlackTask",
+    "black",
+    "IsortTask",
+    "isort",
+    "Flake8Task",
+    "flake8",
+    "MypyTask",
+    "mypy",
+    "PytestTask",
+    "pytest",
+    "InstallTask",
+    "install",
+]
