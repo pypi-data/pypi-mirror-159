9c5e200bb09dac317e1f1d47a72e554ef7eda1e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:38 +0000

    Add package mlfoundry/0.3.32/mlfoundry-0.3.32.tar.gz

---
+++ packages/mlfoundry/mlfoundry-0.3.32.tar.gz/mlfoundry-0.3.32/mlfoundry/__init__.py
@@ -0,0 +1,44 @@
+import logging
+import sys
+
+from mlfoundry.enums import *
+from mlfoundry.log_types import Image, Plot
+from mlfoundry.login import login
+from mlfoundry.mlfoundry_api import get_client
+from mlfoundry.mlfoundry_run import MlFoundryRun
+from mlfoundry.schema import Schema
+from mlfoundry.version import __version__
+
+__all__ = [
+    "FileFormat",
+    "ModelFramework",
+    "DataSlice",
+    "ModelType",
+    "Schema",
+    "get_client",
+    "__version__",
+    "MlFoundryRun",
+    "login",
+    "Image",
+    "Plot",
+]
+
+
+def _configure_logger(level):
+    # TODO (chiragjn): This is just a temporary way to configure logging.
+    #                  Need to move it in client or run init with option to set verbosity
+    logger = logging.getLogger(__name__)
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(level)
+    handler.setFormatter(
+        logging.Formatter(
+            "[mlfoundry] %(asctime)s %(levelname)s %(message)s",
+            datefmt="%Y-%m-%dT%H:%M:%S%z",
+        )
+    )
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+    logger.propagate = False
+
+
+_configure_logger(logging.INFO)
