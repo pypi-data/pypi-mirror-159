e36607a03d4f0d21bf16903d180ec2fcfe04c527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:15 +0000

    Add package tap-exacttarget-remove-sud/1.7.1/tap-exacttarget-remove-sud-1.7.1.tar.gz

---
+++ packages/tap-exacttarget-remove-sud/tap-exacttarget-remove-sud-1.7.1.tar.gz/tap-exacttarget-remove-sud-1.7.1/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='tap-exacttarget-remove-sud',
+    version='1.7.1',
+    description='Singer.io tap for extracting data from the ExactTarget API',
+    author='Fishtown Analytics',
+    url='http://fishtownanalytics.com',
+    classifiers=['Programming Language :: Python :: 3 :: Only'],
+    py_modules=['tap_exacttarget'],
+    install_requires=[
+        'funcy==1.9.1',
+        'singer-python==5.12.1',
+        'python-dateutil==2.6.0',
+        'voluptuous==0.10.5',
+        'Salesforce-FuelSDK-Sans'
+    ],
+    extras_require={
+        'test': [
+            'pylint==2.10.2',
+            'astroid==2.7.3',
+            'nose'
+        ],
+        'dev': [
+            'ipdb==0.11'
+        ]
+    },
+    entry_points='''
+    [console_scripts]
+    tap-exacttarget=tap_exacttarget:main
+    ''',
+    packages=find_packages(),
+    package_data={
+        'tap_exacttarget': ['schemas/*.json']
+    }
+)
