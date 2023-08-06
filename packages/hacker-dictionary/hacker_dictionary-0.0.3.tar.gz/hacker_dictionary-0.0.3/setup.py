e468be55cb92ba95c0be02ed2de79b9c2da49073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:51:26 +0000

    Add package hacker-dictionary/0.0.3/hacker_dictionary-0.0.3.tar.gz

---
+++ packages/hacker-dictionary/hacker_dictionary-0.0.3.tar.gz/hacker_dictionary-0.0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='hacker_dictionary',
+      version='0.0.3',
+      description='string to date',
+      author='YGXB',
+      author_email='839682307@qq.com',
+      license='MIT',
+      packages=['hacker_dictionary'] )
