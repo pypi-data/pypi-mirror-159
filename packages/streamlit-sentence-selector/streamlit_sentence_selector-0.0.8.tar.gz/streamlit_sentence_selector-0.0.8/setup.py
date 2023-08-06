8e56c0887a2156bbc95025e2928e11d131f27368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:19 +0000

    Add package streamlit-sentence-selector/0.0.8/streamlit_sentence_selector-0.0.8.tar.gz

---
+++ packages/streamlit-sentence-selector/streamlit_sentence_selector-0.0.8.tar.gz/streamlit_sentence_selector-0.0.8/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="streamlit_sentence_selector",
+    version="0.0.8",
+    author="omnilab_xiaowu",
+    author_email="1558359609@qq.com",
+    description="A selector for a list of sentences or strings, and return a list of sentences or strings that you selected",
+    long_description="A selector for a list of sentences or strings, and return a list of sentences or strings that you selected",
+    long_description_content_type="text/plain",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+    ],
+)
