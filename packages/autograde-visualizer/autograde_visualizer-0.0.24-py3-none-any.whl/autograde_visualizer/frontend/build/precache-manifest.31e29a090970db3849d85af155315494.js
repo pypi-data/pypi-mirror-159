b402297875118f480b4273962aa6a4217d68e791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:58:48 +0000

    Add package autograde-visualizer/0.0.24/autograde_visualizer-0.0.24-py3-none-any.whl

---
+++ packages/autograde-visualizer/autograde_visualizer-0.0.24-py3-none-any.whl/autograde_visualizer/frontend/build/precache-manifest.31e29a090970db3849d85af155315494.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "315fb97bd691dac46411c353afc81104",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7519515b2a91416fac03",
+    "url": "./static/js/2.f672bc8f.chunk.js"
+  },
+  {
+    "revision": "ea9e8a04b4530945f0b69f09f74b01e4",
+    "url": "./static/js/2.f672bc8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f21e740cee0577e5093d",
+    "url": "./static/js/main.f7db1278.chunk.js"
+  },
+  {
+    "revision": "e14cae3afec548135755",
+    "url": "./static/js/runtime-main.e4cdaade.js"
+  }
+]);
---
+++ packages/autograde-visualizer/autograde_visualizer-0.0.24-py3-none-any.whl/autograde_visualizer/frontend/build/precache-manifest.3f8d79b56e718a77dced4616761a1770.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f877fb653d4181ead9b374bdefd42c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7519515b2a91416fac03",
+    "url": "./static/js/2.f672bc8f.chunk.js"
+  },
+  {
+    "revision": "ea9e8a04b4530945f0b69f09f74b01e4",
+    "url": "./static/js/2.f672bc8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43ec61b954e3cb440698",
+    "url": "./static/js/main.955d15c8.chunk.js"
+  },
+  {
+    "revision": "e14cae3afec548135755",
+    "url": "./static/js/runtime-main.e4cdaade.js"
+  }
+]);
