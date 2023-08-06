9a60d8ca63a9c13fa9704fa391581e6515af8214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:09 +0000

    Add package CDHTMLTable/1.0.0/CDHTMLTable-1.0.0.tar.gz

---
+++ packages/CDHTMLTable/CDHTMLTable-1.0.0.tar.gz/CDHTMLTable-1.0.0/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+
+name = 'CDHTMLTable',
+
+version = '1.0.0',
+
+description = 'library ',
+
+long_description = 'library ',
+
+author = 'di.chen',
+
+author_email = 'di.chen@shopee.com',
+
+url = 'https://github.com/',
+
+license = 'MIT Licence',
+
+keywords = 'html table',
+
+platforms = 'any',
+
+python_requires = '>=3.7.*',
+
+install_requires = [],
+
+package_dir = {'': 'src'},
+
+packages = find_packages('src')
+
+)
