6f1a94ec7ecbf0d12f96751fa6fc6213823592d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:39 +0000

    Add package terminalcolorpy/1.0.0/terminalcolorpy-1.0.0.tar.gz

---
+++ packages/terminalcolorpy/terminalcolorpy-1.0.0.tar.gz/terminalcolorpy-1.0.0/setup.py
@@ -0,0 +1,31 @@
+# Standard library imports
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="terminalcolorpy",
+    version="2.0.0",
+    author="novusys",
+    author_email="amarftw1@gmail.com",
+    description="Lightweight package to print colorized messages using ASCI escape sequences to the terminal.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/novusys/terminalcolorpy/",
+    project_urls={
+        "Bug Tracker": "https://github.com/novusys/terminalcolorpy/issues",
+    },
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    packages=["terminalcolorpy"],
+    install_requires=[],
+    python_requires=">=3.10",
+    license="MIT",
+)
---
+++ packages/terminalcolorpy/terminalcolorpy-1.0.0.tar.gz/terminalcolorpy-1.0.0/terminalcolorpy/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.1.2"
+__all__ = ["prainbow", "blink", "printcolor", "colored", "flip_text"]
+__license__ = "MIT"
+__author__ = "novusys"
+
+from terminalcolorpy.tcp import *
