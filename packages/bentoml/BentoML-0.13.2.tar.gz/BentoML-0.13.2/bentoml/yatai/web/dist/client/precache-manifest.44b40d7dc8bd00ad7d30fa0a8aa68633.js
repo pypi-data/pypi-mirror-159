196332440795ff79e35a2b682b018badc635dd06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:26 +0000

    Add package bentoml/0.13.2/BentoML-0.13.2.tar.gz

---
+++ packages/bentoml/BentoML-0.13.2.tar.gz/BentoML-0.13.2/bentoml/yatai/web/dist/client/precache-manifest.44b40d7dc8bd00ad7d30fa0a8aa68633.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "015310d8f848829f9787399069b866d3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9f20598eaa054f5a7e87",
+    "url": "./static/css/2.839a9555.chunk.css"
+  },
+  {
+    "revision": "2e9e06c888086c6d62c7",
+    "url": "./static/css/main.4c48667a.chunk.css"
+  },
+  {
+    "revision": "9f20598eaa054f5a7e87",
+    "url": "./static/js/2.d8f5929d.chunk.js"
+  },
+  {
+    "revision": "0e9e4d1d98a593ee9ff6fc543843c9bd",
+    "url": "./static/js/2.d8f5929d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e9e06c888086c6d62c7",
+    "url": "./static/js/main.4c4d4ab3.chunk.js"
+  },
+  {
+    "revision": "2b66b08655ed491427c4",
+    "url": "./static/js/runtime-main.d0e17700.js"
+  },
+  {
+    "revision": "21d67caf31c9a92087a54e4754d15b97",
+    "url": "./static/media/bentoml-logo.21d67caf.png"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "./static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "./static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "./static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "./static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "./static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "./static/media/icons-20.cef8cdbb.woff"
+  }
+]);
---
+++ packages/bentoml/BentoML-0.13.2.tar.gz/BentoML-0.13.2/setup.py
@@ -0,0 +1,167 @@
+# Copyright 2019 Atalaya Tech, Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import setuptools
+
+import versioneer
+
+with open("README.md", "r", encoding="utf8") as f:
+    long_description = f.read()
+
+install_requires = [
+    "aiohttp",
+    "aiohttp_cors==0.7.0",
+    "alembic",
+    "urllib3>=1.26.5,<1.27.0",
+    "boto3",
+    "cerberus",
+    "certifi",
+    "click>=7.0",
+    "configparser",
+    "deepmerge",
+    "docker",
+    "flask",
+    "grpcio",
+    "gunicorn",
+    "humanfriendly",
+    "numpy",
+    "packaging",
+    "prometheus_client",
+    "protobuf>=3.8.0,<=3.20.0",
+    "psutil",
+    # python-dateutil required by pandas and boto3, this makes sure the version
+    # works for both
+    "python-dateutil>=2.7.3,<3.0.0",
+    "python-json-logger",
+    "requests",
+    "ruamel.yaml>=0.15.0",
+    "schema",
+    "sqlalchemy-utils<0.36.8",
+    "sqlalchemy>=1.3.0,<1.4.0",
+    "tabulate",
+    'contextvars;python_version < "3.7"',
+    'dataclasses;python_version < "3.7"',
+    "chardet",
+    "simple-di==0.1.0",
+]
+
+yatai_service_requires = [
+    "grpcio~=1.34.0",  # match the grpcio-tools version used in yatai docker image
+    "google-cloud-storage",
+    "azure-cli",
+    "aws-sam-cli==0.33.1",
+    "psycopg2",
+    "psycopg2-binary",
+]
+
+model_server_requires = [
+    "opentracing",
+    "py_zipkin",
+    "jaeger_client",
+]
+
+test_requires = [
+    "idna<=2.8",  # for moto
+    "ecdsa==0.14",  # for moto
+    "black==19.10b0",
+    "codecov",
+    "coverage>=4.4",
+    "flake8>=3.8.2",
+    "imageio>=2.5.0,<=2.9.0",
+    "mock>=2.0.0",
+    "moto==1.3.14",
+    "pandas",
+    "pylint>=2.9.3",
+    "pytest-cov>=2.7.1",
+    "pytest>=5.4.0",
+    "pytest-asyncio",
+    "parameterized",
+    "scikit-learn",
+]
+
+dev_requires = [
+    "flake8>=3.8.2",
+    "gitpython>=2.0.2",
+    # grpcio-tools version must be kept in sync with the version used in
+    # `protos/generate-docker.sh` script
+    "grpcio-tools~=1.34.0",
+    "grpcio-reflection~=1.34.0",
+    "pylint>=2.5.2",
+    "setuptools",
+    "tox-conda>=0.2.0",
+    "tox>=3.12.1",
+    "twine",
+] + test_requires
+
+docs_requires = [
+    "recommonmark",
+    "sphinx<=3.5.4",
+    "sphinx-click",
+    "sphinx_rtd_theme",
+    "sphinxcontrib-fulltoc",
+    "sphinxcontrib-spelling",
+    "sphinx_copybutton",
+    "pyenchant",
+]
+
+types_requires = [
+    "mypy-protobuf",
+    "types-click",
+    "types-protobuf>=0.1.14",
+    "grpc-stubs",
+]
+
+dev_all = install_requires + dev_requires + docs_requires + types_requires
+
+extras_require = {
+    "dev": dev_all,
+    "test": test_requires,
+    "yatai_service": yatai_service_requires,
+    "model_server": model_server_requires,
+    "doc_builder": docs_requires,  # 'doc_builder' is required by readthedocs.io
+    "types_stub": types_requires,
+}
+
+setuptools.setup(
+    name="BentoML",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="bentoml.org",
+    author_email="contact@bentoml.ai",
+    description="A framework for machine learning model serving",
+    long_description=long_description,
+    license="Apache License 2.0",
+    long_description_content_type="text/markdown",
+    install_requires=install_requires,
+    extras_require=extras_require,
+    url="https://github.com/bentoml/BentoML",
+    packages=setuptools.find_packages(exclude=["tests*"]),
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    python_requires=">=3.6.1",
+    entry_points={"console_scripts": ["bentoml=bentoml:commandline_interface"]},
+    project_urls={
+        "Bug Reports": "https://github.com/bentoml/BentoML/issues",
+        "BentoML User Slack Group": "https://bit.ly/2N5IpbB",
+        "Source Code": "https://github.com/bentoml/BentoML",
+    },
+    include_package_data=True,  # Required for '.cfg' files under bentoml/config
+)
