eca41ce61fbbd3b4030598379db91e7600c021df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:52 +0000

    Add package ssltest/0.2.0/ssltest-0.2.0.tar.gz

---
+++ packages/ssltest/ssltest-0.2.0.tar.gz/ssltest-0.2.0/ssltest/sockets/ProfileParser.py
@@ -0,0 +1,24 @@
+import logging
+
+from ..core.utils import read_json
+
+log = logging.getLogger(__name__)
+
+
+class ProfileParser:
+    network_profile_usage = read_json("network_profile_usage.json")
+    network_profiles = read_json("network_profiles.json")
+
+    @classmethod
+    def parse(cls, usage):
+        try:
+            profile_name = cls.network_profile_usage[usage]
+            profile = cls.network_profiles[profile_name]
+        except KeyError:
+            log.error("This key doesn't exist, returning default values")
+            profile = cls.network_profiles["medium"]
+        return (
+            profile["retries_count"],
+            profile["retry_interval"] / 1000,
+            profile["timeout"] / 1000,
+        )
