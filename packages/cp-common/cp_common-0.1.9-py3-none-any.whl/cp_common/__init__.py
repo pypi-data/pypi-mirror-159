083f5d93062074f2e727b0f12fe0ee1f989e6468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:00 +0000

    Add package cp-common/0.1.9/cp_common-0.1.9-py3-none-any.whl

---
+++ packages/cp-common/cp_common-0.1.9-py3-none-any.whl/cp_common/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+Version: 0.1
+Autor: zmf96
+Email: zmf96@qq.com
+Date: 2022-03-22 12:56:42
+LastEditors: zmf96
+LastEditTime: 2022-03-30 22:42:56
+FilePath: /cp_common/__init__.py
+Description: 
+"""
+__version__ = "0.1.9"
