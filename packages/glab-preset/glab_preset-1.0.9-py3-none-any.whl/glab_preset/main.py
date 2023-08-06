7a97e37619cdea770e0a554579cfb19919767ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:48 +0000

    Add package glab-preset/1.0.9/glab_preset-1.0.9-py3-none-any.whl

---
+++ packages/glab-preset/glab_preset-1.0.9-py3-none-any.whl/glab_preset/main.py
@@ -0,0 +1,15 @@
+import click
+
+
+@click.command()
+@click.option('--type', prompt=True, type=click.Choice(['react_oss', 'other']), default='react_oss')
+def cli(**kwargs):
+  if 'react_oss' == kwargs.get('type'):
+    from glab_preset.presets.react_oss import react_oss
+    react_oss()
+  else:
+    click.echo('Not support.')
+
+
+if __name__ == '__main__':
+  cli()
