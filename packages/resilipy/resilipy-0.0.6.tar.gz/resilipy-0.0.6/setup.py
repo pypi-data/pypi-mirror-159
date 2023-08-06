5cefdb5c8afd73cc0feb18a59259bac871841d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:51:17 +0000

    Add package resilipy/0.0.6/resilipy-0.0.6.tar.gz

---
+++ packages/resilipy/resilipy-0.0.6.tar.gz/resilipy-0.0.6/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setuptools.setup(
+    name="resilipy",
+    version="0.0.6",
+    author="Vincent Dietrich",
+    author_email="vdietric@students.uni-mainz.de",
+    packages=setuptools.find_packages(),
+    description="ResiliPy - A machine-learning based Mouse labelling GUI.",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dietvin/resilipy",
+    license='MIT',
+    python_requires='>=3.8',
+    install_requires=[
+        "PyQt6",
+        "xgboost",
+        "scikit-learn",
+        "numpy",
+        "pandas",
+        "Levenshtein",
+        "datetime",
+        "pyqtgraph",
+        "pickle",
+        "os",
+    ],
+    package_data={
+        "resilipy": ["resilipy/models/XGBoost C57 mice.model"]
+    },
+    data_files=[
+        ("ui", ["resilipy/ui/builder.ui", "resilipy/ui/labeler.ui", "resilipy/ui/preprocessor.ui", "resilipy/ui/resilipy.ui", "resilipy/ui/stylesheet.txt"]),
+    ],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+    ]
+)
