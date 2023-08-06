2ffea2e20525852c4622f0b96da23b1362184c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:41 +0000

    Add package DNA-AssemblyLine/0.0.5/DNA-AssemblyLine-0.0.5.tar.gz

---
+++ packages/DNA-AssemblyLine/DNA-AssemblyLine-0.0.5.tar.gz/DNA-AssemblyLine-0.0.5/setup.py
@@ -0,0 +1,27 @@
+#[metadata]
+from setuptools import setup, find_packages
+
+setup(
+    name = "DNA-AssemblyLine",
+    version = "0.0.5",
+    author = "John Bryant",
+    author_email = "jbryant2@vt.edu",
+    description = "A package for automating DNA assembly with an Opentrons liquid handling robot",
+    long_description = "file: README.md",
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/PlantSynBioLab/opentrons",
+    packages=find_packages(where="DNA-AssemblyLine"),
+#    Bug Tracker = []
+    classifiers =[
+        "Programming Language :: Python :: 3",
+    	"Operating System :: OS Independent",
+	    "License :: OSI Approved :: Apache Software License"],
+
+#[options]
+#package_dir =
+#    = src
+#packages = find:
+    python_requires = ">=3.6")
+
+#[options.packages.find]
+#where = src
