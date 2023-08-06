a20f73a8dd28c7740f8aaaf5c0804bd30bd500f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:35 +0000

    Add package pikepdf/5.4.0/pikepdf-5.4.0.tar.gz

---
+++ packages/pikepdf/pikepdf-5.4.0.tar.gz/pikepdf-5.4.0/build-scripts/environ-from-pyproject.py
@@ -0,0 +1,9 @@
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib  # type: ignore
+
+with open('pyproject.toml', 'rb') as f:
+    t = tomllib.load(f)
+env = t['tool']['cibuildwheel']['environment']
+print('\n'.join(f'{k}={v}' for k, v in env.items()))
