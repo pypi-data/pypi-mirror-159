538aafbb5b16f140f07af365c7abfee9b2191b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:02:03 +0000

    Add package expressmoney-points/1.17.2/expressmoney-points-1.17.2.tar.gz

---
+++ packages/expressmoney-points/expressmoney-points-1.17.2.tar.gz/expressmoney-points-1.17.2/setup.py
@@ -0,0 +1,16 @@
+"""
+py setup.py sdist
+twine upload dist/expressmoney-points-1.17.2.tar.gz
+"""
+import setuptools
+
+setuptools.setup(
+    name='expressmoney-points',
+    packages=setuptools.find_packages(),
+    version='1.17.2',
+    description='Service points',
+    author='Development team',
+    author_email='dev@expressmoney.com',
+    install_requires=('expressmoney', 'django-money'),
+    python_requires='>=3.7',
+)
