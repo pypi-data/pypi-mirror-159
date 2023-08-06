da99361da04a60aaae55e8dfa17276274263fbf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:00 +0000

    Add package pytest-wake/0.1.0/pytest_wake-0.1.0-py3-none-any.whl

---
+++ packages/pytest-wake/pytest_wake-0.1.0-py3-none-any.whl/pytest_wake/core.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+"""
+@File    :   wake.py
+@Time    :   2022/07/19 15:37:18
+@Version :   1.0
+@Desc    :   
+"""
+import os
+
+import pytest
+
+
+def pytest_load_initial_conftests(early_config, args, parser):
+    for i in args:
+        if "--env" in i:
+            _, env = i.split("=")
+            os.environ["_PYTEST_ENV"] = env
+        elif "--tailnum" in i:
+            _, tailnum = i.split("=")
+            os.environ["_PYTEST_TAILNUM"] = tailnum
+
+
+@pytest.fixture(scope="session", autouse=True)
+def clear_env():
+    yield
+    del os.environ["_PYTEST_TAILNUM"]  # 必须删除
