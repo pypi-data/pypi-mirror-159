8b2d96f31583bad4af2fb125b1f61da610ed6479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:39 +0000

    Add package autograde-visualizer/0.0.21/autograde_visualizer-0.0.21.tar.gz

---
+++ packages/autograde-visualizer/autograde_visualizer-0.0.21.tar.gz/autograde_visualizer-0.0.21/autograde_visualizer/frontend/build/precache-manifest.373145542e6b1b1d82611afe78685c7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c90cf378466aa49a8329e890217d6d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7519515b2a91416fac03",
+    "url": "./static/js/2.f672bc8f.chunk.js"
+  },
+  {
+    "revision": "ea9e8a04b4530945f0b69f09f74b01e4",
+    "url": "./static/js/2.f672bc8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55415fa1d3e756bb1819",
+    "url": "./static/js/main.40eec251.chunk.js"
+  },
+  {
+    "revision": "e14cae3afec548135755",
+    "url": "./static/js/runtime-main.e4cdaade.js"
+  }
+]);
