e76dc3a1eba2647c0ab9f855ff1608945e308a89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:47 +0000

    Add package cdk-image-pipeline/0.1.5/cdk_image_pipeline-0.1.5-py3-none-any.whl

---
+++ packages/cdk-image-pipeline/cdk_image_pipeline-0.1.5-py3-none-any.whl/cdk_image_pipeline/_jsii/__init__.py
@@ -0,0 +1,22 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-image-pipeline", "0.1.5", __name__[0:-6], "cdk-image-pipeline@0.1.5.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
