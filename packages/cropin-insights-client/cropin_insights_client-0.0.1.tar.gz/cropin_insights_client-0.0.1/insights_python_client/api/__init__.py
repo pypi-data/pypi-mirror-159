ebcdc1c8d159060cf7fcae9d3e0128d00ed19ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:18 +0000

    Add package cropin-insights-client/0.0.1/cropin_insights_client-0.0.1.tar.gz

---
+++ packages/cropin-insights-client/cropin_insights_client-0.0.1.tar.gz/cropin_insights_client-0.0.1/insights_python_client/api/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from insights_python_client.api.boundary_api import BoundaryApi
+from insights_python_client.api.file_api import FileApi
+from insights_python_client.api.metrics_api import MetricsApi
+from insights_python_client.api.token_api import TokenApi
+from insights_python_client.api.weather_api import WeatherApi
+from insights_python_client.api.yield_api import YieldApi
