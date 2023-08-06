ee588ce8277fb1df23e058c7bc9a1d1b4c99fe3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:16 +0000

    Add package network3-medusa/0.1.37/network3_medusa-0.1.37-py3-none-any.whl

---
+++ packages/network3-medusa/network3_medusa-0.1.37-py3-none-any.whl/network3_medusa/network3_medusa.py
@@ -0,0 +1,53 @@
+import os
+import json
+import click
+from dotenv import load_dotenv
+from hedera import Client, AccountId, PrivateKey, Hbar, FileCreateTransaction, FileId, FileContentsQuery
+import subprocess
+
+load_dotenv()
+
+OPERATOR_ID= os.getenv("OPERATOR_ID")
+OPERATOR_KEY= os.getenv("OPERATOR_KEY")
+
+class Network3Medusa():
+    def __init__(self, command):
+        self.command = command
+
+    def network3_medusa(self):
+        self.py2output()
+        parsed_json=self.output.stdout
+        self.add_file_to_hedera(parsed_json)
+        
+    def py2output(self):
+        self.output = subprocess.run(["python", "call_clid.py", self.command], capture_output=True)
+
+    def add_file_to_hedera(self, parsed_json):
+        client = Client.forTestnet()
+        client.setOperator(OPERATOR_ID, OPERATOR_KEY)
+        print(OPERATOR_ID)
+        print(OPERATOR_KEY)
+        print(parsed_json)
+        tran = FileCreateTransaction()
+        resp = tran.setKeys(OPERATOR_KEY.getPublicKey()).setContents(parsed_json).setMaxTransactionFee(Hbar(2)).execute(client)
+        print("nodeId: ",  resp.nodeId.toString())
+        receipt = resp.getReceipt(client)
+        fileId = receipt.fileId
+        print("file: ",  fileId.toString())
+        query = FileContentsQuery()
+        contents = query.setFileId(fileId).execute(client)
+        json_contents = json.loads(contents.toStringUtf8())
+        print(json_contents)        
+
+@click.command()
+@click.option('--command',
+    prompt='Command',
+    help=('A valid Command'),
+    required=True)
+
+def cli(command):
+    invoke_class = Network3Medusa(command)
+    invoke_class.network3_medusa()
+
+if __name__ == '__main__':
+    Network3Medusa.main()
