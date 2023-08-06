e50c3a09d819d03ca7c90920753bd8c4da55ece5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:32 +0000

    Add package enda/0.0.8/enda-0.0.8-py3-none-any.whl

---
+++ packages/enda/enda-0.0.8-py3-none-any.whl/enda/ml_backends/sklearn_estimator.py
@@ -0,0 +1,26 @@
+import pandas
+from enda.estimators import EndaEstimator
+
+
+class EndaSklearnEstimator(EndaEstimator):
+    """
+    This is a simple wrapper around any Scikit-learn estimator.
+    It makes it easier to deal pandas time-series dataframes as input and output.
+    """
+
+    def __init__(self, sklearn_estimator):
+        """
+        Like in scikit-learn we use duck typing here, so we don't check the type of argument 'sklearn_estimator'
+        """
+
+        self.model = sklearn_estimator
+
+    def train(self, df: pandas.DataFrame, target_col: str):
+        x = df.drop(columns=[target_col])
+        y = df[target_col]
+        self.model.fit(x, y)
+
+    def predict(self, df: pandas.DataFrame, target_col: str):
+        a = self.model.predict(df)  # numpy array
+        s = pandas.Series(a, name=target_col, index=df.index)  # pandas series with correct name and index
+        return s.to_frame()
