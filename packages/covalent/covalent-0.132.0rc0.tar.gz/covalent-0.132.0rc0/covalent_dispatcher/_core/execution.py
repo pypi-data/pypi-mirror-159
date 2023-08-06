127904bbb60001fdc7a2ea4a1f7c90e5a498adc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:40 +0000

    Add package covalent/0.132.0rc0/covalent-0.132.0rc0.tar.gz

---
+++ packages/covalent/covalent-0.132.0rc0.tar.gz/covalent-0.132.0rc0/covalent_dispatcher/_core/execution.py
@@ -0,0 +1,504 @@
+# Copyright 2021 Agnostiq Inc.
+#
+# This file is part of Covalent.
+#
+# Licensed under the GNU Affero General Public License 3.0 (the "License").
+# A copy of the License may be obtained with this software package or at
+#
+#      https://www.gnu.org/licenses/agpl-3.0.en.html
+#
+# Use of this file is prohibited except in compliance with the License. Any
+# modifications or derivative works of this file must retain this copyright
+# notice, and modified files must contain a notice indicating that they have
+# been altered from the originals.
+#
+# Covalent is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the License for more details.
+#
+# Relief from the License may be granted by purchasing a commercial license.
+
+"""
+Defines the core functionality of the dispatcher
+"""
+
+import traceback
+from concurrent.futures import Future, ThreadPoolExecutor, wait
+from datetime import datetime, timezone
+from typing import Any, Dict, List
+
+import cloudpickle as pickle
+
+from covalent import dispatch_sync
+from covalent._data_store.datastore import DataStore
+from covalent._results_manager import Result
+from covalent._results_manager import results_manager as rm
+from covalent._results_manager.write_result_to_db import write_sublattice_electron_id
+from covalent._shared_files import logger
+from covalent._shared_files.context_managers import active_lattice_manager
+from covalent._shared_files.defaults import (
+    attr_prefix,
+    electron_dict_prefix,
+    electron_list_prefix,
+    generator_prefix,
+    parameter_prefix,
+    prefix_separator,
+    sublattice_prefix,
+    subscript_prefix,
+)
+from covalent._workflow.deps import Deps
+from covalent._workflow.lattice import Lattice
+from covalent.executor import _executor_manager
+from covalent_ui import result_webhook
+
+from .._db.dispatchdb import DispatchDB
+
+app_log = logger.app_log
+log_stack_info = logger.log_stack_info
+
+
+def generate_node_result(
+    node_id,
+    start_time=None,
+    end_time=None,
+    status=None,
+    output=None,
+    error=None,
+    stdout=None,
+    stderr=None,
+    sublattice_result=None,
+):
+
+    return {
+        "node_id": node_id,
+        "start_time": start_time,
+        "end_time": end_time,
+        "status": status,
+        "output": output,
+        "error": error,
+        "stdout": stdout,
+        "stderr": stderr,
+        "sublattice_result": sublattice_result,
+    }
+
+
+def _get_task_inputs(node_id: int, node_name: str, result_object: Result) -> dict:
+    """
+    Return the required inputs for a task execution.
+    This makes sure that any node with child nodes isn't executed twice and fetches the
+    result of parent node to use as input for the child node.
+
+    Args:
+        node_id: Node id of this task in the transport graph.
+        node_name: Name of the node.
+        result_object: Result object to be used to update and store execution related
+                       info including the results.
+
+    Returns:
+        inputs: Input dictionary to be passed to the task containing args, kwargs,
+                and any parent node execution results if present.
+    """
+
+    if node_name.startswith(electron_list_prefix):
+        values = [
+            result_object.lattice.transport_graph.get_node_value(parent, "output")
+            for parent in result_object.lattice.transport_graph.get_dependencies(node_id)
+        ]
+        task_input = {"args": [], "kwargs": {"x": values}}
+    elif node_name.startswith(electron_dict_prefix):
+        values = {}
+        for parent in result_object.lattice.transport_graph.get_dependencies(node_id):
+
+            edge_data = result_object.lattice.transport_graph.get_edge_data(parent, node_id)
+
+            value = result_object.lattice.transport_graph.get_node_value(parent, "output")
+            for e_key, d in edge_data.items():
+                key = d["edge_name"]
+                values[key] = value
+
+        task_input = {"args": [], "kwargs": {"x": values}}
+    else:
+        task_input = {"args": [], "kwargs": {}}
+
+        for parent in result_object.lattice.transport_graph.get_dependencies(node_id):
+
+            edge_data = result_object.lattice.transport_graph.get_edge_data(parent, node_id)
+            value = result_object.lattice.transport_graph.get_node_value(parent, "output")
+
+            for e_key, d in edge_data.items():
+                if not d.get("wait_for"):
+                    if d["param_type"] == "arg":
+                        task_input["args"].append(value)
+                    elif d["param_type"] == "kwarg":
+                        key = d["edge_name"]
+                        task_input["kwargs"][key] = value
+
+    return task_input
+
+
+def _post_process(lattice: Lattice, node_outputs: Dict, execution_order: List[List]) -> Any:
+    """
+    Post processing function to be called after the lattice execution.
+    This takes care of executing statements that were not an electron
+    but were inside the lattice's function. It also replaces any calls
+    to an electron with the result of that electron execution, hence
+    preventing a local execution of electron's function.
+
+    Note: Here `node_outputs` is used instead of `electron_outputs`
+    since an electron can be called multiple times with possibly different
+    arguments, but every time it's called, it will be executed as a separate node.
+    Thus, output of every node is used.
+
+    Args:
+        lattice: Lattice object that was dispatched.
+        node_outputs: Dictionary containing the output of all the nodes.
+        execution_order: List of lists containing the order of execution of the nodes.
+
+    Reurns:
+        result: The result of the lattice function.
+    """
+
+    ordered_node_outputs = []
+    for i, item in enumerate(node_outputs.items()):
+        key, val = item
+        if not key.startswith(prefix_separator) or key.startswith(sublattice_prefix):
+            ordered_node_outputs.append((i, val))
+
+    with active_lattice_manager.claim(lattice):
+        lattice.post_processing = True
+        lattice.electron_outputs = ordered_node_outputs
+        result = lattice.workflow_function(*lattice.args, **lattice.kwargs)
+        lattice.post_processing = False
+        return result
+
+
+def _run_task(
+    node_id: int,
+    dispatch_id: str,
+    results_dir: str,
+    inputs: Dict,
+    serialized_callable: Any,
+    selected_executor: Any,
+    call_before: List,
+    call_after: List,
+    node_name: str,
+) -> None:
+    """
+    Run a task with given inputs on the selected executor.
+    Also updates the status of current node execution while
+    checking if a redispatch has occurred. Exclude those nodes
+    from execution which were completed.
+
+    Also verifies if execution of this dispatch has been cancelled.
+
+    Args:
+        inputs: Inputs for the task.
+        result_object: Result object being used for current dispatch
+        node_id: Node id of the task to be executed.
+
+    Returns:
+        None
+    """
+
+    inputs = pickle.loads(inputs)
+    selected_executor = pickle.loads(selected_executor)
+
+    # the executor is determined during scheduling and provided in the execution metadata
+    executor = _executor_manager.get_executor(selected_executor)
+
+    # run the task on the executor and register any failures
+    try:
+
+        if node_name.startswith(sublattice_prefix):
+            func = serialized_callable.get_deserialized()
+            sublattice_result = dispatch_sync(func)(*inputs["args"], **inputs["kwargs"])
+            with DispatchDB() as db:
+                write_sublattice_electron_id(
+                    db=DataStore(db._db_dev_path(), initialize_db=True),
+                    parent_dispatch_id=dispatch_id,
+                    sublattice_node_id=node_id,
+                    sublattice_dispatch_id=sublattice_result.dispatch_id,
+                )
+            output = sublattice_result.result
+
+            end_time = datetime.now(timezone.utc)
+
+            node_result = generate_node_result(
+                node_id=node_id,
+                end_time=end_time,
+                status=Result.COMPLETED,
+                output=output,
+                sublattice_result=sublattice_result,
+            )
+
+        else:
+            output, stdout, stderr = executor.execute(
+                function=serialized_callable,
+                args=inputs["args"],
+                kwargs=inputs["kwargs"],
+                call_before=call_before,
+                call_after=call_after,
+                dispatch_id=dispatch_id,
+                results_dir=results_dir,
+                node_id=node_id,
+            )
+
+            end_time = datetime.now(timezone.utc)
+
+            node_result = generate_node_result(
+                node_id=node_id,
+                end_time=end_time,
+                status=Result.COMPLETED,
+                output=output,
+                stdout=stdout,
+                stderr=stderr,
+            )
+
+    except Exception as ex:
+        end_time = datetime.now(timezone.utc)
+
+        node_result = generate_node_result(
+            node_id=node_id,
+            end_time=end_time,
+            status=Result.FAILED,
+            error="".join(traceback.TracebackException.from_exception(ex).format()),
+        )
+
+    return node_result
+
+
+def _run_planned_workflow(result_object: Result, thread_pool: ThreadPoolExecutor) -> Result:
+    """
+    Run the workflow in the topological order of their position on the
+    transport graph. Does this in an asynchronous manner so that nodes
+    at the same level are executed in parallel. Also updates the status
+    of the whole workflow execution.
+
+    Args:
+        result_object: Result object being used for current dispatch
+
+    Returns:
+        None
+    """
+
+    def update_node_result(node_result: dict):
+
+        result_object._update_node(**node_result)
+        with DispatchDB() as db:
+            db.upsert(result_object.dispatch_id, result_object)
+            db.save_db(result_object)
+        result_webhook.send_update(result_object)
+
+    def task_callback(future: Future):
+        node_result = future.result()
+        update_node_result(node_result)
+
+    result_object._status = Result.RUNNING
+    result_object._start_time = datetime.now(timezone.utc)
+
+    order = result_object.lattice.transport_graph.get_topologically_sorted_graph()
+
+    for nodes in order:
+        futures: list = []
+
+        for node_id in nodes:
+            # Get name of the node for the current task
+            node_name = result_object.lattice.transport_graph.get_node_value(node_id, "name")
+
+            if node_name.startswith(
+                (subscript_prefix, generator_prefix, parameter_prefix, attr_prefix)
+            ):
+                if node_name.startswith(parameter_prefix):
+                    output = result_object.lattice.transport_graph.get_node_value(node_id, "value")
+                else:
+                    parent = result_object.lattice.transport_graph.get_dependencies(node_id)[0]
+                    output = result_object.lattice.transport_graph.get_node_value(parent, "output")
+
+                    if node_name.startswith(attr_prefix):
+                        attr = result_object.lattice.transport_graph.get_node_value(
+                            node_id, "attribute_name"
+                        )
+                        output = getattr(output, attr)
+                    else:
+                        key = result_object.lattice.transport_graph.get_node_value(node_id, "key")
+                        output = output[key]
+
+                result_object._update_node(
+                    node_id=node_id,
+                    start_time=datetime.now(timezone.utc),
+                    end_time=datetime.now(timezone.utc),
+                    status=Result.COMPLETED,
+                    output=output,
+                )
+
+                continue
+
+            task_input = _get_task_inputs(node_id, node_name, result_object)
+
+            start_time = datetime.now(timezone.utc)
+            serialized_callable = result_object.lattice.transport_graph.get_node_value(
+                node_id, "function"
+            )
+            selected_executor = result_object.lattice.transport_graph.get_node_value(
+                node_id, "metadata"
+            )["executor"]
+
+            deps = result_object.lattice.transport_graph.get_node_value(node_id, "metadata")[
+                "deps"
+            ]
+
+            # Assemble call_before and call_after from all the deps
+
+            call_before_objs = result_object.lattice.transport_graph.get_node_value(
+                node_id, "metadata"
+            )["call_before"]
+            call_after_objs = result_object.lattice.transport_graph.get_node_value(
+                node_id, "metadata"
+            )["call_after"]
+
+            call_before = []
+
+            for dep_type in ["bash", "pip"]:
+                if dep_type in deps:
+                    dep = deps[dep_type]
+                    call_before.append(dep.apply())
+
+            for dep in call_before_objs:
+                call_before.append(dep.apply())
+
+            call_after = [dep.apply() for dep in call_after_objs]
+
+            update_node_result(
+                generate_node_result(
+                    node_id=node_id,
+                    start_time=start_time,
+                    status=Result.RUNNING,
+                )
+            )
+
+            # Add the task generated for the node to the list of tasks
+            future = thread_pool.submit(
+                _run_task,
+                node_id=node_id,
+                dispatch_id=result_object.dispatch_id,
+                results_dir=result_object.results_dir,
+                serialized_callable=serialized_callable,
+                selected_executor=pickle.dumps(selected_executor),
+                node_name=node_name,
+                call_before=call_before,
+                call_after=call_after,
+                inputs=pickle.dumps(task_input),
+            )
+
+            future.add_done_callback(task_callback)
+
+            futures.append(future)
+
+        # run the tasks for the current iteration concurrently
+        # results are not used right now, but can be in the case of multiprocessing
+        wait(futures)
+        # del futures
+
+        # When one or more nodes failed in the last iteration, don't iterate further
+        for node_id in nodes:
+            if result_object._get_node_status(node_id) == Result.FAILED:
+                result_object._status = Result.FAILED
+                result_object._end_time = datetime.now(timezone.utc)
+                result_object._error = f"Node {result_object._get_node_name(node_id)} failed: \n{result_object._get_node_error(node_id)}"
+                with DispatchDB() as db:
+                    db.upsert(result_object.dispatch_id, result_object)
+                    db.save_db(result_object)
+                result_webhook.send_update(result_object)
+                return
+
+            elif result_object._get_node_status(node_id) == Result.CANCELLED:
+                result_object._status = Result.CANCELLED
+                result_object._end_time = datetime.now(timezone.utc)
+                with DispatchDB() as db:
+                    db.upsert(result_object.dispatch_id, result_object)
+                    db.save_db(result_object)
+                result_webhook.send_update(result_object)
+                return
+
+    # post process the lattice
+    result_object._result = _post_process(
+        result_object.lattice, result_object.get_all_node_outputs(), order
+    )
+
+    result_object._status = Result.COMPLETED
+    result_object._end_time = datetime.now(timezone.utc)
+    with DispatchDB() as db:
+        db.upsert(result_object.dispatch_id, result_object)
+        db.save_db(result_object, write_source=True)
+    result_webhook.send_update(result_object)
+
+
+def _plan_workflow(result_object: Result) -> None:
+    """
+    Function to plan a workflow according to a schedule.
+    Planning means to decide which executors (along with their arguments) will
+    be used by each node.
+
+    Args:
+        result_object: Result object being used for current dispatch
+
+    Returns:
+        None
+    """
+
+    if result_object.lattice.get_metadata("schedule"):
+        # Custom scheduling logic of the format:
+        # scheduled_executors = get_schedule(result_object)
+
+        # for node_id, executor in scheduled_executors.items():
+        #    result_object.lattice.transport_graph.set_node_value(node_id, "executor", executor)
+        pass
+
+
+def run_workflow(dispatch_id: str, results_dir: str, tasks_pool: ThreadPoolExecutor) -> None:
+    """
+    Plan and run the workflow by loading the result object corresponding to the
+    dispatch id and retrieving essential information from it.
+    Returns without changing anything if a redispatch is done of a (partially or fully)
+    completed workflow with the same dispatch id.
+
+    Args:
+        dispatch_id: Dispatch id of the workflow to be run
+        results_dir: Directory where the result object is stored
+
+    Returns:
+        None
+    """
+
+    result_object = rm._get_result_from_file(dispatch_id, results_dir)
+
+    if result_object.status == Result.COMPLETED:
+        return
+
+    try:
+        _plan_workflow(result_object)
+        _run_planned_workflow(result_object, tasks_pool)
+
+    except Exception as ex:
+        result_object._status = Result.FAILED
+        result_object._end_time = datetime.now(timezone.utc)
+        result_object._error = "".join(traceback.TracebackException.from_exception(ex).format())
+        DispatchDB().save_db(result_object)
+        raise
+
+
+def cancel_workflow(dispatch_id: str) -> None:
+    """
+    Cancels a dispatched workflow using publish subscribe mechanism
+    provided by Dask.
+
+    Args:
+        dispatch_id: Dispatch id of the workflow to be cancelled
+
+    Returns:
+        None
+    """
+
+    # shared_var = Variable(dispatch_id)
+    # shared_var.set(str(Result.CANCELLED))
+    pass
---
+++ packages/covalent/covalent-0.132.0rc0.tar.gz/covalent-0.132.0rc0/covalent_dispatcher/_db/dispatchdb.py
@@ -0,0 +1,261 @@
+import json
+import os
+import sqlite3
+from datetime import datetime
+from typing import List, Tuple
+
+import networkx as nx
+import simplejson
+
+import covalent.executor as covalent_executor
+from covalent._data_store import DataStore
+from covalent._results_manager.result import Result
+from covalent._shared_files import logger
+from covalent._shared_files.config import get_config
+from covalent._shared_files.util_classes import Status
+from covalent._shared_files.utils import get_named_params
+
+app_log = logger.app_log
+log_stack_info = logger.log_stack_info
+# DB Schema:
+# TABLE dispatches
+# * dispatch_id text primary key
+# * result_dict text ; json-serialied dictionary representation of Result
+
+# TODO: Move these to a common utils module
+
+
+def extract_graph_node(node):
+    # avoid mutating original node
+    node = node.copy()
+
+    # doc string
+    f = node.get("function")
+    if f is not None:
+        node["doc"] = f.get_deserialized().__doc__
+
+    # metadata
+    node["metadata"] = extract_metadata(node["metadata"])
+
+    # prevent JSON encoding
+    node["kwargs"] = encode_dict(node.get("kwargs"))
+
+    # remove unused fields
+    node.pop("function", None)
+    node.pop("node_name", None)
+
+    return node
+
+
+def extract_metadata(metadata: dict):
+    try:
+        # avoid mutating original metadata
+        metadata = metadata.copy()
+
+        name = metadata["executor"]
+        executor = covalent_executor._executor_manager.get_executor(name=name)
+
+        if executor is not None:
+            # extract attributes
+            metadata["executor"] = encode_dict(executor.__dict__)
+            if isinstance(name, str):
+                metadata["executor_name"] = name
+            else:
+                metadata["executor_name"] = f"<{executor.__class__.__name__}>"
+
+        metadata["deps"] = encode_dict(metadata["deps"])
+        call_before = metadata["call_before"]
+        call_after = metadata["call_after"]
+        for i, dep in enumerate(call_before):
+            call_before[i] = str(dep)
+
+        for i, dep in enumerate(call_after):
+            call_after[i] = str(dep)
+
+        metadata["call_before"] = call_before
+        metadata["call_after"] = call_after
+
+    except (KeyError, AttributeError):
+        pass
+
+    return metadata
+
+
+def encode_dict(d):
+    """Avoid JSON encoding when python str() suffices"""
+    if not isinstance(d, dict):
+        return d
+    return {k: str(v) for (k, v) in d.items()}
+
+
+def extract_graph(graph):
+    graph = nx.json_graph.node_link_data(graph)
+    nodes = list(map(extract_graph_node, graph["nodes"]))
+    return {
+        "nodes": nodes,
+        "links": graph["links"],
+    }
+
+
+def result_encoder(obj):
+    if isinstance(obj, Status):
+        return obj.STATUS
+    if isinstance(obj, datetime):
+        return obj.isoformat()
+    return str(obj)
+
+
+def encode_result(result_obj):
+    lattice = result_obj.lattice
+    ((named_args, named_kwargs),) = (
+        get_named_params(lattice.workflow_function, lattice.args, lattice.kwargs),
+    )
+    result_dict = {
+        "dispatch_id": result_obj.dispatch_id,
+        "status": result_obj.status,
+        "result": result_obj.result,
+        "start_time": result_obj.start_time,
+        "end_time": result_obj.end_time,
+        "results_dir": result_obj.results_dir,
+        "error": result_obj.error,
+        "lattice": {
+            "function_string": lattice.workflow_function_string,
+            "doc": lattice.__doc__,
+            "name": lattice.__name__,
+            "inputs": encode_dict({**named_args, **named_kwargs}),
+            "metadata": extract_metadata(lattice.metadata),
+        },
+        "graph": extract_graph(result_obj.lattice.transport_graph._graph),
+    }
+
+    jsonified_result = simplejson.dumps(result_dict, default=result_encoder, ignore_nan=True)
+
+    return jsonified_result
+
+
+class DispatchDB:
+    """
+    Wrapper for the database of workflows.
+    """
+
+    def __init__(self, dbpath: str = None) -> None:
+        if dbpath:
+            self._dbpath = dbpath
+        else:
+            self._dbpath = get_config("user_interface.dispatch_db")
+
+        # dispatch_id is the primary key
+
+        # Initialize the db if necessary; sqlite3 raises
+        # sqlite3.OperationalError if table already exists.
+        self.conn = sqlite3.connect(self._dbpath)
+        try:
+            self.conn.execute(
+                "CREATE TABLE dispatches \
+                (dispatch_id text primary key, \
+                result_dict text)"
+            )
+            self.conn.commit()
+        except sqlite3.OperationalError:
+            pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type:
+            self.conn.rollback()
+        else:
+            self.conn.commit()
+        self.conn.close()
+
+        return False
+
+    def _db_dev_path(self):
+        """This is a temporary method for the Result.persist DB path.add()
+
+        TODO - Take this out when result.save and persist have been switched.
+        """
+        sqlite = ".sqlite"
+        return f"sqlite+pysqlite:///{self._dbpath.split(sqlite)[0]}_dev{sqlite}"
+
+    def save_db(self, result_object: Result, **kwargs):
+
+        result_object.save(**kwargs)
+
+        try:
+            # set echo=True only if covalent is started in debug /develop mode `covalent start -d`
+            # `initialize_db` flag can be removed as its redundant (sqlalchemy does check if the tables are
+            # created or not before inserting/updating data)
+            result_object.persist(DataStore(self._db_dev_path(), initialize_db=True))
+            # result_object.persist()
+        except Exception as e:
+            app_log.exception(f"Exception occured while saving to DB: {e}.")
+
+    def get(self, dispatch_ids: [] = []) -> List[Tuple[str, str]]:
+        """
+        Retrieve workflows with the given dispatch ids.
+
+        Args:
+            dispatch_ids: A list of dispatch ids for the sought-after workflows.
+
+        Returns:
+            A list of pairs (dispatch_id, [jsonified result dictionary]).
+        """
+        if len(dispatch_ids) > 0:
+            placeholders = "({})".format(", ".join(["?" for i in dispatch_ids]))
+            sql = (
+                "SELECT * FROM dispatches WHERE \
+            dispatch_id in "
+                + placeholders
+            )
+
+            res = self.conn.execute(sql, dispatch_ids).fetchall()
+
+        else:
+            sql = "SELECT * FROM dispatches"
+
+            res = self.conn.execute(sql).fetchall()
+
+        return res
+
+    def upsert(self, dispatch_id: str, result_obj: Result) -> None:
+        """
+        Insert or update the record with the given dispatch_id.
+
+        Args:
+            dispatch_id: The workflow's dispatch_id.
+            result_obj: The Result object for the workflow.
+
+        The Result is turned into a dictionary and stored as json.
+        """
+
+        jsonified_result = encode_result(result_obj)
+
+        try:
+            sql = "INSERT INTO dispatches (dispatch_id, result_dict) VALUES (?, ?)"
+            self.conn.execute(sql, (dispatch_id, jsonified_result))
+            self.conn.commit()
+
+        except sqlite3.IntegrityError:
+            sql = "UPDATE dispatches SET result_dict = ? WHERE dispatch_id = ?"
+            self.conn.execute(sql, (jsonified_result, dispatch_id))
+            self.conn.commit()
+
+        # sql = "INSERT INTO dispatches (dispatch_id, result_dict) VALUES (?, ?) \
+        # ON CONFLICT (dispatch_id) DO UPDATE SET result_dict = excluded.result_dict"
+
+        # self.conn.execute(sql, (dispatch_id, jsonified_result))
+
+    def delete(self, dispatch_ids: []) -> None:
+        """
+        Delete records with the given dispatch ids.
+
+        Args:
+            dispatch_ids: A list of dispatch ids
+        """
+        placeholders = "({})".format(", ".join(["?" for i in dispatch_ids]))
+        sql = "DELETE FROM dispatches WHERE dispatch_id in " + placeholders
+
+        self.conn.execute(sql, dispatch_ids)
+        self.conn.commit()
