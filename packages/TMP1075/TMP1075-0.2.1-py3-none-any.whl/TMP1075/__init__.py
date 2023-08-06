a03a6f8255379ef50031c4b0b6d932885d09c9ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:59 +0000

    Add package TMP1075/0.2.1/TMP1075-0.2.1-py3-none-any.whl

---
+++ packages/TMP1075/TMP1075-0.2.1-py3-none-any.whl/TMP1075/__init__.py
@@ -0,0 +1,23 @@
+import busio
+import board
+from adafruit_bus_device.i2c_device import I2CDevice
+
+
+class TMP1075:
+    """
+    Driver for the TI TMP1075 temperature sensor.
+    See datasheet: http://www.ti.com/lit/ds/symlink/tmp1075.pdf
+
+    """
+    TEMP_REGISTER = bytearray([0x00])
+    CONFIG_REGISTER = bytearray([0x01])
+
+
+    def __init__(self, address: int=0x4f):
+        comm_port = busio.I2C(board.SCL, board.SDA)
+        self.i2c = I2CDevice(comm_port, address)
+
+    def get_temperature(self) -> float:
+        b = bytearray(2)
+        self.i2c.write_then_readinto(self.TEMP_REGISTER, b)
+        return ((b[0] << 4) + (b[1] >> 4)) * 0.0625
