ca46f751044fb9c7fb01040f07beb62f53894c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:51:47 +0000

    Add package django-tables-gcom/0.4.0/django_tables_gcom-0.4.0.tar.gz

---
+++ packages/django-tables-gcom/django_tables_gcom-0.4.0.tar.gz/django_tables_gcom-0.4.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['django_tables_gcom']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'django-tables-gcom',
+    'version': '0.4.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=2.7,<3.0',
+}
+
+
+setup(**setup_kwargs)
