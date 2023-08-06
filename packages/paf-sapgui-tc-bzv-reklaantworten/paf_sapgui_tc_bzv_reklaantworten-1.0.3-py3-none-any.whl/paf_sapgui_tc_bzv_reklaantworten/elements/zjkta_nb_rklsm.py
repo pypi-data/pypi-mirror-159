e752c1da5d4d88067bee6f184af078190592e373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:18:33 +0000

    Add package paf-sapgui-tc-bzv-reklaantworten/1.0.3/paf_sapgui_tc_bzv_reklaantworten-1.0.3-py3-none-any.whl

---
+++ packages/paf-sapgui-tc-bzv-reklaantworten/paf_sapgui_tc_bzv_reklaantworten-1.0.3-py3-none-any.whl/paf_sapgui_tc_bzv_reklaantworten/elements/zjkta_nb_rklsm.py
@@ -0,0 +1,9 @@
+loader_lauf_id = "wnd[0]/usr/ctxtS_RUNID-LOW"
+angelegt_am_von = "wnd[0]/usr/ctxtS_IMPDAT-LOW"
+angelegt_am_bis = "wnd[0]/usr/ctxtS_IMPDAT-HIGH"
+gruppenschluessel_ai = "wnd[0]/usr/ctxtS_AIGRP-LOW"
+status_abo_interface = "wnd[0]/usr/ctxtS_STATUS-LOW"
+objektnummer = "wnd[0]/usr/txtS_LDOBJ-LOW"
+
+tabelle = "wnd[0]/usr/cntlCC_ALV/shellcont/shell"
+detail_tabelle = "wnd[0]/usr/cntlCC_ALV_DETAIL/shellcont/shell"
