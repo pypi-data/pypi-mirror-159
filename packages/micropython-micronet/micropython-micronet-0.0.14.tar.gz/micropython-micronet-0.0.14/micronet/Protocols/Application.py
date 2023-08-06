611fbfbb2e8a79eea06c8ff3ac0f14aaf6f7a86a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:45 +0000

    Add package micropython-micronet/0.0.14/micropython-micronet-0.0.14.tar.gz

---
+++ packages/micropython-micronet/micropython-micronet-0.0.14.tar.gz/micropython-micronet-0.0.14/micronet/Protocols/Application.py
@@ -0,0 +1,84 @@
+from .base import microinterface, microsocket, randbytes
+import struct
+
+class HTTP:
+    pass
+class DHCP:
+    class Message:
+        op :      0b1*8= 1
+        htype:    0b1*8= 1 # See ARP section "Assigned numbers"
+        hlen:     0b1*8= 6
+        hops:     0b1*8= 0
+        xid:      0x4b*8
+        secs:     0x2b*8 = 0
+        flags:    0x2b*8 = 0
+        ciaddr:   0x4b*8 = 0
+        yiaddr:   0x4b*8 = 0
+        siaddr:   0x4b*8 = 0
+        giaddr:   0x4b*8 = 0
+        chaddr:   0x16b*8
+        sname:    0x64b*8 = b'\x00' * 64
+        file:     0x128b*8= b'\x00' * 128
+        options:  None   = bytearray([0x63,0x82,0x53,0x63])
+
+        def pack(self):
+            return struct.pack("!BBBBIHHIIII",
+               self.op, 
+               self.htype,  
+               self.hlen, 
+               self.hops, 
+               self.xid, 
+               self.secs, 
+               self.flags, 
+               self.ciaddr, 
+               self.yiaddr, 
+               self.siaddr, 
+               self.giaddr) +\
+               self.chaddr +\
+               self.sname +\
+               self.file +\
+               self.options 
+        
+        def unpack(data):
+            message = DHCP.Message()
+            (message.op, 
+            message.htype,  
+            message.hlen, 
+            message.hops, 
+            message.xid, 
+            message.secs, 
+            message.flags, 
+            message.ciaddr, 
+            message.yiaddr, 
+            message.siaddr, 
+            message.giaddr) = struct.unpack("!BBBBIHHIIII", data[:28])
+            message.chaddr = data[28:28+16]
+            message.sname = data[44:44+64]
+            message.file = data[108:108+128]
+            message.options = data[236:]
+
+            return message
+        
+
+
+    @microinterface.protocol_wrapper
+    def __init__(self, interface):
+        message = DHCP.Message()
+        message.chaddr = interface.src.mac +  b'\x00' * 8 
+        (message.xid,) = struct.unpack('I',randbytes(4))
+        self.message = message
+        
+    def discover(self):
+        return self.message.pack()
+    def offer(self):
+        pass
+    def request(self):
+        pass
+    def acknowledge(self):        
+        pass
+    def run(self):
+        self.discover()
+        self.offer()
+        self.request()
+        self.acknowledge()
+
