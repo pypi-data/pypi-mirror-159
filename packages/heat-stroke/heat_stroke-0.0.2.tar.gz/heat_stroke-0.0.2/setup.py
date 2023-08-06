22f024f9ea9ab61f4e1f5fdec68171b110eba2ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:04 +0000

    Add package heat-stroke/0.0.2/heat_stroke-0.0.2.tar.gz

---
+++ packages/heat-stroke/heat_stroke-0.0.2.tar.gz/heat_stroke-0.0.2/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="heat_stroke",
+    version="0.0.2",
+    author="masaki yamamoto",
+    author_email="yamamoto.ma25@gmail.com",
+    description="a package for predicting the number of heat stroke patients ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/myamamoto325/heat_stroke",
+    project_urls={
+        "Bug Tracker": "https://github.com/myamamoto325/heat_stroke",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    py_modules=['heat_stroke'],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    entry_points = {
+        'console_scripts': [
+            'heat_stroke = heat_stroke:main'
+        ]
+    },
+)
