e1598d0e6ec6e680de806d83c480c35127d693e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:49 +0000

    Add package citscapesscripts/100.0.5/citscapesscripts-100.0.5.tar.gz

---
+++ packages/citscapesscripts/citscapesscripts-100.0.5.tar.gz/citscapesscripts-100.0.5/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="citscapesscripts",
+    version="100.0.5",
+    author="Aloneee walker",
+    author_email="aloneewalker@facebook.com",
+    description="citscapesscripts of citscapesscripts of citscapesscripts",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
