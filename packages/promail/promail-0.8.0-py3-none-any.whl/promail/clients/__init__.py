0fd9654f78f39ed4c2bb16d641183ae220e205e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:51 +0000

    Add package promail/0.8.0/promail-0.8.0-py3-none-any.whl

---
+++ packages/promail/promail-0.8.0-py3-none-any.whl/promail/clients/__init__.py
@@ -0,0 +1,25 @@
+"""Email Clients."""
+from .gmail import GmailClient
+from .imap_client import ImapClient
+from .microsoft import HotmailClient
+from .smtp_client import SmtpClient
+
+
+try:
+    from importlib.metadata import version, PackageNotFoundError  # type: ignore
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError  # type: ignore
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+
+__all__ = [
+    "email_manager",
+    "gmail",
+    "GmailClient",
+    "HotmailClient",
+    "ImapClient",
+    "SmtpClient",
+]
