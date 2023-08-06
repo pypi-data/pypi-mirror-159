a1ffe10b026db53db1effa1c324eadbf34fb1e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:25 +0000

    Add package dry-pipe/0.2.18/dry_pipe-0.2.18.tar.gz

---
+++ packages/dry-pipe/dry_pipe-0.2.18.tar.gz/dry_pipe-0.2.18/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+minimal_requirements = [
+    'click==8.1.3',
+    'uvicorn==0.17.6',
+    'python-socketio==5.6.0',
+    'psutil==5.9.1',
+    'rich==12.4.4',
+    'requests==2.27.1',
+    'sshkeyboard==2.3.1'
+]
+
+extra_requirements = [
+    'PyYAML==5.4.1',
+    'paramiko==2.11.0'
+]
+
+setup(
+    name='dry_pipe',
+    version='0.2.18',
+    packages=['dry_pipe'],
+    install_requires=minimal_requirements,
+    extras_require={"full": extra_requirements},
+    include_package_data=True,
+    entry_points='''
+        [console_scripts]
+        drypipe=dry_pipe.cli:run_cli
+    ''',
+)
