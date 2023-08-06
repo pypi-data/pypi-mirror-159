c65c2135572cb0eaef9c23ff0bc132e34dd1d545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:03 +0000

    Add package search4file/0.1.2/search4file-0.1.2-py3-none-any.whl

---
+++ packages/search4file/search4file-0.1.2-py3-none-any.whl/search4file/__init__.py
@@ -0,0 +1,2 @@
+__version__ = '0.1.0'
+from search4file.api.search4file import search_by_content
---
+++ packages/search4file/search4file-0.1.2-py3-none-any.whl/tests/test_search4file.py
@@ -0,0 +1,14 @@
+import unittest
+
+from search4file import __version__
+from search4file.api.search4file import search_by_content
+
+
+class TestSearch(unittest.TestCase):
+    def test_version(self):
+        assert __version__ == '0.1.0'
+
+    def test_search(self):
+        search_by_content(search_path=r'D:\workplace\code\test\eval',content='test')
+
+
