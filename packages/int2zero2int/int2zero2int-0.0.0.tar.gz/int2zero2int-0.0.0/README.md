e05a6257a18075f3050a30581325e4f7ea92e956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:06 +0000

    Add package int2zero2int/0.0.0/int2zero2int-0.0.0.tar.gz

---
+++ packages/int2zero2int/int2zero2int-0.0.0.tar.gz/int2zero2int-0.0.0/README.md
@@ -0,0 +1,6 @@
+забавный антистресс!!!
+установка и запуск:
+```
+pip3 install -U int2zero2int && python -m int2zero2int 1234
+```
+
---
+++ packages/int2zero2int/int2zero2int-0.0.0.tar.gz/int2zero2int-0.0.0/setup.py
@@ -0,0 +1,5 @@
+from setuptools import *
+setup(name="int2zero2int", author="wer310Libs", author_email="oficalkapudovo@gmail.com", description="забавный антистресс!!!", packages=["int2zero2int"], long_description=open("README.md").read(), long_description_type="text/markdown")
+
+
+
