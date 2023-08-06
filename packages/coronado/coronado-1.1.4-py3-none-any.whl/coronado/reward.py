e1453d46a91c64f048ae8a3301e0ca27aebf7c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:18 +0000

    Add package coronado/1.1.4/coronado-1.1.4-py3-none-any.whl

---
+++ packages/coronado/coronado-1.1.4-py3-none-any.whl/coronado/reward.py
@@ -0,0 +1,13 @@
+# vim: set fileencoding=utf-8:
+
+
+from coronado import TripleEnum
+
+
+# *** classes and objects ***
+
+# class RewardType(TripleEnum):
+#     CARD_LINKED = 'CARD_LINKED'
+#     AFFILIATE = 'AFFILIATE'
+#     CATEGORICAL = 'CATEGORICAL'
+
