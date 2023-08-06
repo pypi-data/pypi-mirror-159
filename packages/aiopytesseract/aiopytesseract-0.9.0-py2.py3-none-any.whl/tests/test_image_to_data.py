da09bea5ddd79f21cbe25006357373d38ac84a98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:26 +0000

    Add package aiopytesseract/0.9.0/aiopytesseract-0.9.0-py2.py3-none-any.whl

---
+++ packages/aiopytesseract/aiopytesseract-0.9.0-py2.py3-none-any.whl/tests/test_image_to_data.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+import pytest
+
+import aiopytesseract
+from aiopytesseract.exceptions import TesseractRuntimeError
+from aiopytesseract.models import Data
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("image", ["tests/samples/file-sample_150kB.png"])
+async def test_image_to_data_with_str_image(image):
+    data = await aiopytesseract.image_to_data(image)
+    assert isinstance(data, list)
+    assert isinstance(data[0], Data)
+    assert len(data) == 22
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("image", ["tests/samples/file-sample_150kB.png"])
+async def test_image_to_data_with_bytes_image(image):
+    data = await aiopytesseract.image_to_data(Path(image).read_bytes())
+    assert isinstance(data, list)
+    assert isinstance(data[0], Data)
+    assert len(data) == 22
+
+
+@pytest.mark.asyncio
+async def test_image_to_data_with_invalid():
+    with pytest.raises(TesseractRuntimeError):
+        await aiopytesseract.image_to_data("tests/samples/file-sample_150kB.pdf")
+
+
+@pytest.mark.asyncio
+async def test_image_to_data_with_type_not_supported():
+    with pytest.raises(NotImplementedError):
+        await aiopytesseract.image_to_data(None)
