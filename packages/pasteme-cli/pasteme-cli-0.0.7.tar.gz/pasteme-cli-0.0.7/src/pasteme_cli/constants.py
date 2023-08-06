b9f53475fa77d1f2e9a2d5ab3bbfeba2d336be42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:15 +0000

    Add package pasteme-cli/0.0.7/pasteme-cli-0.0.7.tar.gz

---
+++ packages/pasteme-cli/pasteme-cli-0.0.7.tar.gz/pasteme-cli-0.0.7/src/pasteme_cli/constants.py
@@ -0,0 +1,41 @@
+# Language choices
+
+LANGUAGES = {
+    'bash': 'Command Language',
+    'c': 'C Language',
+    'cpp': 'C++ Language',
+    'csharp': 'C# Language',
+    'css': 'CSS Style Sheet Language',
+    'go': 'Go Language',
+    'html': 'HTML Language',
+    'java': 'Java Language',
+    'js': 'JavaScript Language',
+    'json': 'JSON Data Format',
+    'lua': 'Lua Language',
+    'md': 'MarkDown Language',
+    'php': 'PHP Language',
+    'plaintext': 'PlainText',
+    'python': 'Python Language',
+    'rb': 'Ruby Language'
+}
+
+# Hint for using the available languages
+
+LANGUAGES_HINT = f'''snippet language (available languages:
+{", ".join([l for l in LANGUAGES.keys()])})
+'''
+
+# Actual service information
+
+PASTEME_SERVICE_URL = 'https://pasteme.pythonanywhere.com'
+
+PASTEME_API_URL = 'https://pasteme.pythonanywhere.com/api/v1/paste/'
+
+# Traceback messages
+
+CONNECTION_ISSUE_HINT = 'Check your network connection. Make sure the PasteMe service is up and running.'
+
+# Information
+
+EPILOG_DESCRIPTION = '''Author -> Sadra Yahyapour (mailto:lnxpylnxpy@gmail.com)
+GitHub -> https://github.com/collove/pasteme-cli'''
