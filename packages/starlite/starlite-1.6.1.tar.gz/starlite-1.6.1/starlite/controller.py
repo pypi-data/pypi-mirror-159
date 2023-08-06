0741a91429e9c04bb747ee261f0bea800b291d4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:48 +0000

    Add package starlite/1.6.1/starlite-1.6.1.tar.gz

---
+++ packages/starlite/starlite-1.6.1.tar.gz/starlite-1.6.1/starlite/controller.py
@@ -0,0 +1,86 @@
+from copy import copy
+from typing import TYPE_CHECKING, Dict, List, Optional, Union, cast
+
+from starlite.handlers import BaseRouteHandler
+from starlite.utils import normalize_path
+
+if TYPE_CHECKING:
+    from typing import Type
+
+    from pydantic.fields import FieldInfo
+    from starlette.middleware import Middleware
+    from starlette.middleware.base import BaseHTTPMiddleware
+
+    from starlite.provide import Provide
+    from starlite.response import Response
+    from starlite.router import Router
+    from starlite.types import (
+        AfterRequestHandler,
+        AfterResponseHandler,
+        BeforeRequestHandler,
+        ExceptionHandler,
+        Guard,
+        MiddlewareProtocol,
+        ResponseHeader,
+    )
+
+
+class Controller:
+    """
+    Starlite Controller. This is the basic 'view' component of Starlite.
+    """
+
+    __slots__ = (
+        "after_request",
+        "after_response",
+        "before_request",
+        "dependencies",
+        "exception_handlers",
+        "guards",
+        "middleware",
+        "owner",
+        "parameters",
+        "path",
+        "response_class",
+        "response_headers",
+        "tags",
+    )
+
+    after_request: Optional["AfterRequestHandler"]
+    after_response: Optional["AfterResponseHandler"]
+    before_request: Optional["BeforeRequestHandler"]
+    dependencies: Optional[Dict[str, "Provide"]]
+    exception_handlers: Optional[Dict[Union[int, "Type[Exception]"], "ExceptionHandler"]]
+    guards: Optional[List["Guard"]]
+    middleware: Optional[List[Union["Middleware", "Type[BaseHTTPMiddleware]", "Type[MiddlewareProtocol]"]]]
+    owner: "Router"
+    parameters: Optional[Dict[str, "FieldInfo"]]
+    path: str
+    response_class: Optional["Type[Response]"]
+    response_headers: Optional[Dict[str, "ResponseHeader"]]
+    tags: Optional[List[str]]
+
+    def __init__(self, owner: "Router"):
+        for key in self.__slots__:
+            if not hasattr(self, key):
+                setattr(self, key, None)
+
+        self.path = normalize_path(self.path or "/")
+        self.owner = owner
+
+    def get_route_handlers(self) -> List[BaseRouteHandler]:
+        """
+        Returns a list of route handlers defined on the controller
+        """
+        route_handlers: List[BaseRouteHandler] = []
+        route_handler_fields = [
+            f_name
+            for f_name in dir(self)
+            if f_name not in dir(Controller) and isinstance(getattr(self, f_name), BaseRouteHandler)
+        ]
+        for f_name in route_handler_fields:
+            source_route_handler = cast(BaseRouteHandler, getattr(self, f_name))
+            route_handler = copy(source_route_handler)
+            route_handler.owner = self
+            route_handlers.append(route_handler)
+        return route_handlers
---
+++ packages/starlite/starlite-1.6.1.tar.gz/starlite-1.6.1/starlite/exceptions.py
@@ -0,0 +1,84 @@
+from http import HTTPStatus
+from typing import Any, Dict, List, Optional, Union
+
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from starlette.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_401_UNAUTHORIZED,
+    HTTP_403_FORBIDDEN,
+    HTTP_404_NOT_FOUND,
+    HTTP_405_METHOD_NOT_ALLOWED,
+    HTTP_500_INTERNAL_SERVER_ERROR,
+    HTTP_503_SERVICE_UNAVAILABLE,
+)
+
+
+class StarLiteException(Exception):
+    def __init__(self, detail: str = ""):
+        self.detail = detail
+        super().__init__()
+
+    def __repr__(self) -> str:
+        if self.detail:
+            return f"{self.__class__.__name__} - {self.detail}"
+        return self.__class__.__name__
+
+
+class MissingDependencyException(StarLiteException, ImportError):
+    pass
+
+
+class HTTPException(StarLiteException, StarletteHTTPException):
+    status_code = HTTP_500_INTERNAL_SERVER_ERROR
+    extra: Optional[Union[Dict[str, Any], List[Any]]] = None
+
+    def __init__(  # pylint: disable=super-init-not-called
+        self,
+        detail: Optional[str] = None,
+        status_code: Optional[int] = None,
+        extra: Optional[Union[Dict[str, Any], List[Any]]] = None,
+    ):
+        if status_code:
+            self.status_code = status_code
+        self.detail = detail or HTTPStatus(self.status_code).phrase
+        self.extra = extra
+
+    def __repr__(self) -> str:
+        return f"{self.status_code} - {self.__class__.__name__} - {self.detail}"
+
+
+class ImproperlyConfiguredException(HTTPException, ValueError):
+    pass
+
+
+class ValidationException(HTTPException, ValueError):
+    status_code = HTTP_400_BAD_REQUEST
+
+
+class NotAuthorizedException(HTTPException):
+    status_code = HTTP_401_UNAUTHORIZED
+
+
+class PermissionDeniedException(HTTPException):
+    status_code = HTTP_403_FORBIDDEN
+
+
+class NotFoundException(HTTPException, ValueError):
+    status_code = HTTP_404_NOT_FOUND
+
+
+class MethodNotAllowedException(HTTPException):
+    status_code = HTTP_405_METHOD_NOT_ALLOWED
+
+
+class InternalServerException(HTTPException):
+    status_code = HTTP_500_INTERNAL_SERVER_ERROR
+
+
+class ServiceUnavailableException(HTTPException):
+    status_code = HTTP_503_SERVICE_UNAVAILABLE
+
+
+class TemplateNotFound(InternalServerException):
+    def __init__(self, template_name: str):
+        super().__init__(detail=f"Template {template_name} not found.")
