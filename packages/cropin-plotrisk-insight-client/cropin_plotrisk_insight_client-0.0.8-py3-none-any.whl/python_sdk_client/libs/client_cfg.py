dde987eb9715e387809c151eac39e68d03314b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:59:04 +0000

    Add package cropin-plotrisk-insight-client/0.0.8/cropin_plotrisk_insight_client-0.0.8-py3-none-any.whl

---
+++ packages/cropin-plotrisk-insight-client/cropin_plotrisk_insight_client-0.0.8-py3-none-any.whl/python_sdk_client/libs/client_cfg.py
@@ -0,0 +1,8 @@
+class InsightServiceCfg:
+    PROD_BASE_URL = "https://t608jbcx83.execute-api.ap-southeast-1.amazonaws.com/dev/api/v1"
+    QA_BASE_URL = "https://rn0t0s00e2.execute-api.ap-southeast-1.amazonaws.com/qa/api/v1"
+    X_API_KEY = "X-Api-Key"
+    BATCH_SIZE = 300
+    MAX_SIZE = 5000
+
+
