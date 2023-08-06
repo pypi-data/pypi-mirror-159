d1b1995ba122e50ee700d63ba3565e5702bf0817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:00 +0000

    Add package easytradetesting/0.1.1/easytradetesting-0.1.1.tar.gz

---
+++ packages/easytradetesting/easytradetesting-0.1.1.tar.gz/easytradetesting-0.1.1/easytradetesting/BackTestInstance.py
@@ -0,0 +1,24 @@
+class BackTestInstance:
+
+    def __init__(self):
+        self.context = None
+
+    # 实例初始化
+    def init(self):
+        pass
+
+    # 实例销毁
+    def destroy(self):
+        pass
+
+    # 执行
+    def execute(self):
+        pass
+
+    # 执行止损逻辑
+    def executeStopLoss(self):
+        pass
+
+    # 执行止盈逻辑
+    def executeStopProfit(self):
+        pass
