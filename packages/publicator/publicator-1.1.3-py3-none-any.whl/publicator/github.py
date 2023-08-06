3b87d61190e4f686b9fc946c80dfc5f868c424b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:52 +0000

    Add package publicator/1.1.3/publicator-1.1.3-py3-none-any.whl

---
+++ packages/publicator/publicator-1.1.3-py3-none-any.whl/publicator/github.py
@@ -0,0 +1,17 @@
+from urllib.parse import urlencode
+from publicator.git import Repo
+from semmy import Semver
+
+
+class ReleaseException(Exception):
+    pass
+
+
+def new_release_url(repo: Repo, tag: Semver, title: str = "", body: str = "") -> str:
+    if not repo.is_github:
+        raise ReleaseException("Current repository is not hosted on github.com")
+
+    base_url = f"https://{repo.server}/{repo.owner}/{repo.name}/releases/new?"
+    query = {"tag": tag, "title": title, "body": body, "prerelease": int(tag.is_pre_release())}
+
+    return base_url + urlencode(query)
