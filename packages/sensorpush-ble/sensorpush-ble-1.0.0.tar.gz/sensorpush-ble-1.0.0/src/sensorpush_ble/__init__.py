27fac09ea2d4392e7898336e1a48e40051e22f7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:34 +0000

    Add package sensorpush-ble/1.0.0/sensorpush-ble-1.0.0.tar.gz

---
+++ packages/sensorpush-ble/sensorpush-ble-1.0.0.tar.gz/sensorpush-ble-1.0.0/src/sensorpush_ble/__init__.py
@@ -0,0 +1,8 @@
+"""Parser for SensorPush BLE advertisements."""
+from __future__ import annotations
+
+from .parser import SensorPushBluetoothDeviceData
+
+__version__ = "1.0.0"
+
+__all__ = ["SensorPushBluetoothDeviceData"]
