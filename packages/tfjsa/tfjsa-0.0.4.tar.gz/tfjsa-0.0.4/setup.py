edf900c3e94f4e0eea678faff6e87ddc1728ce16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:07 +0000

    Add package tfjsa/0.0.4/tfjsa-0.0.4.tar.gz

---
+++ packages/tfjsa/tfjsa-0.0.4.tar.gz/tfjsa-0.0.4/setup.py
@@ -0,0 +1,42 @@
+# from distutils.core import setup
+from setuptools import find_packages, setup
+import pathlib
+
+setup(
+    # Application name:
+    name="tfjsa",
+    
+    # Version number (initial):
+    version="0.0.4",
+    
+    # Application author details:
+    author="Zhang",
+    author_email="oo@zju.edu.cn",
+    
+    # Packages
+    packages=["tfjsa"],
+
+    package_dir={'templates': 'tfjsa/templates', 'static': 'tfjsa/static'},
+
+    # Include additional files into the package
+    include_package_data=True,
+    
+    # Details
+    url="http://pypi.python.org/pypi/tfjsa/",
+    
+    #
+    license="LICENSE",
+    description="tfjs(Tensorflow js)-based Flask web apps.",
+    
+    # long_description="""# Markdown supported!\n\n* Cheer\n* Celebrate\n""",
+    long_description_content_type='text/markdown',
+    long_description= open('README.md').read(), # (pathlib.Path(__file__).parent / "README.md").read_text(),
+    
+    # Dependent packages (distributions)
+    install_requires=[
+        "flask", # "tkwebview2", "tkinter"
+    ],
+)
+
+# To Build and Publish (for developer only), 
+# Run: python setup.py sdist bdist_wheel; twine upload dist/*
