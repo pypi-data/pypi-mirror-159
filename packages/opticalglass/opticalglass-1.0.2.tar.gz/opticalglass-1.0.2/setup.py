09353b03f72fe29a9dc1ec1b3ad8de903ce7d397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:29 +0000

    Add package opticalglass/1.0.2/opticalglass-1.0.2.tar.gz

---
+++ packages/opticalglass/opticalglass-1.0.2.tar.gz/opticalglass-1.0.2/setup.py
@@ -0,0 +1,46 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="opticalglass",
+    version="1.0.2",
+    author="Michael J Hayford",
+    author_email="mjhoptics@gmail.com",
+    description="Tools for reading commercial optical glass catalogs",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    license="BSD-3-Clause",
+    url="https://github.com/mjhoptics/opticalglass",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Physics",
+    ],
+    keywords=['glass', 'optical glass', 'refractive index', 'optics',
+              'glass catalog'],
+    install_requires=[
+        "numpy>=1.21.5",
+        "scipy>=1.7.3",
+        "matplotlib>=3.5.1",
+        "pandas>=1.3.5",
+        "xlrd>=2.0.1",
+        "openpyxl>=3.0.9",
+        "requests>=2.27.1",
+        "pyyaml>=6.0",
+        "pyqt5<5.16"
+        ],
+    package_data={
+        '': ['data/*.xlsx', 'data/*.xls', 'data/*.txt']
+        },
+    entry_points={
+        'gui_scripts': [
+            'glassmap = opticalglass.glassmapviewer:main',
+        ],
+    },
+)
