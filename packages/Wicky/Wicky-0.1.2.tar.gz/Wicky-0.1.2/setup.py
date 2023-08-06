2ccefc941de9e0320b672310fb9bdff6919853bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:36 +0000

    Add package Wicky/0.1.2/Wicky-0.1.2.tar.gz

---
+++ packages/Wicky/Wicky-0.1.2.tar.gz/Wicky-0.1.2/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+#region Modules
+
+from Cython.Build import cythonize
+from pathlib import Path
+from setuptools import setup, Extension
+
+#endregion
+
+#region Variables
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+#endregion
+
+setup(
+    name = "Wicky",
+    version = '0.1.2',
+    description = "A libre tool for Wicked Engine development",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author = "Maeve Garside",
+    author_email = "60114762+MolassesLover@users.noreply.github.com",
+    url = 'https://github.com/MolassesLover/Wicky',
+    license = 'MIT',
+    packages = ['wicky'],
+    ext_modules=[Extension('Source/Wicky.py', ['Wicky.c'])],
+    Install_requires=[
+        'colorama',
+        'pyyaml'
+    ]
+)
