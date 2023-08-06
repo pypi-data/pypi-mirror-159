e4102dd986fba56cd16938d8577ea8d72c2d74ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:03 +0000

    Add package EtherChainPy/1.0.76/EtherChainPy-1.0.76.tar.gz

---
+++ packages/EtherChainPy/EtherChainPy-1.0.76.tar.gz/EtherChainPy-1.0.76/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="EtherChainPy",
+    version="1.0.76",
+    author="Emmanuel Adigun",
+    author_email="emmanuel@zignal.net",
+    description="A python interface to the ethereum blockchain explorer at https://www.etherscan.io",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ZignalNET/EtherPY",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
