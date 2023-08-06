dbaf4a3304c92158cd93993461264e013f99aa00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:08 +0000

    Add package EtherChainPy/1.0.78/EtherChainPy-1.0.78.tar.gz

---
+++ packages/EtherChainPy/EtherChainPy-1.0.78.tar.gz/EtherChainPy-1.0.78/etherchainpy/__init__.py
@@ -0,0 +1,9 @@
+"""
+    Filename	:	__init__.py
+    Description :   Part of EtherChainPY
+    By          :   Emmanuel Adigun (emmanuel@zignal.net)
+    Date        :   10 Mar 2022
+"""
+
+from .etherchainpy   import EtherChainPy
+
