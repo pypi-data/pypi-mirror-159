4029e4c2f20a3686bc61cba0b79df6f212b0b991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:27 +0000

    Add package multibot/1.6.7/multibot-1.6.7-py3-none-any.whl

---
+++ packages/multibot/multibot-1.6.7-py3-none-any.whl/multibot/models/event_component.py
@@ -0,0 +1,17 @@
+from __future__ import annotations  # todo0 remove in 3.11
+
+__all__ = ['EventComponent']
+
+from typing import Any, TypeVar
+
+from flanautils import DCMongoBase, FlanaBase
+
+T = TypeVar('T', bound='EventComponent')
+
+
+class EventComponent(DCMongoBase, FlanaBase):
+    def _json_repr(self) -> Any:
+        return {k: v for k, v in super()._json_repr().items() if k not in ('original_object', 'original_event')}
+
+    def _mongo_repr(self) -> Any:
+        return {k: v for k, v in super()._mongo_repr().items() if k not in ('original_object', 'original_event')}
