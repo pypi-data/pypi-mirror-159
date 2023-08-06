aa99b45960db6bed1f2bcfb2f39a69349a6a18e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:37 +0000

    Add package ezlambda/0.1.1/ezlambda-0.1.1.tar.gz

---
+++ packages/ezlambda/ezlambda-0.1.1.tar.gz/ezlambda-0.1.1/src/ezlambda/__init__.py
@@ -0,0 +1,18 @@
+from .variable import Variable, VariableOperationMarker
+
+__version__ = "0.1.1"
+
+Var = Variable
+VarMarker = VariableOperationMarker
+
+_ = Var()
+x = Var()
+y = Var()
+z = Var()
+w = Var()
+t = Var()
+a = Var()
+b = Var()
+c = Var()
+
+_m = VarMarker()
