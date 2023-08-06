00324bd12da6072d66a2cc8c4bad2b280f3e914c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:05 +0000

    Add package sensor-state-data/1.5.0/sensor-state-data-1.5.0.tar.gz

---
+++ packages/sensor-state-data/sensor-state-data-1.5.0.tar.gz/sensor-state-data-1.5.0/src/sensor_state_data/__init__.py
@@ -0,0 +1,19 @@
+__version__ = "1.5.0"
+
+from .data import SensorData, SensorUpdate
+from .description import SensorDescription
+from .device import DeviceKey
+from .device_class import DeviceClass
+from .library import SensorLibrary
+from .value import SensorValue
+
+__all__ = [
+    "DeviceClass",
+    "DeviceKey",
+    "SensorData",
+    "SensorDescription",
+    "SensorDescription",
+    "SensorLibrary",
+    "SensorUpdate",
+    "SensorValue",
+]
