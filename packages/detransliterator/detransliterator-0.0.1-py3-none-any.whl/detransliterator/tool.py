a8108169e284439136842b57430401620dba11e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:58 +0000

    Add package detransliterator/0.0.1/detransliterator-0.0.1-py3-none-any.whl

---
+++ packages/detransliterator/detransliterator-0.0.1-py3-none-any.whl/detransliterator/tool.py
@@ -0,0 +1,41 @@
+import sys
+from io import StringIO
+import argparse
+# import csv
+from .detransliterator import Detransliterator
+
+
+def main_parse_command_line_args():
+    parser = argparse.ArgumentParser("latin to nqo detransliterator")
+    parser.add_argument("--model-name", required=False, default="latin2nqo_001.35")
+    parser.add_argument("--input-text-file")
+    parser.add_argument("--input-csv-file")
+    parser.add_argument("--input-csv-file-separator")
+    parser.add_argument("--beam-size", type=int, required=False, default=5)
+    return parser.parse_args()
+
+
+def main_detransliterate(args):
+    std_out_bkp = sys.stdout
+    sys.stdout = StringIO()
+
+    if not args.model_name:
+        raise ValueError("--model-name required for detransliteration")
+
+    detransliterator = Detransliterator(args.model_name)
+
+    sys.stdout = std_out_bkp
+    for latin_line in sys.stdin:
+        nqo_line = detransliterator.detransliterate(
+            latin_line,
+            beam_size=args.beam_size
+        )
+        print(nqo_line)
+
+
+if __name__ == '__main__':
+    args = main_parse_command_line_args()
+    if True:  # args.option == 'command-name':
+        main_detransliterate(args)
+    else:
+        raise ValueError(f"Unknown command: {args.option}")
