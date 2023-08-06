b1651bcea6f0cfab6105ce7ecbf08407a0c36586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:30:48 +0000

    Add package django-treenode/0.18.2/django_treenode-0.18.2-py3-none-any.whl

---
+++ packages/django-treenode/django_treenode-0.18.2-py3-none-any.whl/treenode/utils.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+PKS_SEPARATOR = ","
+
+
+def join_pks(l):
+    if not l:
+        return ""
+    s = PKS_SEPARATOR.join([str(v) for v in l])
+    return s
+
+
+def split_pks(s):
+    if not s:
+        return []
+    l = [v for v in s.split(PKS_SEPARATOR) if v]
+    return l
