2c71aefe0bad23dc3c19e51be9b853bc860fc9a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:29:59 +0000

    Add package fundamental-distributions-gaussian-and-binomial/1.2/fundamental_distributions_gaussian_and_binomial-1.2.tar.gz

---
+++ packages/fundamental-distributions-gaussian-and-binomial/fundamental_distributions_gaussian_and_binomial-1.2.tar.gz/fundamental_distributions_gaussian_and_binomial-1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='fundamental_distributions_gaussian_and_binomial',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['fundamental_distributions'],
+      install_requires=[
+          'matplotlib',
+      ],
+      author='Mohammad Fares',
+      author_email='mohres34@gmail.com',
+      zip_safe=False)
