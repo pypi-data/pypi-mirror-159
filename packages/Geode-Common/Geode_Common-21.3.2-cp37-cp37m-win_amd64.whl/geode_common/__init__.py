7e6d50e32190a89720b571b35c014b4d52c5201a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:58 +0000

    Add package Geode-Common/21.3.2/Geode_Common-21.3.2-cp37-cp37m-win_amd64.whl

---
+++ packages/Geode-Common/Geode_Common-21.3.2-cp37-cp37m-win_amd64.whl/geode_common/__init__.py
@@ -0,0 +1,15 @@
+#
+# Copyright (c) 2019 - 2022 Geode-solutions. All rights reserved.
+#
+
+from .core import *
+from .geometry import *
+from .helpers import *
+from .interpolation import *
+from .mesh_repair import *
+from .modifier_edged_curve import *
+from .modifier_surface import *
+from .modifier_solid import *
+from .modifier_model import *
+from .cutter_surface import *
+from .cutter_solid import *
