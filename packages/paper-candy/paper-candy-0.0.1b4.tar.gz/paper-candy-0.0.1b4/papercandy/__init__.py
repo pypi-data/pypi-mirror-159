dd4335fe6108e385aea9f493692dd2eecce88d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:41 +0000

    Add package paper-candy/0.0.1b4/paper-candy-0.0.1b4.tar.gz

---
+++ packages/paper-candy/paper-candy-0.0.1b4.tar.gz/paper-candy-0.0.1b4/papercandy/__init__.py
@@ -0,0 +1,8 @@
+from papercandy.train import *
+from papercandy.config import *
+from papercandy.drawing import *
+from papercandy.network import *
+from papercandy.dataloader import *
+
+
+CONFIG = new_config("conf.pcc")  # default path
---
+++ packages/paper-candy/paper-candy-0.0.1b4.tar.gz/paper-candy-0.0.1b4/papercandy/config.py
@@ -0,0 +1,5 @@
+from universal import config as _config
+
+
+Config = _config.Config
+new_config = _config.new_config
