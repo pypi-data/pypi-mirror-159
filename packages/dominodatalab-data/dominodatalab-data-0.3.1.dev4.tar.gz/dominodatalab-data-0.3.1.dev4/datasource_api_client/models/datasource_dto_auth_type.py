85b48681ba5dcb4d4c607ed44d57ad571feca244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:13 +0000

    Add package dominodatalab-data/0.3.1.dev4/dominodatalab-data-0.3.1.dev4.tar.gz

---
+++ packages/dominodatalab-data/dominodatalab-data-0.3.1.dev4.tar.gz/dominodatalab-data-0.3.1.dev4/datasource_api_client/models/datasource_dto_auth_type.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class DatasourceDtoAuthType(str, Enum):
+    AZUREBASIC = "AzureBasic"
+    BASIC = "Basic"
+    AWSIAMBASIC = "AWSIAMBasic"
+    AWSIAMROLE = "AWSIAMRole"
+    AWSIAMROLEWITHUSERNAME = "AWSIAMRoleWithUsername"
+    GCPBASIC = "GCPBasic"
+    OAUTH = "OAuth"
+
+    def __str__(self) -> str:
+        return str(self.value)
