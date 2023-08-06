fd63b96ca77a5a505eb0650c5984da91cf894c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:07 +0000

    Add package spotify-to-musi/1.0.0/spotify_to_musi-1.0.0-py3-none-any.whl

---
+++ packages/spotify-to-musi/spotify_to_musi-1.0.0-py3-none-any.whl/spotify_to_musi/typings/musi.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+
+from typing import TypedDict, Literal
+
+
+class MusiItem(TypedDict):
+    cd: int  # int(time.time())
+    pos: int  # index
+    video_id: str
+
+
+class MusiPlaylist(TypedDict):
+    ot: Literal["custom"]
+    items: list[MusiItem]
+    name: str
+    type: Literal["user"]
+    date: int  # int(time.time())
+    ciu: str  # playlist cover image (url)
+
+
+class MusiLibrary(TypedDict):
+    ot: Literal["custom"]
+    items: list[MusiItem]
+    name: Literal["My Library"]
+    date: int  # int(time.time())
+
+
+class MusiVideo(TypedDict):
+    created_date: float  # time.time()
+    video_duration: int  # in seconds
+    video_name: str
+    video_creator: str  # channel name
+    video_id: str
+
+
+class MusiBackupResponse(TypedDict):
+    code: str
+    diff: bool
+    success: str  # message
