b6bb8f8f0fbf08e2553ebf8330a7ca81f21090b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:40 +0000

    Add package opengui/0.8.4/opengui-0.8.4.tar.gz

---
+++ packages/opengui/opengui-0.8.4.tar.gz/opengui-0.8.4/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+setup(
+    name="opengui",
+    version="0.8.4",
+    py_modules = ['opengui'],
+    url="https://github.com/gaf3/opengui",
+    author="Gaffer Fitch",
+    author_email="opengui@gaf3.com",
+    license_files=('LICENSE.txt',),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ]
+)
