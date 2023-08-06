03e04d6c1ef2522901079d3d6fb12856c3f08dd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:53:57 +0000

    Add package pyrecog/1.0.4/pyrecog-1.0.4-py3-none-any.whl

---
+++ packages/pyrecog/pyrecog-1.0.4-py3-none-any.whl/pyrecog/main.py
@@ -0,0 +1,19 @@
+import pyautogui
+import os
+from time import sleep
+
+def find():
+	while True:
+		cwd = os.getcwd()
+		for root, dirs, files in os.walk(cwd):
+			for file in files:
+				if file.endswith(".PNG" or ".png"):
+					image = file
+		if pyautogui.locateOnScreen(image) != None:
+			found = True
+			print ("True")
+			sleep(0.5)
+		else:
+			found = False
+			print ("False")
+			sleep(0.5)
