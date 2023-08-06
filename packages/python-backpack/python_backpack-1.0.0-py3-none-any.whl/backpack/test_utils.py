21edefc831f14f52c095877116e54155d49dd2b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:06 +0000

    Add package python-backpack/1.0.0/python_backpack-1.0.0-py3-none-any.whl

---
+++ packages/python-backpack/python_backpack-1.0.0-py3-none-any.whl/backpack/test_utils.py
@@ -0,0 +1,5 @@
+# ----------------------------------------------------------------------------------------
+# Python-Backpack - Test Utilities
+# Maximiliano Rocamora / maxirocamora@gmail.com
+# https://github.com/MaxRocamora/python-backpack
+# ----------------------------------------------------------------------------------------
