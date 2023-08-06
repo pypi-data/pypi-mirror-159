a2e92d98ffbcd715d192a2c3403d9636a0639313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:55 +0000

    Add package ms-to-datetime/0.0.1/ms-to-datetime-0.0.1.tar.gz

---
+++ packages/ms-to-datetime/ms-to-datetime-0.0.1.tar.gz/ms-to-datetime-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ms-to-datetime',
+    version='0.0.1',
+    description='Convert from ms to datetime',
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Felipe Viana',
+    author_email='',
+    license='MIT',
+    classifiers=["Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent"],
+    keywords='',
+    packages=find_packages(),
+    install_requires=['pytz','datetime']
+   
+)
