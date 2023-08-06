3a8eccc33db35953a2e993fad7db9fde7531a697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:45 +0000

    Add package asgihandler/0.2.17/asgihandler-0.2.17-py3-none-any.whl

---
+++ packages/asgihandler/asgihandler-0.2.17-py3-none-any.whl/asgihandler/userCheck.py
@@ -0,0 +1,14 @@
+import requests, json
+
+class userCheck:
+    def get_auth_check(host, operator, token):
+        context = {
+            "host": host,
+            "operator": operator,
+            "token": token
+        }
+        print(context)
+        response = requests.post('http://192.168.1.3:8007/asgihandler/', json=context)
+        res_feedback = json.loads(response.text.replace("'", '"'))
+        print(res_feedback.get('detail'))
+        print(host, operator, token)
