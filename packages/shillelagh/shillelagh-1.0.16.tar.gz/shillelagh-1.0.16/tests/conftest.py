73170cc71ecd2d66cf3863d8eeaab85f219c8597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:07 +0000

    Add package shillelagh/1.0.16/shillelagh-1.0.16.tar.gz

---
+++ packages/shillelagh/shillelagh-1.0.16.tar.gz/shillelagh-1.0.16/tests/conftest.py
@@ -0,0 +1,29 @@
+"""
+Fixtures for Shillelagh.
+"""
+import json
+import logging
+import os
+from typing import Dict
+
+import pytest
+
+_logger = logging.getLogger(__name__)
+
+
+@pytest.fixture
+def adapter_kwargs() -> Dict[str, str]:
+    """
+    Load adapter configuration.
+
+    This fixture looks for the environment variable `SHILLELAGH_ADAPTER_KWARGS`.
+    The configuration should be encoded as JSON.
+    """
+    kwargs = {}
+    if "SHILLELAGH_ADAPTER_KWARGS" in os.environ:
+        try:
+            kwargs = json.loads(os.environ["SHILLELAGH_ADAPTER_KWARGS"])
+        except Exception as ex:  # pylint: disable=broad-except
+            _logger.warning('Unable to load "SHILLELAGH_ADAPTER_KWARGS": %s', ex)
+
+    return kwargs
---
+++ packages/shillelagh/shillelagh-1.0.16.tar.gz/shillelagh-1.0.16/tests/test_console.py
@@ -0,0 +1,110 @@
+# pylint: disable=invalid-name
+"""
+Tests for shillelagh.console.
+"""
+import os.path
+from io import StringIO
+
+import yaml
+from pyfakefs.fake_filesystem import FakeFilesystem
+from pytest_mock import MockerFixture
+
+from shillelagh import console
+
+
+def test_main(mocker: MockerFixture) -> None:
+    """
+    Test ``main``.
+    """
+    stdout = mocker.patch("sys.stdout", new_callable=StringIO)
+    PromptSession = mocker.patch("shillelagh.console.PromptSession")
+
+    PromptSession.return_value.prompt.side_effect = ["SELECT 1", "", EOFError()]
+    console.main()
+    result = stdout.getvalue()
+    assert result == "  1\n---\n  1\nGoodBye!\n"
+
+
+def test_exception(mocker: MockerFixture) -> None:
+    """
+    Test that exceptions are captured and printed.
+    """
+    stdout = mocker.patch("sys.stdout", new_callable=StringIO)
+    PromptSession = mocker.patch("shillelagh.console.PromptSession")
+
+    PromptSession.return_value.prompt.side_effect = ["SSELECT 1", EOFError()]
+    console.main()
+    result = stdout.getvalue()
+    assert result == 'SQLError: near "SSELECT": syntax error\nGoodBye!\n'
+
+
+def test_ctrl_c(mocker: MockerFixture) -> None:
+    """
+    Test that ``CTRL-C`` does not exit the REPL.
+    """
+    stdout = mocker.patch("sys.stdout", new_callable=StringIO)
+    PromptSession = mocker.patch("shillelagh.console.PromptSession")
+
+    PromptSession.return_value.prompt.side_effect = [
+        KeyboardInterrupt(),
+        "SELECT 1",
+        EOFError(),
+    ]
+    console.main()
+    result = stdout.getvalue()
+    assert result == "  1\n---\n  1\nGoodBye!\n"
+
+
+def test_configuration(mocker: MockerFixture, fs: FakeFilesystem) -> None:
+    """
+    Test loading the configuration file.
+    """
+    config = os.path.expanduser("~/.config/shillelagh/shillelagh.yaml")
+    fs.create_file(config)
+    with open(config, "w", encoding="utf-8") as fp:
+        yaml.dump({"foo": {"bar": "baz"}}, fp)
+
+    connect = mocker.patch("shillelagh.console.connect")
+    mocker.patch("sys.stdout", new_callable=StringIO)
+    PromptSession = mocker.patch("shillelagh.console.PromptSession")
+
+    PromptSession.return_value.prompt.side_effect = [EOFError()]
+    console.main()
+
+    connect.assert_called_with(":memory:", adapter_kwargs={"foo": {"bar": "baz"}})
+
+
+def test_no_configuration(mocker: MockerFixture, fs: FakeFilesystem) -> None:
+    """
+    Test no configuration file found.
+    """
+    config_dir = os.path.expanduser("~/.config/shillelagh/")
+    fs.create_dir(config_dir)
+
+    connect = mocker.patch("shillelagh.console.connect")
+    mocker.patch("sys.stdout", new_callable=StringIO)
+    PromptSession = mocker.patch("shillelagh.console.PromptSession")
+
+    PromptSession.return_value.prompt.side_effect = [EOFError()]
+    console.main()
+
+    connect.assert_called_with(":memory:", adapter_kwargs={})
+
+
+def test_configuration_invalid(mocker: MockerFixture, fs: FakeFilesystem) -> None:
+    """
+    Test that an exception is raised if the configuration is invalid.
+    """
+    config = os.path.expanduser("~/.config/shillelagh/shillelagh.yaml")
+    fs.create_file(config)
+    with open(config, "w", encoding="utf-8") as fp:
+        fp.write("foo: *")
+
+    _logger = mocker.patch("shillelagh.console._logger")
+    mocker.patch("sys.stdout", new_callable=StringIO)
+    PromptSession = mocker.patch("shillelagh.console.PromptSession")
+
+    PromptSession.return_value.prompt.side_effect = [EOFError()]
+    console.main()
+
+    _logger.exception.assert_called_with("Unable to load configuration file")
