f77e1fcdaa802c116f38d4f2d3d766b3538811d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:56 +0000

    Add package chirpstack-api-beta/4.7/chirpstack-api_beta-4.7.tar.gz

---
+++ packages/chirpstack-api-beta/chirpstack-api_beta-4.7.tar.gz/chirpstack-api_beta-4.7/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+REQUIREMENTS = [
+    'grpcio',
+    'google-api-core'
+]
+
+
+CLASSIFIERS = [
+    'Intended Audience :: Developers',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3.7',
+    'Topic :: Communications',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries',
+]
+
+setup(
+    name='chirpstack-api_beta',
+    py_modules=['chirpstack-api'],
+    version = "4.7",
+    url='https://github.com/chirpstack/chirpstack/tree/master/api/python/src',
+    author='Orne Brocaar',
+    author_email='info@brocaar.com',
+    license='MIT',
+    description='Chirpstack Python API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+)
