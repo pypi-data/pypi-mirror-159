5f59afc15f904543cb5b8ce1ee04a62b211d3c87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:59:06 +0000

    Add package m4ld/0.1/m4ld-0.1.tar.gz

---
+++ packages/m4ld/m4ld-0.1.tar.gz/m4ld-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='m4ld',
+    version='0.1',
+    license='MIT',
+    author="Ali Burak Çetin",
+    author_email='aliburakcetin@protonmail.com',
+    packages=find_packages('m4ld'),
+    package_dir={'': 'm4ld'},
+    url='https://github.com/ali019283/m4ld',
+    keywords='m4ld',
+    install_requires=[],
+)
