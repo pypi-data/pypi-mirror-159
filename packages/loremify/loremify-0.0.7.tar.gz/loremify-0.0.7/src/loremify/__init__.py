f694ab4be3f233f23ecd2106a730dc72ee4a1d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:02 +0000

    Add package loremify/0.0.7/loremify-0.0.7.tar.gz

---
+++ packages/loremify/loremify-0.0.7.tar.gz/loremify-0.0.7/src/loremify/__init__.py
@@ -0,0 +1,6 @@
+"""Loremify provides a simple implementation of lorem ipsum text."""
+
+from ._getword import AllWords
+from ._getword import WordFactory
+from ._lorem import paragraph
+from ._lorem import sentence
