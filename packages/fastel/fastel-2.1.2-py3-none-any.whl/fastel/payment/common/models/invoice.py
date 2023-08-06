50916bd9086b1598bf324b92167da9371326302d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:26 +0000

    Add package fastel/2.1.2/fastel-2.1.2-py3-none-any.whl

---
+++ packages/fastel/fastel-2.1.2-py3-none-any.whl/fastel/payment/common/models/invoice.py
@@ -0,0 +1,17 @@
+from enum import Enum
+from typing import Any, Dict
+
+from pydantic import BaseModel
+
+
+class InvoiceStatus(str, Enum):
+    success = "success"
+    failure = "failure"
+    pending = "pending"
+
+
+class InvoiceIssueResp(BaseModel):
+    status: InvoiceStatus
+    invoice_number: str
+    invoice_message: str
+    invoice_detail: Dict[str, Any]
