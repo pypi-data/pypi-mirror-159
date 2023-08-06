82089349d3b841aadcdd0079f4443369b83861ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:33 +0000

    Add package dlt-personio-source/0.0.43/dlt_personio_source-0.0.43-py3-none-any.whl

---
+++ packages/dlt-personio-source/dlt_personio_source-0.0.43-py3-none-any.whl/dlt_personio_source/helpers.py
@@ -0,0 +1,33 @@
+from dlt.pipeline import Pipeline
+from dlt.pipeline.typing import GCPPipelineCredentials
+import os
+
+def save_schema(p, fn):
+    schema_yaml = p.get_default_schema().as_yaml(remove_defaults=True)
+    f = open(f'{fn}.yml', "w")
+    f.write(schema_yaml)
+    f.close()
+
+def load(data=[],
+            table_name='mytbl',
+            schema_name='mypipe',
+            credentials= {},
+            destination_type='bigquery',
+            schema_infile=None,
+            schema_outfile=None,
+            dry_run=False):
+
+    cred = GCPPipelineCredentials.from_services_dict(credentials, dataset_prefix=schema_name[0])
+
+    p = Pipeline(schema_name[1:])
+    if schema_infile:
+        current_path = os.path.dirname(os.path.abspath(__file__))
+        file_path = os.path.join(current_path, f"{schema_infile}.yml")
+        schema_infile = Pipeline.load_schema_from_file(file_path)
+    p.create_pipeline(cred, schema=schema_infile)
+    p.extract(data, table_name=table_name)
+    p.unpack()
+    if schema_outfile:
+        save_schema(p, schema_outfile)
+    if not dry_run:
+        p.load()
