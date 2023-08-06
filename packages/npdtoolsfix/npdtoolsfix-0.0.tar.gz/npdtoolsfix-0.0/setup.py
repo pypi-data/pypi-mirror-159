486ef0dac7edbcfa82c2864aa399fe78f6e86c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:28 +0000

    Add package npdtoolsfix/0.0/npdtoolsfix-0.0.tar.gz

---
+++ packages/npdtoolsfix/npdtoolsfix-0.0.tar.gz/npdtoolsfix-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='npdtoolsfix',
+      version='0.0',
+      description='Удобный инструмент для взаимодействия с НПД (небольшие изменения оригинальной библиотеки npdtools, чтобы она работала с физ лицами без багов)',
+      packages=['npdtoolsfix'],
+      author_email='xmaxer3@yandex.ru',
+      zip_safe=False)
