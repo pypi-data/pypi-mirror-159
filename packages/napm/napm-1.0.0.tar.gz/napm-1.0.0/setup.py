f8629ddbb36b6aafc970912ddd1cc5f19fb8d026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:22 +0000

    Add package napm/1.0.0/napm-1.0.0.tar.gz

---
+++ packages/napm/napm-1.0.0.tar.gz/napm-1.0.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['napm']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['gdown>=4.4.0,<5.0.0',
+ 'loguru>=0.6.0,<0.7.0',
+ 'omegaconf>=2.1.1,<3.0.0',
+ 'requests>=2.27.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'napm',
+    'version': '1.0.0',
+    'description': 'Not A Package Manager.',
+    'long_description': None,
+    'author': 'David Marx',
+    'author_email': 'david.marx84@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
