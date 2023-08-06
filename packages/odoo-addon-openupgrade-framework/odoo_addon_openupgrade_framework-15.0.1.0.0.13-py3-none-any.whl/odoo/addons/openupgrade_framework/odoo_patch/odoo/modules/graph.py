4b9481a793c144b72308752980b8cd5504b5952c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:46 +0000

    Add package odoo-addon-openupgrade-framework/15.0.1.0.0.13/odoo_addon_openupgrade_framework-15.0.1.0.0.13-py3-none-any.whl

---
+++ packages/odoo-addon-openupgrade-framework/odoo_addon_openupgrade_framework-15.0.1.0.0.13-py3-none-any.whl/odoo/addons/openupgrade_framework/odoo_patch/odoo/modules/graph.py
@@ -0,0 +1,25 @@
+# Copyright Odoo Community Association (OCA)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import os
+
+import odoo
+from odoo.modules.graph import Graph
+
+
+def update_from_db(self, cr):
+    """Prevent reloading of demo data from the new version on major upgrade"""
+    Graph.update_from_db._original_method(self, cr)
+    if os.environ.get("OPENUPGRADE_USE_DEMO", "") == "yes":
+        return
+    if (
+        "base" in self
+        and self["base"].dbdemo
+        and self["base"].installed_version < odoo.release.major_version
+    ):
+        cr.execute("UPDATE ir_module_module SET demo = false")
+        for package in self.values():
+            package.dbdemo = False
+
+
+update_from_db._original_method = Graph.update_from_db
+Graph.update_from_db = update_from_db
