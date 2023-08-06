39aab7d821ff73ed32ed6b09cf9124f30e47fe6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:59 +0000

    Add package enkanetwork.py/1.2.6/enkanetwork.py-1.2.6.tar.gz

---
+++ packages/enkanetwork.py/enkanetwork.py-1.2.6.tar.gz/enkanetwork.py-1.2.6/enkanetwork/__init__.py
@@ -0,0 +1,8 @@
+from .client import *
+from .exception import *
+from .model import *
+from .utils import *
+from .info import *
+
+__VERSION__ = VERSION
+__AUTHOR__ = AUTHOR
---
+++ packages/enkanetwork.py/enkanetwork.py-1.2.6.tar.gz/enkanetwork.py-1.2.6/enkanetwork/info.py
@@ -0,0 +1,2 @@
+VERSION = "1.2.6"
+AUTHOR = "M-307"
