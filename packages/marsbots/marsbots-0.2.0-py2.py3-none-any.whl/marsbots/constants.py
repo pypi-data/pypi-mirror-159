858f435a05cde2a01bafe1bf670d6dfc606ffd68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:51:18 +0000

    Add package marsbots/0.2.0/marsbots-0.2.0-py2.py3-none-any.whl

---
+++ packages/marsbots/marsbots-0.2.0-py2.py3-none-any.whl/marsbots/constants.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent.parent
+DOTENV_PATH = ROOT_DIR / ".env"
+LOG_DIR = ROOT_DIR / "logs"
+
+COMMAND_NOT_FOUND_MESSAGE = "Command not found."
+
+UNLIKELY_PREFIX = "fhkjdjksfjkdhkjfhksdjfsdjkfbwereuifbiudbyuifvyudsvyu"
+
+CHANNEL_NOT_FOUND_MESSAGE = "Could not find a text channel matching that name."
