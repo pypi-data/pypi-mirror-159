076027609ae5b5fb4fdb20fd5fd08b5ad3fdf58b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:01 +0000

    Add package thisisjustapackage/0.0.6/thisisjustapackage-0.0.6.tar.gz

---
+++ packages/thisisjustapackage/thisisjustapackage-0.0.6.tar.gz/thisisjustapackage-0.0.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='thisisjustapackage',
+    version='0.0.6',
+    description='this is just a package',
+    author='thisisjust',
+    author_email='this@is.just',
+    packages=['package'],
+    install_requires=['urllib3 == 1.26.9']
+)
