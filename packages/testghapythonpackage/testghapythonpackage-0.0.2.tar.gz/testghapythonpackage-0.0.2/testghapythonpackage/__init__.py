810f3bf99ec86d21e40e2bc04bde0adbb0596565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:35 +0000

    Add package testghapythonpackage/0.0.2/testghapythonpackage-0.0.2.tar.gz

---
+++ packages/testghapythonpackage/testghapythonpackage-0.0.2.tar.gz/testghapythonpackage-0.0.2/testghapythonpackage/__init__.py
@@ -0,0 +1,10 @@
+from importlib import metadata
+
+
+# Export the version defined in project metadata
+__version__ = metadata.version(__package__)
+del metadata
+
+
+def add_one(x):
+    return x + 1
