a1daaa83e3fc6ae5c8e6b31dcc4e4548d46ab8b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:07 +0000

    Add package pyfastani/0.3.0/pyfastani-0.3.0-cp38-cp38-macosx_10_15_x86_64.whl

---
+++ packages/pyfastani/pyfastani-0.3.0-cp38-cp38-macosx_10_15_x86_64.whl/pyfastani/__init__.py
@@ -0,0 +1,9 @@
+from . import _fastani
+from ._fastani import Sketch, Mapper, Hit, MAX_KMER_SIZE
+
+__author__ = "Martin Larralde <martin.larralde@embl.de>"
+__license__ = "MIT"
+__version__ = "0.3.0"
+
+__all__ = ["Sketch", "Mapper", "Hit", "MAX_KMER_SIZE"]
+__doc__ = _fastani.__doc__
