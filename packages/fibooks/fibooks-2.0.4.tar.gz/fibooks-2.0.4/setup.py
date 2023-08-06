5577c366bdb5dfbff46b86b13bf894923260dd99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:02 +0000

    Add package fibooks/2.0.4/fibooks-2.0.4.tar.gz

---
+++ packages/fibooks/fibooks-2.0.4.tar.gz/fibooks-2.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Financial and Insurance Industry',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: Microsoft :: Windows :: Windows 11',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='fibooks',
+    version='2.0.4',
+    packages=['fibooks'],
+    url='https://github.com/TimoKats/fibooks/',
+    license='MIT',
+    author ='Timo Kats',
+    author_email='tpakats@gmail.com',
+    description='Python library for financial statement analysis. More information availible at: https://github.com/TimoKats/fibooks/',
+    long_description='Python library for financial statement analysis. More information availible at: https://github.com/TimoKats/fibooks/',
+    install_requires=['openpyxl','pandas','setuptools'],
+    keywords=['finance', 'data','valuation','excel']
+)
