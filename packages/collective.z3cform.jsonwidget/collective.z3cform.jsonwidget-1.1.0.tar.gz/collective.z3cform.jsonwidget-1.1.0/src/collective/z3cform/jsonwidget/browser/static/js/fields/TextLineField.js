47011dc5e2e73a1b13018695b9d648583a51a43c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:39 +0000

    Add package collective.z3cform.jsonwidget/1.1.0/collective.z3cform.jsonwidget-1.1.0.tar.gz

---
+++ packages/collective.z3cform.jsonwidget/collective.z3cform.jsonwidget-1.1.0.tar.gz/collective.z3cform.jsonwidget-1.1.0/src/collective/z3cform/jsonwidget/browser/static/js/fields/TextLineField.js
@@ -0,0 +1,37 @@
+import React, { useContext, useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import WidgetContext from '../utils/widgetContext';
+
+const TextLineField = ({ value, id, row }) => {
+  const { updateField } = useContext(WidgetContext);
+  const [data, setData] = useState({ text: '', timeout: 0 });
+
+  const updateText = targetValue => {
+    if (data.timeout) {
+      clearInterval(data.timeout);
+    }
+    const timeout = setTimeout(() => {
+      updateField({ row, id, value: targetValue });
+    }, 1000);
+    setData({ text: targetValue, timeout });
+  };
+
+  useEffect(() => {
+    setData({ text: value, timeout: 0 });
+  }, [value]);
+
+  return (
+    <input
+      type="text"
+      value={data.text}
+      onChange={e => updateText(e.target.value)}
+    />
+  );
+};
+TextLineField.propTypes = {
+  value: PropTypes.string,
+  id: PropTypes.string,
+  row: PropTypes.number,
+};
+
+export default TextLineField;
