775c535e3c821b2b198571f070b766beaaf4493f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:25 +0000

    Add package tonic/1.1.1.dev11/tonic-1.1.1.dev11.tar.gz

---
+++ packages/tonic/tonic-1.1.1.dev11.tar.gz/tonic-1.1.1.dev11/docs/gallery/representations/plot_tovoxelgrid.py
@@ -0,0 +1,25 @@
+"""
+=============
+ToVoxelGrid
+=============
+
+This example showcases the ToTimesurface transform with different parameters. 
+"""
+
+import tonic
+
+nmnist = tonic.datasets.NMNIST("../../tutorials/data", train=False)
+events, label = nmnist[0]
+
+####################################
+# ToVoxelGrid n_time_bins=20
+# ---------------------------
+
+transform = tonic.transforms.ToVoxelGrid(
+    sensor_size=nmnist.sensor_size,
+    n_time_bins=20,
+)
+
+frames = transform(events)
+
+ani = tonic.utils.plot_animation(frames)
