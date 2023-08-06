8dc2f84b6fcb7ec831f39efdfdedf156204794ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:00 +0000

    Add package pandas-plotly/0.2.2/pandas_plotly-0.2.2-py3-none-any.whl

---
+++ packages/pandas-plotly/pandas_plotly-0.2.2-py3-none-any.whl/pp/__init__.py
@@ -0,0 +1,8 @@
+# Use 'Viz' - subclasses Base & Data
+#from pp.viz import Viz as GET
+from pp.viz import *
+from pp.data import *
+from pp.io import *
+from pp.app import *
+from pp.util import *
+from pp.constants import *
