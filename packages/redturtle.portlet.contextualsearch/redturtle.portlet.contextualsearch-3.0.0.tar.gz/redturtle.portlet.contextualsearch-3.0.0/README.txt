9689cd1eced26561f650d1e39ee73d7d407c8057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:24 +0000

    Add package redturtle.portlet.contextualsearch/3.0.0/redturtle.portlet.contextualsearch-3.0.0.tar.gz

---
+++ packages/redturtle.portlet.contextualsearch/redturtle.portlet.contextualsearch-3.0.0.tar.gz/redturtle.portlet.contextualsearch-3.0.0/README.txt
@@ -0,0 +1,19 @@
+Introduction
+============
+It's an extension of search portlet that allows search for items starting from a certain context, and choose whether or not to see the advanced search.
+
+You can choose to search in the current path or in a selected folder.
+
+
+NEW FEATURE FOR PLONE 5
+=======================
+
+With a Mosaic view in Plone 5, you can add a contextual search tile.
+
+
+NOTE
+====
+
+For Plone >=5.2 use branch master.
+For Plone <5.2 use branch 2.1.x.
+For Plone 4 use the branch `2.0.x`.
---
+++ packages/redturtle.portlet.contextualsearch/redturtle.portlet.contextualsearch-3.0.0.tar.gz/redturtle.portlet.contextualsearch-3.0.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+
+version = '3.0.0'
+
+setup(name='redturtle.portlet.contextualsearch',
+      version=version,
+      description="A contextual search portlet",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: Addon",
+        "Framework :: Plone :: 5.2",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+      ],
+      keywords='plone portlet search contextual',
+      author='RedTurtle Technology',
+      author_email='sviluppoplone@redturtle.it',
+      url='https://github.com/RedTurtle/redturtle.portlet.contextualsearch',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['redturtle', 'redturtle.portlet'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.tiles',
+      ],
+      entry_points="""
+      # -*- entry_points -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
