4cbc6469946b3f1cddeba9f30c67a957ce2cba5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:18:53 +0000

    Add package xedocs/0.1.9/xedocs-0.1.9.tar.gz

---
+++ packages/xedocs/xedocs-0.1.9.tar.gz/xedocs-0.1.9/xedocs/__init__.py
@@ -0,0 +1,19 @@
+"""Top-level package for xedocs."""
+
+__author__ = """Yossi Mosbacher"""
+__email__ = "joe.mosbacher@gmail.com"
+__version__ = "0.1.9"
+
+from ast import Import
+from ._settings import settings
+from ._frames import frames
+from . import schemas
+from .schemas import *
+from .utils import *
+from .xedocs import *
+from . import api
+
+try:
+    from . import gui
+except ImportError:
+    pass
