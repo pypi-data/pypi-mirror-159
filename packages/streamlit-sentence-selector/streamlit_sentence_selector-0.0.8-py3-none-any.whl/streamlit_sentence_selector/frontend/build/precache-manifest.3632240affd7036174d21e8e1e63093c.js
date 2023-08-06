d6c093ab664d8daa725a8dfa4142233b9a147838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:19 +0000

    Add package streamlit-sentence-selector/0.0.8/streamlit_sentence_selector-0.0.8-py3-none-any.whl

---
+++ packages/streamlit-sentence-selector/streamlit_sentence_selector-0.0.8-py3-none-any.whl/streamlit_sentence_selector/frontend/build/precache-manifest.3632240affd7036174d21e8e1e63093c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ff5e267467c19034eb4bb1eaacdc5e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b4cb76a15f7b3305af88",
+    "url": "./static/js/2.484de6c0.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.484de6c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b25b0c36636a6a561499",
+    "url": "./static/js/main.fa687adc.chunk.js"
+  },
+  {
+    "revision": "6a0c1001a1ce0677c9f6",
+    "url": "./static/js/runtime-main.9807cc7e.js"
+  }
+]);
