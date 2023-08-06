f73cec3fbe7672d520fab4e221d2550bb91fefaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:10 +0000

    Add package oarepo-ui/4.0.10/oarepo_ui-4.0.10-py2.py3-none-any.whl

---
+++ packages/oarepo-ui/oarepo_ui-4.0.10-py2.py3-none-any.whl/oarepo_ui/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2022 CESNET
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+
+from .ext import OARepoUIExtension
+
+__version__ = "4.0.10"
+
+__all__ = ('__version__', 'OARepoUIExtension')
