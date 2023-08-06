a31056b9822db68fc9b50c1eefb2cb28026e22dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:08 +0000

    Add package sklarpy/0.1.2/sklarpy-0.1.2.tar.gz

---
+++ packages/sklarpy/sklarpy-0.1.2.tar.gz/sklarpy-0.1.2/README.rst
@@ -0,0 +1,59 @@
+=============
+**SklarPy**
+=============
+
+.. image:: https://img.shields.io/pypi/v/sklarpy.svg
+   :alt: PyPi Version
+   :scale: 100%
+   :target: https://pypi.python.org/pypi/sklarpy/
+
+.. image:: https://github.com/sklarpy/sklarpy/actions/workflows/tests.yml/badge.svg
+    :alt: Tests
+    :scale: 100%
+    :target: https://github.com/sklarpy/sklarpy/actions/workflows/tests.yml
+
+.. image:: https://img.shields.io/pypi/l/sklarpy.svg
+   :alt: License
+   :scale: 100%
+   :target: https://github.com/sklarpy/sklarpy/blob/main/LICENSE
+
+.. image:: https://img.shields.io/pypi/pyversions/sklarpy.svg
+   :alt: Python versions
+   :scale: 100%
+   :target: https://pypi.python.org/pypi/sklarpy/
+
+.. image:: https://static.pepy.tech/personalized-badge/sklarpy?period=total&units=international_system&left_color=black&right_color=orange&left_text=Downloads
+   :alt: PyPi Downloads
+   :scale: 100%
+   :target: https://pepy.tech/project/sklarpy
+
+.. image:: https://img.shields.io/pypi/dm/sklarpy.svg
+   :alt: PyPi Monthly Downloads
+   :scale: 100%
+   :target: https://pypi.python.org/pypi/sklarpy/
+
+
+SklarPy (pronounced "Sky-Lar-Pee" or "Sky-La-Pie") is an open-source software for probability distribution fitting.
+It contains useful tools for fitting copula and univariate probability distributions.
+Named after Sklar's theorem and Abe Sklar, the American mathematician who proved that multivariate cumulative
+distribution functions can be expressed in terms of copulas and their marginals.
+
+=================
+**Installation**
+=================
+
+SklarPy is available on pip
+
+::
+
+    pip install sklarpy
+
+Developing SklarPy
+##################
+
+To install SklarPy, along with the tool you need to develop and run tests, run the following
+in your virtual environment:
+
+::
+
+    pip install -e .[dev]
---
+++ packages/sklarpy/sklarpy-0.1.2.tar.gz/sklarpy-0.1.2/setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sklarpy',
+    packages=find_packages(),
+    version='0.1.2',
+    license='MIT',
+    long_description="SklarPy (pronounced 'Sky-Lar-Pee' or 'Sky-La-Pie') is an open-source software for probability "
+                     "distribution fitting.  It contains useful tools for fitting copula and univariate probability "
+                     "distributions. Named after Sklar's theorem and Abe Sklar, the American mathematician who proved "
+                     "that multivariate cumulative distribution functions can be expressed in terms of copulas and "
+                     "their marginals.",
+    author='Tyler Mitchell',
+    author_email='sklarpy@gmail.com',
+    url='https://github.com/sklarpy/sklarpy',
+    download_url='https://github.com/sklarpy/sklarpy/archive/refs/tags/v0.1.2.tar.gz',
+    keywords=[
+        'SklarPy',
+        'Sklar',
+        'Copula',
+        'Copulas',
+        'Probability',
+        'Distribution',
+        'Univariate',
+        'Bivariate',
+        'Multivariate',
+        'Joint',
+        'CDF',
+        'PDF',
+        'Modeling',
+        'Quantitative',
+        'Fitting'
+        'SciPy'
+    ],
+    install_requires=[
+        'pandas>=1.4.3',
+        'numpy>=1.23.0',
+        'scipy~=1.8.1',
+        'matplotlib~=3.5.2',
+        'dill~=0.3.5.1'
+    ],
+    extras_require={
+        "dev": [
+            "pytest==7.1.2",
+            "tox==3.25.1",
+        ]
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
