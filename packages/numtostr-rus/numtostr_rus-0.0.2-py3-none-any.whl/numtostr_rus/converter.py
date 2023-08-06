015453bb4f046979f6a55805a992766afb376901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:07 +0000

    Add package numtostr-rus/0.0.2/numtostr_rus-0.0.2-py3-none-any.whl

---
+++ packages/numtostr-rus/numtostr_rus-0.0.2-py3-none-any.whl/numtostr_rus/converter.py
@@ -0,0 +1,12 @@
+from typing import Union
+
+
+# TODO: implement float, Decimal, Rational, Complex.
+#       After implementing Complex, next line should be
+#       Num_T = numbers.Complex
+Num_T = Union[int]
+
+
+def convert(num: Num_T) -> str:
+	"""Placeholder for main function."""
+	return str(num)
