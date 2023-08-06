449545ceea963a5a0a00fb5931681da88fae6ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:34 +0000

    Add package pyWikiCommons/0.0.5/pyWikiCommons-0.0.5-py3.8.egg

---
+++ packages/pyWikiCommons/pyWikiCommons-0.0.5-py3.8.egg/pyWikiCommons/__init__.py
@@ -0,0 +1,11 @@
+__author__ = 'AJ McKenna, https://github.com/amckenna41'
+__authorEmail__ = 'amckenna41@qub.ac.uk'
+__url__ = 'https://github.com/amckenna41/pyWikiCommons'
+__credits__ = ['AJ McKenna']
+__version__ = "0.0.5"
+__license__ = 'MIT'
+__name__ = 'pyWikiCommons'
+__status__ = 'Development'
+__maintainer__ = "AJ McKenna"
+
+from pyWikiCommons import *
