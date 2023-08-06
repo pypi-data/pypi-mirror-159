2e11e0cc1b683dd9212830ec978bbe92f75ed836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:08:25 +0000

    Add package BioSeqTools/0.2.0/BioSeqTools-0.2.0-py3-none-any.whl

---
+++ packages/BioSeqTools/BioSeqTools-0.2.0-py3-none-any.whl/bioseqtools/versions.py
@@ -0,0 +1,18 @@
+def get_versions():
+    return versions[0]["number"]
+
+
+versions = [
+    {
+        "number": "0.2.0",
+        "features": [
+            "1. Rename project",
+        ],
+    },
+    {
+        "number": "0.1.0",
+        "features": [
+            "1. Separate SeqParser from ToolBiox",
+        ],
+    },
+]
