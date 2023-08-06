a739e7d0b8ced2ab9fe422e5964df8d9fce3ec7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:50 +0000

    Add package smac/1.4.0/smac-1.4.0.tar.gz

---
+++ packages/smac/smac-1.4.0.tar.gz/smac-1.4.0/smac/__init__.py
@@ -0,0 +1,32 @@
+import datetime
+import os
+import sys
+import warnings
+
+name = "SMAC3"
+package_name = "smac"
+author = (
+    "\tMarius Lindauer, Katharina Eggensperger, Matthias Feurer, André Biedenkapp, "
+    "Difan Deng,\n\tCarolin Benjamins, Tim Ruhkopf, René Sass and Frank Hutter"
+)
+
+author_email = "fh@cs.uni-freiburg.de"
+description = "SMAC3, a Python implementation of 'Sequential Model-based Algorithm Configuration'."
+url = "https://www.automl.org/"
+project_urls = {
+    "Documentation": "https://https://github.com/automl.github.io/SMAC3/main",
+    "Source Code": "https://github.com/https://github.com/automl/smac",
+}
+copyright = f"""
+    Copyright {datetime.date.today().strftime('%Y')}, Marius Lindauer, Katharina Eggensperger,
+    Matthias Feurer, André Biedenkapp, Difan Deng, Carolin Benjamins, Tim Ruhkopf, René Sass
+    and Frank Hutter
+"""
+version = "1.4.0"
+
+
+if os.name != "posix":
+    warnings.warn(
+        f"Detected unsupported operating system: {sys.platform}."
+        "Please be aware, that SMAC might not run on this system."
+    )
