555a3ed0670ba4f2e10fea5d898401e6c887dcfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:48:37 +0000

    Add package fb-logging/0.5.1/fb_logging-0.5.1.tar.gz

---
+++ packages/fb-logging/fb_logging-0.5.1.tar.gz/fb_logging-0.5.1/lib/fb_logging/local_version.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Frank Brehm
+@contact: frank@brehm-online.com
+@copyright: © 2022 by Frank Brehm, Berlin
+@summary: Python modules to extend the logging mechanism in Python.
+"""
+
+__author__ = 'Frank Brehm <frank@brehm-online.com>'
+__copyright__ = '(C) 2022 by Frank Brehm, Berlin'
+__contact__ = 'frank@brehm-online.com'
+__version__ = '0.5.1'
+__license__ = 'LGPL3+'
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 list
