ec0869207381cc6d777de89f1ead154f989adcd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:46 +0000

    Add package network3-medusa/0.1.7/network3_medusa-0.1.7.tar.gz

---
+++ packages/network3-medusa/network3_medusa-0.1.7.tar.gz/network3_medusa-0.1.7/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['network3_medusa']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-dotenv>=0.20.0,<0.21.0', 'requests>=2.28.1,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['network3_medusa = network3_medusa.script:run']}
+
+setup_kwargs = {
+    'name': 'network3-medusa',
+    'version': '0.1.7',
+    'description': '',
+    'long_description': None,
+    'author': 'John Capobianco',
+    'author_email': 'ptcapo@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
