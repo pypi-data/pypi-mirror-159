123ef123bf5a26ac70a36b2053ae7215a5999e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:01 +0000

    Add package buzzoff/1.1.0/buzzoff-1.1.0-py3-none-any.whl

---
+++ packages/buzzoff/buzzoff-1.1.0-py3-none-any.whl/buzzoff/__init__.py
@@ -0,0 +1,26 @@
+"""buzzoff: word generation"""
+
+__all__ = ["__version__", "words", "buzz"]
+__version__ = "1.1.0"
+
+from collections.abc import Iterable, Iterator
+
+with open("/usr/share/dict/words", "r", encoding="utf-8") as f:
+    words = f.read().splitlines()
+
+
+def buzz(
+    letters: Iterable[str], minlength: int, mandatory: None | str
+) -> Iterator[str]:
+    """
+    Analyze a sequence of letters.
+
+    :param letters: the available letters from which to make words
+    :param minlength: minimum word length allowed
+    :param mandatory: if given, these letters must be included
+    """
+    s = set(letters)
+    filters = [lambda w: len(w) >= minlength, s.issuperset]
+    if mandatory:
+        filters.insert(0, lambda w: all(c in w for c in mandatory))  # type: ignore[union-attr]
+    return filter(lambda w: all(f(w) for f in filters), words)
