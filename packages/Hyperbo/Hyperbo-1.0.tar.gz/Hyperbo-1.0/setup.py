129d67bfc42e059ee79e5c53c93b3bdbb8570fec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:34 +0000

    Add package Hyperbo/1.0/Hyperbo-1.0.tar.gz

---
+++ packages/Hyperbo/Hyperbo-1.0.tar.gz/Hyperbo-1.0/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+    name="Hyperbo",
+    version="1.0",
+    author=["Gilda Bansimba", "Régis Babindamana", "Basile Bossoto"],
+    author_email='bansimbagilda@gmail.com',
+    maintainer="Gilda Bansimba",
+    maintainer_email="bansimbagilda@gmail.com",
+    url="https://github.com/GildaRech/Hyperbo",
+    platforms="Cross platform",
+    keywords=["Fermat", "Hyperbola", "Arithmetic", "Computations", "Morphisms"],
+    license="MIT License",
+    description="Hyperbo is an open-source python package, a mathematical package that implements arithmetical computations on hyperbola. This work in particular, focuses on research findings on lattice points on hyperbolas in relation with Fermat factorization equation.\n It implements points computations, infos, cardinals, isomorphisms, plots over different algebraic structures.",
+    packages=["Hyperbo"]   
+)
