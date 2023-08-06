ef2a37cd36ce8122efc773a1a38b7f5e720c5bbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:49 +0000

    Add package renogymodbus/0.0.4/renogymodbus-0.0.4.tar.gz

---
+++ packages/renogymodbus/renogymodbus-0.0.4.tar.gz/renogymodbus-0.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name="renogymodbus",
+    version="0.0.4",
+    description="",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/rosswarren/renogymodbus",
+    author="Ross Warren",
+    author_email="rosswarren4@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["renogymodbus"],
+    include_package_data=True,
+    install_requires=["minimalmodbus", "retrying"],
+    test_suite="test",
+    entry_points={
+        "console_scripts": [
+            "renogymodbus = renogymodbus.command_line:main",
+        ],
+    },
+)
