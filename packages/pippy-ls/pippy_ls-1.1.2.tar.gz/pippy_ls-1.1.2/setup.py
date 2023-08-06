abb55e1cb308a4a6d1c7a2af73d54ebd2c8c4b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:45 +0000

    Add package pippy-ls/1.1.2/pippy_ls-1.1.2.tar.gz

---
+++ packages/pippy-ls/pippy_ls-1.1.2.tar.gz/pippy_ls-1.1.2/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['pippy_ls']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aws-cdk-lib>=2.28.0', 'constructs>=10.0.0,<11.0.0']
+
+entry_points = \
+{'console_scripts': ['pippy_ls = pippy_ls.__main__:main']}
+
+setup_kwargs = {
+    'name': 'pippy-ls',
+    'version': '1.1.2',
+    'description': 'An open-source python package',
+    'long_description': '## pippy long stockings\nA Python library\n\n',
+    'author': 'Pippy Long Stockings',
+    'author_email': 'zpprado@email.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/pradoz/pippy-ls',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/pippy-ls/pippy_ls-1.1.2.tar.gz/pippy_ls-1.1.2/src/pippy_ls/utils.py
@@ -0,0 +1,42 @@
+"""Supporting utilities.
+"""
+import json
+import os
+from dataclasses import dataclass
+from pathlib import Path
+# from typing import Any, Optional, Sequence, Union
+#
+# from aws_cdk import App as _App
+# from aws_cdk import aws_s3 as s3
+# from aws_cdk import aws_s3_deployment as s3_deploy
+# from aws_cdk.region_info import RegionInfo
+# from checksumdir import dirhash
+from pkg_resources import get_distribution
+from pippy_ls.custom_errors import CDKVersionError
+
+
+ROOT_PATH = str(Path(".").resolve())
+
+
+def check_cdk_version(package: str = "aws-cdk-lib"):
+    installed_cdk_version = get_distribution(package).version
+    desired_version = ""
+    with open(f"{ROOT_PATH}/pyproject.toml", "r") as setup_file:
+        lines = setup_file.readlines()
+        for line in lines:
+            if line.startswith(package):
+                # handle semver checking
+                check = line.split("=")[1].strip().strip('"')
+                if check[0] == "~" or check[0] == "^":
+                    desired_version = check[1:]
+                elif check[0] == ">" or check[0] == "<":
+                    desired_version = check[2:]
+                else:
+                    desired_version = check
+
+    print(f'installed_cdk_version: {installed_cdk_version}')
+    print(f'desired_version: {desired_version}')
+    if installed_cdk_version != desired_version:
+        raise CDKVersionError(installed_cdk_version, desired_version)
+
+
