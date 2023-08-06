e3ec3a6d1e04d7426a9e21dd086435f87376dcf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:17 +0000

    Add package alpino-query/2.1.4/alpino-query-2.1.4.tar.gz

---
+++ packages/alpino-query/alpino-query-2.1.4.tar.gz/alpino-query-2.1.4/alpino_query/__init__.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+from typing import List
+from lxml import etree
+
+from .marker import main as mark
+from .subtree import generate_subtree
+from .xpath_generator import main as generate_xpath
+
+
+class AlpinoQuery:
+    @property
+    def marked_xml(self):
+        return self.__get_xml(self.marked)
+
+    @property
+    def subtree_xml(self):
+        return self.__get_xml(self.subtree)
+
+    def mark(self, inputxml: str, tokens: List[str], attributes: List[str]) -> etree._Element:
+        self.marked = mark(inputxml, tokens, attributes)
+        return self.marked
+
+    def generate_subtree(self, remove: List[str]) -> etree._Element:
+        """
+        Generate subtree, removes the top "rel" and/or "cat"
+        """
+        self.subtree = generate_subtree(self.marked, remove)
+        return self.subtree
+
+    def generate_xpath(self, order: bool) -> str:
+        self.xpath = generate_xpath(self.subtree_xml, order)
+        return self.xpath
+
+    def __get_xml(self, twig) -> str:
+        return etree.tostring(twig, pretty_print=True).decode()
