41180562ca84801295bd0b7e1280e07e17440ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:24 +0000

    Add package jetplot/0.5.0/jetplot-0.5.0.tar.gz

---
+++ packages/jetplot/jetplot-0.5.0.tar.gz/jetplot-0.5.0/jetplot/__init__.py
@@ -0,0 +1,20 @@
+"""Jetpack is a set of useful utility functions for scientific python."""
+
+from . import colors as c, typing
+from .chart_utils import *
+from .colors import *
+from .images import *
+from .plots import *
+from .style import *
+
+from .signals import *
+from .timepiece import *
+
+from .version import __version__
+
+light_mode()
+
+try:
+    set_font("Helvetica")
+except ValueError:
+    pass
