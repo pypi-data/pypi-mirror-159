f91c62e2293f6c30846ba961b3ae88fc0cbe629e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:55:03 +0000

    Add package NDETCStemmer/1.3.1/NDETCStemmer-1.3.1.tar.gz

---
+++ packages/NDETCStemmer/NDETCStemmer-1.3.1.tar.gz/NDETCStemmer-1.3.1/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+name='NDETCStemmer',  
+version='1.3.1',
+py_modules=['NDETCStemmer'] ,
+description="Library untuk stemming kata dalam Bahasa Indonesia menggunakan metode Nondeterministic Context",
+long_description=long_description,
+long_description_content_type="text/markdown",
+url="https://github.com/alifadwitiyap/NDETCStemmer",
+packages=setuptools.find_packages(),
+include_package_data=True,
+zip_safe=False,
+classifiers=[
+	# How mature is this project? Common values are
+	#   3 - Alpha
+	#   4 - Beta
+	#   5 - Production/Stable
+	'Development Status :: 3 - Alpha',
+	# that you indicate whether you support Python 2, Python 3 or both.
+		"Programming Language :: Python :: 3",
+		# Indicate who your project is intended for
+	'Intended Audience :: Information Technology',
+	'Intended Audience :: Science/Research',
+	'Topic :: Text Processing :: Linguistic',
+],
+# What does your project relate to?
+keywords='linguistic stemming indonesian bahasa',
+install_requires=[
+	'nltk',
+	'gensim',
+	'gdown',
+	'checksumdir'
+]
+)
