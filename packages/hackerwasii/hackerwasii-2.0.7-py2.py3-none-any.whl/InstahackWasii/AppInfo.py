f449a6d9e91792049dc526654bde52e7d245429f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:31 +0000

    Add package hackerwasii/2.0.7/hackerwasii-2.0.7-py2.py3-none-any.whl

---
+++ packages/hackerwasii/hackerwasii-2.0.7-py2.py3-none-any.whl/InstahackWasii/AppInfo.py
@@ -0,0 +1,15 @@
+# The MIT License
+# Copyright (C) 2022 Evildevill , Waseem Akram.
+#
+# @filename    : AppInfo.py
+# @description : Application Information that are required
+
+appInfo = {
+    "version": "2.0.7",
+    "name": "Instahack",
+    "description": " Slick Instagram brute force command line tool",
+    "author": "Waseem Akram",
+    "company": "Evildevill",
+    "year": "2022",
+    "example": "example: instahack -c -vvv -u instatestgod__ -pl rockyou.txt"
+}
