7c481a7ce0bc9d8663de6e4511949491a3d8ec99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:55 +0000

    Add package micropython-micronet/0.0.11/micropython-micronet-0.0.11.tar.gz

---
+++ packages/micropython-micronet/micropython-micronet-0.0.11.tar.gz/micropython-micronet-0.0.11/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup,find_packages
+
+setup(    
+    name='micropython-micronet',
+    packages=find_packages(),
+    
+    description='MicroPython library for networking',
+    long_description_content_type="text/markdown",
+    long_description="MicroPython library for networking",
+    url='https://github.com/yeranosyanvahan/micronet',
+    author='Vahan Yeranosyan',
+    author_email='vahan@yeranosyanvahan.com',
+    maintainer='Vahan Yeranosyan',
+    maintainer_email='vahan@yeranosyanvahan.com',
+    license='MIT',
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: Implementation :: MicroPython',
+        'License :: OSI Approved :: MIT License',
+    ],
+    setuptools_git_versioning={
+        "enabled": True,
+    },
+    setup_requires=["setuptools-git-versioning"],
+
+)
