532c9a60817aacdb001ec1637d086703df88c1d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:04 +0000

    Add package pytest-check-libs/3.1/pytest-check-libs-3.1.tar.gz

---
+++ packages/pytest-check-libs/pytest-check-libs-3.1.tar.gz/pytest-check-libs-3.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='pytest-check-libs',
+    version='3.1',
+    author="kangkai",
+    description='check your missing library',
+    classifiers=[
+        'Framework :: Pytest',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python :: 3.7',
+    ],
+    license='proprietary',
+    py_modules=['pytest_check_libs'],
+    keywords=[
+        'pytest', 'py.test', 'pytest-check-libs',
+    ],
+
+    install_requires=[
+        'pytest'
+    ],
+    entry_points={
+        'pytest11': [
+            'check-library = pytest_check_libs',
+        ]
+    }
+)
