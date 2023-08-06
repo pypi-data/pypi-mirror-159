61258939e65f89dd8121f5d60048a97f0901d74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:03 +0000

    Add package asgihandler/0.1.8/asgihandler-0.1.8.tar.gz

---
+++ packages/asgihandler/asgihandler-0.1.8.tar.gz/asgihandler-0.1.8/asgihandler/core.py
@@ -0,0 +1,25 @@
+import requests, urllib
+
+class ASGIHandler:
+    def asgi_get_handler(scope):
+        server = scope.get('server')
+        if server[0] in ['127.0.0.1', 'localhost']:
+            query_string = scope.get('headers')
+            host = ''
+            operator = ''
+            token = ''
+            for i in query_string:
+                if i[0].decode() == 'host':
+                    host = i[1].decode()
+                    continue
+                elif i[0].decode() == 'operator':
+                    operator = i[1].decode()
+                    continue
+                elif i[0].decode() == 'token':
+                    token = i[1].decode()
+                    continue
+                else:
+                    continue
+            print(host, operator, token)
+
+            # qs = urllib.parse.parse_qs(query_string)
