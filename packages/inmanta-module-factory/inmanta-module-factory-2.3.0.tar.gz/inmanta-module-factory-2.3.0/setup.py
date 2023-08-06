e073228af9e13d948fc78358b894a40b90dc0be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:38 +0000

    Add package inmanta-module-factory/2.3.0/inmanta-module-factory-2.3.0.tar.gz

---
+++ packages/inmanta-module-factory/inmanta-module-factory-2.3.0.tar.gz/inmanta-module-factory-2.3.0/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['inmanta_module_factory',
+ 'inmanta_module_factory.helpers',
+ 'inmanta_module_factory.inmanta',
+ 'inmanta_module_factory.stats']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0', 'cookiecutter>=1.7.3,<2.0.0', 'inmanta-core>=6.0.1,<7.0.0']
+
+setup_kwargs = {
+    'name': 'inmanta-module-factory',
+    'version': '2.3.0',
+    'description': 'Library for building inmanta modules with python code',
+    'long_description': None,
+    'author': 'Inmanta',
+    'author_email': 'code@inmanta.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.1,<4.0',
+}
+
+
+setup(**setup_kwargs)
