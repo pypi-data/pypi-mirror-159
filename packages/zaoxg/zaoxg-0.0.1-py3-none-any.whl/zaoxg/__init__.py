270f3f6aea6591971fe809717d470c46230d08dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:45:24 +0000

    Add package zaoxg/0.0.1/zaoxg-0.0.1-py3-none-any.whl

---
+++ packages/zaoxg/zaoxg-0.0.1-py3-none-any.whl/zaoxg/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/20 17:28
+# @Author  : ZAOXG
+# @File    : __init__.py
+
+from .zaoxg import hello
+
+print('注册一个id, zaoxg')
+print('zaoxg NB')
---
+++ packages/zaoxg/zaoxg-0.0.1-py3-none-any.whl/zaoxg/zaoxg.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/20 17:33
+# @Author  : ZAOXG
+# @File    : zaoxg.py
+
+def hello():
+    print('hello')
