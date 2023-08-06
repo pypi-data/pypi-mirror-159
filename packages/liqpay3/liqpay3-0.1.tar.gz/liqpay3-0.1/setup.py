d464259608f195fa7ad43f4974553d6ac1d85bd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:17 +0000

    Add package liqpay3/0.1/liqpay3-0.1.tar.gz

---
+++ packages/liqpay3/liqpay3-0.1.tar.gz/liqpay3-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='liqpay3',
+      version='0.1',
+      description='Liqpay client python 3+',
+      packages=['liqpay3'],
+      author_email='mmi200212@gmail.com',
+      zip_safe=False)
