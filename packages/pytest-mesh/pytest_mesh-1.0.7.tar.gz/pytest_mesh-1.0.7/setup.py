f20caf49c09f7d6e0132f286fee2641be09449e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:36 +0000

    Add package pytest-mesh/1.0.7/pytest_mesh-1.0.7.tar.gz

---
+++ packages/pytest-mesh/pytest_mesh-1.0.7.tar.gz/pytest_mesh-1.0.7/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+pytest_mesh 打包文件
+@author:zhaojiajun
+@file:setup.py
+@time:2022/07/13
+"""
+
+from setuptools import setup
+
+setup(
+    name='pytest_mesh',
+    version='1.0.7',
+    author="zhaojiajun",
+    author_email='zhaojiajun@baicizhan.com',
+    description='pytest_mesh插件',
+    packages=['pytest_mesh'],
+    # 依赖的资源文件
+    include_package_data=True,
+    package_data={"pytest_mesh": ["*.txt", "*.rst"]},
+    # 需要安装的依赖
+    install_requires=[
+        'autodoraemon==1.0.0',
+        'pytest==7.1.2',
+        'allure-pytest==2.9.45'
+    ],
+    entry_points={
+        'pytest11': [
+            'pytest-mesh = pytest_mesh.main',
+        ]
+    }
+)
+
+if __name__ == '__main__':
+    pass
