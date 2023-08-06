f9649f059c0a5eddfdd6149a45d71cbcd75bb4f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:39 +0000

    Add package pod5-format/0.0.21/pod5_format-0.0.21-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/pod5-format/pod5_format-0.0.21-cp39-cp39-macosx_10_9_x86_64.whl/pod5_format/__init__.py
@@ -0,0 +1,12 @@
+"""POD5 Format
+
+Bindings for the POD5 file format
+"""
+
+from ._version import __version__
+
+
+from .api_utils import *
+from .reader_pyarrow import *
+from .reader_utils import *
+from .writer import *
