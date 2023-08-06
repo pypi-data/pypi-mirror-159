002aa7f1bdf56c40e6bf52d13f98107ed815eb24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:01:21 +0000

    Add package passwdmakergiri27/1.1/passwdmakergiri27-1.1.tar.gz

---
+++ packages/passwdmakergiri27/passwdmakergiri27-1.1.tar.gz/passwdmakergiri27-1.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+import pathlib
+readmefile = pathlib.Path(r"README.md")
+readmefile_license = pathlib.Path(r"LICENSE")
+
+setuptools.setup(name='passwdmakergiri27', 
+author_email='gowdagirish333@gmail.com', version='1.1',
+long_description=readmefile.read_text(), packages=setuptools.find_packages(include="passwdmakergiri27",exclude=["LICENSE",'README.md',"setup.py"]),
+license=readmefile_license.read_text(),requires='sys')
