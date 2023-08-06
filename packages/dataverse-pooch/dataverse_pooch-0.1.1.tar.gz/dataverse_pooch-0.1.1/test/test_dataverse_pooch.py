3f82621359101d4105d18a59acc6ecad472432c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:39 +0000

    Add package dataverse-pooch/0.1.1/dataverse_pooch-0.1.1.tar.gz

---
+++ packages/dataverse-pooch/dataverse_pooch-0.1.1.tar.gz/dataverse_pooch-0.1.1/test/test_dataverse_pooch.py
@@ -0,0 +1,13 @@
+import dataverse_pooch
+import os
+
+
+def test_heidata():
+    nakadake = dataverse_pooch.create(
+        server="https://heidata.uni-heidelberg.de", doi="10.11588/data/TJNQZG"
+    )
+
+    assert os.path.exists(nakadake.fetch("nkd_fpl_slope_LT.json"))
+
+    # Also assert a filename with a space
+    assert os.path.exists(nakadake.fetch("How To - Minimal Workflow.pdf"))
