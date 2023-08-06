f2cbcb72f4d1dc3fef2aa310740899d059749c07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:13 +0000

    Add package OliveUtils/0.0.2/OliveUtils-0.0.2.tar.gz

---
+++ packages/OliveUtils/OliveUtils-0.0.2.tar.gz/OliveUtils-0.0.2/OliveUtils/__init__.py
@@ -0,0 +1,1 @@
+from OliveUtils.stats.funcs import print_string
---
+++ packages/OliveUtils/OliveUtils-0.0.2.tar.gz/OliveUtils-0.0.2/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='OliveUtils',
+    version='0.0.2',
+    author='Yuval Cohen',
+    description="Olive's DS utils",
+    packages=['OliveUtils']
+)
