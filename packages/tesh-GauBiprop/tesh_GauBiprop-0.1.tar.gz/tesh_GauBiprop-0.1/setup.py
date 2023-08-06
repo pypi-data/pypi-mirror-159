36c9ac9725b7401ef667c2b257c1de9b983e6199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:17 +0000

    Add package tesh-GauBiprop/0.1/tesh_GauBiprop-0.1.tar.gz

---
+++ packages/tesh-GauBiprop/tesh_GauBiprop-0.1.tar.gz/tesh_GauBiprop-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='tesh_GauBiprop',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['tesh_GauBiprop'],
+      author= 'Emmanuel Mutegi',
+      author_email= 'emmamutegi97@gmail.com',
+      zip_safe=False)
