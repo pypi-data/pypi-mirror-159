2819028ea780f28aa59128b0bbbebe75f4c7dd15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:15 +0000

    Add package arbor/0.7/arbor-0.7-cp39-cp39-macosx_10_15_universal2.whl

---
+++ packages/arbor/arbor-0.7-cp39-cp39-macosx_10_15_universal2.whl/arbor-0.7.data/data/include/arbor/spike_source_cell.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <arbor/common_types.hpp>
+#include <arbor/export.hpp>
+#include <arbor/schedule.hpp>
+
+namespace arb {
+
+// Cell description returned by recipe::cell_description(gid) for cells with
+// recipe::cell_kind(gid) returning cell_kind::spike_source
+
+struct ARB_SYMBOL_VISIBLE spike_source_cell {
+    cell_tag_type source; // Label of source.
+    schedule seq;
+
+    spike_source_cell() = delete;
+    spike_source_cell(cell_tag_type source, schedule seq): source(std::move(source)), seq(std::move(seq)) {};
+};
+
+} // namespace arb
