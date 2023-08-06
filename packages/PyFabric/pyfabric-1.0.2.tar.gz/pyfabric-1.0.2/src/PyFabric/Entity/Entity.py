55756e5dcccc1e69ff46061634d9021237a086aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:27 +0000

    Add package PyFabric/1.0.2/pyfabric-1.0.2.tar.gz

---
+++ packages/PyFabric/pyfabric-1.0.2.tar.gz/pyfabric-1.0.2/src/PyFabric/Entity/Entity.py
@@ -0,0 +1,55 @@
+from ..Entity.Inventory import Inventory
+from ..Networking.Client import Client
+
+
+class Location:
+    def __init__(self, uuid: str, client: Client):
+        self.client = client
+        self.uuid = uuid
+        self.x, self.y, self.z, self.yaw, self.pitch = 0, 0, 0, 0, 0
+        self.update()
+
+    def update(self):
+        raw_location = self.client.request("get_entity_location", self.uuid)
+        self.x, self.y, self.z, self.yaw, self.pitch = raw_location["data"]
+
+    def __str__(self):
+        return "location:{x:~%d,y:~%d,z:~%d,yaw:~%d,pitch:~%d}" % (
+            self.x, self.y, self.z, self.yaw, self.pitch)
+
+
+class Entity:
+    def __init__(self, client: Client, uuid: str):
+        self.uuid = uuid
+        self.client = client
+        self.location = Location(uuid, client)
+
+    def __str__(self):
+        return "{entity:{uuid:%s,location:%s}}" % (self.uuid, self.location)
+
+
+class Player(Entity):
+    def __init__(self, client: Client, uuid: str):
+        super(Player, self).__init__(client, uuid)
+        self.client = client
+
+    def get_block_looking_at(self):
+        return self.client.request_string("block_looking_at", 0, self.uuid)
+
+    def get_dimension(self):
+        return self.client.request_string("get_client_player_world", 0)
+
+    def send_chat(self, message: str):
+        """
+        Send a message to the player
+
+        Note: messages in minecraft can only be 256 characters long (Socket transfer limit)
+        :param message: The message the player should receive. Use § for colors
+        """
+        self.client.notify_server("client_chat", message)
+
+    def send_actionbar(self, action_bar):
+        self.client.notify_server("client_actionbar", action_bar)
+
+    def get_inventory(self) -> Inventory:
+        return Inventory(self.client, self.uuid)
---
+++ packages/PyFabric/pyfabric-1.0.2.tar.gz/pyfabric-1.0.2/src/PyFabric/Fabric.py
@@ -0,0 +1,68 @@
+from ..PyFabric.Events.EventType import EventType
+from ..PyFabric.Networking.Client import Client
+from ..PyFabric.Entity.Entity import Player, Entity
+from ..PyFabric.World.World import ClientWorld
+from pyee.base import EventEmitter
+
+
+class Fabric(EventEmitter):
+    def __init__(self):
+        super().__init__()
+        self.client = Client(self.__handle_event__, 1337)
+        self.cmd = EventEmitter()
+        self.__client_player__ = None
+        self.__ready__ = False
+        self.queue_event = []
+
+    def __handle_event__(self, event_type: str, packet: dict):
+        if not self.__ready__:
+            self.queue_event.append((event_type, packet))
+            return
+
+        val = EventType(event_type)
+        if val == EventType.ON_COMMAND:
+            data = packet['data']
+            if len(data) == 1:
+                self.cmd.emit("/" + data[0][1:], self.get_client_player())
+            else:
+                self.cmd.emit("/" + data[0][1:], self.get_client_player(), data[1:])
+        self.emit(val)
+
+    def ready(self):
+        self.__ready__ = True
+        for event_type, packet in self.queue_event:
+            self.__handle_event__(event_type, packet)
+
+        self.queue_event.clear()
+
+    def get_client_player(self):
+        if not self.client.connected:
+            return None
+
+        if self.__client_player__ is None:
+            uuid = self.client.request_string("get_client_player_uuid", 0)
+            self.__client_player__ = Player(self.client, uuid)
+
+        return self.__client_player__
+
+    def get_player(self, uuid: str):
+        if not self.client.connected:
+            return None
+        return Player(self.client, uuid)
+
+    def get_entity(self, uuid):
+        if not self.client.connected:
+            return None
+        return Entity(self.client, uuid)
+
+    def get_world(self):
+        if not self.client.connected:
+            return None
+        return ClientWorld(self.client)
+
+    def register_command(self, cmd: str):
+        self.client.notify_server("register_command", cmd)
+
+
+def create():
+    return Fabric()
