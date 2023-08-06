a69d9f606be3861878c4a667fa9fd95d3df3fe0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:20 +0000

    Add package AskGoogle/1.0.3/AskGoogle-1.0.3.tar.gz

---
+++ packages/AskGoogle/AskGoogle-1.0.3.tar.gz/AskGoogle-1.0.3/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="AskGoogle",
+    version="1.0.3",
+    author="NathanTW0219",
+    description="Automatically redirects to Google when an exception is thrown.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=["askgoogle"],
+    url="https://github.com/NathanTW0219/AskGoogle.py"
+)
