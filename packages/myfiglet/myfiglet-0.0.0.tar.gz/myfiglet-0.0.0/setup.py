8c82e8c1c3a04087ce1d3236b223c83eb938b455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:15 +0000

    Add package myfiglet/0.0.0/myfiglet-0.0.0.tar.gz

---
+++ packages/myfiglet/myfiglet-0.0.0.tar.gz/myfiglet-0.0.0/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name = "myfiglet",
+    author = "Harsh Gupta",
+    version='0.0.0',
+    author_email = "harshnkgupta@gmail.com",
+    description = "FIGlet Using Python",
+    long_description="This program can be used to display FIGlet fonts using python.After importing the module type myfiglet.help() for further help.",
+    packages=['myfiglet'],
+    install_requires=[]
+    )
