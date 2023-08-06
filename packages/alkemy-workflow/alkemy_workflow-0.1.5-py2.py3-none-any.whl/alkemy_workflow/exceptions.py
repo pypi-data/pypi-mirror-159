5845625d3b36758fb94da8ad8d150a1fdd9a30aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:59:04 +0000

    Add package alkemy-workflow/0.1.5/alkemy_workflow-0.1.5-py2.py3-none-any.whl

---
+++ packages/alkemy-workflow/alkemy_workflow-0.1.5-py2.py3-none-any.whl/alkemy_workflow/exceptions.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+__all__ = [
+    "GenericException",
+    "GenericWarning",
+    "TaskNotFound",
+    "SpaceNotFound",
+    "FolderNotFound",
+    "ListNotFound",
+    "StatusNotFound",
+    "InvalidOption",
+    "CommandNotFound",
+    "GitException",
+    "ConfigException",
+    "ClickUpException",
+]
+
+
+class GenericException(Exception):
+    "Generic exception"
+
+
+class GenericWarning(GenericException):
+    "Generic warning (exit with 0 status)"
+
+
+class TaskNotFound(GenericException):
+    "Task not found"
+
+
+class SpaceNotFound(GenericException):
+    "Space not found"
+
+
+class FolderNotFound(GenericException):
+    "Folder not found"
+
+
+class ListNotFound(GenericException):
+    "List not found"
+
+
+class StatusNotFound(GenericException):
+    "Status not found"
+
+
+class InvalidOption(GenericException):
+    "Invalid option (command line argument)"
+
+
+class CommandNotFound(GenericException):
+    "Command not found"
+
+
+class GitException(GenericException):
+    "Git exception"
+
+
+class ConfigException(GenericException):
+    "Missing configuration exception"
+
+
+class ClickUpException(GenericException):
+    "ClickUp exception"
