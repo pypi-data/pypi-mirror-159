ac966e203c4d106c67eb767d9d5e31be443cb61f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:20:46 +0000

    Add package pyintelowl/4.2.0/pyintelowl-4.2.0-py3-none-any.whl

---
+++ packages/pyintelowl/pyintelowl-4.2.0-py3-none-any.whl/pyintelowl/cli/__init__.py
@@ -0,0 +1,25 @@
+from .analyse import analyse
+from .commands import (
+    analyzer_healthcheck,
+    connector_healthcheck,
+    get_analyzer_config,
+    get_connector_config,
+)
+from .config import config
+from .jobs import jobs
+from .tags import tags
+
+groups = [
+    analyse,
+    config,
+    jobs,
+    tags,
+]
+
+
+cmds = [
+    get_analyzer_config,
+    get_connector_config,
+    analyzer_healthcheck,
+    connector_healthcheck,
+]
