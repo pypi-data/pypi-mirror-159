caff43956650539fb56584c3842509971d392592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:31 +0000

    Add package di1l0o/1.0.0/di1l0o-1.1.2.tar.gz

---
+++ packages/di1l0o/di1l0o-1.1.2.tar.gz/di1l0o-1.0.0/setup.py
@@ -0,0 +1,29 @@
+
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+
+#rn()
+print(111)
+
+setup(
+    name='di1l0o',
+    version='1.0.0', # 项目版本 
+    author='xingxing', # 项目作者 
+    author_email='duxinglin123@gmail.com', # 作者email 
+    url='https://github.com/', # 项目代码仓库
+    description='hello word', # 项目描述 
+    #packages=['portal-api'], # 包名 
+    install_requires=[],
+        classifiers          = [
+        'Topic :: Security',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Developers'
+    ]
+)
+
