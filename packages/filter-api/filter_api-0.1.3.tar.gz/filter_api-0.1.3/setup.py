d28f59e14b7e6585587e1f534bae03ecb0554fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:44 +0000

    Add package filter-api/0.1.3/filter_api-0.1.3.tar.gz

---
+++ packages/filter-api/filter_api-0.1.3.tar.gz/filter_api-0.1.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='filter_api',
+    version='0.1.3',
+    license='MIT',
+    author="Pratyush Makkar",
+    author_email='',
+    packages=find_packages(),
+    url='',
+    keywords='Implement request path level filtering for HTTP Requests in FastAPI',
+    install_requires=[
+          'fastapi',
+          'starlette',
+          'uvicorn'
+      ],
+
+)
