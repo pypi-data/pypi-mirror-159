6057b92ad2823a973cd3744b83ae579637968597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:38 +0000

    Add package con2020/1.1.0/con2020-1.1.0-py3-none-any.whl

---
+++ packages/con2020/con2020-1.1.0-py3-none-any.whl/con2020/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '1.1.0'
+
+from ._Switcher import _Switcher
+from .Test import Test,TestTimingIntVsAn,TestTimingIntVsAnSingle,TestCompareAnalytic,Dump,TestBessel
+from .Model import Model
