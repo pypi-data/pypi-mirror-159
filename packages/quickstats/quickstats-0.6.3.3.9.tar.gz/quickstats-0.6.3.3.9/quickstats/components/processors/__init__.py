44887b373c8f009780b4f1749a71cf756ecee8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:54 +0000

    Add package quickstats/0.6.3.3.9/quickstats-0.6.3.3.9.tar.gz

---
+++ packages/quickstats/quickstats-0.6.3.3.9.tar.gz/quickstats-0.6.3.3.9/quickstats/components/processors/__init__.py
@@ -0,0 +1,3 @@
+from quickstats.components.processors.actions import *
+from quickstats.components.processors.parsers import RooProcConfigParser
+from quickstats.components.processors.roo_processor import RooProcessor
