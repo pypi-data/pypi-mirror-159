c34db57144b67a3d6d471085356d3ad42d5b6f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:46 +0000

    Add package wc-django-jwt/0.2.0/wc-django-jwt-0.2.0.tar.gz

---
+++ packages/wc-django-jwt/wc-django-jwt-0.2.0.tar.gz/wc-django-jwt-0.2.0/wcd_jwt/authentication.py
@@ -0,0 +1,21 @@
+from rest_framework_simplejwt.authentication import JWTAuthentication as Base
+# TODO: Remove dependency over `api_settings`.
+from rest_framework_simplejwt.settings import api_settings
+
+from .services import token_resolver
+from .signals import user_token_authenticated
+
+
+class JWTAuthentication(Base):
+    def get_validated_token(self, raw_token):
+        return token_resolver.resolve_valid(
+            raw_token, from_classes=api_settings.AUTH_TOKEN_CLASSES,
+        )
+
+    def authenticate(self, request):
+        user, token = super().authenticate(request)
+        user_token_authenticated.send(
+            self.__class__, user=user, token=token, request=request
+        )
+
+        return user, token
