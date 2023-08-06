aaa5e9e68c1555176a353cb814daf323d5cf5956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:23 +0000

    Add package ecapture/2.0.1/ecapture-2.0.1.tar.gz

---
+++ packages/ecapture/ecapture-2.0.1.tar.gz/ecapture-2.0.1/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="ecapture",
+    version="2.0.1",
+    scripts=['ecapture/ecapture.py'],
+    description="Webcams made easy",
+    install_requires=["opencv-python"],
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/YFOMNN/ecapture",
+    author="Mohammmed Yaseen",
+    author_email="hmyaseen05@gmail.com",
+    license="GNU GPLv3",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["ecapture"],
+    include_package_data=True,
+)
