4700caf625877ca22d17e502dc0c1844ba87cb98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:49 +0000

    Add package ReverseBox/0.0.26/ReverseBox-0.0.26.tar.gz

---
+++ packages/ReverseBox/ReverseBox-0.0.26.tar.gz/ReverseBox-0.0.26/reversebox/encryption/encryption_xor_retro64_eco.py
@@ -0,0 +1,19 @@
+import struct
+
+# XOR Cipher used in Retro64 *.ECO files
+# Read more here: http://wiki.xentax.com/index.php/Retro64_ECO
+
+
+def xor_cipher_retro64_eco(input_data: bytes, key: int) -> bytes:
+    if len(input_data) == 0:
+        return b''
+
+    result: bytes = b''
+    for raw_byte in input_data:
+        new_key = (201 * key + 11) % 0x7FFF
+        key = new_key
+        decrypted_byte = raw_byte ^ (new_key % 0xFF)
+        result += struct.pack("B", decrypted_byte)
+
+    return result
+
