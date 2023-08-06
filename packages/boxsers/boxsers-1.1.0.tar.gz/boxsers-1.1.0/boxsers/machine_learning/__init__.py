60c340750b66ab5d53ff29d8030af8776c07874a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:50:12 +0000

    Add package boxsers/1.1.0/boxsers-1.1.0.tar.gz

---
+++ packages/boxsers/boxsers-1.1.0.tar.gz/boxsers-1.1.0/boxsers/machine_learning/__init__.py
@@ -0,0 +1,5 @@
+from boxsers.machine_learning.classification import SpectroRF, SpectroSVM, SpectroLDA
+from boxsers.machine_learning.neural_networks import SpectroCNN
+from boxsers.machine_learning.clustering import SpectroGmixture, SpectroKmeans
+from boxsers.machine_learning.dimension_reduction import SpectroPCA
+from boxsers.machine_learning.metrics import eval_confmatrix, eval_classifreport
