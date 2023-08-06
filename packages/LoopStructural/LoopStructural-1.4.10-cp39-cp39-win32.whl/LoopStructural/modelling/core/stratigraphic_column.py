4cfb3ae7f70907fa64ca3ddb490c94606982c652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:14 +0000

    Add package LoopStructural/1.4.10/LoopStructural-1.4.10-cp39-cp39-win32.whl

---
+++ packages/LoopStructural/LoopStructural-1.4.10-cp39-cp39-win32.whl/LoopStructural/modelling/core/stratigraphic_column.py
@@ -0,0 +1,17 @@
+class StratigraphicColumn(dict):
+    def __init__(self):
+        pass
+
+    def add_series(self, feature, name, maximum, minimum, colour):
+        pass
+
+    def get_cmap(self):
+        pass
+
+    @property
+    def faults(self):
+        return
+
+    @property
+    def stratigraphy(self):
+        return
