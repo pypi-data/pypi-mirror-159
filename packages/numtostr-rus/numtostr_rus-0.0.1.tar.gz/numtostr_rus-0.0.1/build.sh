6938f26c36c56c6e20baec927baa0dc83a210b43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:52 +0000

    Add package numtostr-rus/0.0.1/numtostr_rus-0.0.1.tar.gz

---
+++ packages/numtostr-rus/numtostr_rus-0.0.1.tar.gz/numtostr_rus-0.0.1/build.sh
@@ -0,0 +1,3 @@
+python3 -m build
+# Run this command afterwards:
+# python3 -m twine upload --repository pypi dist/*
---
+++ packages/numtostr-rus/numtostr_rus-0.0.1.tar.gz/numtostr_rus-0.0.1/tests/int_tests.py
@@ -0,0 +1,5 @@
+from src.numtostr_rus import convert
+
+
+if __name__ == "__main__":
+	print(convert(42))
