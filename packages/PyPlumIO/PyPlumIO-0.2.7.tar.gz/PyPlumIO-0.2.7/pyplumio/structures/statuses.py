93e567b8e17865b1325bf8a3815e8e4c55d5b784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:31 +0000

    Add package PyPlumIO/0.2.7/PyPlumIO-0.2.7.tar.gz

---
+++ packages/PyPlumIO/PyPlumIO-0.2.7.tar.gz/PyPlumIO-0.2.7/pyplumio/structures/statuses.py
@@ -0,0 +1,32 @@
+"""Contains outputs structure decoder."""
+from __future__ import annotations
+
+from typing import Final, Optional, Tuple
+
+from pyplumio.helpers.typing import DeviceDataType
+
+HEATING_TARGET: Final = "heating_target"
+HEATING_STATUS: Final = "heating_status"
+WATER_HEATER_TARGET: Final = "water_heater_target"
+WATER_HEATER_STATUS: Final = "water_heater_status"
+STATUSES: Tuple[str, ...] = (
+    HEATING_TARGET,
+    HEATING_STATUS,
+    WATER_HEATER_TARGET,
+    WATER_HEATER_STATUS,
+)
+
+
+def from_bytes(
+    message: bytearray, offset: int = 0, data: Optional[DeviceDataType] = None
+) -> Tuple[DeviceDataType, int]:
+    """Decode bytes and return message data and offset."""
+    if data is None:
+        data = {}
+
+    for index, status in enumerate(STATUSES):
+        data[status] = message[offset + index]
+
+    offset += 4
+
+    return data, offset
