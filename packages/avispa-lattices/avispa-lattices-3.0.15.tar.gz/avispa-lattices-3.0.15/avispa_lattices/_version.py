f1df522700c15cd4112ca529fe0dabc33ba35b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:51 +0000

    Add package avispa-lattices/3.0.15/avispa-lattices-3.0.15.tar.gz

---
+++ packages/avispa-lattices/avispa-lattices-3.0.15.tar.gz/avispa-lattices-3.0.15/avispa_lattices/_version.py
@@ -0,0 +1,20 @@
+from typing import Optional
+from typing_extensions import Literal
+from packaging.version import parse
+from ._VERSION import VERSION
+
+
+def version_cmp(a: str, b: Optional[str] = None) -> Literal[0, -1, 1]:
+    if b is None:
+        b = VERSION
+    A = parse(a)
+    B = parse(b)
+    if A == B:
+        return 0
+    if A < B:
+        return -1
+    return 1
+
+
+def version_is_at_least(a: str):
+    return version_cmp(a) <= 0
