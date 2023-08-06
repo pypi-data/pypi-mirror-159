c055b5603bb29f6fcffeef2f9bcf58345558911a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:16 +0000

    Add package mathhew/0.1.2/mathhew-0.1.2.tar.gz

---
+++ packages/mathhew/mathhew-0.1.2.tar.gz/mathhew-0.1.2/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'lib'}
+
+packages = \
+['src']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.1.3,<9.0.0']
+
+entry_points = \
+{'console_scripts': ['matthew = mathhew:main']}
+
+setup_kwargs = {
+    'name': 'mathhew',
+    'version': '0.1.2',
+    'description': 'Fast and Simple Math Interpreter',
+    'long_description': None,
+    'author': 'micziz',
+    'author_email': 'miczicontent@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
