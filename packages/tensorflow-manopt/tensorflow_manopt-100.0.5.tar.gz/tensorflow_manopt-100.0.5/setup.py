0a8c1d6199f9d9d88e48a0c4ab7215c9596e2482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:50 +0000

    Add package tensorflow-manopt/100.0.5/tensorflow_manopt-100.0.5.tar.gz

---
+++ packages/tensorflow-manopt/tensorflow_manopt-100.0.5.tar.gz/tensorflow_manopt-100.0.5/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="tensorflow_manopt",
+    version="100.0.5",
+    author="Aloneee walker",
+    author_email="aloneewalker@facebook.com",
+    description="tensorflow_manopt of tensorflow_manopt of tensorflow_manopt",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
