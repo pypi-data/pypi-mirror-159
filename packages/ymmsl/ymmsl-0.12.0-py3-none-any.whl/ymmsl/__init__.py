57fae583abe5404a78f7868542e5c6d971e9c876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:52:09 +0000

    Add package ymmsl/0.12.0/ymmsl-0.12.0-py3-none-any.whl

---
+++ packages/ymmsl/ymmsl-0.12.0-py3-none-any.whl/ymmsl/__init__.py
@@ -0,0 +1,28 @@
+"""Python bindings for yMMSL.
+
+This package contains all the classes needed to represent a yMMSL file,
+as well as to read and write yMMSL files.
+"""
+
+from ymmsl.component import Component, Operator, Port, Ports
+from ymmsl.configuration import Configuration, PartialConfiguration
+from ymmsl.execution import (
+        ExecutionModel, Implementation, MPICoresResReq, MPINodesResReq,
+        ResourceRequirements, ThreadedResReq)
+from ymmsl.settings import Settings, SettingValue
+from ymmsl.identity import Identifier, Reference
+from ymmsl.io import load, dump, save
+from ymmsl.model import Conduit, Model, ModelReference
+
+
+__version__ = '0.12.0'
+__author__ = 'Lourens Veen'
+__email__ = 'l.veen@esciencecenter.nl'
+
+
+__all__ = [
+        'Component', 'Conduit', 'Configuration', 'dump', 'ExecutionModel',
+        'Identifier', 'Implementation', 'load', 'Model', 'ModelReference',
+        'MPICoresResReq', 'MPINodesResReq', 'Operator', 'PartialConfiguration',
+        'Port', 'Ports', 'Reference', 'ResourceRequirements', 'save',
+        'Settings', 'SettingValue', 'ThreadedResReq']
