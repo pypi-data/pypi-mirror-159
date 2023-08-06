93bd3dc82672a6f50407f43306313b2a2bc5098b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:58:36 +0000

    Add package Demo8799/1.0/Demo8799-1.0-py3.9.egg

---
+++ packages/Demo8799/Demo8799-1.0-py3.9.egg/Demo8799/demo1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+    @Author: runqian_lee
+    @Blog: https://blog.csdn.net
+    @Time: 2020/12/17 15:50
+    @Desc: demo1 模块设计
+"""
+
+
+def func():
+    print('This is Demo1')
---
+++ packages/Demo8799/Demo8799-1.0-py3.9.egg/Demo8799/demo2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+    @Author: runqian_lee
+    @Blog: https://blog.csdn.net
+    @Time: 2020/12/17 15:51
+    @Desc: demo2 模块设计
+"""
+
+
+def func():
+    print('This is Demo2')
