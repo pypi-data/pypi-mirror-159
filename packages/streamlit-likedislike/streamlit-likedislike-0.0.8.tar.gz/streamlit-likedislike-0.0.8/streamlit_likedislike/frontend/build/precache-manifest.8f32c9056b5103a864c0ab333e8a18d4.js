71428a288878546a462ea264cd8a93baf98f802f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:12 +0000

    Add package streamlit-likedislike/0.0.8/streamlit-likedislike-0.0.8.tar.gz

---
+++ packages/streamlit-likedislike/streamlit-likedislike-0.0.8.tar.gz/streamlit-likedislike-0.0.8/streamlit_likedislike/frontend/build/precache-manifest.8f32c9056b5103a864c0ab333e8a18d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd0d573bad6f309bdc6ec4549af4fa1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "01b86264b44dc0a9cc41",
+    "url": "./static/css/main.de9c1f8e.chunk.css"
+  },
+  {
+    "revision": "b50d41fb63acf3429db6",
+    "url": "./static/js/2.1f2b4149.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.1f2b4149.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01b86264b44dc0a9cc41",
+    "url": "./static/js/main.373b998f.chunk.js"
+  },
+  {
+    "revision": "f66c720174f9b8b612cd",
+    "url": "./static/js/runtime-main.bad378fd.js"
+  }
+]);
