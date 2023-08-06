8c4c8496209ade20a8556b7f72efcfb8a3463132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:53 +0000

    Add package pyobs-asi/0.20.0/pyobs-asi-0.20.0.tar.gz

---
+++ packages/pyobs-asi/pyobs-asi-0.20.0.tar.gz/pyobs-asi-0.20.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyobs_asi']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['astropy>=5.0,<6.0', 'numpy>=1.21,<2.0', 'pyobs-core>0.18', 'zwoasi>=0.1,<0.2']
+
+setup_kwargs = {
+    'name': 'pyobs-asi',
+    'version': '0.20.0',
+    'description': 'pyobs model for ASI cameras',
+    'long_description': None,
+    'author': 'Tim-Oliver Husser',
+    'author_email': 'thusser@uni-goettingen.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<3.11',
+}
+
+
+setup(**setup_kwargs)
