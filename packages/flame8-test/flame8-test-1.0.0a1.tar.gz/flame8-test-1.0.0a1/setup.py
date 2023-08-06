adbbdf99c8e31e7bc902b96ef9c0e2cf17cab454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:53:30 +0000

    Add package flame8-test/1.0.0a1/flame8-test-1.0.0a1.tar.gz

---
+++ packages/flame8-test/flame8-test-1.0.0a1.tar.gz/flame8-test-1.0.0a1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['flame8_test']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['flake8>=3.7,<4.0']
+
+entry_points = \
+{'console_scripts': ['flake8-markdown = flake8_markdown:main']}
+
+setup_kwargs = {
+    'name': 'flame8-test',
+    'version': '1.0.0a1',
+    'description': 'Lints Python code blocks in Markdown files using flake8',
+    'long_description': 'Test publishing',
+    'author': 'Abdel Jaidi',
+    'author_email': 'flake8@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/johnfraney/flake8-markdown',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
