0897972aa9adcf8f2686c7709c7f30efe7b90042
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:06 +0000

    Add package smallscheme/0.0.9/smallscheme-0.0.9.tar.gz

---
+++ packages/smallscheme/smallscheme-0.0.9.tar.gz/smallscheme-0.0.9/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+import versioneer
+
+
+desc = ("A small implementation of enough Scheme "
+        "to follow along in SICP")
+homeurl = "https://github.com/eigenhombre/smallscheme"
+
+
+setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    name="smallscheme",
+    author="John Jacobsen",
+    author_email="eigenhombre@gmail.com",
+    description=desc,
+    long_description=(desc +
+                      ".  See " + homeurl +
+                      " for more information."),
+    license="MIT",
+    keywords="lisp sicp scheme",
+    url=homeurl,
+    packages=['smallscheme'],
+    entry_points={
+        'console_scripts': [
+            'smallscheme = smallscheme.scheme:main',
+        ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
