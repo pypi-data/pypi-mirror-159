63a9abf6810fb802f7a9408b161db9232f3b3f62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:21 +0000

    Add package kraken-cli/0.1.7/kraken_cli-0.1.7-py3-none-any.whl

---
+++ packages/kraken-cli/kraken_cli-0.1.7-py3-none-any.whl/kraken/cli/locking/lockfile.py
@@ -0,0 +1,107 @@
+from __future__ import annotations
+
+import dataclasses
+import datetime
+import platform
+import sys
+from typing import Any
+
+from .requirements import LocalRequirement, RequirementSpec
+
+DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.Z"
+
+
+def dt2json(dt: datetime.datetime) -> str:
+    return dt.strftime(DATETIME_FORMAT)
+
+
+def json2dt(value: str) -> datetime.datetime:
+    return datetime.datetime.strptime(value, DATETIME_FORMAT)
+
+
+@dataclasses.dataclass
+class LockfileMetadata:
+    """Metadata for the lock file."""
+
+    #: The date and time when the lock file was created.
+    created_at: datetime.datetime
+
+    #: The Python version that the lock file was created with.
+    python_version: str
+
+    #: The system uname that the lock file was created with.
+    uname: str
+
+    #: The version of the Kraken CLI that the lock file was created with.
+    kraken_cli_version: str
+
+    @staticmethod
+    def new() -> LockfileMetadata:
+        from kraken.cli import __version__
+
+        return LockfileMetadata(
+            created_at=datetime.datetime.utcnow(),
+            python_version=sys.version,
+            uname=str(platform.uname()),  # TODO (@NiklasRosenstein): Format uname struct
+            kraken_cli_version=__version__,
+        )
+
+    @staticmethod
+    def from_json(data: dict[str, Any]) -> LockfileMetadata:
+        return LockfileMetadata(
+            created_at=json2dt(data.pop("created_at")),
+            **data,
+        )
+
+    def to_json(self) -> dict[str, Any]:
+        return {
+            "created_at": dt2json(self.created_at),
+            "python_version": self.python_version,
+            "uname": self.uname,
+            "kraken_cli_version": self.kraken_cli_version,
+        }
+
+
+@dataclasses.dataclass
+class Lockfile:
+    """This structure encodes all the data that needs to be present in a lock file to replicate an environment."""
+
+    #: Metadata for when the lock file was created.
+    metadata: LockfileMetadata
+
+    #: The requirements from which the environment was originally populated.
+    requirements: RequirementSpec
+
+    #: Exact versions for packages that were installed into the environment.
+    pinned: dict[str, str]
+
+    @staticmethod
+    def from_json(data: dict[str, Any]) -> Lockfile:
+        return Lockfile(
+            metadata=LockfileMetadata.from_json(data["metadata"]),
+            requirements=RequirementSpec.from_json(data["requirements"]),
+            pinned=data["pinned"],
+        )
+
+    def to_json(self) -> dict[str, Any]:
+        return {
+            "metadata": self.metadata.to_json(),
+            "requirements": self.requirements.to_json(),
+            "pinned": self.pinned,
+        }
+
+    def to_args(self) -> list[str]:
+        """Converts the pinned versions in the lock file to Pip install args."""
+
+        args = self.requirements.to_args(with_requirements=False)
+        local_dependencies = {
+            dep.name: dep.path for dep in self.requirements.requirements if isinstance(dep, LocalRequirement)
+        }
+        for key, value in self.pinned.items():
+            if key in local_dependencies:
+                # NOTE (@NiklasRosenstein): The purpose here is to keep local dependencies installed from the same
+                #       local source.
+                args += [str(local_dependencies[key])]
+            else:
+                args += [f"{key}=={value}"]
+        return args
---
+++ packages/kraken-cli/kraken_cli-0.1.7-py3-none-any.whl/kraken/cli/locking/project.py
@@ -0,0 +1,128 @@
+from __future__ import annotations
+
+import abc
+import dataclasses
+import json
+from pathlib import Path
+
+from .lockfile import Lockfile
+from .requirements import RequirementSpec, parse_requirements_file, parse_requirements_from_python_script
+
+
+class ProjectInterface(abc.ABC):
+    @abc.abstractmethod
+    def get_requirement_spec(self) -> RequirementSpec:
+        """Called to read the requirement spec of the project."""
+
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def read_lock_file(self) -> Lockfile | None:
+        """Return the lock file in the project."""
+
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def write_lock_file(self, data: Lockfile) -> None:
+        """Update the lock file in the project."""
+
+        raise NotImplementedError
+
+
+class DefaultProjectImpl(ProjectInterface):
+    """This implementation looks for requirements in the following way:
+
+    * Load `.kraken/requirements.txt` or `.kraken.requirements.txt`
+    * Otherwise find a build script using the Kraken Core loaders interface and load the requirements from the first
+        comment block (see :class:`parse_requirements_from_python_script`).
+
+    The Kraken lock file will, if it exists, be loaded from the following locations:
+
+    * `.kraken/lock.json` or `.kraken.lock.json`
+
+    If it does not exist, it will be placed next to the build script if it exists in a `.kraken` folder, otherwise
+    into the project root as `.kraken.lock.json`.
+    """
+
+    REQUIREMENT_FILES = [Path(".kraken/requirements.txt"), Path(".kraken.requirements.txt")]
+    LOCK_FILES = [Path(".kraken/lock.json"), Path(".kraken.lock.json")]
+
+    @dataclasses.dataclass
+    class Files:
+        requirements: Path | None
+        script: Path | None
+        lock: Path
+
+    def __init__(self, project_dir: Path | None = None) -> None:
+        self.project_dir = project_dir or Path.cwd()
+        self._files: DefaultProjectImpl.Files | None = None
+
+    def _get_files(self) -> Files:
+        """Determines the files to read/write."""
+
+        from kraken.core.loader import get_loader_implementations
+
+        if self._files is not None:
+            return self._files
+
+        # Find a file we can load requirements from.
+        requirements_file: Path | None = None
+        script_file: Path | None = None
+        for path in self.REQUIREMENT_FILES:
+            requirements_file = self.project_dir / path
+            if requirements_file.is_file():
+                break
+        else:
+            requirements_file = None
+            for loader in get_loader_implementations():
+                script_file = loader.detect_in_project_directory(self.project_dir)
+                if script_file:
+                    break
+            else:
+                raise RuntimeError("could not find a requirements file or Kraken build script")
+
+        assert requirements_file or script_file
+
+        # Find the lock file.
+        alternative_lock_file: Path | None = None
+        for path in self.LOCK_FILES:
+            lock_file = self.project_dir / path
+            if lock_file.parent.is_dir() and not alternative_lock_file:
+                alternative_lock_file = lock_file
+            if lock_file.exists():
+                break
+        else:
+            assert alternative_lock_file
+            lock_file = alternative_lock_file
+
+        self._files = self.Files(requirements_file, script_file, lock_file)
+        return self._files
+
+    def get_requirement_spec(self) -> RequirementSpec:
+        files = self._get_files()
+        if files.requirements:
+            with files.requirements.open() as fp:
+                return RequirementSpec.from_args(parse_requirements_file(fp))
+        elif files.script:
+            with files.script.open() as fp:
+                return RequirementSpec.from_args(parse_requirements_from_python_script(fp))
+        else:
+            assert False, files
+
+    def read_lock_file(self) -> Lockfile | None:
+        files = self._get_files()
+        if files.lock.is_file():
+            with files.lock.open() as fp:
+                return Lockfile.from_json(json.load(fp))
+        return None
+
+    def write_lock_file(self, data: Lockfile) -> None:
+        files = self._get_files()
+        try:
+            file = files.lock.relative_to(Path.cwd())
+        except ValueError:
+            file = files.lock
+        print("writing lock file", file)
+        files.lock.parent.mkdir(parents=True, exist_ok=True)
+        with files.lock.open("w") as fp:
+            json.dump(data.to_json(), fp)
