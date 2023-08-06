740d64bf56775bed1d6384ed3e5e97ec23dbab10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:41 +0000

    Add package DrawTick/0.0.2/DrawTick-0.0.2.tar.gz

---
+++ packages/DrawTick/DrawTick-0.0.2.tar.gz/DrawTick-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='DrawTick',
+        version='0.0.2',
+        keywords = ["pip", "DrawTick"],
+        description= "DrawTick",
+        license = "MIT Licence",
+        url='',
+        author='liujun',
+        author_email='62462156@qq.com',
+        packages=find_packages(),
+        include_package_data=True,
+        plaforms = 'any',
+        install_requires=["datetime","pandas", "matplotlib", "mpl_finance", "numpy", "json"]
+)
