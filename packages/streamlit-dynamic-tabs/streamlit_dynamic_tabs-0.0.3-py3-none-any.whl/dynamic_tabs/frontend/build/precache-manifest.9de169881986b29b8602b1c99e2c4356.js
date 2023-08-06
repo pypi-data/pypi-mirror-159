3900a637f9175953419ca0e498ff352a048f27ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:35 +0000

    Add package streamlit-dynamic-tabs/0.0.3/streamlit_dynamic_tabs-0.0.3-py3-none-any.whl

---
+++ packages/streamlit-dynamic-tabs/streamlit_dynamic_tabs-0.0.3-py3-none-any.whl/dynamic_tabs/frontend/build/precache-manifest.9de169881986b29b8602b1c99e2c4356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d8598dc7c75246a7634ed9d2a44d2e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2dd79c6ae2ffc37302b9",
+    "url": "./static/css/main.310e05dc.chunk.css"
+  },
+  {
+    "revision": "3594f21b102a28c4c203",
+    "url": "./static/js/2.945cd767.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.945cd767.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dd79c6ae2ffc37302b9",
+    "url": "./static/js/main.967eb671.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
