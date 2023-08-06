68913dcae7bcc618ef3f4dea266ea6ce80bd02d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:09 +0000

    Add package openmapflow/0.1.0/openmapflow-0.1.0-py3-none-any.whl

---
+++ packages/openmapflow/openmapflow-0.1.0-py3-none-any.whl/openmapflow/templates/datasets.py
@@ -0,0 +1,31 @@
+"""
+File for storing references to datasets.
+"""
+from typing import List
+
+from openmapflow.labeled_dataset import LabeledDataset, create_datasets
+
+# from openmapflow.raw_labels import RawLabels
+
+datasets: List[LabeledDataset] = [
+    # --------------------------------------------------------------------------
+    # Example LabeledDataset (remove once you have your own)
+    # --------------------------------------------------------------------------
+    # LabeledDataset(
+    #     dataset="example_dataset",
+    #     country="Togo",
+    #     raw_labels=(
+    #         RawLabels(
+    #             filename="Togo_2019.csv",
+    #             longitude_col="longitude",
+    #             latitude_col="latitude",
+    #             class_prob=lambda df: df["crop"],
+    #             start_year=2019,
+    #             x_y_from_centroid=False,
+    #         ),
+    #     ),
+    # ),
+]
+
+if __name__ == "__main__":
+    create_datasets(datasets)
