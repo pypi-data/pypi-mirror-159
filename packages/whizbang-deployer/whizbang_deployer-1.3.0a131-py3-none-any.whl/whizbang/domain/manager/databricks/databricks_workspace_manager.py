675dbaad8dc7c8d081b66ceae8b3df7383a28942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:29 +0000

    Add package whizbang-deployer/1.3.0a131/whizbang_deployer-1.3.0a131-py3-none-any.whl

---
+++ packages/whizbang-deployer/whizbang_deployer-1.3.0a131-py3-none-any.whl/whizbang/domain/manager/databricks/databricks_workspace_manager.py
@@ -0,0 +1,19 @@
+from whizbang.data.databricks.databricks_client_args import DatabricksClientArgs
+from whizbang.domain.manager.databricks.databricks_manager_base import DatabricksManagerBase, IDatabricksManager
+from whizbang.domain.models.databricks.databricks_notebook import DatabricksNotebook
+from whizbang.domain.repository.databricks.databricks_workspace_repository import IDatabricksWorkspaceRepository
+
+
+class IDatabricksWorkspaceManager(IDatabricksManager):
+    """"""
+
+
+class DatabricksWorkspaceManager(DatabricksManagerBase, IDatabricksWorkspaceManager):
+    def __init__(self, repository: IDatabricksWorkspaceRepository):
+        DatabricksManagerBase.__init__(self, repository)
+
+    def save(self, client_args: DatabricksClientArgs, t_object: DatabricksNotebook):
+        if t_object.source_path is None:
+            return None
+        else:
+            return self.repository.create(client_args=client_args, t_object=t_object)
