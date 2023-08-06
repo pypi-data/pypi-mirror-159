d916719986206d0aacbbce5b7686e97733cf6cf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:20 +0000

    Add package rexys/0.0.0/rexys-0.0.0.tar.gz

---
+++ packages/rexys/rexys-0.0.0.tar.gz/rexys-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'rexys',
+    packages = ['rexys'],
+    version = '0.0.0',
+    description = 'description',
+    author = '',
+    author_email = '',
+    url = '',
+    download_url = '',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
