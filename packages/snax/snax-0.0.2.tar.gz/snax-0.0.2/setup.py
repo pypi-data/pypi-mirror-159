85f777304fc5b5bdb635bb857254b787d1dfc0df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:58:35 +0000

    Add package snax/0.0.2/snax-0.0.2.tar.gz

---
+++ packages/snax/snax-0.0.2.tar.gz/snax-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='snax',
+    version='0.0.2',
+    author='Josef Ondrej',
+    author_email='josef.ondrej@outlook.com',
+    description='The truly lightweight feature store',
+    long_description='The truly lightweight feature store',
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=[]
+)
