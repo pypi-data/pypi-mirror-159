b70361f68e9f23ff965b3b921023554d35bfa4b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:51:15 +0000

    Add package PyEnzymeKinetics/1.1.1/PyEnzymeKinetics-1.1.1.tar.gz

---
+++ packages/PyEnzymeKinetics/PyEnzymeKinetics-1.1.1.tar.gz/PyEnzymeKinetics-1.1.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="PyEnzymeKinetics",
+    version="1.1.1",
+    url="https://github.com/haeussma/PyEnzymeKinetics",
+    author="Haeussler, Max",
+    author_email="max.haeussler@gmail.com",
+    packages=setuptools.find_packages(),
+    requires=["numpy", "matplotlib.pyplot", "lmfit"]
+)
