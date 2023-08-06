6ebd333daeddcfdd4e657c30dc5fc1b7276ba1cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:37 +0000

    Add package micropython-micronet/0.0.2/micropython-micronet-0.0.2.tar.gz

---
+++ packages/micropython-micronet/micropython-micronet-0.0.2.tar.gz/micropython-micronet-0.0.2/setup.py
@@ -0,0 +1,28 @@
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system.
+sys.path.pop(0)
+from setuptools import setup
+
+setup(    
+    name='micropython-micronet',
+    py_modules=['micronet'],
+    description='MicroPython library for networking',
+    long_description_content_type="text/markdown",
+    long_description="MicroPython library for networking",
+    url='https://github.com/yeranosyanvahan/micronet',
+    author='Vahan Yeranosyan',
+    author_email='vahan@yeranosyanvahan.com',
+    maintainer='Vahan Yeranosyan',
+    maintainer_email='vahan@yeranosyanvahan.com',
+    license='MIT',
+    classifiers = [
+        'Programming Language :: Python :: Implementation :: MicroPython',
+        'License :: OSI Approved :: MIT License',
+    ],
+    setuptools_git_versioning={
+        "enabled": True,
+    },
+    setup_requires=["setuptools-git-versioning"],
+
+)
