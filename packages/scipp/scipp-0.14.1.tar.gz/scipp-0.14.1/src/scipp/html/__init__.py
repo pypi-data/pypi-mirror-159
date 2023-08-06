656f91c3c7425e458b8624b82c6f4e5e450a4e61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:52:55 +0000

    Add package scipp/0.14.1/scipp-0.14.1.tar.gz

---
+++ packages/scipp/scipp-0.14.1.tar.gz/scipp-0.14.1/src/scipp/html/__init__.py
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+# @file
+# @author Dimitar Tasev
+
+from __future__ import annotations
+
+from ..typing import VariableLike
+from ..core import Variable
+
+
+def make_html(container: VariableLike) -> str:
+    from .formatting_html import dataset_repr, variable_repr
+    if isinstance(container, Variable):
+        return variable_repr(container)
+    else:
+        return dataset_repr(container)
+
+
+def to_html(container: VariableLike):
+    from IPython.display import display, HTML
+    display(HTML(make_html(container)))
