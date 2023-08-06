04dc51d7831019a6a3407487c71ca56e367290b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:18:20 +0000

    Add package bilix/0.6.13/bilix-0.6.13-py3-none-any.whl

---
+++ packages/bilix/bilix-0.6.13-py3-none-any.whl/bilix/utils.py
@@ -0,0 +1,27 @@
+import html
+import re
+
+
+def legal_title(title, add_name=''):
+    """
+
+    :param title: 主标题
+    :param add_name: 分P名
+    :return:
+    """
+    title, add_name = _replace(title), _replace(add_name)
+    title = _truncate(title)  # avoid OSError caused by title too long
+    return f'{title}-{add_name}' if add_name else title
+
+
+def _replace(s: str):
+    s = s.strip()
+    s = html.unescape(s)  # handel & "...
+    s = re.sub(r"[/\\:*?\"<>|]", '', s)  # replace illegal filename character
+    return s
+
+
+def _truncate(s: str, target=150):
+    while len(s.encode('utf8')) > target - 3:
+        s = s[:-1]
+    return s
