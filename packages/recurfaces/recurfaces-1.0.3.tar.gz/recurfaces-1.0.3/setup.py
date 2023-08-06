e1d2e2151d966cd8a3701afb9bfc42cf3990d9c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:18 +0000

    Add package recurfaces/1.0.3/recurfaces-1.0.3.tar.gz

---
+++ packages/recurfaces/recurfaces-1.0.3.tar.gz/recurfaces-1.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name="recurfaces",
+    packages=[
+        "recurfaces"
+    ],
+    version="1.0.3",
+    license="MIT",
+    description="A pygame framework used to organise Surfaces into a chain structure",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    author="immijimmi",
+    author_email="imranhamid99@msn.com",
+    url="https://github.com/immijimmi/recurfaces",
+    download_url="https://github.com/immijimmi/recurfaces/archive/refs/tags/v1.0.3.tar.gz",
+    keywords=["ui", "gui", "graphical", "user", "interface", "game"],
+    install_requires=[
+        "pygame~=2.0.1"
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
