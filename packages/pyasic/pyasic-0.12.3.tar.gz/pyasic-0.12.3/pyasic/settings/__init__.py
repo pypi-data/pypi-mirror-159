649c1da87392a6e2a74afcd925bda8ff618c9d05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:21 +0000

    Add package pyasic/0.12.3/pyasic-0.12.3.tar.gz

---
+++ packages/pyasic/pyasic-0.12.3.tar.gz/pyasic-0.12.3/pyasic/settings/__init__.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+@dataclass
+class PyasicSettings(metaclass=Singleton):
+    network_ping_retries: int = 1
+    network_ping_timeout: int = 3
+    network_scan_threads: int = 300
+
+    miner_factory_get_version_retries: int = 1
+
+    miner_get_data_retries: int = 1
+
+    global_whatsminer_password = "admin"
+
+    debug: bool = False
+    logfile: bool = False
