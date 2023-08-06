b975f7b20d99758df4030653d22457cf5086d51f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:06 +0000

    Add package streamlit-likedislike/0.0.6/streamlit_likedislike-0.0.6-py3-none-any.whl

---
+++ packages/streamlit-likedislike/streamlit_likedislike-0.0.6-py3-none-any.whl/streamlit_likedislike/frontend/build/precache-manifest.7937682da2fdc10df4016edae5cae8a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86df3e61a40746877c7561ee7732434c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9cf8dc67061863190907",
+    "url": "./static/css/main.c6d044e3.chunk.css"
+  },
+  {
+    "revision": "88a323ce65ff27c86af4",
+    "url": "./static/js/2.12481390.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.12481390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cf8dc67061863190907",
+    "url": "./static/js/main.91721807.chunk.js"
+  },
+  {
+    "revision": "f66c720174f9b8b612cd",
+    "url": "./static/js/runtime-main.bad378fd.js"
+  }
+]);
