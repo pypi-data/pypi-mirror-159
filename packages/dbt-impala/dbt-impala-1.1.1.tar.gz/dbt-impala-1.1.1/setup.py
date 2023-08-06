0ee538d5faa9e07af41c1a6d7e3e1f7503acbe71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:50 +0000

    Add package dbt-impala/1.1.1/dbt-impala-1.1.1.tar.gz

---
+++ packages/dbt-impala/dbt-impala-1.1.1.tar.gz/dbt-impala-1.1.1/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# Copyright 2022 Cloudera Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import find_namespace_packages, setup
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+package_name = "dbt-impala"
+# make sure this always matches dbt/adapters/dbt_impala/__version__.py
+package_version = "1.1.1"
+description = """The Impala adapter plugin for dbt"""
+
+setup(
+    name=package_name,
+    version=package_version,
+    description="Impala adapter for DBT",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Cloudera",
+    author_email="innovation-feedback@cloudera.com",
+    url="https://github.com/cloudera/dbt-impala",
+    packages=find_namespace_packages(include=['dbt', 'dbt.*']),
+    include_package_data=True,
+    install_requires=[
+        "dbt-core>=1.1.0",
+        "impyla>=0.18a5"
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: Apache Software License"
+    ],
+    zip_safe=False
+)
