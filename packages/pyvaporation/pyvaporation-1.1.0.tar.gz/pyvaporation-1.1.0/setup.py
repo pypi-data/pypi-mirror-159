b9acfb43ffcb5ca9b78935d0b16b626ea30313a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:16 +0000

    Add package pyvaporation/1.1.0/pyvaporation-1.1.0.tar.gz

---
+++ packages/pyvaporation/pyvaporation-1.1.0.tar.gz/pyvaporation-1.1.0/setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+
+setup(
+    name="pyvaporation",
+    packages=["pyvaporation"],
+    version="1.1.0",
+    license="Apache license 2.0",
+    description="Set of tools for modelling pervaporation processes",
+    author="Denis Sapegin, Aleksei Chekmachev",
+    author_email="a.checkmachev@gmail.com",
+    url="https://github.com/Membrizard/PyVaporation",
+    download_url="https://github.com/Membrizard/PyVaporation/archive/refs/tags/v1.1.0.tar.gz",
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type="text/markdown",
+    keywords=[
+        "pervaporation",
+        "membrane",
+        "chemistry",
+        "modelling",
+    ],
+    install_requires=[
+        "joblib>=1.1.0",
+        "matplotlib>=3.5.2",
+        "pandas>=1.3.5",
+        "scipy>=1.7.3",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
