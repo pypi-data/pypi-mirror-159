191d63ef920d60427610a31131fcfd36752df14d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:03 +0000

    Add package gopro-overlay/0.47.0/gopro-overlay-0.47.0.tar.gz

---
+++ packages/gopro-overlay/gopro-overlay-0.47.0.tar.gz/gopro-overlay-0.47.0/gopro_overlay/layout_components.py
@@ -0,0 +1,55 @@
+from .framemeta import Window
+from .timeunits import timeunits
+from .widgets import Text, CachingText
+from .widgets_chart import SimpleChart
+from .widgets_map import MovingMap, JourneyMap
+
+
+def journey_map(at, entry, **kwargs):
+    return JourneyMap(
+        at=at,
+        location=lambda: entry().point,
+        **kwargs
+    )
+
+
+def moving_map(at, entry, **kwargs):
+    return MovingMap(
+        at=at,
+        location=lambda: entry().point,
+        azimuth=lambda: entry().azi,
+        **kwargs
+    )
+
+
+def metric_value(entry, accessor, converter, formatter, default="-"):
+    def value():
+        v = accessor(entry())
+        if v is not None:
+            v = converter(v)
+            return formatter(v.magnitude)
+        return default
+
+    return value
+
+
+def text(cache=True, **kwargs):
+    if cache:
+        return CachingText(**kwargs)
+    else:
+        return Text(**kwargs)
+
+
+def metric(entry, accessor, formatter, converter=lambda x: x, cache=True, **kwargs):
+    return text(cache, value=metric_value(entry, accessor, converter, formatter), **kwargs)
+
+
+def gradient_chart(at, timeseries, entry, font_title):
+    window = Window(timeseries, duration=timeunits(minutes=5), samples=256,
+                    key=lambda e: e.alt, fmt=lambda v: v.to("meter").magnitude)
+    return SimpleChart(
+        at=at,
+        value=lambda: window.view(timeunits(millis=entry().timestamp.magnitude)),
+        font=font_title,
+        filled=True
+    )
