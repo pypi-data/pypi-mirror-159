e25060779de0be353fc053bea45a5877fdc6a6d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:52:16 +0000

    Add package lisainstrument/1.0.5/lisainstrument-1.0.5.tar.gz

---
+++ packages/lisainstrument/lisainstrument-1.0.5.tar.gz/lisainstrument-1.0.5/lisainstrument/__init__.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""LISA Instrument module."""
+
+from .meta import __version__
+from .meta import __author__
+from .meta import __email__
+
+from .instrument import Instrument
