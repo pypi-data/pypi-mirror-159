3ba2f138ec3ac672d068df3c666d7b43f453c6fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:46 +0000

    Add package sqlfluff/1.2.1/sqlfluff-1.2.1-py3-none-any.whl

---
+++ packages/sqlfluff/sqlfluff-1.2.1-py3-none-any.whl/sqlfluff/rules/L005.py
@@ -0,0 +1,56 @@
+"""Implementation of Rule L005."""
+from typing import Optional
+
+from sqlfluff.core.parser import RawSegment
+from sqlfluff.core.rules.base import BaseRule, LintResult, LintFix, RuleContext
+from sqlfluff.core.rules.doc_decorators import document_fix_compatible, document_groups
+
+
+@document_groups
+@document_fix_compatible
+class Rule_L005(BaseRule):
+    """Commas should not have whitespace directly before them.
+
+    Unless it's an indent. Trailing/leading commas are dealt with
+    in a different rule.
+
+    **Anti-pattern**
+
+    The ``•`` character represents a space.
+    There is an extra space in line two before the comma.
+
+    .. code-block:: sql
+       :force:
+
+        SELECT
+            a•,
+            b
+        FROM foo
+
+    **Best practice**
+
+    Remove the space before the comma.
+
+    .. code-block:: sql
+
+        SELECT
+            a,
+            b
+        FROM foo
+    """
+
+    groups = ("all", "core")
+
+    def _eval(self, context: RuleContext) -> Optional[LintResult]:
+        """Commas should not have whitespace directly before them."""
+        anchor: Optional[RawSegment] = context.raw_segment_pre
+        if (
+            # We need at least one segment previous segment for this to work.
+            anchor is not None
+            and context.segment.is_type("comma")
+            and anchor.is_type("whitespace")
+            and anchor.pos_marker.line_pos > 1
+        ):
+            return LintResult(anchor=anchor, fixes=[LintFix.delete(anchor)])
+        # Otherwise fine.
+        return None
---
+++ packages/sqlfluff/sqlfluff-1.2.1-py3-none-any.whl/sqlfluff/rules/L021.py
@@ -0,0 +1,57 @@
+"""Implementation of Rule L021."""
+from typing import Optional
+
+from sqlfluff.core.rules.base import BaseRule, LintResult, RuleContext
+import sqlfluff.core.rules.functional.segment_predicates as sp
+from sqlfluff.core.rules.doc_decorators import document_groups
+
+
+@document_groups
+class Rule_L021(BaseRule):
+    """Ambiguous use of ``DISTINCT`` in a ``SELECT`` statement with ``GROUP BY``.
+
+    When using ``GROUP BY`` a `DISTINCT`` clause should not be necessary as every
+    non-distinct ``SELECT`` clause must be included in the ``GROUP BY`` clause.
+
+    **Anti-pattern**
+
+    ``DISTINCT`` and ``GROUP BY`` are conflicting.
+
+    .. code-block:: sql
+
+        SELECT DISTINCT
+            a
+        FROM foo
+        GROUP BY a
+
+    **Best practice**
+
+    Remove ``DISTINCT`` or ``GROUP BY``. In our case, removing ``GROUP BY`` is better.
+
+    .. code-block:: sql
+
+        SELECT DISTINCT
+            a
+        FROM foo
+    """
+
+    groups = ("all", "core")
+
+    def _eval(self, context: RuleContext) -> Optional[LintResult]:
+        """Ambiguous use of DISTINCT in select statement with GROUP BY."""
+        segment = context.functional.segment
+        if (
+            segment.all(sp.is_type("select_statement"))
+            # Do we have a group by clause
+            and segment.children(sp.is_type("groupby_clause"))
+        ):
+            # Do we have the "DISTINCT" keyword in the select clause
+            distinct = (
+                segment.children(sp.is_type("select_clause"))
+                .children(sp.is_type("select_clause_modifier"))
+                .children(sp.is_type("keyword"))
+                .select(sp.is_name("distinct"))
+            )
+            if distinct:
+                return LintResult(anchor=distinct[0])
+        return None
