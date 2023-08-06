f0c3dbb5cd9a5c443dcc34cbd176631c6ecdd0a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:47:31 +0000

    Add package aiosumma/2.4.3/aiosumma-2.4.3-py3-none-any.whl

---
+++ packages/aiosumma/aiosumma-2.4.3-py3-none-any.whl/aiosumma/tree_transformers/morphy.py
@@ -0,0 +1,40 @@
+from izihawa_nlptools.morph import EnglishMorphology, RussianMorphology
+
+from ..parser.elements import (
+    Boost,
+    SynonymsGroup,
+    Word,
+)
+from .base import TreeTransformer
+
+
+class MorphyTreeTransformer(TreeTransformer):
+    """
+    Creates forms of words
+    """
+    morphology = {
+        'en': EnglishMorphology(),
+        'ru': RussianMorphology()
+    }
+
+    def __init__(self, enable_morph=True, enable_accent=True, score: str = '0.65', ignore_nodes=None):
+        super().__init__(ignore_nodes=ignore_nodes)
+        self.enable_morph = enable_morph
+        self.enable_accent = enable_accent
+        self.score = score
+
+    def visit_word(self, node, context, parents=None):
+        forms = [node]
+
+        if self.enable_accent and 'ё' in node.value:
+            forms.append(Word(node.value.replace('ё', 'е')))
+
+        if self.enable_morph and context.language in self.morphology:
+            for w in self.morphology[context.language].derive_forms(node.value):
+                if node.value != w:
+                    forms.append(Boost(Word(w), score=self.score))
+
+        if len(forms) == 1:
+            return node, True
+
+        return SynonymsGroup(*forms), True
