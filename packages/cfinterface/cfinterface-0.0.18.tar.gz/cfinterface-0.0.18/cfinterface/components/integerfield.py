14b2423f5d0e7f017621b1dc2e895f423e67fb2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:27 +0000

    Add package cfinterface/0.0.18/cfinterface-0.0.18.tar.gz

---
+++ packages/cfinterface/cfinterface-0.0.18.tar.gz/cfinterface-0.0.18/cfinterface/components/integerfield.py
@@ -0,0 +1,46 @@
+from typing import Optional
+
+from cfinterface.components.field import Field
+
+
+class IntegerField(Field):
+    """
+    Class for representing an integer field for being read from and
+    written to a file.
+    """
+
+    def __init__(
+        self,
+        size: int = 16,
+        starting_column: int = 0,
+        value: Optional[int] = None,
+    ) -> None:
+        super().__init__(size, starting_column, value)
+
+    # Override
+    def read(self, line: str) -> Optional[int]:
+        linevalue = line[self._starting_column : self._ending_column].strip()
+        self._value = int(linevalue) if linevalue.isdigit() else None
+        return self._value
+
+    # Override
+    def write(self, line: str) -> str:
+        if self.value is None:
+            value = ""
+        else:
+            value = str(self.value)
+        if len(line) < self._ending_column:
+            line = line.ljust(self._ending_column)
+        return (
+            line[: self._starting_column]
+            + value.rjust(self._size)
+            + line[self._ending_column :]
+        )
+
+    @property
+    def value(self) -> Optional[int]:
+        return self._value
+
+    @value.setter
+    def value(self, val: int):
+        self._value = val
