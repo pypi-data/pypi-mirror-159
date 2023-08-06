336926fe0c50d0aab1ad283a8779c9b5ef5f8182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:49 +0000

    Add package py-fastapi-logging/0.1.5/py-fastapi-logging-0.1.5.tar.gz

---
+++ packages/py-fastapi-logging/py-fastapi-logging-0.1.5.tar.gz/py-fastapi-logging-0.1.5/py_fastapi_logging/formatters/colored.py
@@ -0,0 +1,45 @@
+import logging
+from py_fastapi_logging.formatters.base import BaseFormatter
+
+
+class ColoredLogFormatter(BaseFormatter):
+
+    @staticmethod
+    def _format_log(record: logging.LogRecord) -> dict:
+        t = self.formatTime(record)
+        level = record.levelname
+        line = record.lineno
+        color = COLOR_MAPPING.get(level) or '\33[34m'  # blue
+        default_color = '\33[0m'
+        msg = record.getMessage()
+        tags = record.__dict__.get("tags", None)
+        tags = tags if not tags else ' '.join(tags)
+
+        if record.exc_info:
+            # Cache the traceback text to avoid converting it multiple times
+            # (it's constant anyway)
+            if not record.exc_text:
+                record.exc_text = self.formatException(record.exc_info)
+        s = f'{color}{t} {level} '
+
+        s += f'{record.__dict__.get("request_id", "")}'
+        if tags:
+            s += f' {tags}'
+        s += f'\n{record.pathname} line {line}{default_color}\n' \
+            f'{msg}\n'
+
+        if record.exc_info:
+            # Cache the traceback text to avoid converting it multiple times
+            # (it's constant anyway)
+            if not record.exc_text:
+                record.exc_text = self.formatException(record.exc_info)
+        if record.exc_text:
+            if s[-1:] != "\n":
+                s = s + "\n"
+            s = s + record.exc_text
+        if record.stack_info:
+            if s[-1:] != "\n":
+                s = s + "\n"
+            s = s + self.formatStack(record.stack_info)
+
+        return s
