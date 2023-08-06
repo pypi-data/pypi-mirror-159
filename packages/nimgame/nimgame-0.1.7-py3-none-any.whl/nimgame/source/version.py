8b658292acd7dcaed79e397670dbc0509dd302b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:32 +0000

    Add package nimgame/0.1.7/nimgame-0.1.7-py3-none-any.whl

---
+++ packages/nimgame/nimgame-0.1.7-py3-none-any.whl/nimgame/source/version.py
@@ -0,0 +1,18 @@
+"""Version in a separate module so that it can be imported in several places
+
+The version info and string has the following elements:
+
+    Major version
+        to increase, when something substantial changes or not backward compatible
+        anymore
+
+    Minor version
+        to increase, when new (backward compatible) features are introduced
+
+    Patches
+        to fix issues
+"""
+
+# Version of the package
+__version_info__ = ('0', '1', '7')
+__version__ = '.'.join(__version_info__)
