45115f345e0e28cb430ac3ee66e04badb9a3f381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:55 +0000

    Add package py-math-help/0.0.1/py_math_help-0.0.1.tar.gz

---
+++ packages/py-math-help/py_math_help-0.0.1.tar.gz/py_math_help-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name = "py_math_help",
+    author = "Harsh Gupta",
+    version='0.0.1',
+    author_email = "harshnkgupta@gmail.com",
+    description = "A Perfect Math Module For Beginners. ",
+    long_description="This module is extensively designed for non-math coders and beginners. The user need not know math to solve complex math problems. This module does it all for you.The most special feature of this module is its search() function. Now Just search your query after importing the module and it would be resolved. This module consists over 50+ math formulas and is still developing.\n\n Syntax: \n >>>import py_math_help \n >>>py_math_help.search(<Type your problem here>) \n Example: >>>py_math_help.search(\'area of triangle\') \n\n\nType >>>py_math_help.help() for further help.",
+    packages=['py_math_help'],
+    install_requires=[]
+    )
