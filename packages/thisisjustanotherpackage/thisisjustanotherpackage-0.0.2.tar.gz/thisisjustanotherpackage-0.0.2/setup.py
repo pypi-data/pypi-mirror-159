503491ce0f0cabbcbe544e4f72cd91b2d30210db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:19 +0000

    Add package thisisjustanotherpackage/0.0.2/thisisjustanotherpackage-0.0.2.tar.gz

---
+++ packages/thisisjustanotherpackage/thisisjustanotherpackage-0.0.2.tar.gz/thisisjustanotherpackage-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='thisisjustanotherpackage',
+    version='0.0.2',
+    description='this is just another package',
+    author='thisisjust',
+    author_email='this@is.just',
+    packages=['package'],
+    install_requires=['urllib3']
+)
