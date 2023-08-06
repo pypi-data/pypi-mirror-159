869e783e87c881f14b2c9aefbb178db3b9fb3495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:51 +0000

    Add package sceleto2/1.1.2/sceleto2-1.1.2.tar.gz

---
+++ packages/sceleto2/sceleto2-1.1.2.tar.gz/sceleto2-1.1.2/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+setup(
+name='sceleto2',
+license='MIT',
+author='Jongeun Park',
+author_email='jp24@kaist.ac.kr',
+description='sceleto2 is a wrapper package developed by SCMGL to aid in single cell analysis.',
+
+include_package_data=True,
+python_requires='>=3.7',
+
+keywords=['sceleto', 'single cell', 'scRNA-seq'],
+
+packages=find_packages(include=['sceleto2','sceleto2.*','.']),
+
+install_requires=[
+   'pandas',
+   'numpy',
+   'scanpy',
+   'scipy',
+   'seaborn',
+   'networkx',
+   'python-igraph',
+   'bbknn',
+   'geosketch',
+   'joblib',
+   'datetime',
+   'harmonypy',
+   'matplotlib',
+   'geosketch',
+   'scrublet',
+   'adjustText',
+   'numba',
+   'scikit-learn',
+   'diffxpy',
+   ],
+version='1.1.2',
+long_description=open('README.md').read(),
+long_description_content_type='text/markdown',
+zip_safe=False,
+)
