3b7ce2ad9377b2bedb0b8c3db0a7ef694023da12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:19 +0000

    Add package aws-prototyping-sdk.awsui-react-ts-website/0.3.12/aws_prototyping_sdk.awsui_react_ts_website-0.3.12.tar.gz

---
+++ packages/aws-prototyping-sdk.awsui-react-ts-website/aws_prototyping_sdk.awsui_react_ts_website-0.3.12.tar.gz/aws_prototyping_sdk.awsui_react_ts_website-0.3.12/src/aws_prototyping_sdk/awsui_react_ts_website/_jsii/__init__.py
@@ -0,0 +1,24 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import projen._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-prototyping-sdk/awsui-react-ts-website",
+    "0.3.12",
+    __name__[0:-6],
+    "awsui-react-ts-website@0.3.12.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
