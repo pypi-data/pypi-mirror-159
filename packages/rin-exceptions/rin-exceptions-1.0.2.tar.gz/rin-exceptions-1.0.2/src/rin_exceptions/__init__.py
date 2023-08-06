ef9cfd5ad198ae56aa608a54d616fc31227b6361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:09 +0000

    Add package rin-exceptions/1.0.2/rin-exceptions-1.0.2.tar.gz

---
+++ packages/rin-exceptions/rin-exceptions-1.0.2.tar.gz/rin-exceptions-1.0.2/src/rin_exceptions/__init__.py
@@ -0,0 +1,14 @@
+from .main import (Error, HTTPException, InvalidToken, NoItemsError,
+                   NotFoundHTTPException, ThereIsaRSlashInSubreddit,
+                   UnknownPlayer, ItemNotFound)
+
+__all__ = [
+    "UnknownPlayer",
+    "NotFoundHTTPException",
+    "NoItemsError",
+    "Error",
+    "ThereIsaRSlashInSubreddit",
+    "HTTPException",
+    "InvalidToken",
+    "ItemNotFound"
+]
