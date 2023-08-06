7482bb4466a07566163bc823187d616a9f733f91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:30 +0000

    Add package tsopt/0.0.3/tsopt-0.0.3.tar.gz

---
+++ packages/tsopt/tsopt-0.0.3.tar.gz/tsopt-0.0.3/src/tsopt/data.py
@@ -0,0 +1,268 @@
+# Maintainer:     Ryan Young
+# Last Modified:  Jul 11, 2022
+import pandas as pd
+import numpy as np
+import re
+from typing import List
+
+
+class DV:
+    '''
+    Contains the metadata that describes a network's layers and nodes.
+    Holds a list of each layer name, a list of abbreviations for each layer,
+    and a list of the names of nodes in each layer.
+    --
+    Uses strict rules for getting/setting values so that nobody accidentally
+    changes something that must not change.
+    '''
+    def __init__(self, layers: list, sizes:list):
+        self.__layers = [layer.capitalize() for layer in layers]
+        self.__abbrevs = ["".join([s[0] for s in re.split("[ -\._]", layer)]).upper() for layer in layers]
+        self.__sizes = sizes
+
+        assert len(set(self.__abbrevs)) == len(self.__abbrevs), \
+                "\nLayer names must start with different letters.\n" \
+                "You can work around this by using multiple words for layer names\n' \
+                'For instance, 'manufacturing center' becomes 'MC')"
+
+        assert [not s[0].isdigit() for s in self.__abbrevs], \
+                f"Layer names must not start with a number."
+
+    @property
+    def layers(self):
+        return self.__layers
+
+    @property
+    def abbrevs(self):
+        return self.__abbrevs
+
+    @property
+    def nodes(self):
+        try:
+            return self.__nodes
+        except AttributeError:
+            if self.__sizes != None:
+                self.__nodes = [
+                        [f'{abbrev}{i+1}' for i in range(0, length)]
+                    for abbrev, length in zip(self.abbrevs, self.sizes)
+                ]
+                return self.__nodes
+
+    @property
+    def sizes(self):
+        return self.__sizes
+
+    @sizes.setter
+    def sizes(self, new):
+        assert len(new) == len(self.__layers), 'Must provide a size for each layer'
+        del self.__nodes
+        self.__sizes = new
+
+
+
+class SourceData:
+    """
+    Responsible for processing user data into everthing needed to build a pyomo model
+    for transportation problems.
+    """
+    def __init__(self,
+            cost: list,
+            capacity: str or pd.DataFrame or dict,
+            demand: str or pd.DataFrame or dict,
+            layers: list,
+            excel_file=None,
+        ):
+
+        if type(excel_file) == str:
+            self.excel_file = pd.ExcelFile(excel_file)
+        elif type(excel_file) == pd.ExcelFile:
+            self.excel_file = excel_file
+        else:
+            self.excel_file = None
+
+        # Process user data into dataframes, handling unknown table formatting
+        self.cost = self.process_cost_data(cost, layers)
+
+        sizes = [df.shape[0] for df in self.cost] + [self.cost[-1].shape[1]]
+        self.dv = DV(layers, sizes)
+
+        # Set columns and indexes according to node names
+        # Indexes are input nodes, columns are output nodes
+        for i, df in enumerate(self.cost):
+            df.index     = self.dv.nodes[i]
+            df.columns   = self.dv.nodes[i+1]
+
+        self.capacity = self.process_capacity_data(capacity)
+        self.demand = self.process_demand_data(demand)
+
+        self.validate_capacity_demand()
+
+
+    def sheet_format(self, df) -> list:
+        """
+        Returns location of headers and index col in dataframe of unknown format
+        1. Headers (None, 0 or 1)
+        2. Indexes (None or 0)
+        --
+        These values can be passed to pd.read_excel() and pd.read_csv() to
+        accurately retrieve a data table from a source with unknown formatting
+        """
+
+        if 1 in df.shape:
+            if df.shape[1] == 1 and not self.valid_dtype(df.iloc[0,0]):
+                return 0, None
+            if df.shape[0] == 1 and not self.valid_dtype(df.iloc[0,0]):
+                return None, 0
+
+            return None, None
+
+        if not self.valid_dtype(df.iloc[1,1]):
+            return (1 if not self.valid_dtype(df.iloc[2, 0]) else 0), 0
+
+        return [(0 if not self.valid_dtype(df.iloc[0,1]) else None),
+                (0 if not self.valid_dtype(df.iloc[1,0]) else None)]
+
+
+    def valid_dtype(self, val) -> bool:
+        """
+        A foolproof way to determine if a value is a number.
+        We need this (instead of type() or isnumeric())
+        because pandas and numpy are annoying.
+        """
+        try:
+            float(val)
+            return True
+        except ValueError:
+            return False
+
+
+    def read_file(self, table:str, idx=None, hdr=None) -> pd.DataFrame:
+        """
+        Param 'table' will be a filename if reading from csv,
+        or a sheet name if reading from excel
+        """
+        if table.endswith('.csv'):
+            return pd.read_csv(table, index_col=idx, header=hdr)
+        if table.endswith('.xlsx'):
+            return pd.read_excel(table, index_col=idx, header=hdr)
+
+        return pd.read_excel(self.excel_file, table, index_col=idx, header=hdr)
+
+
+    def process_file(self, table: str or pd.DataFrame) -> pd.DataFrame:
+        '''
+        Takes the name of an excel table or csv file,
+        and returns a dataframe formatted properly, except
+        for columns and indexes
+        '''
+        if type(table) == pd.DataFrame:
+            return table
+        # Start by reading in the file with headers=None, index_col=None.
+        # This way ALL provided data will be placed INSIDE the dataframe as values
+        df = self.read_file(table)
+
+        # Find the headers and indexes, if any exist
+        headers, indexes = self.sheet_format(df)
+
+        # Now read the file again, passing index and header locations
+        df = self.read_file(table, idx=indexes, hdr=headers)
+        df = df.replace(' ', np.NaN)
+        df = df.dropna(axis=1, how='all')
+        df = df.dropna(axis=0, how='all')
+        df.index.name = None
+        return df
+
+
+    def process_cost_data(self, tables:list, layers:list) -> list:
+        '''
+        Given a list of strings or dataframes representing cost data,
+        process them into dataframes and validate.
+        '''
+        if type(tables) != list:
+            tables = [tables]
+        assert len(tables) == len(layers)-1, \
+                "Number of layers must be one more than number of cost stages"
+        dfs = [self.process_file(table) for table in tables]
+
+        for i in range(0, len(dfs)-1):
+            assert dfs[i].shape[1] == dfs[i+1].shape[0], \
+                    f"Number of columns in Stage {i} costs ({dfs[i].shape[1]}) ' \
+                    f'and rows in Stage {i+1} costs ({dfs[i+1].shape[0]}) must match"
+
+        return dfs
+
+
+    def process_capacity_data(self, tables:dict or str) -> dict:
+        '''
+        Given a dict of capacity tables, keyed by layer number, process
+        their values into dataframes, validate, and format rows and cols
+        '''
+        if type(tables) != dict:
+            tables = {0: tables}
+
+        assert 0 in tables, 'Must provide a capacity constraint table for first layer'
+
+        final = dict()
+        for key, table in tables.items():
+            assert type(key) == int, 'Must use integer as key in capacity dictionary'
+            assert 0 <= key < len(self.cost), \
+                    f"Capacity constraints can only be made for layers 0 through {len(self.cost)-1}"
+            df = self.process_file(table)
+            assert df.shape[1] == 1, f"Capacity table {key} must have only one column"
+            assert len(self.dv.nodes[key]) == df.shape[0], \
+                    f"Number of rows in Capacity {key} and Costs {key} must match"
+
+            # Set index and cols based on node and layer names
+            df.index = self.dv.nodes[key]
+            df.columns = [f'Capacity: {self.dv.layers[key]}']
+            final[key] = df
+
+        return final
+
+
+    def process_demand_data(self, tables:dict or str) -> dict:
+        '''
+        Given a dict of demand tables, keyed by layer number, process
+        their values into dataframes, validate, and format rows and cols
+        '''
+        if type(tables) != dict:
+            tables = {len(self.cost): tables}
+
+        assert len(self.cost) in tables, 'Must provide a demand constraint table for final layer'
+
+        final = dict()
+        for key, table in tables.items():
+            assert type(key) == int, 'Must use integer as key in demand dictionary'
+            assert 0 < key <= len(self.cost), \
+                    f"Demand constraints only allowed for layers 1 through {len(self.cost)}"
+            df = self.process_file(table)
+            assert df.shape[1] == 1, f"Demand table {key} must have only one column"
+            assert len(self.dv.nodes[key]) == df.shape[0], \
+                    f"Number of rows in Demand {key} and Costs {key} must match"
+
+            # Set index and cols based on node and layer names
+            df.index = self.dv.nodes[key]
+            df.columns = [f'Demand: {self.dv.layers[key]}']
+            final[key] = df
+
+        return final
+
+
+    def validate_capacity_demand(self) -> bool:
+        # Any total layer capacity must be greater than
+        # or equal to all demand that follows
+        for i, cap in self.capacity.items():
+            later_demands = {k: v for k, v in self.demand.items() if k > i}
+            for j, dem in later_demands.items():
+                assert dem.iloc[:, 0].sum() <= cap.iloc[:, 0].sum(), \
+                        f'{self.dv.layers[i]} capacity is less than {self.dv.layers[j]} demand. ' \
+                        f'Total capacity at a layer must be greater than or equal to total demand ' \
+                        f'at all following layers.'
+
+
+
+
+
+
+
+
---
+++ packages/tsopt/tsopt-0.0.3.tar.gz/tsopt-0.0.3/src/tsopt/model.py
@@ -0,0 +1,154 @@
+import pandas as pd
+import numpy as np
+import pyomo.environ as pe
+from pyomo.opt import SolverStatus, TerminationCondition
+import matplotlib.pyplot as plt
+import seaborn as sns
+from dataclasses import dataclass
+
+from tsopt.data import SourceData
+from tsopt.solution import Solution
+
+
+
+class Model(SourceData):
+    """
+    Pyomo wrapper for multi-stage transshipment optimization problems,
+    where you have 3 or more location layers, and product transport between them.
+    For example, you have 3 manufacturing plants, 2 distributors, and 5 warehouses,
+    and you need to minimize cost from plant to distributor and from distributor to warehouse,
+    while staying within capacity and meeting demand requirements.
+    """
+    solver = pe.SolverFactory('glpk')
+
+    def __init__(self, cell_constraints=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.mod = self.new_model()
+
+
+    @property
+    def constraints(self):
+        return {str(c): c for c in self.mod.component_objects(pe.Constraint)}
+
+
+    def new_model(self) -> pe.ConcreteModel:
+        return pe.ConcreteModel()
+
+
+    def add_decision_var(self, name:str, variable:pe.Var or any, domain=pe.NonNegativeReals, **kwargs) -> pe.Var:
+        if type(variable) == pe.Var:
+            setattr(self.mod, name, variable)
+        else:
+            setattr(self.mod, name, pe.Var(variable, domain=domain, **kwargs))
+        return getattr(self.mod, name)
+
+
+    def add_constraint(self, name, constr, prefix=None, **kwargs) -> pe.Constraint:
+        name = name if prefix == None else f'{prefix}_{name}'
+        if type(constr) == pe.Constraint:
+            setattr(self.mod, name, constr, **kwargs)
+        else:
+            setattr(self.mod, name, pe.Constraint(expr=constr))
+
+        return getattr(self.mod, name)
+
+    def set_decision_vars(self):
+        for stage in range(0, len(self.cost)):
+            for node in self.dv.nodes[stage]:
+                self.add_decision_var(node, self.dv.nodes[stage+1])
+
+
+    def set_objective(self):
+        total_cost = 0
+        for stage, df in enumerate(self.cost):
+            for node_in in self.dv.nodes[stage]:
+                for node_out in self.dv.nodes[stage+1]:
+                    total_cost += df.loc[node_in, node_out] * getattr(self.mod, node_in)[node_out]
+        self.mod.obj = pe.Objective(expr = total_cost, sense=pe.minimize)
+
+
+    def set_capacity_constraints(self):
+        for i, coefs in self.capacity.items():
+            input_nodes = self.dv.nodes[i]
+            output_nodes = self.dv.nodes[i+1]
+            for node_in in input_nodes:
+                capacity = coefs.loc[node_in, coefs.columns[0]]
+                expr = capacity >= sum(getattr(self.mod, node_in)[node_out] for node_out in output_nodes)
+                self.add_constraint(node_in, expr, 'capacity')
+
+
+    def set_demand_constraints(self):
+        for i, coefs in self.demand.items():
+            input_nodes = self.dv.nodes[i-1]
+            output_nodes = self.dv.nodes[i]
+            for node_out in output_nodes:
+                demand = coefs.loc[node_out, coefs.columns[0]]
+                expr = demand <= sum(getattr(self.mod, node_in)[node_out] for node_in in input_nodes)
+                self.add_constraint(node_out, expr, 'demand')
+
+
+    def set_flow_constraints(self):
+        '''
+        Equal flow for all stages.
+        A flow is a layer which takes input from a previous layer and
+        outputs to the next layer. If there are 3 layers, then there must
+        be two stages, and one flow: the middle layer.
+        '''
+        flow_layer_indexes = range(1, len(self.dv.nodes)-1)
+        for flow in flow_layer_indexes:
+            for node_curr in self.dv.nodes[flow]:
+                input_nodes = self.dv.nodes[flow-1]
+                output_nodes = self.dv.nodes[flow+1]
+                inflow = sum([getattr(self.mod, node_in)[node_curr] for node_in in input_nodes])
+                outflow = sum([getattr(self.mod, node_curr)[node_out] for node_out in output_nodes])
+                expr = inflow == outflow
+                name = f'{node_curr}_{self.dv.abbrevs[flow+1]}'
+                self.add_constraint(name, expr, 'flow')
+
+
+    def build_model(self) -> pe.ConcreteModel:
+        self.mod = self.new_model()
+        self.set_decision_vars()
+        self.set_objective()
+        self.set_capacity_constraints()
+        self.set_demand_constraints()
+        self.set_flow_constraints()
+        return self.mod
+
+
+    def solve_model(self) -> Solution:
+        success = Model.solver.solve(self.mod)
+        status = success.solver.status
+        termination_condition = success.solver.termination_condition
+        if status != 'ok':
+            print('Error: Status:', status)
+        if termination_condition != 'optimal':
+            print('Error: Termination condition:', termination_condition)
+
+        self.solution = Solution(self.dv, self.cost, self.mod, self.constraints, status, termination_condition)
+        return self.solution
+
+
+    def run(self) -> Solution:
+        self.build_model()
+        return self.solve_model()
+
+
+    def display(self):
+        for i, df in self.capacity.items():
+            print(f'Output capacity from {self.dv.layers[i]}')
+            display(df)
+        for i, df in self.demand.items():
+            print(f'Demand required from {self.dv.layers[i]}')
+            display(df)
+        for i, df in enumerate(self.cost):
+            print(f'{self.dv.layers[i]} to {self.dv.layers[i+1]} costs')
+            display(df)
+
+
+    def print_dv_indexes(self):
+        for i, nodes in enumerate(self.dv.nodes):
+            print(f"{self.dv.layers[i]}:\n- ", end="")
+            print(*nodes, sep=", ")
+
