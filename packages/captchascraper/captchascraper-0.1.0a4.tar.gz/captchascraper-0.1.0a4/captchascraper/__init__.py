5de8f9856b5c444dae0fc2b2a89174cce01a56e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:45 +0000

    Add package captchascraper/0.1.0a4/captchascraper-0.1.0a4.tar.gz

---
+++ packages/captchascraper/captchascraper-0.1.0a4.tar.gz/captchascraper-0.1.0a4/captchascraper/__init__.py
@@ -0,0 +1,17 @@
+import requests
+
+
+
+def get(url,apikey,**kwargs):
+      return requests.get(f"http://api.scraperapi.com?api_key={apikey}&url=" + url + "&keep_headers=True",**kwargs)
+      
+def post(url,apikey,data,**kwargs):
+      return requests.post(f"http://api.scraperapi.com?api_key={apikey}&url=" + url + "&keep_headers=True",data=data,**kwargs)
+      
+def put(url,apikey,**kwargs):
+      return requests.put(f"http://api.scraperapi.com?api_key={apikey}&url=" + url + "&keep_headers=True",**kwargs)
+def patch(url,apikey,data,**kwargse):
+      return requests.patch(f"http://api.scraperapi.com?api_key={apikey}&url=" + url + "&keep_headers=True",data=data,**kwargse)
+
+def delete(url,apikey,**kwargs):
+      return requests.delete(f"http://api.scraperapi.com?api_key={apikey}&url=" + url + "&keep_headers=True",**kwargs)
