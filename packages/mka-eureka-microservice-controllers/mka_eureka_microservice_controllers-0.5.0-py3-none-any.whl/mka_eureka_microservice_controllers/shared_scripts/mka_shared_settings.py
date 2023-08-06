3502583ba59282de0cf7e1bb142a6746a2b0affe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:36 +0000

    Add package mka-eureka-microservice-controllers/0.5.0/mka_eureka_microservice_controllers-0.5.0-py3-none-any.whl

---
+++ packages/mka-eureka-microservice-controllers/mka_eureka_microservice_controllers-0.5.0-py3-none-any.whl/mka_eureka_microservice_controllers/shared_scripts/mka_shared_settings.py
@@ -0,0 +1,116 @@
+
+
+
+
+import os
+import datetime
+
+ALLOWED_HOSTS = ['*']
+
+
+DEBUG = True
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_PATH=os.path.abspath(os.path.dirname(__name__))
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'rest_framework_swagger'
+]
+
+
+
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework_yaml.parsers.JSONParser',
+        'rest_framework_yaml.parsers.MultiPartParser',
+        'rest_framework_yaml.parsers.FormParser',
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+}
+SIMPLE_JWT  = {
+    'JWT_ALLOW_REFRESH': True,
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=2),
+    'JWT_AUTH_HEADER_PREFIX': 'Token',
+    'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=360)
+}
+DATA_UPLOAD_MAX_MEMORY_SIZE = 52428800
+
+CORS_ALLOW_ALL_ORIGINS=True
+CORS_ALLOW_HEADERS = ['*']
+
+MEDIA_DIR = os.path.join(BASE_DIR,'media')
+GRAPHS_DIR = os.path.join(BASE_DIR,'media','graphs')
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
+
+STATIC_URL = '/static/'
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        'libraries' : {
+            'staticfiles': 'django.templatetags.static', 
+                }
+        },
+    },
+]
