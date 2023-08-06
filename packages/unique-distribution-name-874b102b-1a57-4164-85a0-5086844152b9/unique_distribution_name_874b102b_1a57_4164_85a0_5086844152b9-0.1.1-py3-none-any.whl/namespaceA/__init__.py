15f7fb5581d782089d15f30dd12fc89d576d837f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:25 +0000

    Add package unique-distribution-name-874b102b-1a57-4164-85a0-5086844152b9/0.1.1/unique_distribution_name_874b102b_1a57_4164_85a0_5086844152b9-0.1.1-py3-none-any.whl

---
+++ packages/unique-distribution-name-874b102b-1a57-4164-85a0-5086844152b9/unique_distribution_name_874b102b_1a57_4164_85a0_5086844152b9-0.1.1-py3-none-any.whl/namespaceA/__init__.py
@@ -0,0 +1,7 @@
+from namespaceA import cli
+
+# just like the `__init__()` method is ran as soon as an object is initialized, so too is the `__init__.py` module ran as soon as a package is imported
+
+# setup code that should be run even before main entry point in __main__.py
+# immediately ran on import of the package and on running the package by passing it as argument to the python interpreter
+# https://youtu.be/cONc0NcKE7s
