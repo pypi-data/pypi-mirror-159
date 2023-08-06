9549b11008a913fc4d7e02696327d4cc7cd219cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:22 +0000

    Add package jupyter-Pandas-GUI/0.7.0rc8/jupyter_Pandas_GUI-0.7.0rc8.tar.gz

---
+++ packages/jupyter-Pandas-GUI/jupyter_Pandas_GUI-0.7.0rc8.tar.gz/jupyter_Pandas_GUI-0.7.0rc8/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="jupyter_Pandas_GUI",
+    version="0.7.0rc8",
+    description="Pandas expression composers using Jupyter widgets.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://jupyterphysscilab.github.io/jupyter_Pandas_GUI/",
+    author="Jonathan Gutow",
+    author_email="gutow@uwosh.edu",
+    keywords="fitting, data-analysis, plotting, learning to code",
+    license="GPL-3.0+",
+    packages=setuptools.find_packages(),
+    package_data={},
+    include_package_data=True,
+    install_requires=[
+        # 'python>=3.6',
+        'jupyter>=1.0.0',
+        'pandas>=1.2.5',
+        'numpy>=1.19.0',
+        'plotly>=5.5.0',
+        'ipywidgets>=7.6.2, <8.0',
+        'JPSLUtils>=0.7.0rc3',
+        'lmfit>=1.0.3',
+        'round-using-error>=1.1.1',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: JavaScript',
+        'Operating System :: OS Independent'
+    ]
+)
