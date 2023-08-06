afe2870c64775229a2dfa161dc52112d903c3ada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:17 +0000

    Add package pyfbsdk-stub-generator/0.0.2/pyfbsdk-stub-generator-0.0.2.tar.gz

---
+++ packages/pyfbsdk-stub-generator/pyfbsdk-stub-generator-0.0.2.tar.gz/pyfbsdk-stub-generator-0.0.2/pyfbsdk_stub_generator/additions_pyfbsdk.py
@@ -0,0 +1,11 @@
+"""
+Stub file generated for MotionBuilder {MOTIONBUILDER_VERSION} using:
+https://github.com/nils-soderman/pyfbsdk-stub-generator
+"""
+from __future__ import annotations
+from typing import overload
+class _Enum:
+    __slots__:tuple
+    names:dict
+    values:dict
+class ETimeFormats:...
