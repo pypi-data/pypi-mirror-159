2802c38de6d3e19d311b11e0c382d2f2f0342bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:11 +0000

    Add package keepitrunning/1.0.0/keepitrunning-1.0.0.tar.gz

---
+++ packages/keepitrunning/keepitrunning-1.0.0.tar.gz/keepitrunning-1.0.0/src/keepitrunning/host.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from threading import Thread
+
+class Host:
+  app = Flask('')
+
+  @app.route('/')
+  def main(self):
+    return 'The project is running. Please copy the link and host it on uptimerobot.com'
+    
+  def run(self, port):
+    app.run(host="0.0.0.0", port=port)
+  def keep_running(self, port = 8080):
+    server = Thread(target=run(port=port))
+    server.start()
