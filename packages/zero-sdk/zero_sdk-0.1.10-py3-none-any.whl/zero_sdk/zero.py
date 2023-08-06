5d2a511af65e50f257c2b0e196f4b6cbd87c33b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:52 +0000

    Add package zero-sdk/0.1.10/zero_sdk-0.1.10-py3-none-any.whl

---
+++ packages/zero-sdk/zero_sdk-0.1.10-py3-none-any.whl/zero_sdk/zero.py
@@ -0,0 +1,62 @@
+"""Zero SDK for Python"""
+import asyncio
+from typing import List, Dict
+
+from python_graphql_client import GraphqlClient
+
+
+class ZeroException(Exception):
+    pass
+
+
+class ZeroApiClient:
+    """The Zero API"""
+
+    query = """
+        query Secrets($token: String!, $pick: [String!]) {
+            secrets(zeroToken: $token, pick: $pick) {
+                name
+
+                fields {
+                    name
+                    value
+                }
+            }
+        }
+    """
+
+    def __init__(self, url: str, token: str, pick: List[str]):
+        self.client = GraphqlClient(endpoint=url)
+        self.variables = {"token": token, "pick": pick}
+
+    def fetch(self) -> Dict[str, Dict[str, str]]:
+        """Grab the secrets from the Zero API"""
+
+        response_body = asyncio.run(
+            self.client.execute_async(
+                query=self.query,
+                variables=self.variables
+            )
+        )
+
+        if response_body.get('errors') is not None:
+            raise ZeroException(response_body.get('errors')[0].get('message'))
+
+        try:
+            return {
+                secret.get("name"): {
+                    field.get("name"): field.get("value") for field in secret.get("fields")
+                } for secret in response_body.get("data").get("secrets")
+            }
+        except AttributeError:
+            raise AttributeError('Pick should be a list of strings')
+
+
+def zero(*, token: str, pick: List[str]) -> ZeroApiClient:
+    """Create a Zero API client"""
+
+    assert type(token) is str, "Zero token should be str"
+    assert len(token) > 0, "Zero token is required"
+    assert type(pick) is list, "Pick should be a list of strings"
+
+    return ZeroApiClient("https://core.tryzero.com/v1/graphql", token, pick)
