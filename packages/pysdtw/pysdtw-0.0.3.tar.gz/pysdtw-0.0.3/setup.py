b1168875ad208b16c96d8145f9023d3199965c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:54 +0000

    Add package pysdtw/0.0.3/pysdtw-0.0.3.tar.gz

---
+++ packages/pysdtw/pysdtw-0.0.3.tar.gz/pysdtw-0.0.3/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+from os import path
+import io
+
+with open("requirements.txt", "r") as requirements:
+    requirements = requirements.read()
+
+
+# create long description from readme for pypi
+here = path.abspath(path.dirname(__file__))
+with io.open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='pysdtw',
+      version='0.0.3',
+      description='Torch implementation of Soft-DTW, supports CUDA devices.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Antoine Loriette',
+      author_email='antoine.loriette@gmail.com',
+      url='https://github.com/toinsson/pysdtw',
+      packages=find_packages(exclude=['tests']),
+      python_requires='>=3.7',
+      install_requires=requirements.split("\n"),
+     )
