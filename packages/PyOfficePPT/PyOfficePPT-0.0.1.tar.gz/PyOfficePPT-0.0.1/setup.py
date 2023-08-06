a2986bfdb4deb11b0b4298cfaac18b88ecacc6e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:14 +0000

    Add package PyOfficePPT/0.0.1/PyOfficePPT-0.0.1.tar.gz

---
+++ packages/PyOfficePPT/PyOfficePPT-0.0.1.tar.gz/PyOfficePPT-0.0.1/setup.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: hey
+# Mail: 1957875073@qq.com
+# Created Time:  2022-1-5 10:17:34
+#############################################
+
+from setuptools import setup
+
+setup()
