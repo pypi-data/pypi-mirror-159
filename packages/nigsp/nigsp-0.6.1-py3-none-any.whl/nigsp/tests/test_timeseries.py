8a05986b4f170bc392161fcd008bc2a4b1ce409c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:59:04 +0000

    Add package nigsp/0.6.1/nigsp-0.6.1-py3-none-any.whl

---
+++ packages/nigsp/nigsp-0.6.1-py3-none-any.whl/nigsp/tests/test_timeseries.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Tests for operations.timeseries."""
+
+from pytest import mark, raises
+
+from nigsp.operations import timeseries
+
+
+# ### Unit tests
+
+# ### Break tests
