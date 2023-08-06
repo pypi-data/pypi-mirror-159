b703a163764300d685d6b781c7c48ac6d2c31ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:38 +0000

    Add package hfmonthlyreport/0.2/hfmonthlyreport-0.2.tar.gz

---
+++ packages/hfmonthlyreport/hfmonthlyreport-0.2.tar.gz/hfmonthlyreport-0.2/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='hfmonthlyreport',
+      version='0.2',
+      description='Send monthly file count only to monthly report server',
+      author='VishalJain_NIOT',
+      author_email='vishaljain9516@gmail.com',
+      packages=['hfmonthlyreport'],
+      install_requires=['requests','PyAutoGUI==0.9.0','pyperclip==1.8.2','qrcode'])
+
