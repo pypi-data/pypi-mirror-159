e08d6a09d45046c896a8b42d9a780252ddae1d97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:29 +0000

    Add package sqlglot/4.1.1/sqlglot-4.1.1-py3-none-any.whl

---
+++ packages/sqlglot/sqlglot-4.1.1-py3-none-any.whl/sqlglot/optimizer/expand_multi_table_selects.py
@@ -0,0 +1,16 @@
+from sqlglot import exp
+
+
+def expand_multi_table_selects(expression):
+    for from_ in expression.find_all(exp.From):
+        parent = from_.parent
+
+        for query in from_.args["expressions"][1:]:
+            parent.join(
+                query,
+                join_type="CROSS",
+                copy=False,
+            )
+            from_.args["expressions"].remove(query)
+
+    return expression
