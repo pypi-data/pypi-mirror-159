5aa225738c0c2444a8ac9731c6f69ae3caecf39b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:29 +0000

    Add package dbpoolpy/0.5.11/dbpoolpy-0.5.11-py3-none-any.whl

---
+++ packages/dbpoolpy/dbpoolpy-0.5.11-py3-none-any.whl/dbpoolpy/__init__.py
@@ -0,0 +1,18 @@
+try:
+    from pymysql import install_as_MySQLdb
+    install_as_MySQLdb()
+except Exception:
+    pass
+
+from .dbpool import init_pool
+from .dbpool import connect_db
+from .dbpool import connect_without_exception
+from .dbpool import with_connect_db
+from .table import SelectTable as Select
+from .table import UpdateTable as Update
+from .table import DeleteTable as Delete
+from .table import InsertTable as Insert
+from .table import Table
+from .config import settings
+
+__version__ = "0.5.11"
