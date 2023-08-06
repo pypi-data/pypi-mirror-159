2e959d53b2342897882f8bf6966b05267073c289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:49 +0000

    Add package aa-freight/1.6.5/aa-freight-1.6.5.tar.gz

---
+++ packages/aa-freight/aa-freight-1.6.5.tar.gz/aa-freight-1.6.5/freight/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+from . import __version__
+
+
+class FreightConfig(AppConfig):
+    name = "freight"
+    label = "freight"
+    verbose_name = "Freight v{}".format(__version__)
