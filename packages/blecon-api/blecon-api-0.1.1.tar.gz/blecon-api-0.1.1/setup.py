a5ea9b519a564abb5b799ce2b11adc2bad9fda8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:53:04 +0000

    Add package blecon-api/0.1.1/blecon-api-0.1.1.tar.gz

---
+++ packages/blecon-api/blecon-api-0.1.1.tar.gz/blecon-api-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['blecon_api']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests[all]>=2.27.1,<3.0.0', 'typer[all]>=0.4.1,<0.5.0']
+
+entry_points = \
+{'console_scripts': ['blecon-api = blecon_api.main:app']}
+
+setup_kwargs = {
+    'name': 'blecon-api',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'danros',
+    'author_email': 'dan.ros@blecon.net',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
