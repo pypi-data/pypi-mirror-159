513d6183e0646e6b663c5bddbdcc94e36e6f600d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:48 +0000

    Add package h2o-autodoc/1.0.7/h2o_autodoc-1.0.7-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/h2o-autodoc/h2o_autodoc-1.0.7-cp36-cp36m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/h2o_autodoc/build_info.py
@@ -0,0 +1,11 @@
+# h2o_autodoc - Copyright H2O.ai (2022)
+
+base_version = "1.0.7"
+build_number = "10"
+commit = "ac8bafb0e31ef720f0e1787d4ae8f97a2c2534de"
+branch = "rel-1.0.7"
+describe = "ac8bafb"
+build_machine = "mr-0x20"
+build_date = "Tue May 17 07:09:57 PDT 2022"
+build_user = "jenkins"
+full_version = "1.0.7"
