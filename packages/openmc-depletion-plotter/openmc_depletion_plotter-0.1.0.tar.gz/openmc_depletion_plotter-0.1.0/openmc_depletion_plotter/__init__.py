3673f4823ad945f45d04fcfb3db2a40180291436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:42 +0000

    Add package openmc-depletion-plotter/0.1.0/openmc_depletion_plotter-0.1.0.tar.gz

---
+++ packages/openmc-depletion-plotter/openmc_depletion_plotter-0.1.0.tar.gz/openmc_depletion_plotter-0.1.0/openmc_depletion_plotter/__init__.py
@@ -0,0 +1,23 @@
+try:
+    # this works for python 3.7 and lower
+    from importlib.metadata import version, PackageNotFoundError
+except (ModuleNotFoundError, ImportError):
+    # this works for python 3.8 and higher
+    from importlib_metadata import version, PackageNotFoundError
+try:
+    __version__ = version("openmc_depletion_plotter")
+except PackageNotFoundError:
+    from setuptools_scm import get_version
+
+    __version__ = get_version(root="..", relative_to=__file__)
+
+__all__ = ["__version__"]
+
+from .core import plot_material
+from .utils import find_most_abundant_nuclides_in_material
+from .utils import find_most_abundant_nuclides_in_materials
+from .utils import get_nuclide_atom_densities_from_materials
+from .utils import find_most_active_nuclides_in_material
+from .utils import find_most_active_nuclides_in_materials
+from .utils import get_nuclide_activities_from_materials
+from .utils import get_atoms_from_material
