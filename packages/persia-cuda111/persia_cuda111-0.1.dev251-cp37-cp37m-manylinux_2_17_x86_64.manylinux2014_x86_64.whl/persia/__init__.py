467b0e1fde334bf418459bfda7b5f18c7b5b38ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:02 +0000

    Add package persia-cuda111/0.1.dev251/persia_cuda111-0.1.dev251-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/persia-cuda111/persia_cuda111-0.1.dev251-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/persia/__init__.py
@@ -0,0 +1,9 @@
+from .version import __version__
+
+from persia import logger as logger
+from persia import prelude as prelude
+from persia import ctx as ctx
+from persia import embedding as embedding
+from persia import data as data
+from persia import service as service
+from persia import utils as utils
