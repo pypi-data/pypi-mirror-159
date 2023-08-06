bda53ea13459d16257931f30dab0515694662963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:23 +0000

    Add package asgihandler/0.1.4/asgihandler-0.1.4-py3-none-any.whl

---
+++ packages/asgihandler/asgihandler-0.1.4-py3-none-any.whl/asgihandler/core.py
@@ -0,0 +1,9 @@
+import requests, urllib
+
+class ASGIHandler:
+    def asgi_get_handler(scope):
+        server = scope.get('server')
+        query_string = scope.get('headers')
+        for i in query_string:
+            print(i.decode())
+        # qs = urllib.parse.parse_qs(query_string)
