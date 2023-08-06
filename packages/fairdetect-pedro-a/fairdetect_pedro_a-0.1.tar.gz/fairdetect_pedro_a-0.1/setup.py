bbb7f6ecddb5a5c0530237ee04119765e0ce37bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:59:26 +0000

    Add package fairdetect-pedro-a/0.1/fairdetect_pedro_a-0.1.tar.gz

---
+++ packages/fairdetect-pedro-a/fairdetect_pedro_a-0.1.tar.gz/fairdetect_pedro_a-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fairdetect_pedro_a',
+      version='0.1',  # Development release
+      description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
+      url='https://github.com/manoelgadi/pythonpackagesample',
+      author='Pedro V',
+      author_email='pedrovesteban@student.ie.edu',
+      license='MIT',
+          packages=['fairdetect_pedro_a'],
+      zip_safe=False)
