6e459052791f33c98173f475a61023ed1c29b0fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:55 +0000

    Add package nigsp/0.6.0/nigsp-0.6.0.tar.gz

---
+++ packages/nigsp/nigsp-0.6.0.tar.gz/nigsp-0.6.0/nigsp/tests/test_blocks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Tests for blocks."""
+
+from pytest import mark, raises
+
+from nigsp import blocks
+
+
+# ### Unit tests
+
+# ### Break tests
---
+++ packages/nigsp/nigsp-0.6.0.tar.gz/nigsp-0.6.0/nigsp/tests/test_metrics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Tests for operations.metrics."""
+
+from pytest import mark, raises
+
+from nigsp.operations import metrics
+
+
+# ### Unit tests
+
+# ### Break tests
