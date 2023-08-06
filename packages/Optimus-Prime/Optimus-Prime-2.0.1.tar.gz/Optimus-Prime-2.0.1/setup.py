e67399449054c1d5b34ef3451589be7d97a7ba66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:52 +0000

    Add package Optimus-Prime/2.0.1/Optimus-Prime-2.0.1.tar.gz

---
+++ packages/Optimus-Prime/Optimus-Prime-2.0.1.tar.gz/Optimus-Prime-2.0.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.command.register import register as register_orig
+from distutils.command.upload import upload as upload_orig
+from setuptools import setup, find_packages
+import os
+
+
+class register(register_orig):
+
+    def _get_rc_file(self):
+        return os.path.join('.', '.pypirc')
+
+class upload(upload_orig):
+
+    def _get_rc_file(self):
+        return os.path.join('.', '.pypirc')
+
+
+setup(
+    name='Optimus-Prime',
+    version='2.0.1',
+    author='Elazar Chodjayev',
+    author_email='zenmyx@gmail.com',
+    packages=find_packages(),
+    cmdclass={
+        'register': register,
+        'upload': upload,
+    }
+)
