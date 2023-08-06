dd3dce2e4b101bd2ec5decdac9b7d4a8c048c1d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:22 +0000

    Add package spt-factory/0.0.4/spt_factory-0.0.4.tar.gz

---
+++ packages/spt-factory/spt_factory-0.0.4.tar.gz/spt_factory-0.0.4/test/test.py
@@ -0,0 +1,30 @@
+import os
+
+from spt_factory import MongoFactory
+
+
+if __name__ == '__main__':
+    f = MongoFactory(
+        mongo_url=os.getenv('MONGO_URL'),
+        tlsCAFile=os.getenv('SSLROOT'),
+    )
+
+    print(f'moniback-telegram = {f.get_any_creds_credentials(type="moniback-telegram")}')
+    print(f'moniback-mlg = {f.get_any_creds_credentials(type="moniback-mlg")}')
+    print(f'postgres = {f.get_postgres_credentials()}')
+
+
+    print(f.get_postgres_credentials(**{
+        'host': 'localhost',
+        'port': '5432',
+        'dbname': 'moniback'
+    }))
+
+    with f.get_postgres(dbname='moniback') as conn:
+        print("Happy coding")
+
+    # one object, two links
+    model_manager_1 = f.get_model_manager()
+    model_manager_2 = f.get_model_manager()
+
+    pipeline_manager = f.get_pipeline_manager()
