c80057c91d757ec3118bd23ebf1115bc0c0658f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:27 +0000

    Add package loremify/0.0.9/loremify-0.0.9-py3-none-any.whl

---
+++ packages/loremify/loremify-0.0.9-py3-none-any.whl/loremify/_paragraph.py
@@ -0,0 +1,25 @@
+"""Creates a paragraph by concatenating sentences"""
+from random import shuffle
+
+from icecream import ic
+
+from loremify import Sentence
+
+ic.configureOutput(includeContext=True)
+
+
+class Paragraph:
+  """Static methods"""
+
+  @staticmethod
+  def atCharLen(n_: int) -> str:
+    """Creates a paragraph of length n containing s sentences."""
+    S = []
+    n = n_
+    while n > 20:
+      S.append(int(n * 0.6))
+      n -= S[-1]
+    S.append(n + 1)
+    shuffle(S)
+    out = [Sentence.atLen(s) for s in S]
+    return ' '.join(out)
