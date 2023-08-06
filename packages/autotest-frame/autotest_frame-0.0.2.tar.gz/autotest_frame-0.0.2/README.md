b31976ccf98b5cc410fdec745bc92bc7fdea039f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:36 +0000

    Add package autotest-frame/0.0.2/autotest_frame-0.0.2.tar.gz

---
+++ packages/autotest-frame/autotest_frame-0.0.2.tar.gz/autotest_frame-0.0.2/README.md
@@ -0,0 +1,8 @@
+📦 autotest_frame for humans
+=======================
+
+This package exists to provide One-stop solution for HTTP(S) automated testing, that can be used
+to aotumated testing in your next Python project. 
+
+[Check out the package!]
+
---
+++ packages/autotest-frame/autotest_frame-0.0.2.tar.gz/autotest_frame-0.0.2/autotest_frame/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+from autotest_frame.running.runner import main
+from autotest_frame.utils.log import logger
+from autotest_frame.utils.decorate import *
+from autotest_frame.case import *
+
+__version__ = "0.0.2"
+__description__ = "全栈自动化测试框架"
