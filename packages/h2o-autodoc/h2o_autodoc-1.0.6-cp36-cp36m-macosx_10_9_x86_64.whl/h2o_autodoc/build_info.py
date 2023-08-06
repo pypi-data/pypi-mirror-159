90c75f390e8fdbedfc2ac4ad8f5ea2e50f566014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:33 +0000

    Add package h2o-autodoc/1.0.6/h2o_autodoc-1.0.6-cp36-cp36m-macosx_10_9_x86_64.whl

---
+++ packages/h2o-autodoc/h2o_autodoc-1.0.6-cp36-cp36m-macosx_10_9_x86_64.whl/h2o_autodoc/build_info.py
@@ -0,0 +1,11 @@
+# h2o_autodoc - Copyright H2O.ai (2022)
+
+base_version = "1.0.6"
+build_number = "8"
+commit = "640d55d0e94be9c1eabeeef83673c9282f79696a"
+branch = "rel-1.0.6"
+describe = "640d55d"
+build_machine = "mr-0xb11.0xdata.loc"
+build_date = "Sun Apr  3 05:57:52 PDT 2022"
+build_user = "jenkins"
+full_version = "1.0.6"
