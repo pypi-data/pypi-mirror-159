8c7e4220b121b073466a56fd4900ed042cc50e07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:56 +0000

    Add package pyasic/0.12.2/pyasic-0.12.2-py3-none-any.whl

---
+++ packages/pyasic/pyasic-0.12.2-py3-none-any.whl/pyasic/miners/antminer/bmminer/X19/__init__.py
@@ -0,0 +1,9 @@
+from .S19 import BMMinerS19
+from .S19_Pro import BMMinerS19Pro
+
+from .S19j import BMMinerS19j
+from .S19j_Pro import BMMinerS19jPro
+
+from .S19a import BMMinerS19a
+
+from .T19 import BMMinerT19
