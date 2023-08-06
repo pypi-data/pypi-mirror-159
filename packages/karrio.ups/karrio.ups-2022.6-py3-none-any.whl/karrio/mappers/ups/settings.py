01c7ece99c46974a92a6f19deecc0c150b44efc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:44 +0000

    Add package karrio.ups/2022.6/karrio.ups-2022.6-py3-none-any.whl

---
+++ packages/karrio.ups/karrio.ups-2022.6-py3-none-any.whl/karrio/mappers/ups/settings.py
@@ -0,0 +1,24 @@
+"""Karrio UPS connection settings."""
+
+import attr
+from karrio.providers.ups.utils import Settings as BaseSettings
+
+
+@attr.s(auto_attribs=True)
+class Settings(BaseSettings):
+    """UPS connection settings."""
+
+    username: str
+    password: str
+    access_license_number: str
+    account_number: str = None
+    account_country_code: str = None
+    metadata: dict = {}
+
+    id: str = None
+    test_mode: bool = False
+    carrier_id: str = "ups"
+
+    @property
+    def carrier_name(self):
+        return "ups"
