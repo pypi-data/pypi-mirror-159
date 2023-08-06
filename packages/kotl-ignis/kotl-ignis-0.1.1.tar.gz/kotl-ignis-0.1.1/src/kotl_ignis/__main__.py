530488a1b5b69b6dc2fb1e95ff72975d946cdf5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:24 +0000

    Add package kotl-ignis/0.1.1/kotl-ignis-0.1.1.tar.gz

---
+++ packages/kotl-ignis/kotl-ignis-0.1.1.tar.gz/kotl-ignis-0.1.1/src/kotl_ignis/__main__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import click
+import json
+import os
+import subprocess
+
+from .settings import THEMES_DIR, HOME
+from .utils import json_to_xresources
+
+@click.command()
+@click.argument('theme', required=False)
+@click.option('-l', '--list-themes', is_flag=True, help='List built-in themes.')
+@click.option('--reverse', is_flag=True, help='Reverse changes.')
+def kotl(theme, list_themes, reverse):
+    themes = os.listdir(THEMES_DIR)
+    if theme:
+        theme += '.json'
+        if theme in themes:
+            output = json_to_xresources(theme)
+            subprocess.run(['xrdb', str(output)])
+        else:
+            print(f'No theme named {theme} found')
+            exit(1)
+    elif list_themes:
+        for theme in themes:
+            print(os.path.splitext(theme)[0])
+    elif reverse:
+        subprocess.run(['xrdb', '-remove'])
+        xresources_paths = [
+            os.path.join(HOME, '.Xresources'),
+            os.path.join(HOME, '.config/x11/xresources')
+        ]
+        for path in xresources_paths:
+            if os.path.exists(path):
+                subprocess.run(['xrdb', '-merge', path])
