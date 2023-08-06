3c39bb2b0cd8c6d137d8a612ded525cd03a7cabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:59:40 +0000

    Add package EthTx/0.3.17/EthTx-0.3.17.tar.gz

---
+++ packages/EthTx/EthTx-0.3.17.tar.gz/EthTx-0.3.17/ethtx/decoders/semantic/balances.py
@@ -0,0 +1,40 @@
+#  Copyright 2021 DAI Foundation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from typing import List
+
+from ethtx.models.decoded_model import DecodedBalance, DecodedTransactionMetadata
+from .abc import SemanticSubmoduleAbc
+from .helpers.utils import get_badge
+
+
+class SemanticBalancesDecoder(SemanticSubmoduleAbc):
+    """Semantic Balances Decoder."""
+
+    def decode(
+        self, balances: List[DecodedBalance], tx_metadata: DecodedTransactionMetadata
+    ) -> List[DecodedBalance]:
+        """Semantically decode balances."""
+
+        for balance in balances:
+
+            # decode the proper holder badge
+            balance.holder.badge = get_badge(
+                balance.holder.address,
+                tx_metadata.sender.address,
+                tx_metadata.receiver.address,
+            )
+
+            for token in balance.tokens:
+                token["balance"] = f"{token['balance']:,.4f}"
+
+        return balances
---
+++ packages/EthTx/EthTx-0.3.17.tar.gz/EthTx-0.3.17/ethtx/providers/ens_provider.py
@@ -0,0 +1,68 @@
+#  Copyright 2021 DAI Foundation
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import logging
+from abc import ABC, abstractmethod
+from typing import Any, TypeVar, Type
+
+from ens import ENS
+from web3 import Web3, exceptions
+
+log = logging.getLogger(__name__)
+
+T = TypeVar("T")
+
+
+class ENSProviderBase(ABC):
+    @abstractmethod
+    def name(self, provider: Type[T], address: Any):
+        ...
+
+    @abstractmethod
+    def address(self, provider: Type[T], name: Any):
+        ...
+
+
+class Web3ENSProvider(ENSProviderBase):
+    ns: ENS
+
+    def name(self, provider: Web3, address: str) -> str:
+        ns = self._set_provider(provider)
+        check_sum_address = Web3.toChecksumAddress(address)
+
+        try:
+            name = ns.name(address=check_sum_address)
+        except exceptions.BadFunctionCallOutput:
+            log.warning(
+                "ENS name not found for address: %s. There is no code associated with this address.",
+                address,
+            )
+            name = None
+
+        if name:
+            log.info("ENS resolved an address: %s to name: %s", address, name)
+
+        return name if name else address
+
+    def address(self, provider: Web3, name: str) -> str:
+        ns = self._set_provider(provider)
+        address = ns.address(name=name)
+
+        if address:
+            log.info("ENS resolved name: %s to address: %s", name, address)
+
+        return address if address else name
+
+    def _set_provider(self, provider: Web3) -> ENS:
+        return ENS.fromWeb3(provider)
+
+
+ENSProvider = Web3ENSProvider()
