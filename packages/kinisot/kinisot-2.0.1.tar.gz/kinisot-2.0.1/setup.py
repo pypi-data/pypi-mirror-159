d78f21126bf77b3f68d5992b63f03e7789e777d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:56 +0000

    Add package kinisot/2.0.1/kinisot-2.0.1.tar.gz

---
+++ packages/kinisot/kinisot-2.0.1.tar.gz/kinisot-2.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import io
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with io.open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+  name = 'kinisot',
+  packages = ['kinisot'],
+  version = '2.0.1',
+  description = 'Compute Kinetic Isotope Effects for compchem outputs',
+  author = 'Paton Research Group',
+  author_email = 'patonlab@colostate.edu',
+  url = 'https://github.com/patonlab/kinisot',
+  download_url = 'https://github.com/patonlab/Kinisot/archive/refs/tags/v2.0.1.zip',
+  keywords = ['compchem', 'kie', 'dft', 'vibrational-scaling-factors', 'kinisot', 'frequencies' 'quiver'],
+  classifiers = [],
+  install_requires=['numpy'],
+  python_requires='>=3.0',
+  include_package_data=True,
+)
