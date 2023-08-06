f7ba09dfbd8589e202507b8a685927fd7318fc2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:53 +0000

    Add package jovialengine/0.9.0/jovialengine-0.9.0.tar.gz

---
+++ packages/jovialengine/jovialengine-0.9.0.tar.gz/jovialengine-0.9.0/src/jovialengine/__init__.py
@@ -0,0 +1,7 @@
+from .saveable import Saveable
+from .animsprite import AnimSprite
+from .modebase import ModeBase
+from .fontwrap import FontWrap
+from . import load
+from . import utility
+from .game import getGame
