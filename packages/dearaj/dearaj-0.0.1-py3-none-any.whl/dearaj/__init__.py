92324863790c7404a302aa2494a02f009a6d3250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:08:14 +0000

    Add package dearaj/0.0.1/dearaj-0.0.1-py3-none-any.whl

---
+++ packages/dearaj/dearaj-0.0.1-py3-none-any.whl/dearaj/__init__.py
@@ -0,0 +1,22 @@
+from config import *
+
+__version__ = "0.0.1"
+
+__all__ = [
+    "CONFIG_FILE_DIR",
+    "PACKAGE_ABS_DIR",
+    "DATA_FILES_PATH",
+    "GEN_PERIOD_DICT",
+    "get_start_of",
+    "get_end_of",
+    "suffix_of",
+    "get_normal_page_of",
+    "get_conf_vod_link",
+    "get_conf_movie_info",
+    "get_conf_file_info",
+    "get_conferences_of",
+    "get_conf_vod_chunks",
+    "get_conf_pdf",
+    "Conference",
+    "Conferences",
+]
