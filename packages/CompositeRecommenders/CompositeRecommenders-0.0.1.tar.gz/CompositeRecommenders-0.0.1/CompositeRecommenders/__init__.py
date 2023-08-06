72eb4c68f017432b365dafb6d6640cde05a13cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:54 +0000

    Add package CompositeRecommenders/0.0.1/CompositeRecommenders-0.0.1.tar.gz

---
+++ packages/CompositeRecommenders/CompositeRecommenders-0.0.1.tar.gz/CompositeRecommenders-0.0.1/CompositeRecommenders/__init__.py
@@ -0,0 +1,4 @@
+"""CompositeRecommenders package"""
+from CompositeRecommenders.LSAEndowedSMR import LSAEndowedSMR
+from CompositeRecommenders.SMR_to_LSA import SMR_to_LSA
+#from CompositeRecommenders.CompositeRecommenders import CompositeRecommender
