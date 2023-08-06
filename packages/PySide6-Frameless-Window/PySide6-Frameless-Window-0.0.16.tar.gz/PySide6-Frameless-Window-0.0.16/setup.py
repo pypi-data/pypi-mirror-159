2ef7cf1bf9c210fdd8915596436ab99d271a6428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:14 +0000

    Add package PySide6-Frameless-Window/0.0.16/PySide6-Frameless-Window-0.0.16.tar.gz

---
+++ packages/PySide6-Frameless-Window/PySide6-Frameless-Window-0.0.16.tar.gz/PySide6-Frameless-Window-0.0.16/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="PySide6-Frameless-Window",
+    version="0.0.16",
+    keywords="pyside6 frameless",
+    author="Ray Chen",
+    author_email="tankimarshal2@gmail.com",
+    description="Based off PyQt5-Frameless-Window but migrated to PySide6.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/rayzchen/PySide6-Frameless-Window",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "PySide6",
+        "pywin32; platform_system == 'Windows'",
+        "xcffib; platform_system != 'Windows'",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
