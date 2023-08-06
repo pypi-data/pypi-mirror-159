a559170f02dab26f9a58476ec3f0b47276d8cb84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:49 +0000

    Add package indonlp/0.2.0/indoNLP-0.2.0.tar.gz

---
+++ packages/indonlp/indoNLP-0.2.0.tar.gz/indoNLP-0.2.0/indoNLP/__init__.py
@@ -0,0 +1,8 @@
+""" Indonesian NLP written in python """
+
+from indoNLP.preprocessing import *
+
+__all__ = ["preprocessing"]
+__version__ = "0.2.0"
+__author__ = "Wahyu Setianto"
+__source__ = "https://github.com/Hyuto/indo-nlp/"
