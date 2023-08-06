c7552073653c5dd759e48c48bbf1ccaf0f1a700a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:06 +0000

    Add package metastreamlit/0.4.6rc40/metastreamlit-0.4.6rc40.tar.gz

---
+++ packages/metastreamlit/metastreamlit-0.4.6rc40.tar.gz/metastreamlit-0.4.6rc40/metastreamlit/image_position_overlays/build/precache-manifest.00331ca5bf47498412043295349d2351.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0197dd0549420728d65efaf1e7fe27a1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d5e495eaa60596712181",
+    "url": "./static/css/2.a7708dfc.chunk.css"
+  },
+  {
+    "revision": "d5e495eaa60596712181",
+    "url": "./static/js/2.a24c58de.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.a24c58de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5ce2621a008dbc84779",
+    "url": "./static/js/main.30a77405.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
