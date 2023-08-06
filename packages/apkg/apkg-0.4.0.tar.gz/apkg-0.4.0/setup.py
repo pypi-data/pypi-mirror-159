5b89de5cb4e77eadd972cc81a17a25ddf3744a7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:58:02 +0000

    Add package apkg/0.4.0/apkg-0.4.0.tar.gz

---
+++ packages/apkg/apkg-0.4.0.tar.gz/apkg-0.4.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import setuptools
+
+
+def get_version_and_cmdclass(package_path):
+    """Load version.py module without importing the whole package.
+
+    Template code from miniver
+    """
+    import os
+    from importlib.util import module_from_spec, spec_from_file_location
+
+    spec = spec_from_file_location(
+        "version", os.path.join(package_path, "_version.py"))
+    module = module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module.__version__, module.cmdclass
+
+
+version, cmdclass = get_version_and_cmdclass("apkg")
+
+
+setuptools.setup(
+    name='apkg',
+    version=version,
+    cmdclass=cmdclass,
+)
