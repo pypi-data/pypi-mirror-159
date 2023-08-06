0de7657041e1ad5392bcbf2402f165481ecebacc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:54 +0000

    Add package questionextractorpackage/0.0.1/questionextractorpackage-0.0.1.tar.gz

---
+++ packages/questionextractorpackage/questionextractorpackage-0.0.1.tar.gz/questionextractorpackage-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= 'questionextractorpackage',
+    version= '0.1',
+    license= 'MIT',
+    author= 'Maisa',
+    packages=find_packages('src'),
+    url= '',
+    keywords= 'question_extractor',
+    install_requires= [
+        'cleantext',
+        'visualise-spacy-tree',
+        'sentence-transformers',
+        'nltk'
+    ],
+
+)
