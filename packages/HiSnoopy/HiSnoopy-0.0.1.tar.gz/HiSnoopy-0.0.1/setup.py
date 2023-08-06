0823f1f468e79b17511f1a961ecd322b894aa297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:11 +0000

    Add package HiSnoopy/0.0.1/HiSnoopy-0.0.1.tar.gz

---
+++ packages/HiSnoopy/HiSnoopy-0.0.1.tar.gz/HiSnoopy-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Say hi lib'
+LONG_DESCRIPTION = 'Say hi library.'
+
+# Setting up
+setup(
+    name="HiSnoopy",
+    version=VERSION,
+    author="Przemyslaw Zaradzki",
+    author_email="<przemyslaw.zaradzki@wp.pl>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
