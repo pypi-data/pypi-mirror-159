f7421361be74a72e405022930f6b499d2ea6be9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:51:15 +0000

    Add package PyEnzymeKinetics/1.0/PyEnzymeKinetics-1.0.tar.gz

---
+++ packages/PyEnzymeKinetics/PyEnzymeKinetics-1.0.tar.gz/PyEnzymeKinetics-1.0/pyenzymekinetics/__init__.py
@@ -0,0 +1,1 @@
+import pyenzymekinetics.utility
---
+++ packages/PyEnzymeKinetics/PyEnzymeKinetics-1.0.tar.gz/PyEnzymeKinetics-1.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools 
+
+setuptools.setup(
+    name="PyEnzymeKinetics",
+    version="1.0",
+    author="Max Haeussler",
+    author_email="max.haeussler@gmail.com",
+    packages=setuptools.find_packages(),
+    requires=["pyenzyme"]
+)
