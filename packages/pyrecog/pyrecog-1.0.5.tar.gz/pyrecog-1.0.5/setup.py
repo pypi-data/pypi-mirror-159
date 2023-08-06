5a73cee3f3a94e6fb795259bd18625f1e01a1c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:37 +0000

    Add package pyrecog/1.0.5/pyrecog-1.0.5.tar.gz

---
+++ packages/pyrecog/pyrecog-1.0.5.tar.gz/pyrecog-1.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '1.0.5'
+DESCRIPTION = 'An image recognition package.'
+
+# Setting up
+setup(
+    name="pyrecog",
+    version=VERSION,
+    author="FullStackJosh",
+    author_email="joshuafxs@yahoo.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pyautogui'],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
