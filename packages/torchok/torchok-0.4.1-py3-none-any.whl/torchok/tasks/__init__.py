0c4baa6b318fda07b354c4e5e17f2df3f814dad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:42 +0000

    Add package torchok/0.4.1/torchok-0.4.1-py3-none-any.whl

---
+++ packages/torchok/torchok-0.4.1-py3-none-any.whl/torchok/tasks/__init__.py
@@ -0,0 +1,5 @@
+from torchok.tasks.base import BaseTask
+from torchok.tasks.classification import ClassificationTask
+# from src.tasks.moby import 
+# from src.tasks.segmentation import 
+# from src.tasks.representation import 
