9c3ece26586579bbaa74e67199a9f6f1bb018419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:51:32 +0000

    Add package rubaliz/0.1.11/rubaliz-0.1.11-py3-none-any.whl

---
+++ packages/rubaliz/rubaliz-0.1.11-py3-none-any.whl/rubaliz/__version__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  6 14:57:29 2022
+
+@author: rfuchs
+"""
+
+VERSION = (0, 1, 11)
+__version__ = '.'.join(map(str, VERSION))
---
+++ packages/rubaliz/rubaliz-0.1.11-py3-none-any.whl/rubaliz/seabird/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  5 18:50:19 2022
+
+@author: rfuchs
+"""
+
+from .cnv import fCNV
