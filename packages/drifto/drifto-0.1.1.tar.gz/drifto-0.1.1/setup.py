b8f103d00415deeb8d6785a7105f7d9459baead0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:17 +0000

    Add package drifto/0.1.1/drifto-0.1.1.tar.gz

---
+++ packages/drifto/drifto-0.1.1.tar.gz/drifto-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='drifto',
+    version='0.1.1',
+    license='ELv2',
+    description='Automatic featurization and ML for event analytics',
+    author='Drifto Technologies Inc',
+    author_email='founders@driftoml.com',
+    packages=['drifto', 'drifto.ml'],
+    url = 'https://github.com/drifto-ml/drifto',
+    keywords = ['data', 'machine learning', 'feature engineering', 'analytics', 'autoML'],
+    install_requires=['pyarrow',
+                      'duckdb',
+                      'torch',
+                      'pytorch_lightning',
+                      'onnx']
+)
