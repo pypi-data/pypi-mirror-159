ab9936868c46395e994ae1447682c0870da1fc8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:53:46 +0000

    Add package randomgen/1.23.0/randomgen-1.23.0.tar.gz

---
+++ packages/randomgen/randomgen-1.23.0.tar.gz/randomgen-1.23.0/ci/cirrus-install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+pkg install -y python39  py39-numpy py39-cython wget git
+python3.9 -m ensurepip --upgrade
+python3.9 -m pip install wheel setuptools_scm[toml] pytest
+python3.9 -m pip list
+git fetch --tags
+python3.9 -m pip install . --no-build-isolation
+
