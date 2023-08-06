237b9cb567bfa0db58893587fe26de8a25467f59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:32 +0000

    Add package bdtime/1.0.3/bdtime-1.0.3.tar.gz

---
+++ packages/bdtime/bdtime-1.0.3.tar.gz/bdtime-1.0.3/bdtime/__init__.py
@@ -0,0 +1,22 @@
+name = "bdtime"
+
+from .my_time import *
+from .utils import *
+from .my_log import log_config_dc, show_all_loggers
+
+
+def version():
+    """
+    # 更新日志
+
+    - 增加datetime和标准北京时间相关方法         # 1.0.0
+    - 增加logger相关信息                        # 1.0.1
+    - 更新了日期时间格式, 将分隔符'/'替换为'-'
+    - 增加log_level_types                      # 1.0.2
+    - 检查是否最新版本check_version
+    - 修复部分bug                               # 1.0.3
+    """
+    ret = '1.0.3'
+    return ret
+
+
