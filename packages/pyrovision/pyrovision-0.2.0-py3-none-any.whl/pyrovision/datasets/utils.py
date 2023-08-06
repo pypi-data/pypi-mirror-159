9701053c6ad1ef58c06d02fb4e55cfcd9729372a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:00 +0000

    Add package pyrovision/0.2.0/pyrovision-0.2.0-py3-none-any.whl

---
+++ packages/pyrovision/pyrovision-0.2.0-py3-none-any.whl/pyrovision/datasets/utils.py
@@ -0,0 +1,195 @@
+# Copyright (C) 2019-2022, Pyronear.
+
+# This program is licensed under the Apache License 2.0.
+# See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0> for full license details.
+
+import multiprocessing as mp
+from functools import partial
+from multiprocessing.pool import ThreadPool
+from pathlib import Path
+from typing import Any, Callable, List, Optional, Sequence, Tuple, TypeVar
+from urllib.parse import urlparse
+
+import requests
+from torchvision.datasets.utils import check_integrity
+from tqdm import tqdm
+
+Inp = TypeVar("Inp")
+Out = TypeVar("Out")
+
+__all__ = ["download_url", "download_urls"]
+
+
+def url_retrieve(url: str, outfile: Path, timeout: int = 4) -> None:
+    """Download the content of an URL request to a specified location
+
+    Args:
+        url (str): URL to request
+        outfile (pathlib.Path): path of the file where the response will be saved
+        timeout (float, optional): number of seconds before the request times out
+    """
+
+    response = requests.get(url, timeout=timeout, allow_redirects=True)
+    if response.status_code != 200:
+        raise requests.exceptions.ConnectionError(f"Error code {response.status_code} - could not download {url}")
+
+    outfile.write_bytes(response.content)
+
+
+def get_fname(url: str, default_extension: str = "jpg", max_base_length: Optional[int] = None) -> str:
+    """Find extension of file located by URL
+
+    Args:
+        url (str): URL of the file
+        default_extension (str, optional): default extension
+        max_base_length (int, optional): max base filename's length
+
+    Returns:
+        str: file name
+    """
+
+    name_split = urlparse(url).path.rpartition("/")[-1].split("?")[0].split("&")[0].split(";")[0].split(".")
+    # Check if viable extension
+    if len(name_split) > 1 and all(c.isalpha() or c.isdigit() for c in name_split[-1].lower()):
+        base, extension = ".".join(name_split[:-1]), name_split[-1].lower()
+    # Fallback on default extension
+    else:
+        base, extension = name_split[-1], default_extension
+    # Check base length
+    if isinstance(max_base_length, int) and len(base) > max_base_length:
+        base = base[:max_base_length]
+
+    return f"{base}.{extension}"
+
+
+def download_url(
+    url: str,
+    root: Path,
+    filename: Optional[str] = None,
+    md5: Optional[str] = None,
+    timeout: int = 4,
+    retries: int = 4,
+    verbose: bool = False,
+    silent: bool = False,
+) -> None:
+    """Download a file accessible via URL with mutiple retries
+
+    Args:
+        url (str or tuple<str, str>): URL to request
+        root (pathlib.Path): folder where the file will be saved in
+        filename (str, optional): name of the output file
+        md5 (str, optional): md5 for integrity verification
+        timeout (float, optional): number of seconds before the request times out
+        retries (int, optional): number of additional allowed download attempts
+        verbose (bool, optional): whether status can be displayed in console
+        silent (bool, optional): whether Exception should be raised upon download failure
+    """
+
+    if isinstance(url, tuple):
+        url, filename = url
+
+    if not isinstance(url, str):
+        raise TypeError("expected argument url to be of type <str>")
+
+    # Root folder
+    root = Path(root).expanduser()
+    root.mkdir(parents=True, exist_ok=True)
+    if not filename:
+        filename = get_fname(url)
+
+    fpath = root.joinpath(filename)
+
+    # Download file
+    if check_integrity(fpath, md5):
+        if verbose:
+            print(f"Using downloaded and verified file: {fpath}")
+    else:
+        success = False
+        # Allow multiple retries
+        for idx in range(retries + 1):
+            try:
+                url_retrieve(url, fpath, timeout)
+                success = True
+            except Exception as e:
+                # Try switching to http
+                if url.startswith("https"):
+                    try:
+                        url_retrieve(url.replace("https:", "http:"), fpath, timeout)
+                        success = True
+                    except Exception:
+                        success = False
+                # Handle exception
+                if not success and (idx == retries):
+                    if not silent:
+                        raise e
+                    elif verbose:
+                        print(e)
+            if success:
+                break
+
+
+def parallel(
+    func: Callable[[Inp], Out],
+    arr: Sequence[Inp],
+    num_threads: Optional[int] = None,
+    progress: bool = False,
+    **kwargs: Any,
+) -> Sequence[Out]:
+    """Download a file accessible via URL with mutiple retries
+
+    Args:
+        func (callable): function to be executed on multiple workers
+        arr (iterable): function argument's values
+        num_threads (int, optional): number of workers to be used for multiprocessing
+        kwargs: keyword arguments of tqdm
+
+    Returns:
+        list: list of function's results
+    """
+
+    if num_threads is None:
+        num_threads = min(16, mp.cpu_count())
+    if num_threads < 2:
+        if progress:
+            results = list(map(func, tqdm(arr, total=len(arr), **kwargs)))
+        else:
+            results = map(func, arr)  # type: ignore[assignment]
+    else:
+        with ThreadPool(num_threads) as tp:
+            if progress:
+                results = list(tqdm(tp.imap(func, arr), total=len(arr), **kwargs))
+            else:
+                results = tp.map(func, arr)
+
+    return results
+
+
+def download_urls(
+    entries: List[Tuple[str, str]],
+    root: Path,
+    timeout: int = 4,
+    retries: int = 4,
+    num_threads: Optional[int] = None,
+    silent: bool = True,
+    progress: bool = True,
+) -> None:
+    """Download multiple URLs a file accessible via URL with mutiple retries
+
+    Args:
+        entries (list<str, str>): URL and destination filen
+        root (pathlib.Path): folder where the files will be saved in
+        timeout (float, optional): number of seconds before the request times out
+        retries (int, optional): number of additional allowed download attempts
+        num_threads (int, optional): number of threads to be used for multiprocessing
+        silent (bool, optional): whether Exception should be raised upon download failure
+        progress (bool, optional): whether progress should be displayed
+    """
+
+    parallel(
+        partial(download_url, root=root, timeout=timeout, retries=retries, silent=silent),
+        entries,
+        num_threads=num_threads,
+        desc="Downloading files",
+        progress=progress,
+        leave=False,
+    )
---
+++ packages/pyrovision/pyrovision-0.2.0-py3-none-any.whl/references/classification/train.py
@@ -0,0 +1,285 @@
+# Copyright (C) 2019-2022, Pyronear.
+
+# This program is licensed under the Apache License 2.0.
+# See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0> for full license details.
+
+
+import datetime
+import logging
+import os
+import time
+from shutil import copyfile
+
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import wandb
+from codecarbon import track_emissions
+from holocron.models.presets import IMAGENET
+from holocron.optim import AdamP
+from holocron.trainer import BinaryClassificationTrainer
+from PIL import Image
+from torch.utils.data import RandomSampler, SequentialSampler
+from torchvision.datasets import ImageFolder
+from torchvision.transforms import transforms as T
+from torchvision.transforms.functional import InterpolationMode, resize, to_pil_image
+
+from pyrovision import models
+from pyrovision.datasets import OpenFire
+
+logging.getLogger("codecarbon").disabled = True
+
+
+def target_transform(target):
+
+    target = torch.tensor(target, dtype=torch.float32)
+
+    return target.unsqueeze(dim=0)
+
+
+def plot_samples(images, targets, num_samples=4):
+    # Unnormalize image
+    nb_samples = min(num_samples, images.shape[0])
+    _, axes = plt.subplots(1, nb_samples, figsize=(20, 5))
+    for idx in range(nb_samples):
+        img = images[idx]
+        img *= torch.tensor(IMAGENET["std"]).view(-1, 1, 1)
+        img += torch.tensor(IMAGENET["mean"]).view(-1, 1, 1)
+        img = to_pil_image(img)
+
+        axes[idx].imshow(img)
+        axes[idx].axis("off")
+        _targets = targets.squeeze()
+        if _targets.ndim == 1:
+            axes[idx].set_title(_targets[idx].item())
+        else:
+            class_idcs = torch.where(_targets[idx] > 0)[0]
+            _info = [f"{_idx.item()} ({_targets[idx, _idx]:.2f})" for _idx in class_idcs]
+            axes[idx].set_title(" ".join(_info))
+
+    plt.show()
+
+
+@track_emissions()
+def main(args):
+
+    print(args)
+
+    torch.backends.cudnn.benchmark = True
+
+    # Data loading code
+    normalize = T.Normalize(mean=IMAGENET["mean"], std=IMAGENET["std"])
+
+    interpolation = InterpolationMode.BILINEAR
+    target_size = (args.img_size, args.img_size)
+
+    train_transforms = T.Compose(
+        [
+            # Photometric
+            T.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.1),
+            T.RandomApply([T.GaussianBlur(kernel_size=(3, 3), sigma=(0.1, 3))], p=0.5),
+            # Geometric
+            T.RandomHorizontalFlip(),
+            T.RandomResizedCrop(size=target_size, scale=(0.8, 1.0), interpolation=interpolation),
+            T.RandomPerspective(distortion_scale=0.2, interpolation=interpolation, p=0.8),
+            # Conversion
+            T.PILToTensor(),
+            T.ConvertImageDtype(torch.float32),
+            normalize,
+            T.RandomErasing(p=0.9, scale=(0.02, 0.1), value="random"),
+        ]
+    )
+
+    val_transforms = T.Compose(
+        [
+            T.Resize(size=args.img_size, interpolation=interpolation),
+            T.CenterCrop(size=args.img_size),
+            T.ToTensor(),
+            normalize,
+        ]
+    )
+
+    print("Loading data")
+    st = time.time()
+    if args.openfire:
+
+        # Prefetch images and resize them to avoid RAM overload
+        prefetch_fn = None
+        if isinstance(args.prefetch_size, int):
+
+            def prefetch_fn(img_paths):
+                # Unpack paths
+                src_path, dest_path = img_paths
+                img = Image.open(src_path, mode="r").convert("RGB")
+                # Resize & save
+                if all(dim > args.prefetch_size for dim in img.size):
+                    resized_img = resize(img, args.prefetch_size, interpolation=interpolation)
+                    resized_img.save(dest_path)
+                # Copy
+                else:
+                    copyfile(src_path, dest_path)
+
+        train_set = OpenFire(
+            args.data_path, train=True, download=True, transform=train_transforms, prefetch_fn=prefetch_fn
+        )
+    else:
+        train_dir = os.path.join(args.data_path, "train")
+        train_set = ImageFolder(train_dir, train_transforms, target_transform=target_transform)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_set,
+        batch_size=args.batch_size,
+        drop_last=True,
+        sampler=RandomSampler(train_set),
+        num_workers=args.workers,
+        pin_memory=True,
+    )
+
+    print(f"Training set loaded in {time.time() - st:.2f}s ({len(train_set)} samples in {len(train_loader)} batches)")
+
+    if args.show_samples:
+        x, target = next(iter(train_loader))
+        plot_samples(x, target)
+        return
+
+    st = time.time()
+    if args.openfire:
+        val_set = OpenFire(
+            args.data_path, train=False, download=True, transform=val_transforms, prefetch_fn=prefetch_fn
+        )
+    else:
+        val_dir = os.path.join(args.data_path, "val")
+        val_set = ImageFolder(val_dir, val_transforms, target_transform=target_transform)
+
+    val_loader = torch.utils.data.DataLoader(
+        val_set,
+        batch_size=args.batch_size,
+        drop_last=False,
+        sampler=SequentialSampler(val_set),
+        num_workers=args.workers,
+        pin_memory=True,
+    )
+    print(f"Validation set loaded in {time.time() - st:.2f}s ({len(val_set)} samples in {len(val_loader)} batches)")
+
+    print("Creating model")
+    model = models.__dict__[args.arch](args.pretrained, num_classes=1)
+
+    criterion = nn.BCEWithLogitsLoss()
+
+    # Create the contiguous parameters.
+    model_params = [p for p in model.parameters() if p.requires_grad]
+    if args.opt == "sgd":
+        optimizer = torch.optim.SGD(model_params, args.lr, momentum=0.9, weight_decay=args.weight_decay)
+    elif args.opt == "radam":
+        optimizer = torch.optim.RAdam(
+            model_params, args.lr, betas=(0.95, 0.99), eps=1e-6, weight_decay=args.weight_decay
+        )
+    elif args.opt == "adamp":
+        optimizer = AdamP(model_params, args.lr, betas=(0.95, 0.99), eps=1e-6, weight_decay=args.weight_decay)
+
+    log_wb = lambda metrics: wandb.log(metrics) if args.wb else None
+    trainer = BinaryClassificationTrainer(
+        model,
+        train_loader,
+        val_loader,
+        criterion,
+        optimizer,
+        args.device,
+        args.output_file,
+        amp=args.amp,
+        on_epoch_end=log_wb,
+    )
+    if args.resume:
+        print(f"Resuming {args.resume}")
+        checkpoint = torch.load(args.resume, map_location="cpu")
+        trainer.load(checkpoint)
+
+    if args.test_only:
+        print("Running evaluation")
+        eval_metrics = trainer.evaluate()
+        print(trainer._eval_metrics_str(eval_metrics))
+        return
+
+    if args.find_lr:
+        print("Looking for optimal LR")
+        trainer.find_lr(args.freeze_until, num_it=min(len(train_loader), 100), norm_weight_decay=args.norm_wd)
+        trainer.plot_recorder()
+        return
+
+    # Training monitoring
+    current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    exp_name = f"{args.arch}-{current_time}" if args.name is None else args.name
+
+    # W&B
+    if args.wb:
+
+        run = wandb.init(
+            name=exp_name,
+            project="pyrovision-image-classification",
+            config={
+                "learning_rate": args.lr,
+                "scheduler": args.sched,
+                "weight_decay": args.weight_decay,
+                "epochs": args.epochs,
+                "batch_size": args.batch_size,
+                "architecture": args.arch,
+                "input_size": args.img_size,
+                "prefetch_size": args.prefetch_size,
+                "optimizer": args.opt,
+                "dataset": "openfire" if args.openfire else "custom",
+                "loss": "bce",
+            },
+        )
+
+    print("Start training")
+    start_time = time.time()
+    trainer.fit_n_epochs(args.epochs, args.lr, args.freeze_until, args.sched, norm_weight_decay=args.norm_wd)
+    total_time_str = str(datetime.timedelta(seconds=int(time.time() - start_time)))
+    print(f"Training time {total_time_str}")
+
+    if args.wb:
+        run.finish()
+
+
+def get_parser():
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Pyronear Classification Training", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument("data_path", type=str, help="path to dataset folder")
+    parser.add_argument("--name", type=str, default=None, help="Name of your training experiment")
+    parser.add_argument("--arch", default="rexnet1_0x", type=str, help="model")
+    parser.add_argument("--openfire", help="whether OpenFire should be used", action="store_true")
+    parser.add_argument("--freeze-until", default=None, type=str, help="Last layer to freeze")
+    parser.add_argument("--device", default=None, type=int, help="device")
+    parser.add_argument("-b", "--batch-size", default=32, type=int, help="batch size")
+    parser.add_argument("--epochs", default=20, type=int, help="number of total epochs to run")
+    parser.add_argument("-j", "--workers", default=16, type=int, help="number of data loading workers")
+    parser.add_argument("--img-size", default=224, type=int, help="image size")
+    parser.add_argument(
+        "--prefetch-size", default=None, type=int, help="prefetched images will be resized to lower RAM usage"
+    )
+    parser.add_argument("--opt", default="adamp", type=str, help="optimizer")
+    parser.add_argument("--sched", default="onecycle", type=str, help="Scheduler to be used")
+    parser.add_argument("--lr", default=1e-3, type=float, help="initial learning rate")
+    parser.add_argument("--wd", "--weight-decay", default=0, type=float, help="weight decay", dest="weight_decay")
+    parser.add_argument("--norm-wd", default=None, type=float, help="weight decay of norm parameters")
+    parser.add_argument("--find-lr", dest="find_lr", action="store_true", help="Should you run LR Finder")
+    parser.add_argument("--show-samples", action="store_true", help="Whether training samples should be displayed")
+    parser.add_argument("--output-file", default="./model.pth", help="path where to save")
+    parser.add_argument("--resume", default="", help="resume from checkpoint")
+    parser.add_argument("--test-only", dest="test_only", help="Only test the model", action="store_true")
+    parser.add_argument(
+        "--pretrained", dest="pretrained", help="Use pre-trained models from the modelzoo", action="store_true"
+    )
+    parser.add_argument("--amp", help="Use Automatic Mixed Precision", action="store_true")
+    parser.add_argument("--wb", action="store_true", help="Log to Weights & Biases")
+
+    return parser
+
+
+if __name__ == "__main__":
+    args = get_parser().parse_args()
+    main(args)
