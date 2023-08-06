1899d6a268895c6078963a783f925d3a7d46fe30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:55 +0000

    Add package haokan-download-icexmoon/0.0.3/haokan_download_icexmoon-0.0.3-py3-none-any.whl

---
+++ packages/haokan-download-icexmoon/haokan_download_icexmoon-0.0.3-py3-none-any.whl/haokan_download/config.py
@@ -0,0 +1,18 @@
+import platform
+import os
+pwd = os.getcwd()
+windowsHomePath = pwd
+LinuxHomePath = pwd
+config = {}
+sysName = platform.system()
+if sysName == "Windows":
+    config["path"] = "\\"
+    config["saveHome"] = windowsHomePath
+elif sysName == "Linux":
+    config["path"] = "/"
+    config["saveHome"] = LinuxHomePath
+else:
+    print("本程序不支持当前操作系统")
+    exit()
+part = __file__.rpartition('\\')
+config["sysHome"] = part[0]
