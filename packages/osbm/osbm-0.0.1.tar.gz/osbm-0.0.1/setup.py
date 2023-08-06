25e2a4bd26e5279149944a0d501676dc154d4605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:57 +0000

    Add package osbm/0.0.1/osbm-0.0.1.tar.gz

---
+++ packages/osbm/osbm-0.0.1.tar.gz/osbm-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='osbm',
+    version="0.0.1",
+    description='osbm personal library',
+    long_description=open('README.md').read(),
+    author='osbm',
+    packages=find_packages(),
+    
+)
