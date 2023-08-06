ea092b957c1a957bbb3e28ffacd347fcf275cdad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:46:29 +0000

    Add package rapidfuzz/2.2.0/rapidfuzz-2.2.0-cp36-cp36m-win_amd64.whl

---
+++ packages/rapidfuzz/rapidfuzz-2.2.0-cp36-cp36m-win_amd64.whl/rapidfuzz/distance/__init__.py
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2022 Max Bachmann
+
+from rapidfuzz.utils import _fallback_import
+
+_mod = "rapidfuzz.distance._initialize"
+Editop = _fallback_import(_mod, "Editop")
+Editops = _fallback_import(_mod, "Editops")
+Opcode = _fallback_import(_mod, "Opcode")
+Opcodes = _fallback_import(_mod, "Opcodes")
+ScoreAlignment = _fallback_import(_mod, "ScoreAlignment")
+
+from . import Hamming, Indel, Jaro, JaroWinkler, Levenshtein, LCSseq
