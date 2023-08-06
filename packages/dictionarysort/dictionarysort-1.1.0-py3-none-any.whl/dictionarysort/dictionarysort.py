ec508702c890f728dba0962f78742ed541413541
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:11 +0000

    Add package dictionarysort/1.1.0/dictionarysort-1.1.0-py3-none-any.whl

---
+++ packages/dictionarysort/dictionarysort-1.1.0-py3-none-any.whl/dictionarysort/dictionarysort.py
@@ -0,0 +1,14 @@
+'''
+This module provides ways to sort a python dictionary
+Dictionary could be sorted by keys or by values
+'''
+
+def bykeys(x: dict) -> dict:
+    val = {k:x[k] for k in sorted(x, key=lambda y:y)}
+    return val
+
+
+def byvalues(x: dict) -> dict:
+    val = {k: x[k] for k in sorted(x, key=lambda y: x[y])}
+    return val
+
