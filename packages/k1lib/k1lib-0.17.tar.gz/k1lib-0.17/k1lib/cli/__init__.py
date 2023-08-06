275e963175320adfa91e3b56c716d450b2e02742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:30 +0000

    Add package k1lib/0.17/k1lib-0.17.tar.gz

---
+++ packages/k1lib/k1lib-0.17.tar.gz/k1lib-0.17/k1lib/cli/__init__.py
@@ -0,0 +1,31 @@
+from k1lib import wrapMod as _wrapMod
+
+_scatteredClis = [] # everywhere else in the system can add objects here, and k1lib.imports will make all of them available.
+def scatteredClis(): return [c.__name__ for c in _scatteredClis]
+
+from .init import *
+
+# common utils
+from .filt import *
+from .grep import *
+from .inp import *
+from .modifier import *
+from .output import *
+from .structural import *
+from .conv import *
+from .utils import *
+from .others import *
+
+# bio specific
+from .entrez import *
+from .bio import *
+from . import mgi;  mgi  = _wrapMod(mgi)
+
+# file formats
+from . import kxml; kxml = _wrapMod(kxml)
+from . import kcsv; kcsv = _wrapMod(kcsv)
+from . import sam;  sam  = _wrapMod(sam)
+from . import gb;   gb   = _wrapMod(gb)
+from . import nb;   nb   = _wrapMod(nb)
+
+from .trace import * # has to be last, to wait for others to load up
