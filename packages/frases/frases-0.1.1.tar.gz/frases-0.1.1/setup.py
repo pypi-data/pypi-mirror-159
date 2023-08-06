8f2742003f7b88f9864798d0a59386afb352d1cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:58 +0000

    Add package frases/0.1.1/frases-0.1.1.tar.gz

---
+++ packages/frases/frases-0.1.1.tar.gz/frases-0.1.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+f = open("README.md", "r")
+long_description = f.read()
+f.close()
+
+setuptools.setup(
+  name="frases",
+  packages=["frases"],
+  version="0.1.1",
+  author="Luqaska",
+  description="API de frases",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://frases.atico.ga/api",
+  project_urls={
+    "Changelog": "https://github.com/Luqaska/frases.py/releases",
+    "Bug Tracker": "https://github.com/Luqaska/frases.py/issues",
+    "Source Code": "https://github.com/Luqaska/frases.py"
+  },
+  requires=["requests"],
+  keywords=["API", "frases"]
+)
