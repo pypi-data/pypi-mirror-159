703837abd0388c8465a94d128828c335abf54cdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:09 +0000

    Add package famapy-fm/1.0.0/famapy_fm-1.0.0-py3-none-any.whl

---
+++ packages/famapy-fm/famapy_fm-1.0.0-py3-none-any.whl/famapy/metamodels/fm_metamodel/operations/fm_count_leafs.py
@@ -0,0 +1,23 @@
+from famapy.core.operations import CountLeafs
+
+from famapy.metamodels.fm_metamodel.models import FeatureModel
+
+
+class FMCountLeafs(CountLeafs):
+
+    def __init__(self) -> None:
+        self.result = 0
+
+    def get_result(self) -> int:
+        return self.result
+
+    def execute(self, model: FeatureModel) -> 'FMCountLeafs':
+        self.result = count_leaf_features(model)
+        return self
+
+    def get_number_of_leafs(self) -> int:
+        return self.get_result()
+
+
+def count_leaf_features(feature_model: FeatureModel) -> int:
+    return sum(f.is_leaf() for f in feature_model.get_features())
