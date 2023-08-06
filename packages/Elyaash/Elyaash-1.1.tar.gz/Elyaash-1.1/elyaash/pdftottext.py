e60ee7f73a2d73155cd6e0d59c3d02e29a3a053b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:45 +0000

    Add package Elyaash/1.1/Elyaash-1.1.tar.gz

---
+++ packages/Elyaash/Elyaash-1.1.tar.gz/Elyaash-1.1/elyaash/pdftottext.py
@@ -0,0 +1,17 @@
+""" This module provide the functions to convert a PDF to Text
+
+    A more detailed explanation
+"""
+
+
+def convert(path):
+    """ 
+    Conver the given PDF to text.
+
+    Parameters:
+    path (str): The path to a PDF file
+
+    Returns:
+    str: The content of the PDF file as text
+    """
+    print(f"Converted pdf file...{path}")
