a3db1117e46496bded664b33fe5639feac52952d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:05:20 +0000

    Add package alfred-pyflow/0.2.0/alfred-pyflow-0.2.0.tar.gz

---
+++ packages/alfred-pyflow/alfred-pyflow-0.2.0.tar.gz/alfred-pyflow-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyflow']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'alfred-pyflow',
+    'version': '0.2.0',
+    'description': 'Minimal library for the development of Alfred Workflows.',
+    'long_description': '# alfred-pyflow',
+    'author': 'Fede Calendino',
+    'author_email': 'fede@calendino.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/fedecalendino/alfred-pyflow',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
