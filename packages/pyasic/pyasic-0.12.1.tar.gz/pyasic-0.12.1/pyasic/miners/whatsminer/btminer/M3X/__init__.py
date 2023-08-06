17a77f5d3f755608ecf9eb32441ecb55f622de41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:28 +0000

    Add package pyasic/0.12.1/pyasic-0.12.1.tar.gz

---
+++ packages/pyasic/pyasic-0.12.1.tar.gz/pyasic-0.12.1/pyasic/miners/whatsminer/btminer/M3X/__init__.py
@@ -0,0 +1,23 @@
+from .M30S import (
+    BTMinerM30S,
+    BTMinerM30SVE10,
+    BTMinerM30SVE20,
+    BTMinerM30SVG20,
+    BTMinerM30SV50,
+)
+from .M30S_Plus import (
+    BTMinerM30SPlus,
+    BTMinerM30SPlusVF20,
+    BTMinerM30SPlusVE40,
+    BTMinerM30SPlusVG60,
+)
+from .M30S_Plus_Plus import (
+    BTMinerM30SPlusPlus,
+    BTMinerM30SPlusPlusVG40,
+    BTMinerM30SPlusPlusVG30,
+)
+
+from .M31S import BTMinerM31S
+from .M31S_Plus import BTMinerM31SPlus, BTMinerM31SPlusVE20
+
+from .M32S import BTMinerM32S
