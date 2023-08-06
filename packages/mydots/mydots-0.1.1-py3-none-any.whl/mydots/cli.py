b0ce5c57b25ec4f5fe6db5e312837eefd823d5ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:20 +0000

    Add package mydots/0.1.1/mydots-0.1.1-py3-none-any.whl

---
+++ packages/mydots/mydots-0.1.1-py3-none-any.whl/mydots/cli.py
@@ -0,0 +1,28 @@
+import optparse
+
+from mydots import extractor
+
+
+parser = optparse.OptionParser()
+parser.add_option(
+    "-s",
+    "--separator",
+    action="store",
+    dest="separator",
+    default="\n",
+)
+parser.add_option(
+    "-e",
+    "--escape-strings",
+    action="store_false",
+    dest="raw_strings",
+    default=True,
+)
+
+
+def main():
+    options, args = parser.parse_args()
+    if len(args) == 0:
+        print("Please provide patterns.")
+        exit(1)
+    extractor.Extractor(options, args).extract()
