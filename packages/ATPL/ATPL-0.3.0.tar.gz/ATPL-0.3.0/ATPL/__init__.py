554a5781a03354c2abdabffea58048336b926e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:00 +0000

    Add package ATPL/0.3.0/ATPL-0.3.0.tar.gz

---
+++ packages/ATPL/ATPL-0.3.0.tar.gz/ATPL-0.3.0/ATPL/__init__.py
@@ -0,0 +1,65 @@
+
+import sys
+import sys,os
+sys.path.append(os.getcwd())
+from gooey import Gooey, GooeyParser
+from common.comFuction import get_list
+from main_flow.main_flow import main_process
+
+
+@Gooey(
+       target=None,
+       program_name='Audio Test Lite v1.0',
+       progress_regex=r"^progress: (?P<current>\d+)/(?P<total>\d+)$",
+       progress_expr="current / total * 100",
+       )
+
+def start_up_window():
+    firsrtkey,input_core,out_core = get_list()
+    parser = GooeyParser()
+    paltform = parser.add_argument_group('Params config page')
+    paltform.add_argument('driverType',
+                        metavar='driverType',
+                        default=firsrtkey,
+                        choices=[firsrtkey],
+                        help='choose one of the driver types',
+                          )
+    paltform.add_argument('mosSwitch',
+                        metavar='mosSwitch',
+                        default='False',
+                        choices=['True','False'],
+                        help='MOS test takes another 10 seconds',
+                          )
+    paltform.add_argument('inPutDivice',
+                        metavar='inPutDivice',
+                        default=input_core[0],
+                        choices=input_core,
+                        help='choose one of the inPutDivices',
+                          )
+    paltform.add_argument('outPutDivice',
+                        metavar='outPutDivice',
+                        default=out_core[0],
+                        choices=out_core,
+                        help='choose one of the outPutDivices',
+                          )
+    paltform.add_argument('inputVolume',
+                        metavar='inputVolume',
+                        default= '100',
+                        choices=['10','20','30','40','50','60','70','80','90','100'],
+                        help='do not adjust volume unless necessary！',
+                          )
+    paltform.add_argument('outputVolume',
+                        metavar='outputVolume',
+                        default= '100',
+                        choices=['10','20','30','40','50','60','70','80','90','100'],
+                        help='do not adjust volume unless necessary！',
+                          )
+    args = parser.parse_args()
+    mp = main_process(args)
+    mp.run_all_process()
+
+
+
+if __name__ == '__main__':
+    start_up_window()
+
---
+++ packages/ATPL/ATPL-0.3.0.tar.gz/ATPL-0.3.0/ATPL/main_flow/main_flow.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import sys,os
+sys.path.append(os.path.dirname(os.getcwd()))
+
+from common.comFuction import str_2_bool,half_wave_file,set_mac_volume,play_and_rec,recovery_mac_volume,match_sig,polqa_client_test
+
+class main_process():
+    def __init__(self,UIsysConfig):
+        """
+        :param UIsysConfig:
+        """
+        self.__init_global_variable(UIsysConfig)
+        pass
+
+    def __init_global_variable(self,sysConfig):
+        """
+        testPaltform ：测试的Demo或者app
+        version ： 版本信息
+        最终根据这两个变量生成相应名称的报告和过程数据,不能用中文
+        """
+        self.inputFile = sys.prefix + '/speech.wav'
+        self.outFile = sys.prefix + '/out.wav'
+        self.inputDeviceId = self.__parse_id_from_str(sysConfig.inPutDivice)
+        self.outputDeviceId = self.__parse_id_from_str(sysConfig.outPutDivice)
+        self.mosSwitch = str_2_bool(sysConfig.mosSwitch)
+        self.inVolume = sysConfig.inputVolume
+        self.outVolume = sysConfig.outputVolume
+        print("********************************************************************")
+        print('当前选择的声卡输入为:{}'.format(sysConfig.inPutDivice))
+        print('当前选择的声卡输出为:{}'.format(sysConfig.outPutDivice))
+
+    def __parse_id_from_str(self,sdSTR):
+        """
+        :return:
+        """
+        return int(sdSTR.split(",")[0].split('(')[1])
+    def run_all_process(self):
+        """
+        :return:
+        """
+        print('progress: {0}/100'.format(0))
+        print("********************************************************************")
+        print("开始测试")
+        print("设置声卡音量")
+        set_mac_volume(self.outVolume,self.inVolume)
+        print('progress: {0}/100'.format(5))
+        print('Done!')
+        print("录制音频文件")
+        play_and_rec(self.inputFile, self.outFile,[self.inputDeviceId,self.outputDeviceId])
+        print('Done!')
+        print('progress: {0}/100'.format(35))
+        recovery_mac_volume()
+        print("计算延时")
+        MATCH = match_sig(self.inputFile,self.outFile)
+        if MATCH is not None:
+            print('当前的端到端延时:{}ms'.format(round(MATCH/48000 * 1000),2))
+        else:
+            print('端到端延时测试失败！')
+            MATCH=0
+        print('Done!')
+        print('progress: {0}/100'.format(50))
+        if self.mosSwitch:
+            print("计算MOS分")
+            ref = half_wave_file(self.inputFile,0,MATCH)
+            test  = half_wave_file(self.outFile,1,MATCH)
+            polqa_client_test(ref, test,48000)
+            print('Done!')
+            os.remove(ref)
+            os.remove(test)
+        print('progress: {0}/100'.format(100))
+        #os.remove(self.outFile)
+
+
+
