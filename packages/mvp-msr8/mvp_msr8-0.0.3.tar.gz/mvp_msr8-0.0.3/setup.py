6590a9e781ed88709db26be034da622501afb09c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:38 +0000

    Add package mvp-msr8/0.0.3/mvp_msr8-0.0.3.tar.gz

---
+++ packages/mvp-msr8/mvp_msr8-0.0.3.tar.gz/mvp_msr8-0.0.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open('README.md') as f:
+    long_desc = f.read()
+
+setup(
+    name='mvp_msr8',
+    version='0.0.3',
+    description='hi!',
+    py_modules=['mvp_msr8'],
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    install_requires=['rich'],
+    package_dir={'': 'src'},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent'
+    ]
+)
+
+
+
+'''
+name:        what you will pip install, not import
+py_modules:  what they will import
+
+!! NAME OF THE MAIN FILE IN THE src DIR SHOULD BE SAME AS PACKAGE NAME
+'''
+
+
