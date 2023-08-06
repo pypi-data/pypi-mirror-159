35a6a91f220821b4658a9c255b2eb27334d837d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:18:49 +0000

    Add package xedocs/0.1.8/xedocs-0.1.8.tar.gz

---
+++ packages/xedocs/xedocs-0.1.8.tar.gz/xedocs-0.1.8/xedocs/schemas/__init__.py
@@ -0,0 +1,6 @@
+from .base_schemas import VersionedXeDoc, XeDoc
+from .bodega import Bodega
+from .fax import FaxConfig
+from .corrections import *
+from .calibrations import *
+from .operations_reports import *
