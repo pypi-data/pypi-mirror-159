686928e330fdfb242948a0e29c8a93a8536c13ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:18 +0000

    Add package docgenerator/0.1.2/docgenerator-0.1.2.tar.gz

---
+++ packages/docgenerator/docgenerator-0.1.2.tar.gz/docgenerator-0.1.2/helpers/logger.py
@@ -0,0 +1,20 @@
+from helpers import settings
+
+class Colors:
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    BLUE = '\033[94m'
+    YELLOW = '\033[93m'
+    CLEAN = '\033[0m'
+
+
+def logger(message, color):
+    if settings.VERBOSE:
+        if color == "RED":
+            print(Colors.RED + message + Colors.CLEAN)
+        if color == "GREEN":
+            print(Colors.GREEN + message + Colors.CLEAN)
+        if color == "BLUE":
+            print(Colors.BLUE + message + Colors.CLEAN)
+        if color == "YELLOW":
+            print(Colors.YELLOW + message + Colors.CLEAN)
