5ea536545a6dfadfc3c56fd0ed3538ce645260bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:35 +0000

    Add package pymoode/0.1.8/pymoode-0.1.8.tar.gz

---
+++ packages/pymoode/pymoode-0.1.8.tar.gz/pymoode-0.1.8/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+  name = 'pymoode',
+  packages = ['pymoode'],
+  version = '0.1.8',
+  license='Apache License 2.0',
+  description = 'A Python optimization package using Differential Evolution.',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  author = 'Bruno Scalia C. F. Leite',
+  author_email = 'mooscaliaproject@gmail.com',
+  url = 'https://github.com/mooscaliaproject/pymoode',
+  download_url = 'https://github.com/mooscaliaproject/pymoode',
+  keywords = ['Multi-objective optimization',
+              'NSGA-II',
+              'GDE3',
+              'NSDE',
+              'SA-NSDE',
+              'NSDE-R',
+              'Differential Evolution',
+              'Genetic Algorithm',
+              'Evolutionary Algorithms',
+              'Evolutionary optimization'],
+  install_requires=[
+          'numpy>=1.19.*',
+          'pymoo==0.5.*',
+          'scikit-learn>=1.0.*',
+          'scipy>=1.7.*',
+      ],
+)
