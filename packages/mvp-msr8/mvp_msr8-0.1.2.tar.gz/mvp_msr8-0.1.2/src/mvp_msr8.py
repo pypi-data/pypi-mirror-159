25297fc4b9100962499db553b479a134865e5943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:55 +0000

    Add package mvp-msr8/0.1.2/mvp_msr8-0.1.2.tar.gz

---
+++ packages/mvp-msr8/mvp_msr8-0.1.2.tar.gz/mvp_msr8-0.1.2/src/mvp_msr8.py
@@ -0,0 +1,10 @@
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('text', type=str, help='an integer for the accumulator')
+args = parser.parse_args()
+text = args.text
+
+
+
+print(f'{text} (0.1.2)')
