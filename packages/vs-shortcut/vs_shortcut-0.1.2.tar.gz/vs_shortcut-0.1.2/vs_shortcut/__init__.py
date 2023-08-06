081b683eee69c9f635c56581ebbcca31e294955b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:41 +0000

    Add package vs-shortcut/0.1.2/vs_shortcut-0.1.2.tar.gz

---
+++ packages/vs-shortcut/vs_shortcut-0.1.2.tar.gz/vs_shortcut-0.1.2/vs_shortcut/__init__.py
@@ -0,0 +1,29 @@
+"""
+    vs_shortcut - Microsoft Visual Studio shortcut for Anaconda-navigator
+    =====================================================================
+
+    Provides an application shortcut for Microsoft Visual Studio in
+    Anaconda-navigator to prevent environment bugs. The shortcut includes an
+    icon and a description just like spyder IDE or VSCode.
+
+"""
+
+__author__ = "Maxime Tousignant-Tremblay"
+__copyright__ = "Copyright (C) 2022 Maxime Tousignant-Tremblay"
+__license__ = "AGPL-3.0"
+__status__ = "Prototype"
+
+# Standard library
+from importlib.metadata import PackageNotFoundError  # pragma: no cover
+
+# Local import
+from ._version import get_versions  # pragma: no cover
+
+
+try:
+    vinfo: dict[str, str] = get_versions()
+    __version__ = vinfo["version"]
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+finally:
+    del vinfo, get_versions, PackageNotFoundError
---
+++ packages/vs-shortcut/vs_shortcut-0.1.2.tar.gz/vs_shortcut-0.1.2/vs_shortcut/__main__.py
@@ -0,0 +1,24 @@
+"""
+    Entry point for the launching script of Microsoft Visual Studio.
+
+"""
+
+__author__ = "Maxime Tousignant-Tremblay"
+__copyright__ = "Copyright (C) 2022 Maxime Tousignant-Tremblay"
+__license__ = "AGPL-3.0"
+__status__ = "Prototype"
+
+# Standard library
+import sys
+from subprocess import call
+
+# Local import
+from . import __version__
+
+
+def main():
+    call("C:/Program Files/Microsoft Visual Studio/2022/Enterprise/Common7/IDE/devenv.exe")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
