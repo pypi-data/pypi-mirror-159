d1951f0f90e9c17e677ebcd27be9b3fd1f52ce07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:59 +0000

    Add package gradio/3.0.25/gradio-3.0.25.tar.gz

---
+++ packages/gradio/gradio-3.0.25.tar.gz/gradio-3.0.25/gradio/templates/cdn/assets/color.a29481e6.js
@@ -0,0 +1,7 @@
+import { ac as ordered_colors } from './index.b7e8b606.js';
+
+const get_next_color = (index) => {
+  return ordered_colors[index % ordered_colors.length];
+};
+
+export { get_next_color as g };
