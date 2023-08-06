da9dc2396829ced8028cd942ce3e07a31e7a50f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:47:53 +0000

    Add package defichainUtils/0.0.15/defichainUtils-0.0.15.tar.gz

---
+++ packages/defichainUtils/defichainUtils-0.0.15.tar.gz/defichainUtils-0.0.15/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.15'
+DESCRIPTION = 'DefichainUtils'
+LONG_DESCRIPTION = 'Python Utils to handle DefiChain'
+
+# Setting up
+setup(
+    name="defichainUtils",
+    version=VERSION,
+    author="RupineLabs",
+    author_email="<abc@def.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=[
+        'defichainUtils',
+        'defichainUtils.utils',
+        # 'utils.bech32',
+        # 'utils.CustomTx',
+        # 'utils.CustomTxDecode',
+        # 'utils.GovVariablesAttributes',
+        # 'utils.ParamType',
+        # 'utils.utils'
+        
+    ],
+    install_requires=[],
+    keywords=['web3'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
