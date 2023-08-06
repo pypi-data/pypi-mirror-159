bb37ca2710419d5024d073a769c32ea44d5590e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:06 +0000

    Add package fred-py-api/0.1.3/fred_py_api-0.1.3.tar.gz

---
+++ packages/fred-py-api/fred_py_api-0.1.3.tar.gz/fred_py_api-0.1.3/src/fred/api/__init__.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Fred API core.
+"""
+from .categories import FredAPICategories
+from .exceptions import BaseFredAPIError, FredAPIRequestError
+from .releases import FredAPIReleases
+from .series import FredAPISeries
+from .sources import FredAPISources
+from .tags import FredAPITags
+
+
+class FredAPI(FredAPISeries, FredAPIReleases, FredAPICategories, FredAPISources, FredAPITags):
+    """
+    Fred API.
+    https://fred.stlouisfed.org/docs/api/fred/
+    """
+
+    pass
+
+
+__all__ = [
+    "BaseFredAPIError",
+    "FredAPIRequestError",
+    "FredAPI",
+    "FredAPICategories",
+    "FredAPIReleases",
+    "FredAPISeries",
+    "FredAPISources",
+    "FredAPITags",
+]
