017157bf97e1d0c1f26cdc2db6b1e5f3d604802d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:49:22 +0000

    Add package mufid/1.1/mufid-1.1.tar.gz

---
+++ packages/mufid/mufid-1.1.tar.gz/mufid-1.1/src/mufid/mkdir.py
@@ -0,0 +1,8 @@
+from genericpath import exists
+
+
+from utils.do_nothing import f as do_nothing
+from os.path import exists
+from os import mkdir
+
+def f(path) : [mkdir(path) if not exists(path) else do_nothing(), path][1]
