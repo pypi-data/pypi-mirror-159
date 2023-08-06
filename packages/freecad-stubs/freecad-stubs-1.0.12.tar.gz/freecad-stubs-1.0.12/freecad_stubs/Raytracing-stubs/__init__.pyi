64f74701bc04296f7cf0f14ef5afca49b25cd789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:25 +0000

    Add package freecad-stubs/1.0.12/freecad-stubs-1.0.12.tar.gz

---
+++ packages/freecad-stubs/freecad-stubs-1.0.12.tar.gz/freecad-stubs-1.0.12/freecad_stubs/Raytracing-stubs/__init__.pyi
@@ -0,0 +1,37 @@
+import typing
+
+import FreeCAD
+import Part as PartModule
+
+
+# AppRaytracingPy.cpp
+def writeProjectFile(arg1: typing.Sequence[str], /) -> None:
+    """Possible exceptions: (Exception)."""
+
+
+def getPartAsPovray(arg1: str, arg2: PartModule.Shape, arg3: float = None, arg4: float = None, arg5: float = None, /) -> str:
+    """Possible exceptions: (Exception)."""
+
+
+def getPartAsLux(arg1: str, arg2: PartModule.Shape, arg3: float = None, arg4: float = None, arg5: float = None, /) -> str:
+    """Possible exceptions: (Exception)."""
+
+
+def writePartFile(arg1: str, arg2: str, arg3: PartModule.Shape, /) -> None:
+    """Possible exceptions: (Exception)."""
+
+
+def writeDataFile(arg1: str, arg2: str, arg3: FreeCAD.ComplexGeoData, /) -> None:
+    """Possible exceptions: (Exception)."""
+
+
+def writePartFileCSV(arg1: PartModule.Shape, arg2: str, arg3: float, arg4: float, /) -> None:
+    """Possible exceptions: (Exception)."""
+
+
+def writeCameraFile(arg1: str, arg2: tuple, arg3: tuple, arg4: tuple, arg5: tuple, /) -> None:
+    """Possible exceptions: (Exception, ValueError)."""
+
+
+def copyResource(arg1: str, arg2: str, /) -> None:
+    """Possible exceptions: (Exception)."""
---
+++ packages/freecad-stubs/freecad-stubs-1.0.12.tar.gz/freecad-stubs-1.0.12/freecad_stubs/Sandbox-stubs/__init__.pyi
@@ -0,0 +1,35 @@
+import FreeCAD
+import Sandbox
+
+
+# AppSandbox.cpp
+def DocumentProtector(Document: FreeCAD.Document, /) -> Sandbox.DocumentProtector:
+    """
+    DocumentProtector(Document)
+    Possible exceptions: (Exception).
+    """
+
+
+def DocumentObjectProtector(DocumentObject: FreeCAD.DocumentObject, /) -> Sandbox.DocumentObjectProtector:
+    """
+    DocumentObjectProtector(DocumentObject)
+    Possible exceptions: (Exception).
+    """
+
+
+# DocumentProtectorPy.cpp
+class DocumentProtectorPy:
+    """Python binding class for the document protector class"""
+
+    def addObject(self, type: str, name: str = None, /):
+        """addObject(type,name)"""
+
+    def recompute(self) -> None:
+        """recompute()"""
+
+
+class DocumentObjectProtectorPy:
+    """Python binding class for the document object protector class"""
+
+    def purgeTouched(self):
+        """purgeTouched()"""
