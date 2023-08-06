a18a1c253daed7d4cb0ff35081c0377aca690cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:49 +0000

    Add package nimgame/0.1.3/nimgame-0.1.3-py3-none-any.whl

---
+++ packages/nimgame/nimgame-0.1.3-py3-none-any.whl/nimgame/source/playing/cli/ansi.py
@@ -0,0 +1,27 @@
+"""ANSI codes for manipulating console display"""
+
+
+# An empty command seems to make the Windows cmd terminal capable of ANSI.
+import os; os.system('')
+
+
+ESC = '\033'
+CURSOR_UP = ESC + '[A'
+CURSOR_BACK = ESC + '[200D'
+DELETE_LINE = ESC + '[K'
+
+
+def clean_this_line():
+    """Clean the input prompt inline"""
+    print(
+        CURSOR_BACK + DELETE_LINE, 
+        end='' #supress line feed
+    )
+
+
+def clean_prev_line():
+    """Clean the input prompt and answer"""
+    print(
+        CURSOR_UP + DELETE_LINE, 
+        end='' #supress line feed
+    )
