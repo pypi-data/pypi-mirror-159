7c7b7344382ef1fd499104eafb1c6d93b23a0676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:29 +0000

    Add package MIPMLP/1.1.1/MIPMLP-1.1.1.tar.gz

---
+++ packages/MIPMLP/MIPMLP-1.1.1.tar.gz/MIPMLP-1.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='MIPMLP',
+   version='1.1.1',
+   description='preprocess microbiome data',
+   author='YOLO lab',
+   author_email='louzouy@math.biu.ac.il',
+   packages=['MIPMLP'],
+   install_requires=['pandas', 'numpy', 'matplotlib','sklearn','scipy','networkx'], #external packages as dependencies
+)
