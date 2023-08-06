0b5aea37f897cb0e85fa7837f588d0c69e762192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:55 +0000

    Add package test-zws/0.1/test_zws-0.1.tar.gz

---
+++ packages/test-zws/test_zws-0.1.tar.gz/test_zws-0.1/README.md
@@ -0,0 +1,1 @@
+## Test for pypi package
---
+++ packages/test-zws/test_zws-0.1.tar.gz/test_zws-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='test_zws',
+      version='0.1',
+      description='Gaussian and Binormial distributions',
+      packages=['test_zws'],
+      zip_safe=False)
