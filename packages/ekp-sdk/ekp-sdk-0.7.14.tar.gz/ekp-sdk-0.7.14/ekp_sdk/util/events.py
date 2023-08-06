c49151ba5d0822fc1e7d1d1f1dfb794d9340c50a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:47 +0000

    Add package ekp-sdk/0.7.14/ekp-sdk-0.7.14.tar.gz

---
+++ packages/ekp-sdk/ekp-sdk-0.7.14.tar.gz/ekp-sdk-0.7.14/ekp_sdk/util/events.py
@@ -0,0 +1,84 @@
+def client_currency(event):
+    if (event is None):
+        return None
+
+    if ("state" not in event.keys()):
+        return None
+
+    if ("client" not in event["state"].keys()):
+        return None
+
+    if ("selectedCurrency" not in event["state"]["client"].keys()):
+        return None
+
+    return event["state"]["client"]["selectedCurrency"]
+
+
+def client_path(event):
+    if (event is None):
+        return None
+
+    if ("state" not in event.keys()):
+        return None
+
+    if ("client" not in event["state"].keys()):
+        return None
+
+    if ("path" not in event["state"]["client"].keys()):
+        return None
+
+    return event["state"]["client"]["path"]
+
+
+def form_value(event, form_name, property_name):
+    if (event is None):
+        return None
+
+    if ("state" not in event.keys()):
+        return None
+
+    if ("forms" not in event["state"].keys()):
+        return None
+
+    if (form_name not in event["state"]["forms"].keys()):
+        return None
+
+    if (property_name not in event["state"]["forms"][form_name].keys()):
+        return None
+
+    return event["state"]["forms"][form_name][property_name]
+
+
+def form_values(event, form_name):
+    if (event is None):
+        return None
+
+    if ("state" not in event.keys()):
+        return None
+
+    if ("forms" not in event["state"].keys()):
+        return None
+
+    if (form_name not in event["state"]["forms"].keys()):
+        return None
+
+    return event["state"]["forms"][form_name]
+
+
+def client_query_param(event, key):
+    if (event is None):
+        return None
+
+    if ("state" not in event.keys()):
+        return None
+
+    if ("client" not in event["state"].keys()):
+        return None
+
+    if ("queryParams" not in event["state"]["client"].keys()):
+        return None
+
+    if (key not in event["state"]["client"]["queryParams"].keys()):
+        return None
+
+    return event["state"]["client"]["queryParams"][key]
