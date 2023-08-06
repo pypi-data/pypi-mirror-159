4053547176fcadab516510f0e8c015d90cc8f085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:54 +0000

    Add package easypost/7.3.0/easypost-7.3.0-py3-none-any.whl

---
+++ packages/easypost/easypost-7.3.0-py3-none-any.whl/easypost/webhook.py
@@ -0,0 +1,19 @@
+from easypost.requestor import (
+    RequestMethod,
+    Requestor,
+)
+from easypost.resource import (
+    AllResource,
+    CreateResource,
+    DeleteResource,
+)
+
+
+class Webhook(AllResource, CreateResource, DeleteResource):
+    def update(self, **params) -> "Webhook":
+        """Update a webhook."""
+        requestor = Requestor(local_api_key=self._api_key)
+        url = self.instance_url()
+        response, api_key = requestor.request(method=RequestMethod.PATCH, url=url, params=params)
+        self.refresh_from(values=response, api_key=api_key)
+        return self
