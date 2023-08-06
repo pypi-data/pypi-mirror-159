1588af08a77e33e74ff321479e94901a00c8ea73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:58:55 +0000

    Add package robusta-cli/0.9.18a2/robusta_cli-0.9.18a2-py3-none-any.whl

---
+++ packages/robusta-cli/robusta_cli-0.9.18a2-py3-none-any.whl/robusta/core/playbooks/base_trigger.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+from typing import Dict, Optional, Type, List
+
+from ..model.events import ExecutionBaseEvent
+from ...utils.documented_pydantic import DocumentedModel
+from ..model.k8s_operation_type import K8sOperationType
+from ..reporting.base import Finding
+import abc
+
+
+class TriggerEvent(BaseModel):
+    @abc.abstractmethod
+    def get_event_name(self) -> str:
+        """Return trigger event name"""
+        return ""
+
+
+class BaseTrigger(DocumentedModel):
+    def get_trigger_event(self) -> str:
+        pass
+
+    def should_fire(self, event: TriggerEvent, playbook_id: str):
+        return True
+
+    def build_execution_event(
+        self, event: TriggerEvent, sink_findings: Dict[str, List[Finding]]
+    ) -> Optional[ExecutionBaseEvent]:
+        pass
+
+    @staticmethod
+    def get_execution_event_type() -> Type[ExecutionBaseEvent]:
+        pass
