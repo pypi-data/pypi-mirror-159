3f453063d38bc91849dc98f8dbd7e463d293ae02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:30 +0000

    Add package pydiscordwrapper/0.1.4/PyDiscordWrapper-0.1.4.tar.gz

---
+++ packages/pydiscordwrapper/PyDiscordWrapper-0.1.4.tar.gz/PyDiscordWrapper-0.1.4/src/pydiscordwrapper/__init__.py
@@ -0,0 +1,13 @@
+# src/pydiscordwrapper/__init__.py
+"""Simple discord api wrapper"""
+try:
+    from importlib.metadata import version, PackageNotFoundError  # type: ignore
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError  # type: ignore
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+
+from .types import *
+from ._client import Client,AsyncClient
