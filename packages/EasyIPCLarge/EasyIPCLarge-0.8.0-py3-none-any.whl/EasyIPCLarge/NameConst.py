0b1345239a2fbff2a67ecbfe36bc5bf603b06467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:37 +0000

    Add package EasyIPCLarge/0.8.0/EasyIPCLarge-0.8.0-py3-none-any.whl

---
+++ packages/EasyIPCLarge/EasyIPCLarge-0.8.0-py3-none-any.whl/EasyIPCLarge/NameConst.py
@@ -0,0 +1,23 @@
+class NameConst:
+    kSessionID = "SessionID"
+
+    kAction   = "Action"    # noqa
+    vOpen     = "Open"      # noqa
+    vClose    = "Close"     # noqa
+    vSend     = "Send"      # noqa
+    vExternal = "External"
+
+    kShareType    = "ShareType"  # noqa
+    vSharedMemory = 1
+    vSocket       = 2            # noqa
+
+    kSharedMemoryName = "SharedMemoryName"
+
+    kRange = "Range"
+    kSeq   = "Seq"    # noqa
+    kTime  = "Time"   # noqa
+    kBody  = "Body"   # noqa
+
+    kErr         = "Err"        # noqa
+    vErrOK       = "OK"         # noqa
+    vErrNoMemory = "No memory"
