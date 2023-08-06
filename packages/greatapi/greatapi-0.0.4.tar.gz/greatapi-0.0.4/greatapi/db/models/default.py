145ca64589770e4b054cfd8b157fc605bfc60199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:06 +0000

    Add package greatapi/0.0.4/greatapi-0.0.4.tar.gz

---
+++ packages/greatapi/greatapi-0.0.4.tar.gz/greatapi-0.0.4/greatapi/db/models/default.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import Integer
+from sqlalchemy import String
+from sqlalchemy.sql import func
+
+from greatapi.db.database import Base
+# from sqlalchemy import ForeignKey
+
+# from sqlalchemy.orm import relationship
+
+
+class History(Base):
+    __tablename__ = 'history'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    type = Column(String)
+    created_date = Column(DateTime(timezone=True), server_default=func.now())
+    edited_date = Column(DateTime(timezone=True), onupdate=func.now())
+
+    # blogs = relationship("Blog", back_populates="creator")
