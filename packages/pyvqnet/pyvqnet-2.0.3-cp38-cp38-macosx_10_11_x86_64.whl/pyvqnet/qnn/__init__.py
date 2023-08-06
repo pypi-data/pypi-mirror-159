f2dc64a9a7cc65f2fc5f14364114499f38f769f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:36 +0000

    Add package pyvqnet/2.0.3/pyvqnet-2.0.3-cp38-cp38-macosx_10_11_x86_64.whl

---
+++ packages/pyvqnet/pyvqnet-2.0.3-cp38-cp38-macosx_10_11_x86_64.whl/pyvqnet/qnn/__init__.py
@@ -0,0 +1,8 @@
+"""
+Init for qnn
+"""
+# pylint: disable=redefined-builtin
+from .measure import *
+from .quantumlayer import *
+from .template import *
+from . import pqc, qae, qdrl, qgan, qlinear, qcnn, qvc, utils, svm
